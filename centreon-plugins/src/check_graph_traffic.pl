fd53f8792b4a0c42d29fd9c1c9981654bdbb7efa
Julien Mathis <jmathis@merethis.com>
2007-04-19 17:06:45 +0000

    git-svn-id: http://svn.centreon.com/Plugins/Dev@2180 6bcd3966-0018-0410-8128-fd23d134de7e

--- centreon-plugins/src/check_graph_traffic.pl
+++ centreon-plugins/src/check_graph_traffic.pl
@@ -26,8 +26,7 @@ use strict;
 use Net::SNMP qw(:snmp oid_lex_sort);
 use FindBin;
 use lib "$FindBin::Bin";
-#use lib "@NAGIOS_PLUGINS@";
-use lib "/srv/nagios/libexec/";
+use lib "@NAGIOS_PLUGINS@";
 use utils qw($TIMEOUT %ERRORS &print_revision &support);
 
 if (eval "require oreon" ) {
