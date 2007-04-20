6c3e1b33a3641a216ea8fa5d44596fe7898e5c9d
Julien Mathis <jmathis@merethis.com>
2007-04-20 16:41:42 +0000

    add old plugins without graph by plugin
    git-svn-id: http://svn.centreon.com/Plugins/Dev@2182 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ src/check_oreon_snmp_process.pl
@@ -0,0 +1,171 @@
+#! /usr/bin/perl -w
+###################################################################
+# Oreon is developped with GPL Licence 2.0 
+#
+# GPL License: http://www.gnu.org/licenses/gpl.txt
+#
+# Developped by : Julien Mathis - Romain Le Merlus 
+#                 Christophe Coraboeuf
+#
+###################################################################
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+#    For information : contact@merethis.com
+####################################################################
+#
+# Script init
+#
+
+use strict;
+use Net::SNMP qw(:snmp oid_lex_sort);
+use FindBin;
+use lib "$FindBin::Bin";
+use lib "@NAGIOS_PLUGINS@";
+use utils qw($TIMEOUT %ERRORS &print_revision &support);
+
+use vars qw($PROGNAME);
+use Getopt::Long;
+use vars qw($opt_V $opt_h $opt_v $opt_C $opt_p $opt_H $opt_D $opt_n $result @result %process_list %STATUS);
+
+# Plugin var init
+
+my($proc, $proc_run);
+
+$PROGNAME = "check_graph_process";
+sub print_help ();
+sub print_usage ();
+
+
+%STATUS=(1=>'running',2=>'runnable',3=>'notRunnable',4=>'invalid');
+
+Getopt::Long::Configure('bundling');
+GetOptions
+    ("h"   => \$opt_h, "help"         => \$opt_h,
+     "V"   => \$opt_V, "version"      => \$opt_V,
+     "g"   => \$opt_g, "rrdgraph"     => \$opt_g,
+     "rrd_step=s" => \$opt_step, "f"  => \$opt_f,
+     "n"   => \$opt_n, "number"       => \$opt_n,
+     "v=s" => \$opt_v, "snmp=s"       => \$opt_v,
+     "C=s" => \$opt_C, "community=s"  => \$opt_C,
+     "p=s" => \$opt_p, "process=s"    => \$opt_p,
+     "S=s" => \$opt_S, "ServiceId=s"  => \$opt_S,
+     "H=s" => \$opt_H, "hostname=s"   => \$opt_H);
+
+if ($opt_V) {
+    print_revision($PROGNAME,'$Revision: 1.2 $');
+    exit $ERRORS{'OK'};
+}
+
+if ($opt_h) {
+    print_help();
+    exit $ERRORS{'OK'};
+}
+
+$opt_H = shift unless ($opt_H);
+(print_usage() && exit $ERRORS{'OK'}) unless ($opt_H);
+
+($opt_v) || ($opt_v = shift) || ($opt_v = "1");
+my $snmp = $1 if ($opt_v =~ /(\d)/);
+
+($opt_C) || ($opt_C = shift) || ($opt_C = "public");
+
+my $process = "";
+if ($opt_p){
+    $process = $1 if ($opt_p =~ /([-.A-Za-z0-9]+)/);
+} 
+
+my $name = $0;
+$name =~ s/\.pl.*//g;
+
+# Plugin snmp requests
+
+my $OID_SW_RunName = $oreon{MIB2}{SW_RUNNAME};
+my $OID_SW_RunIndex =$oreon{MIB2}{SW_RUNINDEX};
+my $OID_SW_RunStatus =$oreon{MIB2}{SW_RUNSTATUS};
+
+my ( $session, $error ) = Net::SNMP->session(-hostname  => $opt_H,-community => $opt_C, -version  => $snmp);
+if ( !defined($session) ) {
+    print("UNKNOWN: $error");
+    exit $ERRORS{'UNKNOWN'};
+}
+
+$result = $session->get_table(Baseoid => $OID_SW_RunName);
+if (!defined($result)) {
+    printf("UNKNOWN: %s.\n", $session->error);
+    $session->close;
+    exit $ERRORS{'UNKNOWN'};
+}
+
+$proc = 0;
+foreach my $key (oid_lex_sort(keys %$result)) {
+    my @oid_list = split (/\./,$key);
+    $process_list{$$result{$key}} =  pop (@oid_list) ;
+    if (defined($opt_p) && $opt_p ne ""){
+		if ($$result{$key} eq $opt_p){
+	    	$proc++;
+		}
+    } else {
+		$proc++;
+    }
+}
+
+if (!($opt_n))
+{
+    if ($process_list{$process}) {
+        $result = $session->get_request(-varbindlist => [$OID_SW_RunStatus . "." . $process_list{$process}]);
+        if (!defined($result)) {
+            printf("UNKNOWN: %s.\n", $session->error);
+            $session->close;
+            exit $ERRORS{'UNKNOWN'};
+        }
+	$proc_run =  $result->{$OID_SW_RunStatus . "." . $process_list{$process} };
+	print $proc_run;
+    }
+}
+
+# Plugin return code
+
+if ($opt_n){
+    print "Processes OK - Number of current processes: $proc|nbproc=$proc\n";
+    exit $ERRORS{'OK'};
+} else {
+    if ($proc_run){
+        print "Process OK - $process: $STATUS{$proc_run}|procstatus=$proc_run\n";
+        exit $ERRORS{'OK'};
+    } else {
+        print "Process CRITICAL - $process not in 'running' state\n";
+        exit $ERRORS{'CRITICAL'};
+    }
+}
+
+sub print_usage () {
+    print "\nUsage:\n";
+    print "$PROGNAME\n";
+    print "   -H (--hostname)   Hostname to query - (required)\n";
+    print "   -C (--community)  SNMP read community (defaults to public,\n";
+    print "                     used with SNMP v1 and v2c\n";
+    print "   -v (--snmp_version)  1 for SNMP v1 (default)\n";
+    print "                        2 for SNMP v2c\n";
+    print "   -n (--number)     Return the number of current running processes. \n";
+    print "   -p (--process)    Set the process name ex: by default smbd\n";
+    print "   -V (--version)    Plugin version\n";
+    print "   -h (--help)       usage help\n";
+}
+
+sub print_help () {
+    print "######################################################\n";
+    print "#      Copyright (c) 2004-2007 Oreon-project         #\n";
+	print "#      Bugs to http://www.oreon-project.org/         #\n";
+	print "######################################################\n";
+    print_usage();
+    print "\n";
+}
+
