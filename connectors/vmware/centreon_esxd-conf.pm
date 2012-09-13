8c277b8947512e78c35ad8dbcd72abd7bbbb8692
Quentin Garnier <qgarnier@merethis.com>
2012-09-13 13:36:49 +0000

    Evolution #3956 Système de log plus évolué
    git-svn-id: http://svn.merethis.net/centreon-esxd/trunk@12 a5eaa968-4c79-4d68-970d-af6011b5b055

--- connectors/vmware/centreon_esxd-conf.pm
+++ connectors/vmware/centreon_esxd-conf.pm
@@ -6,6 +6,13 @@ our $TIMEOUT_VSPHERE = 60;
 our $TIMEOUT = 60;
 our $TIMEOUT_KILL = 30;
 our $REFRESH_KEEPER_SESSION = 15;
+# Log Mode: 0 = stdout, 1 = file, 2 = syslog
+our $log_mode = 1;
+# # Criticity: 0 = nothing, 1 = critical, 2 = all
+our $log_crit = 1;
+# Specify if $log_mode = 2 and CPAN Module Unix::Syslog is installed
+our $log_facility;
+#our $log_facility = LOG_DAEMON;
 our $LOG = "/tmp/centreon_esxd.log";
 
 1;
