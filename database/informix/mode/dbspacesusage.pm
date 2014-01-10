aa5e2a8f7150ce7be514ca7b88e93e07d32da3fd
Quentin Garnier <qgarnier@merethis.com>
2014-01-10 16:01:26 +0100

    + Add mode dbspaces + some minor fix

--- database/informix/mode/dbspacesusage.pm
+++ database/informix/mode/dbspacesusage.pm
@@ -33,12 +33,13 @@
 #
 ####################################################################################
 
-package database::postgres::mode::querytime;
+package database::informix::mode::dbspacesusage;
 
 use base qw(centreon::plugins::mode);
 
 use strict;
 use warnings;
+use centreon::plugins::misc;
 
 sub new {
     my ($class, %options) = @_;
@@ -50,8 +51,7 @@ sub new {
                                 { 
                                   "warning:s"               => { name => 'warning', },
                                   "critical:s"              => { name => 'critical', },
-                                  "exclude:s"               => { name => 'exclude', },
-                                  "exclude-user:s"          => { name => 'exclude_user', },
+                                  "filter:s"                => { name => 'filter', },
                                 });
 
     return $self;
@@ -78,58 +78,48 @@ sub run {
 
     $self->{sql}->connect();
 
-    my $query;
-    if ($self->{sql}->is_version_minimum(version => '9.2')) {
-        $query = q{
-SELECT datname, datid, pid, usename, client_addr, query AS current_query, state AS state,
-       CASE WHEN client_port < 0 THEN 0 ELSE client_port END AS client_port,
-       COALESCE(ROUND(EXTRACT(epoch FROM now()-query_start)),0) AS seconds
-FROM pg_stat_activity WHERE (query_start IS NOT NULL AND (state NOT LIKE 'idle%' OR state IS NULL))
-ORDER BY query_start, pid DESC
+    my $query = q{
+SELECT  name dbspace,              
+        sum(chksize) pages_size,    -- sum of all chunks size pages
+        sum(nfree) pages_free       -- sum of all chunks free pages
+FROM    sysdbspaces d, syschunks c
+WHERE   d.dbsnum = c.dbsnum
+GROUP BY dbspace
+ORDER BY dbspace
 };
-    } else {
-        $query = q{
-SELECT datname, datid, procpid AS pid, usename, client_addr, current_query AS current_query, '' AS state,
-       CASE WHEN client_port < 0 THEN 0 ELSE client_port END AS client_port,
-       COALESCE(ROUND(EXTRACT(epoch FROM now()-query_start)),0) AS seconds
-FROM pg_stat_activity WHERE (query_start IS NOT NULL AND current_query NOT LIKE '<IDLE>%')
-ORDER BY query_start, procpid DESC
-};
-    }
     
     $self->{sql}->query(query => $query);
 
     $self->{output}->output_add(severity => 'OK',
-                                short_msg => "All databases queries time are ok.");
+                                short_msg => "All dbspaces usage are ok.");
     my $dbquery = {};
     while ((my $row = $self->{sql}->fetchrow_hashref())) {
-        if (defined($self->{option_results}->{exclude}) && $row->{datname} !~ /$self->{option_results}->{exclude}/) {
+        my $name = centreon::plugins::misc::trim($row->{dbspace});
+        if (defined($self->{option_results}->{filter}) && $name !~ /$self->{option_results}->{filter}/) {
             next;
         }
-        if (defined($self->{option_results}->{exclude_user}) && $row->{usename} !~ /$self->{option_results}->{exclude_user}/) {
-            next;
+        my $prct_used;
+        if ($row->{pages_free} == 0) {
+            $prct_used = 100;
+        } else {
+            $prct_used = ($row->{pages_size} - $row->{pages_free}) * 100 / $row->{pages_size};
         }
         
-        my $exit_code = $self->{perfdata}->threshold_check(value => $row->{seconds}, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-        if ($self->{output}->is_status(value => $exit_code, compare => 'ok', litteral => 1)) {
-            $self->{output}->output_add(long_msg => sprintf("Request from client '%s' too long (%d sec) on database '%s': %s",
-                                                            $row->{client_addr}, $row->{seconds}, $row->{datname}, $row->{current_query}));
-            $dbquery->{$row->{datname}}->{total}++;
-            $dbquery->{$row->{datname}}->{code}->{$exit_code}++;
+        $self->{output}->output_add(long_msg => sprintf("Dbspace '%s': Used: %.2f%% Free: %.2f%%",
+                                                         $name, $prct_used, 100 - $prct_used));
+        my $exit_code = $self->{perfdata}->threshold_check(value => $prct_used, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+        
+        if (!$self->{output}->is_status(value => $exit_code, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit_code,
+                                        short_msg => sprintf("Dbspace '%s': Used: %.2f%% Free: %.2f%%",
+                                                             $name, $prct_used, 100 - $prct_used));
         }
-    }
-    
-    foreach my $dbname (keys %$dbquery) {
-        $self->{output}->perfdata_add(label => $dbname . '_qtime_num',
-                                      value => $dbquery->{$dbname}->{total},
+        
+        $self->{output}->perfdata_add(label => 'used_' . $name, unit => '%',
+                                      value => sprintf("%.2f", $prct_used),
                                       warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
                                       critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
-                                      min => 0);
-        foreach my $exit_code (keys %{$dbquery->{$dbname}->{code}}) {
-            $self->{output}->output_add(severity => $exit_code,
-                                        short_msg => sprintf("%d request exceed " . lc($exit_code) . " threshold on database '%s'",
-                                                             $dbquery->{$dbname}->{code}->{$exit_code}, $dbname));
-        }
+                                      min => 0, max => 100);
     }
 
     $self->{output}->display();
@@ -142,25 +132,21 @@ __END__
 
 =head1 MODE
 
-Checks the time of running queries for one or more databases
+Check usage for one or more dbspaces.
 
 =over 8
 
 =item B<--warning>
 
-Threshold warning in seconds.
+Threshold warning in percent.
 
 =item B<--critical>
 
-Threshold critical in seconds.
+Threshold critical in percent.
 
-=item B<--exclude>
+=item B<--filter>
 
-Filter databases.
-
-=item B<--exclude-user>
-
-Filter users.
+Filter dbspaces.
 
 =back
 
