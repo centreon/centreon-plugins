15ad34623431df9542e7f3c1c542e1581da688d7
Quentin Garnier <qgarnier@merethis.com>
2014-01-13 10:35:46 +0100

    + Add traffic for juniper. Need to use ifDesc for Juniper MAG. It seems to have a SNMP bug with Juniper MAG

--- centreon-plugins/network/juniper/mag/plugin.pm
+++ centreon-plugins/network/juniper/mag/plugin.pm
@@ -53,6 +53,7 @@ sub new {
                          'users'                => 'network::juniper::common::ive::mode::users',
                          'memory'               => 'snmp_standard::mode::memory',
                          'swap'                 => 'snmp_standard::mode::swap',
+                         'traffic'              => 'snmp_standard::mode::traffic',
                          'blade-temperature'    => 'network::juniper::mag::mode::bladetemperature',
                          );
 
