ebf1e9cc866919184d1ce8cc017b5c2cba9bc6c1
Quentin Garnier <qgarnier@merethis.com>
2014-02-07 12:05:21 +0100

    Refs #5185 Beginning

---
+++ centreon-plugins/hardware/server/ibm/mgmt_cards/imm/snmp/mode/hardware.pm
@@ -0,0 +1,151 @@
+################################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package hardware::server::ibm::mgmt_cards::imm::snmp::mode::environment;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+use hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::globalstatus;
+use hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::temperature;
+use hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::voltage;
+use hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::fan;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "exclude:s"        => { name => 'exclude' },
+                                  "component:s"      => { name => 'component', default => 'all' },
+                                });
+    $self->{components} = {};
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub global {
+    my ($self, %options) = @_;
+
+    hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::globalstatus::check($self);
+    hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::temperature::check($self);
+    hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::voltage::check($self);
+    hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::fan::check($self);
+}
+
+sub run {
+    my ($self, %options) = @_;
+    # $options{snmp} = snmp object
+    $self->{snmp} = $options{snmp};
+    
+    if ($self->{option_results}->{component} eq 'all') {
+        $self->global();
+    } elsif ($self->{option_results}->{component} eq 'globalstatus') {
+        hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::globalstatus::check($self);
+    } elsif ($self->{option_results}->{component} eq 'temperature') {
+        hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::temperature::check($self);
+    } elsif ($self->{option_results}->{component} eq 'voltage') {
+        hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::voltage::check($self);
+    } elsif ($self->{option_results}->{component} eq 'fan') {
+        hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::fan::check($self);
+    } else {
+        $self->{output}->add_option_msg(short_msg => "Wrong option. Cannot find component '" . $self->{option_results}->{component} . "'.");
+        $self->{output}->option_exit();
+    }
+    
+    my $total_components = 0;
+    my $display_by_component = '';
+    my $display_by_component_append = '';
+    foreach my $comp (sort(keys %{$self->{components}})) {
+        # Skipping short msg when no components
+        next if ($self->{components}->{$comp}->{total} == 0);
+        $total_components += $self->{components}->{$comp}->{total};
+        $display_by_component .= $display_by_component_append . $self->{components}->{$comp}->{total} . ' ' . $self->{components}->{$comp}->{name};
+        $display_by_component_append = ', ';
+    }
+    
+    $self->{output}->output_add(severity => 'OK',
+                                short_msg => sprintf("All %s components [%s] are ok.", 
+                                                     $total_components,
+                                                     $display_by_component
+                                                    )
+                                );
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+sub check_exclude {
+    my ($self, $section) = @_;
+
+    if (defined($self->{option_results}->{exclude}) && $self->{option_results}->{exclude} =~ /(^|\s|,)$section(\s|,|$)/) {
+        $self->{output}->output_add(long_msg => sprintf("Skipping $section section."));
+        return 1;
+    }
+    return 0;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check sensors (Fans, Temperatures, Voltages).
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: 'all').
+Can be: 'globalstatus', 'fan', 'temperature', 'voltage'.
+
+=item B<--exclude>
+
+Exclude some parts (comma seperated list) (Example: --exclude=temperatures,fans).
+
+=back
+
+=cut
+    
