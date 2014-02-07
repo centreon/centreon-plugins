73be264a2b6f380593eb366d143d0a897c8b69b4
Quentin Garnier <qgarnier@merethis.com>
2014-02-07 12:05:21 +0100

    Refs #5185 Beginning

---
+++ hardware/server/ibm/mgmt_cards/imm/snmp/mode/components/voltage.pm
@@ -0,0 +1,97 @@
+################################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package hardware::server::ibm::mgmt_cards::imm::snmp::mode::components::voltage;
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+
+sub check {
+    my ($self) = @_;
+
+    $self->{components}->{voltages} = {name => 'voltages', total => 0};
+    $self->{output}->output_add(long_msg => "Checking voltages");
+    return if ($self->check_exclude('voltages'));
+    
+    my $oid_voltEntry = '.1.3.6.1.4.1.2.3.51.3.1.1.2.1';
+    my $oid_voltDescr = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.2';
+    my $oid_voltReading = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.3';
+    my $oid_voltCritLimitHigh = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.6';
+    my $oid_voltNonCritLimitHigh = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.7';
+    my $oid_voltCritLimitLow = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.9';
+    my $oid_voltNonCritLimitLow = '.1.3.6.1.4.1.2.3.51.3.1.2.2.1.10';
+    
+    my $result = $self->{snmp}->get_table(oid => $oid_voltEntry);
+    return if (scalar(keys %$result) <= 0);
+
+    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$result)) {
+        next if ($key !~ /^$oid_voltDescr\.(\d+)$/;
+        my $instance = $1;
+    
+        my $volt_descr = centreon::plugins::misc($result->{$oid_voltDescr . '.' . $instance});
+        my $volt_value = $result->{$oid_voltReading . '.' . $instance};
+        my $volt_crit_high = $result->{$oid_voltCritLimitHigh . '.' . $instance};
+        my $volt_warn_high = $result->{$oid_voltNonCritLimitHigh . '.' . $instance};
+        my $volt_crit_low = $result->{$oid_voltCritLimitLow . '.' . $instance};
+        my $volt_warn_low = $result->{$oid_voltNonCritLimitLow . '.' . $instance};
+        
+        my $warn_threshold = '';
+        $warn_threshold = $volt_warn_low . ':' . $volt_warn_high;
+        my $crit_threshold = '';
+        $crit_threshold = $volt_crit_low . ':' . $volt_crit_high;
+        
+        $self->{perfdata}->threshold_validate(label => 'warning_' . $instance, value => $warn_threshold);
+        $self->{perfdata}->threshold_validate(label => 'critical_' . $instance, value => $crit_threshold);
+        
+        my $exit = $self->{perfdata}->threshold_check(value => $temp_value, threshold => [ { label => 'critical_' . $instance, 'exit_litteral' => 'critical' }, { label => 'warning_' . $instance, exit_litteral => 'warning' } ]);
+        
+        $self->{components}->{temperatures}->{total}++;
+        $self->{output}->output_add(long_msg => sprintf("Voltage '%s' value is %s.", 
+                                    $volt_descr, $volt_value));
+        if (!$self->{output}->is_status(litteral => 1, value => $exit, compare => 'ok')) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Voltage '%s' value is %s", $volt_descr, $volt_value));
+        }
+        
+        $self->{output}->perfdata_add(label => 'volt_' . $volt_descr,
+                                      value => $volt_value,
+                                      warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning_' . $instance),
+                                      critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical_' . $instance),
+                                      );
+    }
+}
+
+1;
---
+++ hardware/server/ibm/mgmt_cards/imm/snmp/mode/eventlog.pm
@@ -0,0 +1,158 @@
+################################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package hardware::server::ibm::mgmt_cards::imm::snmp::mode::eventlog;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+use centreon::plugins::misc;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "filter-severity:s"   => { name => 'filter_severity', default => 'error' },
+                                  "filter-message:s"    => { name => 'filter_message' },
+                                  "memory"              => { name => 'memory' },
+                                });
+    $self->{statefile_cache} = centreon::plugins::statefile->new(%options);
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    if (defined($self->{option_results}->{memory})) {
+        $self->{statefile_cache} = centreon::plugins::statefile->new(%options);
+    }
+}
+
+
+sub run {
+    my ($self, %options) = @_;
+    # $options{snmp} = snmp object
+    $self->{snmp} = $options{snmp};
+    $self->{hostname} = $self->{snmp}->get_hostname();
+    $self->{snmp_port} = $self->{snmp}->get_port();
+    my $datas = {};
+    
+    if (defined($self->{option_results}->{memory})) {
+        $self->{statefile_cache}->read(statefile => "cache_imm_" . $self->{hostname}  . '_' . $self->{snmp_port} . '_' . $self->{mode});
+        $self->{output}->output_add(severity => 'OK', 
+                                    short_msg => "No new problems on system.");
+    } else {
+        $self->{output}->output_add(severity => 'OK', 
+                                    short_msg => "No problems on system.");
+    }
+    
+    #### Get OIDS
+    ## Not need to check from an index point (not so much values. and can clear)
+    
+    my $oid_eventLogEntry = '.1.3.6.1.4.1.2.3.51.3.2.1.1.1';
+    my $oid_eventLogString = '.1.3.6.1.4.1.2.3.51.3.2.1.1.1.2';
+    my $oid_eventLogSeverity = '.1.3.6.1.4.1.2.3.51.3.2.1.1.1.3';
+    my $oid_eventLogDate = '.1.3.6.1.4.1.2.3.51.3.2.1.1.1.4'; # Month/Day/YEAR
+    my $oid_eventLogTime = '.1.3.6.1.4.1.2.3.51.3.2.1.1.1.5'; # Hour::Min::Sec
+    
+    my $result = $self->{snmp}->get_table(oid => $oid_eventLogEntry);
+
+    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$result)) {
+        next if ($key !~ /^$oid_eventLogString\.(\d+)$/;
+        my $instance = $1;
+
+        my $message = centreon::plugins::misc($result->{$oid_eventLogString . '.' . $instance});
+        my $severity = $result->{$oid_eventLogSeverity . '.' . $instance};
+        my $date = $result->{$oid_eventLogDate . '.' . $instance};
+        my $time = $result->{$oid_eventLogTime . '.' . $instance};
+        
+        my $date_compare = '';
+        $date =~ {(\d+)/(\d+)/(\d+)};
+        $date_compare = $3 . $1 . $2;
+        $time =~ {(\d+)::(\d+)::(\d+)};
+        $date_compare .= $1 . $2 . $3
+        
+        
+    }
+        
+    $self->{output}->output_add(severity => 'OK',
+                                short_msg => sprintf("All %s components [%s] are ok.", 
+                                                     $total_components,
+                                                     $display_by_component
+                                                    )
+                                );
+    
+    if (defined($self->{option_results}->{memory})) {
+        $self->{statefile_cache}->write(data => $datas);
+    }
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check eventlogs.
+
+=over 8
+
+=item B<--memory>
+
+Only check new eventlogs.
+
+=item B<--filter-severity>
+
+Filter on severity. (Default: error)
+Can be: error, warning, information, other. 
+
+=item B<--filter-message>
+
+Filter on event message. (Default: none)
+
+=back
+
+=cut
+    
