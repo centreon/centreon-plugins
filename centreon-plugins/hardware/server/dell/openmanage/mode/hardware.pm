14fe374dccb01e7db58b55239048aee761d321bb
Kevin Duret <kduret@merethis.com>
2014-02-18 15:52:12 +0000

    Add ESM log component #5151

--- centreon-plugins/hardware/server/dell/openmanage/mode/hardware.pm
+++ centreon-plugins/hardware/server/dell/openmanage/mode/hardware.pm
@@ -49,6 +49,7 @@ use hardware::server::dell::openmanage::mode::components::cachebattery;
 use hardware::server::dell::openmanage::mode::components::memory;
 use hardware::server::dell::openmanage::mode::components::physicaldisk;
 use hardware::server::dell::openmanage::mode::components::logicaldrive;
+use hardware::server::dell::openmanage::mode::components::esmlog;
 
 sub new {
     my ($class, %options) = @_;
@@ -85,6 +86,7 @@ sub global {
     hardware::server::dell::openmanage::mode::components::memory::check($self);
     hardware::server::dell::openmanage::mode::components::physicaldisk::check($self);
     hardware::server::dell::openmanage::mode::components::logicaldrive::check($self);
+    hardware::server::dell::openmanage::mode::components::esmlog::check($self);
 
     my $total_components = 0;
     my $display_by_component = '';
@@ -126,6 +128,8 @@ sub component {
         hardware::server::dell::openmanage::mode::components::physicaldisk::check($self);
     } elsif ($self->{option_results}->{component} eq 'logicaldrive') {
         hardware::server::dell::openmanage::mode::components::logicaldrive::check($self);
+    } elsif ($self->{option_results}->{component} eq 'esmlog') {
+        hardware::server::dell::openmanage::mode::components::esmlog::check($self);
     }else {
         $self->{output}->add_option_msg(short_msg => "Wrong option. Cannot find component '" . $self->{option_results}->{component} . "'.");
         $self->{output}->option_exit();
