b2574eb3e8d2c30a739237c7607cab10f025462f
Quentin Garnier <qgarnier@merethis.com>
2014-02-25 12:08:03 +0100

    Some fix on apache plugin

--- centreon-plugins/apps/apache/serverstatus/mode/workers.pm
+++ centreon-plugins/apps/apache/serverstatus/mode/workers.pm
@@ -1,38 +1,38 @@
-################################################################################
-## Copyright 2005-2013 MERETHIS
-## Centreon is developped by : Julien Mathis and Romain Le Merlus under
-## GPL Licence 2.0.
-## 
-## This program is free software; you can redistribute it and/or modify it under 
-## the terms of the GNU General Public License as published by the Free Software 
-## Foundation ; either version 2 of the License.
-## 
-## This program is distributed in the hope that it will be useful, but WITHOUT ANY
-## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-## PARTICULAR PURPOSE. See the GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License along with 
-## this program; if not, see <http://www.gnu.org/licenses>.
-## 
-## Linking this program statically or dynamically with other modules is making a 
-## combined work based on this program. Thus, the terms and conditions of the GNU 
-## General Public License cover the whole combination.
-## 
-## As a special exception, the copyright holders of this program give MERETHIS 
-## permission to link this program with independent modules to produce an timeelapsedutable, 
-## regardless of the license terms of these independent modules, and to copy and 
-## distribute the resulting timeelapsedutable under terms of MERETHIS choice, provided that 
-## MERETHIS also meet, for each linked independent module, the terms  and conditions 
-## of the license of that module. An independent module is a module which is not 
-## derived from this program. If you modify this program, you may extend this 
-## exception to your version of the program, but you are not obliged to do so. If you
-## do not wish to do so, delete this exception statement from your version.
-## 
-## For more information : contact@centreon.com
-## Author : Simon BOMM <sbomm@merethis.com>
-##
-## Based on De Bodt Lieven plugin
-#####################################################################################
+###############################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an timeelapsedutable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting timeelapsedutable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Author : Simon BOMM <sbomm@merethis.com>
+#
+# Based on De Bodt Lieven plugin
+####################################################################################
 
 package apps::apache::serverstatus::mode::workers;
 
@@ -50,70 +50,66 @@ sub new {
     $self->{version} = '1.0';
     $options{options}->add_options(arguments =>
             {
-            "hostname:s"        => { name => 'hostname' },
-            "port:i"    => { name => 'port' },
-            "proto:s"    => { name => 'proto', default => "http" },
+            "hostname:s"    => { name => 'hostname' },
+            "port:s"        => { name => 'port' },
+            "proto:s"       => { name => 'proto', default => "http" },
             "proxyurl:s"    => { name => 'proxyurl' },
-            "warning:s"    => { name => 'warning' },
+            "warning:s"     => { name => 'warning' },
             "critical:s"    => { name => 'critical' },
-            "timeout:i"        => { name => 'timeout', default => '3' },
+            "timeout:s"     => { name => 'timeout', default => '3' },
             });
     return $self;
 }
 
 sub check_options {
-
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
 
     if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{warning} . "'.");
+        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
         $self->{output}->option_exit();
     }
     if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{critical} . "'.");
+        $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
         $self->{output}->option_exit();
     }
     if (($self->{option_results}->{proto} ne 'http') && ($self->{option_results}->{proto} ne 'https')) {
-        $self->{output}->add_option_msg(short_msg => "Unsupported protocol specified '" . $self->{proto} . "'.");
-                $self->{output}->option_exit();
+        $self->{output}->add_option_msg(short_msg => "Unsupported protocol specified '" . $self->{option_results}->{proto} . "'.");
+        $self->{output}->option_exit();
     }
     if (!defined($self->{option_results}->{hostname})) {
         $self->{output}->add_option_msg(short_msg => "Please set the hostname option");
         $self->{output}->option_exit();
     }
-
 }
 
 sub run {
-
     my ($self, %options) = @_;
     my $ua = LWP::UserAgent->new( protocols_allowed => ['http','https'], timeout => $self->{option_results}->{timeout});
     
     my $response = '';
     
     if ($self->{option_results}->{proto} eq "https") {
-            if (defined $self->{option_results}->{proxyurl}) {
-                    $ua->proxy(['https'], $self->{option_results}->{proxyurl});
-                }
-                if (!defined $self->{option_results}->{port}) {
-                        $response = $ua->get($self->{option_results}->{proto}."://" .$self->{option_results}->{hostname}.'/server-status');
-                } else  {
-                        $response = $ua->get('https://'.$self->{option_results}->{hostname}.':'.$self->{option_results}->{port}.'/server-status');
-                }
-        } else {
-                if (defined $self->{option_results}->{proxyurl}) {
-                        $ua->proxy(['http'], $self->{option_results}->{proxyurl});
-                }
-                if (!defined $self->{option_results}->{port}) {
-                        $response = $ua->get($self->{option_results}->{proto}."://" .$self->{option_results}->{hostname}.'/server-status');
-                } else  {
-                        $response = $ua->get('http://'.$self->{option_results}->{hostname}.':'.$self->{option_results}->{port}.'/server-status');
-                }
+        if (defined $self->{option_results}->{proxyurl}) {
+            $ua->proxy(['https'], $self->{option_results}->{proxyurl});
         }
+        if (!defined $self->{option_results}->{port}) {
+            $response = $ua->get($self->{option_results}->{proto}."://" .$self->{option_results}->{hostname}.'/server-status');
+        } else  {
+            $response = $ua->get('https://'.$self->{option_results}->{hostname}.':'.$self->{option_results}->{port}.'/server-status');
+        }
+    } else {
+        if (defined $self->{option_results}->{proxyurl}) {
+            $ua->proxy(['http'], $self->{option_results}->{proxyurl});
+        }
+        if (!defined $self->{option_results}->{port}) {
+            $response = $ua->get($self->{option_results}->{proto}."://" .$self->{option_results}->{hostname}.'/server-status');
+        } else  {
+            $response = $ua->get('http://'.$self->{option_results}->{hostname}.':'.$self->{option_results}->{port}.'/server-status');
+        }
+    }
 
-    if ($response->is_success) {
-        
+    if ($response->is_success) {   
         my $BusyWorkers;
         my $IdleWorkers;
         my $webcontent=$response->content;
@@ -131,18 +127,18 @@ sub run {
         $prct_busy = $BusyWorkers / ($BusyWorkers + $IdleWorkers) * 100;
     
         my $exit = $self->{perfdata}->threshold_check(value => $prct_busy,
-                threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+                                                      threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
         $self->{output}->output_add(severity => $exit,
-                short_msg => sprintf("Busy workers: %d Idle workers: %d ( %d %% )", $BusyWorkers, $IdleWorkers, $prct_busy));
+                                    short_msg => sprintf("Busy workers: %d Idle workers: %d ( %d %% )", $BusyWorkers, $IdleWorkers, $prct_busy));
         $self->{output}->perfdata_add(label => "idle_workers",
-                value => $IdleWorkers);
+                                      value => $IdleWorkers);
         $self->{output}->perfdata_add(label => "busy_workers",
-                value => $BusyWorkers,
-                warning => $self->{option_results}->{warning},
-                critical => $self->{option_results}->{critical});
+                                      value => $BusyWorkers,
+                                      warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
+                                      critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'));
     } else {
         $self->{output}->output_add(severity => 'UNKNOWN',
-                short_msg => $response->status_line);
+                                    short_msg => $response->status_line);
     }
 
     $self->{output}->display();
