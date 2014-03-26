a6b02fd758fdf3818ebf2493c768b40745e30846
Kevin Duret <kduret@merethis.com>
2014-03-26 14:02:56 +0100

    Add local storage check on aix

---
+++ centreon-plugins/os/aix/local/mode/storage.pm
@@ -0,0 +1,302 @@
+################################################################################
+# Copyright 2005-2014 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package os::aix::local::mode::storage;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                  "hostname:s"        => { name => 'hostname' },
+                                  "remote"            => { name => 'remote' },
+                                  "ssh-option:s@"     => { name => 'ssh_option' },
+                                  "ssh-path:s"        => { name => 'ssh_path' },
+                                  "ssh-command:s"     => { name => 'ssh_command', default => 'ssh' },
+                                  "timeout:s"         => { name => 'timeout', default => 30 },
+                                  "sudo"              => { name => 'sudo' },
+                                  "command:s"         => { name => 'command', default => 'df' },
+                                  "command-path:s"    => { name => 'command_path' },
+                                  "command-options:s" => { name => 'command_options', default => '-P -k 2>&1' },
+                                  "filter-fs:s"       => { name => 'filter_fs', },
+                                  "warning:s"         => { name => 'warning' },
+                                  "critical:s"        => { name => 'critical' },
+                                  "units:s"           => { name => 'units', default => '%' },
+                                  "free"              => { name => 'free' },
+                                  "name:s"            => { name => 'name' },
+                                  "regexp"              => { name => 'use_regexp' },
+                                  "regexp-isensitive"   => { name => 'use_regexpi' },
+                                  "space-reservation:s" => { name => 'space_reservation' },
+                                });
+    $self->{result} = {};
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+       $self->{output}->option_exit();
+    }
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $stdout = centreon::plugins::misc::execute(output => $self->{output},
+                                                  options => $self->{option_results},
+                                                  sudo => $self->{option_results}->{sudo},
+                                                  command => $self->{option_results}->{command},
+                                                  command_path => $self->{option_results}->{command_path},
+                                                  command_options => $self->{option_results}->{command_options});
+    my @lines = split /\n/, $stdout;
+    # Header not needed
+    shift @lines;
+    foreach my $line (@lines) {
+        next if ($line !~ /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.*)/);
+        my ($fs, $size, $used, $available, $percent, $mount) = ($1, $2, $3, $4, $5, $6);
+        
+        next if (defined($self->{option_results}->{filter_fs}) && $self->{option_results}->{filter_fs} ne '' &&
+                 $fs !~ /$self->{option_results}->{filter_fs}/);
+        
+        next if (defined($self->{option_results}->{name}) && defined($self->{option_results}->{use_regexp}) && defined($self->{option_results}->{use_regexpi}) 
+            && $mount !~ /$self->{option_results}->{name}/i);
+        next if (defined($self->{option_results}->{name}) && defined($self->{option_results}->{use_regexp}) && !defined($self->{option_results}->{use_regexpi}) 
+            && $mount !~ /$self->{option_results}->{name}/);
+        next if (defined($self->{option_results}->{name}) && !defined($self->{option_results}->{use_regexp}) && !defined($self->{option_results}->{use_regexpi})
+            && $mount ne $self->{option_results}->{name});
+
+	next if ($size !~ m/^\d+$/ || $used !~ m/^\d+$/ || $available !~ m/^\d+$/);
+
+        $self->{result}->{$mount} = {fs => $fs, total => $size, used => $used, free => $available};
+    }
+    
+    if (scalar(keys %{$self->{result}}) <= 0) {
+        if (defined($self->{option_results}->{name})) {
+            $self->{output}->add_option_msg(short_msg => "No storage found for mount point '" . $self->{option_results}->{name} . "'.");
+        } else {
+            $self->{output}->add_option_msg(short_msg => "No storage found.");
+        }
+        $self->{output}->option_exit();
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+	
+    $self->manage_selection();
+    if (!defined($self->{option_results}->{name}) || defined($self->{option_results}->{use_regexp})) {
+        $self->{output}->output_add(severity => 'OK',
+                                    short_msg => 'All storages are ok.');
+    }
+    
+    my $num_disk_check = 0;
+    foreach my $name (sort(keys %{$self->{result}})) {
+        my $total_size = $self->{result}->{$name}->{total} * 1024;
+        if (defined($self->{option_results}->{space_reservation})) {
+            $total_size = $total_size - ($self->{option_results}->{space_reservation} * $total_size / 100);
+        }
+        next if ($total_size == 0);
+        
+        $num_disk_check++;
+        my $total_used = $self->{result}->{$name}->{used} * 1024;
+        my $total_free = $self->{result}->{$name}->{free} * 1024;
+        my $prct_used = $total_used * 100 / $total_size;
+        my $prct_free = 100 - $prct_used;
+
+        my ($exit, $threshold_value);
+
+        $threshold_value = $total_used;
+        $threshold_value = $total_free if (defined($self->{option_results}->{free}));
+        if ($self->{option_results}->{units} eq '%') {
+            $threshold_value = $prct_used;
+            $threshold_value = $prct_free if (defined($self->{option_results}->{free}));
+        } 
+        $exit = $self->{perfdata}->threshold_check(value => $threshold_value, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+
+        my ($total_size_value, $total_size_unit) = $self->{perfdata}->change_bytes(value => $total_size);
+        my ($total_used_value, $total_used_unit) = $self->{perfdata}->change_bytes(value => $total_used);
+        my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => ($total_size - $total_used));
+
+        $self->{output}->output_add(long_msg => sprintf("Storage '%s' Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)", $name,
+                                            $total_size_value . " " . $total_size_unit,
+                                            $total_used_value . " " . $total_used_unit, $prct_used,
+                                            $total_free_value . " " . $total_free_unit, $prct_free));
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1) || (defined($self->{option_results}->{name}) && !defined($self->{option_results}->{use_regexp}))) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Storage '%s' Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)", $name,
+                                            $total_size_value . " " . $total_size_unit,
+                                            $total_used_value . " " . $total_used_unit, $prct_used,
+                                            $total_free_value . " " . $total_free_unit, $prct_free));
+        }    
+
+        my $label = 'used';
+        my $value_perf = $total_used;
+        if (defined($self->{option_results}->{free})) {
+            $label = 'free';
+            $value_perf = $total_free;
+        }
+        my $extra_label = '';
+        $extra_label = '_' . $name if (!defined($self->{option_results}->{name}) || defined($self->{option_results}->{use_regexp}));
+        my %total_options = ();
+        if ($self->{option_results}->{units} eq '%') {
+            $total_options{total} = $total_size;
+            $total_options{cast_int} = 1; 
+        }
+        $self->{output}->perfdata_add(label => $label . $extra_label, unit => 'B',
+                                      value => $value_perf,
+                                      warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning', %total_options),
+                                      critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical', %total_options),
+                                      min => 0, max => $total_size);
+    }
+
+    if ($num_disk_check == 0) {
+        $self->{output}->add_option_msg(short_msg => "No storage checked.");
+        $self->{output}->option_exit();
+    }
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check storage usages.
+
+=over 8
+
+=item B<--remote>
+
+Execute command remotely in 'ssh'.
+
+=item B<--hostname>
+
+Hostname to query (need --remote).
+
+=item B<--ssh-option>
+
+Specify multiple options like the user (example: --ssh-option='-l=centreon-engine' --ssh-option='-p=52').
+
+=item B<--ssh-path>
+
+Specify ssh command path (default: none)
+
+=item B<--ssh-command>
+
+Specify ssh command (default: 'ssh'). Useful to use 'plink'.
+
+=item B<--timeout>
+
+Timeout in seconds for the command (Default: 30).
+
+=item B<--sudo>
+
+Use 'sudo' to execute the command.
+
+=item B<--command>
+
+Command to get information (Default: 'df').
+Can be changed if you have output in a file.
+
+=item B<--command-path>
+
+Command path (Default: none).
+
+=item B<--command-options>
+
+Command options (Default: '-P -k -T 2>&1').
+
+=item B<--warning>
+
+Threshold warning.
+
+=item B<--critical>
+
+Threshold critical.
+
+=item B<--units>
+
+Units of thresholds (Default: '%') ('%', 'B').
+
+=item B<--free>
+
+Thresholds are on free space left.
+
+=item B<--name>
+
+Set the storage mount point (empty means 'check all storages')
+
+=item B<--regexp>
+
+Allows to use regexp to filter storage mount point (with option --name).
+
+=item B<--regexp-isensitive>
+
+Allows to use regexp non case-sensitive (with --regexp).
+
+=item B<--filter-type>
+
+Filter filesystem type (regexp can be used).
+
+=item B<--filter-fs>
+
+Filter filesystem (regexp can be used).
+
+=item B<--space-reservation>
+
+Some filesystem has space reserved (like ext4 for root).
+The value is in percent of total (Default: none).
+
+=back
+
+=cut
