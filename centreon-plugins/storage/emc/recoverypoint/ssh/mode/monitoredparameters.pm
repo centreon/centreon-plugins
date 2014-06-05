dd4962b30988dd8b052bf7b60a0eca1aebbd6270
Kevin Duret <root@localhost.localdomain>
2014-06-05 14:36:19 +0200

    Add plugin refs #5597

---
+++ centreon-plugins/storage/emc/recoverypoint/ssh/mode/monitoredparameters.pm
@@ -0,0 +1,199 @@
+################################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package storage::emc::recoverypoint::ssh::mode::monitoredparameters;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "hostname:s"        => { name => 'hostname' },
+                                  "ssh-option:s@"     => { name => 'ssh_option' },
+                                  "ssh-path:s"        => { name => 'ssh_path' },
+                                  "ssh-command:s"     => { name => 'ssh_command', default => 'ssh' },
+                                  "timeout:s"         => { name => 'timeout', default => 30 },
+                                  "sudo"              => { name => 'sudo' },
+                                  "command:s"         => { name => 'command', default => 'get_monitored_parameters' },
+                                  "command-path:s"    => { name => 'command_path' },
+                                  "command-options:s" => { name => 'command_options', default => '' },
+                                  "min-severity:s"    => { name => 'min_severity', default => 'minor' },
+                                  "warning:s"         => { name => 'warning', },
+                                  "critical:s"        => { name => 'critical', },
+                                });
+    $self->{manage_returns} = {};
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    if (!defined($self->{option_results}->{command})) {
+       $self->{output}->add_option_msg(short_msg => "Need to specify command option.");
+       $self->{output}->option_exit();
+    }
+
+    if (!defined($self->{option_results}->{hostname})) {
+       $self->{output}->add_option_msg(short_msg => "Need to specify hostname.");
+       $self->{output}->option_exit();
+    }
+
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{option_results}->{min_severity} !~ /minor/m) && ($self->{option_results}->{min_severity} !~ /major/m) && ($self->{option_results}->{min_severity} !~ /critical/m)) {
+        $self->{output}->add_option_msg(short_msg => 'Min-severity must be minor, major or critical.');
+        $self->{output}->option_exit();
+    } 
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my $min_severity = ' min_severity=' . $self->{option_results}->{min_severity};
+    $self->{option_results}->{remote} = 1;
+
+    my ($stdout, $exit_code) = centreon::plugins::misc::execute(output => $self->{output},
+                                                                options => $self->{option_results},
+                                                                sudo => $self->{option_results}->{sudo},
+                                                                command => $self->{option_results}->{command},
+                                                                command_path => $self->{option_results}->{command_path},
+                                                                command_options => $self->{option_results}->{command_options} . $min_severity,
+                                                                );
+    my $long_msg = $stdout;
+    $long_msg =~ s/\|/~/mg;
+
+    my $count = 0;
+    foreach (split(/\n/, $stdout)) {
+        if (/^\s*Type:/im) {
+            $count++;
+        }
+    }    
+
+    $exit_code = $self->{perfdata}->threshold_check(value => $count, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+
+    $self->{output}->output_add(long_msg => $long_msg);
+    $self->{output}->output_add(severity => $exit_code, 
+                                short_msg => sprintf("%i problems found.",
+                                            $count));
+
+    $self->{output}->perfdata_add(label => "problems",
+                                  value => $count,
+                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
+                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
+                                  min => 0);
+
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check monitored paramaters by RecoveryPoint Appliance.
+
+=over 8
+
+=item B<--hostname>
+
+Hostname to query (need --remote).
+
+=item B<--ssh-option>
+
+Specify multiple options like the user (example: --ssh-option='-l=centreon-engine' --ssh-option='-pw=password').
+
+=item B<--ssh-path>
+
+Specify ssh command path (default: none)
+
+=item B<--ssh-command>
+
+Specify ssh command (default: 'ssh'). Useful to use 'plink'.
+
+=item B<--timeout>
+
+Timeout in seconds for the command (Default: 30).
+
+=item B<--sudo>
+
+Use 'sudo' to execute the command.
+
+=item B<--command>
+
+Command to test (Default: get_monitored_parameters).
+You can use 'sh' to use '&&' or '||'.
+
+=item B<--command-path>
+
+Command path (Default: none).
+
+=item B<--command-options>
+
+Command options.
+
+=item B<--min-severity>
+
+Minimum severity level you want to count (Default: minor).
+Can be 'minor', 'major' or 'critical'.
+
+=item B<--warning>
+
+Threshold warning.
+
+=item B<--critical>
+
+Threshold critical.
+
+=back
+
+=cut
---
+++ centreon-plugins/storage/emc/recoverypoint/ssh/mode/systemstatus.pm
@@ -0,0 +1,167 @@
+################################################################################
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package storage::emc::recoverypoint::ssh::mode::systemstatus;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "hostname:s"        => { name => 'hostname' },
+                                  "remote"            => { name => 'remote' },
+                                  "ssh-option:s@"     => { name => 'ssh_option' },
+                                  "ssh-path:s"        => { name => 'ssh_path' },
+                                  "ssh-command:s"     => { name => 'ssh_command', default => 'ssh' },
+                                  "timeout:s"         => { name => 'timeout', default => 30 },
+                                  "sudo"              => { name => 'sudo' },
+                                  "command:s"         => { name => 'command', default => 'get_system_status' },
+                                  "command-path:s"    => { name => 'command_path' },
+                                  "command-options:s" => { name => 'command_options', default => 'category=system summary=yes' },
+                                });
+    $self->{manage_returns} = {};
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    if (!defined($self->{option_results}->{command})) {
+       $self->{output}->add_option_msg(short_msg => "Need to specify command option.");
+       $self->{output}->option_exit();
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    $self->{option_results}->{remote} = 1;
+
+    my ($stdout, $exit_code) = centreon::plugins::misc::execute(output => $self->{output},
+                                                                options => $self->{option_results},
+                                                                sudo => $self->{option_results}->{sudo},
+                                                                command => $self->{option_results}->{command},
+                                                                command_path => $self->{option_results}->{command_path},
+                                                                command_options => $self->{option_results}->{command_options},
+                                                                );
+    my $long_msg = $stdout;
+    $long_msg =~ s/\|/~/mg;
+
+    my ($system, $clusters, $wans, $groups);
+    foreach (split(/\n/, $stdout)) {
+        if (/^System:\s+(.*)$/im) {
+            $system = $1;
+        } elsif (/^Clusters:\s+(.*)$/im) {
+            $clusters = $1;
+        } elsif (/^Wans:\s+(.*)$/im) {
+            $wans = $1;
+        } elsif (/^Groups:\s+(.*)$/im) {
+            $groups = $1;
+        }
+    }    
+
+    $exit_code = 'ok';
+    if (($system !~ /OK/im) || ($clusters !~ /OK/im) || ($wans !~ /OK/im) || ($groups !~ /OK/im)) {
+        $exit_code = 'critical'
+    }
+
+    $self->{output}->output_add(long_msg => $long_msg);
+    $self->{output}->output_add(severity => $exit_code, 
+                                short_msg => sprintf("System %s, Clusters %s, WANs %s, Groups %s.",
+                                            $system, $clusters, $wans, $groups));
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check system status.
+
+=over 8
+
+=item B<--hostname>
+
+Hostname to query (need --remote).
+
+=item B<--ssh-option>
+
+Specify multiple options like the user (example: --ssh-option='-l=centreon-engine' --ssh-option='-pw=password').
+
+=item B<--ssh-path>
+
+Specify ssh command path (default: none)
+
+=item B<--ssh-command>
+
+Specify ssh command (default: 'ssh'). Useful to use 'plink'.
+
+=item B<--timeout>
+
+Timeout in seconds for the command (Default: 30).
+
+=item B<--sudo>
+
+Use 'sudo' to execute the command.
+
+=item B<--command>
+
+Command to test (Default: get_system_status).
+You can use 'sh' to use '&&' or '||'.
+
+=item B<--command-path>
+
+Command path (Default: none).
+
+=item B<--command-options>
+
+Command options (Default: category=system summary=yes).
+
+=back
+
+=cut
