c860a64b12a476feeccc390ced7b44b75572684b
Florian Asche <info@florian-asche.de>
2014-06-06 16:19:12 +0200

    Refs #5499: Working on Varnish Plugin

--- apps/varnish/local/mode/connections.pm
+++ apps/varnish/local/mode/connections.pm
@@ -33,7 +33,7 @@
 #
 ####################################################################################
 
-package apps::varnish::mode::cache;
+package apps::varnish::mode::connections;
 
 use base qw(centreon::plugins::mode);
 use centreon::plugins::misc;
