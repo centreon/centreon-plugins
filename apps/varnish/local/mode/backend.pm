5229edd6684d803481e5f8b04c90ed9a83d32fec
Florian Asche <info@florian-asche.de>
2014-06-06 15:17:02 +0200

    Refs #5499: Working on Varnish Plugin

--- apps/varnish/local/mode/backend.pm
+++ apps/varnish/local/mode/backend.pm
@@ -33,7 +33,7 @@
 #
 ####################################################################################
 
-package apps::varnish::mode::client;
+package apps::varnish::mode::backend;
 
 use base qw(centreon::plugins::mode);
 use centreon::plugins::misc;
