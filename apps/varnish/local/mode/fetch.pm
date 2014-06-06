6d24ed374746b8cf3bd73bb6b2d1f687d74b431d
Florian Asche <info@florian-asche.de>
2014-06-06 15:17:02 +0200

    Refs #5499: Working on Varnish Plugin

--- apps/varnish/local/mode/fetch.pm
+++ apps/varnish/local/mode/fetch.pm
@@ -33,7 +33,7 @@
 #
 ####################################################################################
 
-package apps::varnish::mode::client;
+package apps::varnish::mode::fetch;
 
 use base qw(centreon::plugins::mode);
 use centreon::plugins::misc;
