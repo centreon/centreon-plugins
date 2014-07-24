5f89fc95265443df6f705a77e90997663685cc85
Quentin Garnier <qgarnier@merethis.com>
2014-07-24 14:22:11 +0200

    Fix #5696 cpu-detailed can work with other system (don't put '.0' but '.1' in index) (like juniper mag) traffic mode: if speed64 = 0, we try the 32 bits counter for speed Add plugin for juniper SA

--- centreon-plugins/snmp_standard/mode/cpudetailed.pm
+++ centreon-plugins/snmp_standard/mode/cpudetailed.pm
@@ -42,17 +42,17 @@ use warnings;
 use centreon::plugins::statefile;
 
 my $oids = {
-    '.1.3.6.1.4.1.2021.11.50.0' => { counter => 'user', output => 'User %.2f %%' }, # ssCpuRawUser
-    '.1.3.6.1.4.1.2021.11.51.0' => { counter => 'nice', output => 'Nice %.2f %%' }, # ssCpuRawNice
-    '.1.3.6.1.4.1.2021.11.52.0' => { counter => 'system', output => 'System %.2f %%' }, # ssCpuRawSystem
-    '.1.3.6.1.4.1.2021.11.53.0' => { counter => 'idle', output => 'Idle %.2f %%' }, # ssCpuRawIdle
-    '.1.3.6.1.4.1.2021.11.54.0' => { counter => 'wait', output => 'Wait %.2f %%' }, # ssCpuRawWait
-    '.1.3.6.1.4.1.2021.11.55.0' => { counter => 'kernel', output => 'Kernel %.2f %%' }, # ssCpuRawKernel
-    '.1.3.6.1.4.1.2021.11.56.0' => { counter => 'interrupt', output => 'Interrupt %.2f %%' }, # ssCpuRawInterrupt
-    '.1.3.6.1.4.1.2021.11.61.0' => { counter => 'softirq', output => 'Soft Irq %.2f %%' }, # ssCpuRawSoftIRQ
-    '.1.3.6.1.4.1.2021.11.64.0' => { counter => 'steal', output => 'Steal %.2f %%' }, # ssCpuRawSteal
-    '.1.3.6.1.4.1.2021.11.65.0' => { counter => 'guest', output => 'Guest %.2f %%' }, # ssCpuRawGuest
-    '.1.3.6.1.4.1.2021.11.66.0' => { counter => 'guestnice', output => 'Guest Nice %.2f %%' }, # ssCpuRawGuestNice
+    '.1.3.6.1.4.1.2021.11.50' => { counter => 'user', output => 'User %.2f %%' }, # ssCpuRawUser
+    '.1.3.6.1.4.1.2021.11.51' => { counter => 'nice', output => 'Nice %.2f %%' }, # ssCpuRawNice
+    '.1.3.6.1.4.1.2021.11.52' => { counter => 'system', output => 'System %.2f %%' }, # ssCpuRawSystem
+    '.1.3.6.1.4.1.2021.11.53' => { counter => 'idle', output => 'Idle %.2f %%' }, # ssCpuRawIdle
+    '.1.3.6.1.4.1.2021.11.54' => { counter => 'wait', output => 'Wait %.2f %%' }, # ssCpuRawWait
+    '.1.3.6.1.4.1.2021.11.55' => { counter => 'kernel', output => 'Kernel %.2f %%' }, # ssCpuRawKernel
+    '.1.3.6.1.4.1.2021.11.56' => { counter => 'interrupt', output => 'Interrupt %.2f %%' }, # ssCpuRawInterrupt
+    '.1.3.6.1.4.1.2021.11.61' => { counter => 'softirq', output => 'Soft Irq %.2f %%' }, # ssCpuRawSoftIRQ
+    '.1.3.6.1.4.1.2021.11.64' => { counter => 'steal', output => 'Steal %.2f %%' }, # ssCpuRawSteal
+    '.1.3.6.1.4.1.2021.11.65' => { counter => 'guest', output => 'Guest %.2f %%' }, # ssCpuRawGuest
+    '.1.3.6.1.4.1.2021.11.66' => { counter => 'guestnice', output => 'Guest Nice %.2f %%' }, # ssCpuRawGuestNice
 };
 
 sub new {
@@ -98,71 +98,111 @@ sub run {
     $self->{hostname} = $self->{snmp}->get_hostname();
     $self->{snmp_port} = $self->{snmp}->get_port();
     
-    my $result = $self->{snmp}->get_leef(oids => [keys %$oids], nothing_quit => 1);
+    my $result = $self->{snmp}->get_table(oid => '.1.3.6.1.4.1.2021.11',
+                                          start => '.1.3.6.1.4.1.2021.11.50',
+                                          nothing_quit => 1);
     
-    # Manage values
-    my ($total, $old_total, $buffer_creation) = (0, 0, 0);
+    # construct values
+    my $info_indexes = {};
     my $new_datas = {};
     my $old_datas = {};
     $new_datas->{last_timestamp} = time();
     $self->{statefile_value}->read(statefile => "snmpstandard_" . $self->{hostname}  . '_' . $self->{snmp_port} . '_' . $self->{mode});
-    foreach (keys %{$oids}) {
-        next if (!defined($result->{$_}));
-        $new_datas->{$oids->{$_}->{counter}} = $result->{$_};
-        $old_datas->{$oids->{$_}->{counter}} = $self->{statefile_value}->get(name => $oids->{$_}->{counter});
-        if (!defined($old_datas->{$oids->{$_}->{counter}})) {
-            $buffer_creation = 1;
+    
+    foreach my $oid (keys %{$result}) {
+        $oid =~ /(.*)\.(\d+)$/;
+        my ($oid_base, $index) = ($1, $2);
+        # If not, we skip oid
+        next if (!defined($oids->{$oid_base}));
+        
+        $new_datas->{$oids->{$oid_base}->{counter} . '_' . $index} = $result->{$oid};
+        $old_datas->{$oids->{$oid_base}->{counter} . '_' . $index} = $self->{statefile_value}->get(name => $oids->{$oid_base}->{counter} . '_' . $index);
+        $info_indexes->{$index} = { total => 0, old_total => 0, buffer_creation => 0 } if (!defined($info_indexes->{$index}));
+        if (!defined($old_datas->{$oids->{$oid_base}->{counter} . '_' . $index})) {
+            $info_indexes->{$index}->{buffer_creation} = 1;
             next;
         }
-        if ($new_datas->{$oids->{$_}->{counter}} < $old_datas->{$oids->{$_}->{counter}}) {
-            $buffer_creation = 1;
+        if ($new_datas->{$oids->{$oid_base}->{counter} . '_' . $index} < $old_datas->{$oids->{$oid_base}->{counter} . '_' . $index}) {
+            $info_indexes->{$index}->{buffer_creation} = 1;
             next;
         }
-        $total += $new_datas->{$oids->{$_}->{counter}};
-        $old_total += $old_datas->{$oids->{$_}->{counter}};
+        $info_indexes->{$index}->{total} += $new_datas->{$oids->{$oid_base}->{counter} . '_' . $index};
+        $info_indexes->{$index}->{old_total} += $old_datas->{$oids->{$oid_base}->{counter} . '_' . $index};
     }
-
     $self->{statefile_value}->write(data => $new_datas);
-    if ($buffer_creation == 1) {
-        $self->{output}->output_add(severity => 'OK',
-                                    short_msg => "Buffer creation...");
-        $self->{output}->display();
-        $self->{output}->exit();
-    }
-    if ($total - $old_total == 0) {
-        $self->{output}->output_add(severity => 'OK',
-                                    short_msg => "Counter not moved. Have to wait.");
-        $self->{output}->display();
-        $self->{output}->exit();
+    
+    my $multiple = 0;
+    if (scalar(keys %$info_indexes) > 1) {
+        $multiple = 1;
     }
     
-    my @exits;
-    foreach (keys %{$oids}) {
-        next if (!defined($result->{$_}));
-        my $value = (($new_datas->{$oids->{$_}->{counter}} - $old_datas->{$oids->{$_}->{counter}}) * 100) / ($total - $old_total);
-        push @exits, $self->{perfdata}->threshold_check(value => $value, threshold => [ { label => 'critical-' . $oids->{$_}->{counter}, 'exit_litteral' => 'critical' }, { label => 'warning-' . $oids->{$_}->{counter}, 'exit_litteral' => 'warning' }]);
+    if ($multiple == 1) {
+        $self->{output}->output_add(severity => 'OK',
+                                   short_msg => "All CPU usage are ok.");
     }
-
-    my $exit = $self->{output}->get_most_critical(status => [ @exits ]);
-    my $str_output = "CPU Usage: ";
-    my $str_append = '';
-    foreach (keys %{$oids}) {
-        next if (!defined($result->{$_}));
+    
+    # Manage output
+    foreach my $index (keys %$info_indexes) {
+        if ($info_indexes->{$index}->{buffer_creation} == 1) {
+            if ($multiple == 0) {
+                $self->{output}->output_add(severity => 'OK',
+                                            short_msg => "Buffer creation...");
+            } else {
+                $self->{output}->output_add(long_msg => "CPU '$index': Buffer creation...");
+            }
+            next;
+        }
+        if ($info_indexes->{$index}->{total} - $info_indexes->{$index}->{old_total} == 0) {
+            if ($multiple == 0) {
+                $self->{output}->output_add(severity => 'OK',
+                                            short_msg => "Counter not moved. Have to wait.");
+            } else {
+                $self->{output}->output_add(long_msg => "CPU '$index': Counter not moved. Have to wait.");
+            }
+            next;
+        }
         
-        my $value = (($new_datas->{$oids->{$_}->{counter}} - $old_datas->{$oids->{$_}->{counter}}) * 100) / ($total - $old_total);
-        $str_output .= $str_append . sprintf($oids->{$_}->{output}, $value);
-        $str_append = ', ';
-        my $warning = $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $oids->{$_}->{counter});
-        my $critical = $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $oids->{$_}->{counter});
+        my @exits = ();
+        foreach my $oid (keys %{$result}) {
+            $oid =~ /(.*)\.(\d+)$/;
+            my ($oid_base, $index2) = ($1, $2);
+            # If not, we skip oid
+            next if ($index2 != $index || !defined($oids->{$oid_base}));
+            my $value = (($new_datas->{$oids->{$oid_base}->{counter} . '_' . $index} - $old_datas->{$oids->{$oid_base}->{counter} . '_' . $index}) * 100) / ($info_indexes->{$index}->{total} - $info_indexes->{$index}->{old_total});
+            push @exits, $self->{perfdata}->threshold_check(value => $value, threshold => [ { label => 'critical-' . $oids->{$oid_base}->{counter}, 'exit_litteral' => 'critical' }, { label => 'warning-' . $oids->{$oid_base}->{counter}, 'exit_litteral' => 'warning' }]);
+        }
 
-        $self->{output}->perfdata_add(label => $oids->{$_}->{counter}, unit => '%',
-                                      value => sprintf("%.2f", $value),
-                                      warning => $warning,
-                                      critical => $critical,
-                                      min => 0, max => 100);
+        my $exit = $self->{output}->get_most_critical(status => [ @exits ]);
+        my $str_output = "CPU Usage: ";
+        $str_output = "CPU '$index' Usage: " if ($multiple == 1);
+        my $str_append = '';
+        foreach my $oid (keys %{$result}) {
+            $oid =~ /(.*)\.(\d+)$/;
+            my ($oid_base, $index2) = ($1, $2);
+            # If not, we skip oid
+            next if ($index2 != $index || !defined($oids->{$oid_base}));
+            
+            my $value = (($new_datas->{$oids->{$oid_base}->{counter} . '_' . $index} - $old_datas->{$oids->{$oid_base}->{counter} . '_' . $index}) * 100) / ($info_indexes->{$index}->{total} - $info_indexes->{$index}->{old_total});
+            $str_output .= $str_append . sprintf($oids->{$oid_base}->{output}, $value);
+            $str_append = ', ';
+            my $warning = $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $oids->{$oid_base}->{counter});
+            my $critical = $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $oids->{$oid_base}->{counter});
+
+            my $extra_perf_label = '';
+            $extra_perf_label = $index if ($multiple == 1);
+            $self->{output}->perfdata_add(label => $oids->{$oid_base}->{counter} . $extra_perf_label, unit => '%',
+                                          value => sprintf("%.2f", $value),
+                                          warning => $warning,
+                                          critical => $critical,
+                                          min => 0, max => 100);
+        }
+        if ($multiple == 0) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => $str_output);
+        }
+        $self->{output}->output_add(long_msg => $str_output);
     }
-    $self->{output}->output_add(severity => $exit,
-                                short_msg => $str_output);
+    
     $self->{output}->display();
     $self->{output}->exit();
 }
