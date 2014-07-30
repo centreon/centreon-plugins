db4a505d177ddf5b13d3f77acc383c65f8358399
Simon Bomm <sbomm@merethis.com>
2014-07-30 16:32:57 +0200

    Add f5 (BIG-IP) hardware and connections monitoring

---
+++ network/f5/mode/hardware.pm
@@ -0,0 +1,176 @@
+# Copyright 2005-2013 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Simon BOMM <sbomm@merethis.com>
+#
+####################################################################################
+
+package network::f5::mode::hardware;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+use network::f5::mode::components::fan;
+use network::f5::mode::components::psu;
+use network::f5::mode::components::temperature;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "exclude:s"        => { name => 'exclude' },
+                                  "component:s"      => { name => 'component', default => 'all' },
+				  "warning:s"        => { name => 'warning', default => '' },
+                                  "critical:s"       => { name => 'critical', default => '' },
+                                });
+    $self->{components} = {};
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+        $self->{output}->option_exit();
+    }
+
+}
+
+sub global {
+    my ($self, %options) = @_;
+
+    network::f5::mode::components::temperature::check($self);
+    network::f5::mode::components::fan::check($self);
+    network::f5::mode::components::psu::check($self);
+}
+
+sub check_exclude {
+    my ($self, %options) = @_;
+
+    if (defined($options{instance})) {
+        if (defined($self->{option_results}->{exclude}) && $self->{option_results}->{exclude} =~ /(^|\s|,)${options{section}}[^,]*#\Q$options{instance}\E#/) {
+            $self->{components}->{$options{section}}->{skip}++;
+            $self->{output}->output_add(long_msg => sprintf("Skipping $options{section} section $options{instance} instance."));
+            return 1;
+        }
+    } elsif (defined($self->{option_results}->{exclude}) && $self->{option_results}->{exclude} =~ /(^|\s|,)$options{section}(\s|,|$)/) {
+        $self->{output}->output_add(long_msg => sprintf("Skipping $options{section} section."));
+        return 1;
+    }
+    return 0;
+}
+
+
+sub run {
+    my ($self, %options) = @_;
+    # $options{snmp} = snmp object
+    $self->{snmp} = $options{snmp};
+    
+    if ($self->{option_results}->{component} eq 'all') {
+        $self->global();
+    } elsif ($self->{option_results}->{component} eq 'fan') {
+        network::f5::mode::components::fan::check($self);
+    } elsif ($self->{option_results}->{component} eq 'psu') {
+        network::f5::mode::components::psu::check($self);
+    } elsif ($self->{option_results}->{component} eq 'temperature') {
+        network::f5::mode::components::temperature::check($self);
+    } else {
+        $self->{output}->add_option_msg(short_msg => "Wrong option. Cannot find component '" . $self->{option_results}->{component} . "'.");
+        $self->{output}->option_exit();
+    }
+
+    
+    my $total_components = 0;
+    my $display_by_component = '';
+    my $display_by_component_append = '';
+    foreach my $comp (sort(keys %{$self->{components}})) {
+        # Skipping short msg when no components
+        next if ($self->{components}->{$comp}->{total} == 0);
+        $total_components += $self->{components}->{$comp}->{total};
+        $display_by_component .= $display_by_component_append . $self->{components}->{$comp}->{total} . ' ' . $self->{components}->{$comp}->{name};
+        $display_by_component_append = ', ';
+    }
+    
+    $self->{output}->output_add(severity => 'OK',
+                                short_msg => sprintf("All %s components [%s] are ok.", 
+                                                     $total_components,
+                                                     $display_by_component
+                                                    )
+                                );
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check hardware (fans, temperatures, power supplies).
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: 'all').
+Can be: 'fan', 'psu', 'temperature'.
+
+=item B<--exclude>
+
+Exclude some parts (comma seperated list) (Example: --exclude=fans,psus)
+Can also exclude specific instance: --exclude=fans#1#2#,modules#1#,psus
+
+=item B<--warning>
+
+Threshold warning in degree celsius.
+
+=item B<--critical>
+
+Threshold critical in degree celsius.
+
+=back
+
+=cut
+ 
