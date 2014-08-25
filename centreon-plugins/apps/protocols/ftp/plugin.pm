3e316a52533278eff483e88f30ae64634397cf82
Quentin Garnier <qgarnier@merethis.com>
2014-08-25 16:09:21 +0200

    Refs #5388 Add a date mode for files.

--- centreon-plugins/apps/protocols/ftp/plugin.pm
+++ centreon-plugins/apps/protocols/ftp/plugin.pm
@@ -47,8 +47,9 @@ sub new {
 
     $self->{version} = '0.1';
     %{$self->{modes}} = (
-                        'login'            => 'apps::protocols::ftp::mode::login',
-                        'commands'         => 'apps::protocols::ftp::mode::commands',
+                        'login'     => 'apps::protocols::ftp::mode::login',
+                        'commands'  => 'apps::protocols::ftp::mode::commands',
+                        'date'      => 'apps::protocols::ftp::mode::date',
                         );
     return $self;
 }
