e20eefcf0791c7bdd14db8a1e9acd02e24bfe0ca
Quentin Garnier <qgarnier@merethis.com>
2014-10-15 14:57:06 +0200

    Refs #5884 Typo fix

--- apps/selenium/plugin.pm
+++ apps/selenium/plugin.pm
@@ -40,17 +40,17 @@ use warnings;
 use base qw(centreon::plugins::script_simple);
 
 sub new {
-	my ($class, %options) = @_;
-	my $self = $class->SUPER::new(package => __PACKAGE__, %options);
-	bless $self, $class;
-# $options->{options} = options object
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    # $options->{options} = options object
 
-	$self->{version} = '0.1';
-	%{$self->{modes}} = (
-            'scenario'     => 'apps::selenium::mode::scenario',
-			);
+    $self->{version} = '0.1';
+    %{$self->{modes}} = (
+        'scenario'  => 'apps::selenium::mode::scenario',
+    );
 
-	return $self;
+    return $self;
 }
 
 1;
