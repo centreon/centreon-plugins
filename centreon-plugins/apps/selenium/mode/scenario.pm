93fd83bebe4002137926c2213858d300e990fa5f
Simon Bomm <sbomm@merethis.com>
2014-10-20 11:41:30 +0200

    correct { error

--- centreon-plugins/apps/selenium/mode/scenario.pm
+++ centreon-plugins/apps/selenium/mode/scenario.pm
@@ -106,7 +106,7 @@ sub check_options {
         $self->{option_results}->{timeout} > 0) {
         alarm($self->{option_results}->{timeout});
     }
-    if (!defined($self->{option_results}->{scenario})) 
+    if (!defined($self->{option_results}->{scenario})) { 
         $self->{output}->add_option_msg(short_msg => "Please specify a scenario name" . $self->{option_results}->{scenario} . "'.");
         $self->{output}->option_exit();
     }
