81c09a2db62180baf34b24bb57e8c846264c0d49
Kevin Duret <kduret@merethis.com>
2014-11-21 15:05:48 +0100

    fix options and failedjobs refs #5643

--- database/mssql/mode/failedjobs.pm
+++ database/mssql/mode/failedjobs.pm
@@ -39,6 +39,7 @@ use base qw(centreon::plugins::mode);
 
 use strict;
 use warnings;
+use Time::Local;
 
 my %states = (
     0 => 'failed',
@@ -60,6 +61,8 @@ sub new {
                                   "skip"                    => { name => 'skip', },
                                   "warning:s"               => { name => 'warning', },
                                   "critical:s"              => { name => 'critical', },
+                                  "warning-duration:s"      => { name => 'warning_duration', },
+                                  "critical-duration:s"     => { name => 'critical_duration', },
                                   "lookback:s"              => { name => 'lookback', },
                                 });
 
@@ -69,6 +72,23 @@ sub new {
 sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
+
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning_duration})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong warning duration threshold '" . $self->{option_results}->{warning_duration} . "'.");
+       $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical_duration})) == 0) {
+       $self->{output}->add_option_msg(short_msg => "Wrong critical duration threshold '" . $self->{option_results}->{critical_duration} . "'.");
+       $self->{output}->option_exit();
+    }
 }
 
 sub run {
@@ -84,7 +104,7 @@ sub run {
     my $count = 0;
     my $count_failed = 0;
 
-    my $query = "SELECT j.[name] AS [JobName], run_status, h.run_date AS LastRunDate, h.run_time AS LastRunTime,
+    my $query = "SELECT j.[name] AS [JobName], run_status, run_duration, h.run_date AS LastRunDate, h.run_time AS LastRunTime,
                  CASE
                     WHEN h.[run_date] IS NULL OR h.[run_time] IS NULL THEN NULL
                     ELSE datediff(Minute, CAST(
@@ -103,27 +123,45 @@ sub run {
                  FROM msdb.dbo.sysjobhistory h GROUP BY (h.job_id))";
     $self->{sql}->query(query => $query);
     my $result = $self->{sql}->fetchall_arrayref();
+    my @job_failed;
     foreach my $row (@$result) {
         next if (defined($self->{option_results}->{filter}) && $$row[0] !~ /$self->{option_results}->{filter}/);
-        next if (defined($self->{option_results}->{lookback}) && $$row[4] > $self->{option_results}->{lookback});
+        next if (defined($self->{option_results}->{lookback}) && $$row[5] > $self->{option_results}->{lookback});
         $count++;
         my $job_name = $$row[0];
         my $run_status = $$row[1];
-        my $run_date = $$row[2];
-        $run_date =~ s/(\d{4})(\d{2})(\d{2})/$1-$2-$3/;
-        my $run_time = $$row[3];
-        $self->{output}->output_add(long_msg => sprintf("Job '%s' status %s [Date : %s] [Runtime : %ss]", $job_name, $states{$run_status}, $run_date, $run_time));
+        my $run_duration;
+        my $run_date = $$row[3];
+        my ($year,$month,$day) = $run_date =~ /(\d{4})(\d{2})(\d{2})/;
+        my $run_time = $$row[4];
+        my ($hour,$minute,$second) = $run_time =~ /(\d{2})(\d{2})(\d{2})/;
+
+        if (defined($$row[2])) {
+            $run_duration = $$row[2];
+        } else {
+            my $start_time = timelocal($second,$minute,$hour,$day,$month-1,$year);
+            $run_duration = (time() - $start_time) / 60;
+        }
+
         if ($run_status == 0) {
             $count_failed++;
+            push (@job_failed, $job_name);
+        } else {
+            my $exit_code1 = $self->{perfdata}->threshold_check(value => $run_duration, threshold => [ { label => 'critical_duration', 'exit_litteral' => 'critical' }, { label => 'warning_duration', exit_litteral => 'warning' } ]);
+            if (!$self->{output}->is_status(value => $exit_code1, compare => 'ok', litteral => 1)) {
+                $self->{output}->output_add(severity => $exit_code1,
+                                            short_msg => sprintf("Job '%s' duration : %d minutes", $job_name, $run_duration));
+            }
         }
+        $self->{output}->output_add(long_msg => sprintf("Job '%s' status %s [Runtime : %s %s] [Duration : %d minutes]", $job_name, $states{$run_status}, $run_date, $run_time, $run_duration));
     }
 
-    my $exit_code = $self->{perfdata}->threshold_check(value => $count_failed, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+    my $exit_code2 = $self->{perfdata}->threshold_check(value => $count_failed, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
     if(!defined($self->{option_results}->{skip}) && $count == 0) {
         $self->{output}->output_add(severity => 'Unknown',
                                     short_msg => "No job found.");
-    } elsif (!$self->{output}->is_status(value => $exit_code, compare => 'ok', litteral => 1)) {
-        $self->{output}->output_add(severity => $exit_code,
+    } elsif (!$self->{output}->is_status(value => $exit_code2, compare => 'ok', litteral => 1)) {
+        $self->{output}->output_add(severity => $exit_code2,
                                     short_msg => sprintf("%d failed job(s)", $count_failed));
     }
 
@@ -161,6 +199,14 @@ Threshold warning.
 =item B<--critical>
 
 Threshold critical.
+
+=item B<--warning-duration>
+
+Threshold warning for job duration.
+
+=item B<--critical-duration>
+
+Threshold critical for job duration.
 
 =item B<--lookback>
 
