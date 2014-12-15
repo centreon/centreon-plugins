5f8d5a1ed29a41336cf211451b6a1a1837b0b564
Quentin Garnier <qgarnier@merethis.com>
2014-12-15 16:41:28 +0100

    Refs #5627 Improve netapp

--- storage/netapp/mode/globalstatus.pm
+++ storage/netapp/mode/globalstatus.pm
@@ -39,6 +39,39 @@ use base qw(centreon::plugins::mode);
 
 use strict;
 use warnings;
+use centreon::plugins::statefile;
+use centreon::plugins::values;
+
+my $maps_counters = {
+    read   => { class => 'centreon::plugins::values', obj => undef,
+                set => {
+                        key_values => [
+                                        { name => 'read', diff => 1 },
+                                      ],
+                        per_second => 1,
+                        output_template => 'Read I/O : %s %s/s',
+                        output_change_bytes => 1,
+                        perfdatas => [
+                            { value => 'read_per_second', template => '%d',
+                              unit => 'B/s', min => 0 },
+                        ],
+                    }
+               },
+    write   => { class => 'centreon::plugins::values', obj => undef,
+                 set => {
+                        key_values => [
+                                        { name => 'write', diff => 1 },
+                                      ],
+                        per_second => 1,
+                        output_template => 'Write I/O : %s %s/s',
+                        output_change_bytes => 1,
+                        perfdatas => [
+                            { value => 'write_per_second', template => '%d',
+                              unit => 'B/s', min => 0 },
+                        ],
+                    }
+               },
+};
 
 my %states = (
     1 => ['other', 'WARNING'], 
@@ -48,6 +81,15 @@ my %states = (
     5 => ['critical', 'CRITICAL'],
     6 => ['nonRecoverable', 'WARNING'],
 );
+
+my $oid_miscGlobalStatus = '.1.3.6.1.4.1.789.1.2.2.4.0';
+my $oid_miscGlobalStatusMessage = '.1.3.6.1.4.1.789.1.2.2.25.0';
+my $oid_misc64DiskReadBytes = '.1.3.6.1.4.1.789.1.2.2.32.0';
+my $oid_misc64DiskWriteBytes = '.1.3.6.1.4.1.789.1.2.2.33.0';
+my $oid_miscHighDiskReadBytes = '.1.3.6.1.4.1.789.1.2.2.15.0';
+my $oid_miscLowDiskReadBytes = '.1.3.6.1.4.1.789.1.2.2.16.0';
+my $oid_miscHighDiskWriteBytes = '.1.3.6.1.4.1.789.1.2.2.17.0';
+my $oid_miscLowDiskWriteBytes = '.1.3.6.1.4.1.789.1.2.2.18.0';
 
 sub new {
     my ($class, %options) = @_;
@@ -59,29 +101,111 @@ sub new {
                                 {
                                 });
 
+    $self->{statefile_value} = centreon::plugins::statefile->new(%options);  
+    foreach (keys %{$maps_counters}) {
+        $options{options}->add_options(arguments => {
+                                                     'warning-' . $_ . ':s'    => { name => 'warning-' . $_ },
+                                                     'critical-' . $_ . ':s'    => { name => 'critical-' . $_ },
+                                      });
+        my $class = $maps_counters->{$_}->{class};
+        $maps_counters->{$_}->{obj} = $class->new(statefile => $self->{statefile_value},
+                                                  output => $self->{output}, perfdata => $self->{perfdata},
+                                                  label => $_);
+        $maps_counters->{$_}->{obj}->set(%{$maps_counters->{$_}->{set}});
+    }
     return $self;
 }
 
 sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
+    
+    foreach (keys %{$maps_counters}) {
+        $maps_counters->{$_}->{obj}->init(option_results => $self->{option_results});
+    }
+    
+    $self->{statefile_value}->check_options(%options);
 }
 
 sub run {
     my ($self, %options) = @_;
     # $options{snmp} = snmp object
     $self->{snmp} = $options{snmp};
+    $self->{hostname} = $self->{snmp}->get_hostname();
+    $self->{snmp_port} = $self->{snmp}->get_port();
 
-    my $oid_miscGlobalStatus = '.1.3.6.1.4.1.789.1.2.2.4.0';
-    my $oid_miscGlobalStatusMessage = '.1.3.6.1.4.1.789.1.2.2.25.0';
-    my $result = $self->{snmp}->get_leef(oids => [$oid_miscGlobalStatus, $oid_miscGlobalStatusMessage], nothing_quit => 1);
+    $self->manage_selection();
     
-    $self->{output}->output_add(severity =>  ${$states{$result->{$oid_miscGlobalStatus}}}[1],
+    $self->{output}->output_add(severity =>  ${$states{$self->{results}->{$oid_miscGlobalStatus}}}[1],
                                 short_msg => sprintf("Overall global status is '%s' [message: '%s']", 
-                                                ${$states{$result->{$oid_miscGlobalStatus}}}[0], $result->{$oid_miscGlobalStatusMessage}));
+                                                ${$states{$self->{results}->{$oid_miscGlobalStatus}}}[0], $self->{results}->{$oid_miscGlobalStatusMessage}));
+    
+    $self->{new_datas} = {};
+    $self->{statefile_value}->read(statefile => "cache_netapp_" . $self->{hostname}  . '_' . $self->{snmp_port} . '_' . $self->{mode});
+    $self->{new_datas}->{last_timestamp} = time();
+    
+    my ($short_msg, $short_msg_append, $long_msg, $long_msg_append) = ('', '', '', '');
+    my @exits;
+    foreach (sort keys %{$maps_counters}) {
+        $maps_counters->{$_}->{obj}->set(instance => 'global');
+    
+        my ($value_check) = $maps_counters->{$_}->{obj}->execute(values => $self->{global},
+                                                                 new_datas => $self->{new_datas});
 
+        if ($value_check != 0) {
+            $long_msg .= $long_msg_append . $maps_counters->{$_}->{obj}->output_error();
+            $long_msg_append = ', ';
+            next;
+        }
+        my $exit2 = $maps_counters->{$_}->{obj}->threshold_check();
+        push @exits, $exit2;
+
+        my $output = $maps_counters->{$_}->{obj}->output();
+        $long_msg .= $long_msg_append . $output;
+        $long_msg_append = ', ';
+        
+        if (!$self->{output}->is_status(litteral => 1, value => $exit2, compare => 'ok')) {
+            $short_msg .= $short_msg_append . $output;
+            $short_msg_append = ', ';
+        }
+        
+        $maps_counters->{$_}->{obj}->perfdata();
+    }
+
+    my $exit = $self->{output}->get_most_critical(status => [ @exits ]);
+    if (!$self->{output}->is_status(litteral => 1, value => $exit, compare => 'ok')) {
+        $self->{output}->output_add(severity => $exit,
+                                    short_msg => "$short_msg"
+                                    );
+    } else {
+        $self->{output}->output_add(short_msg => "$long_msg");
+    }
+    
+    $self->{statefile_value}->write(data => $self->{new_datas});
     $self->{output}->display();
     $self->{output}->exit();
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+    
+    my $request = [$oid_miscGlobalStatus, $oid_miscGlobalStatusMessage, 
+                   $oid_miscHighDiskReadBytes, $oid_miscLowDiskReadBytes,
+                   $oid_miscHighDiskWriteBytes, $oid_miscLowDiskWriteBytes];
+    if (!$self->{snmp}->is_snmpv1()) {
+        push @{$request}, ($oid_misc64DiskReadBytes, $oid_misc64DiskWriteBytes);
+    }
+    
+    $self->{results} = $self->{snmp}->get_leef(oids => $request, nothing_quit => 1);
+    
+    $self->{global} = {};
+    $self->{global}->{read} = defined($self->{results}->{$oid_misc64DiskReadBytes}) ?
+                                $self->{results}->{$oid_misc64DiskReadBytes} : 
+                                ($self->{results}->{$oid_miscHighDiskReadBytes} << 32) + $self->{results}->{$oid_miscLowDiskReadBytes};
+    $self->{global}->{write} = defined($self->{results}->{$oid_misc64DiskWriteBytes}) ?
+                                $self->{results}->{$oid_misc64DiskWriteBytes} : 
+                                ($self->{results}->{$oid_miscHighDiskWriteBytes} << 32) + $self->{results}->{$oid_miscLowDiskWriteBytes};
+
 }
 
 1;
@@ -90,9 +214,19 @@ __END__
 
 =head1 MODE
 
-Check the overall status of the appliance.
+Check the overall status of the appliance and some metrics (total read bytes per seconds and total write bytes per seconds).
 
 =over 8
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'read', 'write'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'read', 'write'.
 
 =back
 
