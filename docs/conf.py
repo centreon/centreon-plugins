af392c10f1f67edf473012ce1fa4202af0c3c713
rwerquin <rwerquin@merethis.com>
2014-12-19 15:57:43 +0100

    modif conf doc

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'user_guide'
+master_doc = 'index'
 
 # General information about the project.
 project = u'Centreon'
