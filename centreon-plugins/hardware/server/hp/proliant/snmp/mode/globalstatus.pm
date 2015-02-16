6d77b8343f515c75cf399075e767da75ce68da58
garnier-quentin <qgarnier@centreon.com>
2015-02-16 17:42:13 +0100

    + Fix global-status check

--- centreon-plugins/hardware/server/hp/proliant/snmp/mode/globalstatus.pm
+++ centreon-plugins/hardware/server/hp/proliant/snmp/mode/globalstatus.pm
@@ -79,7 +79,7 @@ sub check_options {
             $self->{output}->add_option_msg(short_msg => "Wrong threshold-overload option '" . $val . "'.");
             $self->{output}->option_exit();
         }
-        my ($section, $status, $filter) = ('global', $2, $3);
+        my ($section, $status, $filter) = ('global', $1, $2);
         if ($self->{output}->is_litteral_status(status => $status) == 0) {
             $self->{output}->add_option_msg(short_msg => "Wrong threshold-overload status '" . $val . "'.");
             $self->{output}->option_exit();
