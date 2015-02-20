359abc75a24f6abb9515200b4011549bd7b179e0
Quentin Garnier <qgarnier@merethis.com>
2015-02-20 14:23:44 +0100

    + Add a mode for vmware connector

---
+++ apps/vmware/connector/mode/vmoperationcluster.pm
@@ -0,0 +1,118 @@
+################################################################################
+# Copyright 2005-2014 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package apps::vmware::connector::mode::vmoperationcluster;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                  "cluster:s"               => { name => 'cluster' },
+                                  "filter"                  => { name => 'filter' },
+                                  "warning-svmotion:s"      => { name => 'warning_svmotion' },
+                                  "critical-svmotion:s"     => { name => 'critical_svmotion' },
+                                  "warning-vmotion:s"       => { name => 'warning_vmotion' },
+                                  "critical-vmotion:s"      => { name => 'critical_vmotion' },
+                                  "warning-clone:s"         => { name => 'warning_clone' },
+                                  "critical-clone:s"        => { name => 'critical_clone' },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    foreach my $label (('warning_svmotion', 'critical_svmotion', 'warning_vmotion', 'critical_vmotion', 
+                        'warning_clone', 'critical_clone')) {
+        if (($self->{perfdata}->threshold_validate(label => $label, value => $self->{option_results}->{$label})) == 0) {
+            my ($label_opt) = $label;
+            $label_opt =~ tr/_/-/;
+            $self->{output}->add_option_msg(short_msg => "Wrong " . $label_opt . " threshold '" . $self->{option_results}->{$label} . "'.");
+            $self->{output}->option_exit();
+        }
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+    $self->{connector} = $options{custom};
+
+    $self->{connector}->add_params(params => $self->{option_results},
+                                   command => 'vmoperationcluster');
+    $self->{connector}->run();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check virtual machines operations on cluster(s).
+
+=over 8
+
+=item B<--cluster>
+
+Cluster to check.
+If not set, we check all clusters.
+
+=item B<--filter>
+
+Cluster is a regexp.
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: svmotion, vmotion, clone.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: svmotion, vmotion, clone.
+
+=back
+
+=cut
