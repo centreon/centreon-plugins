a528cc00acc8fe7eecf9568f2f30a6fa087a96cf
Quentin Garnier <qgarnier@merethis.com>
2015-03-13 10:28:49 +1100

    Refs #6201 Indentation please ;)

--- storage/hp/3par/7000/mode/storage.pm
+++ storage/hp/3par/7000/mode/storage.pm
@@ -48,17 +48,17 @@ sub new {
     $self->{version} = '1.0';
     $options{options}->add_options(arguments =>
                                 { 
-								  "hostname:s"              => { name => 'hostname' },
+                                  "hostname:s"              => { name => 'hostname' },
                                   "timeout:s"               => { name => 'timeout', default => 30 },
                                   "sudo"                    => { name => 'sudo' },
                                   "ssh-option:s@"           => { name => 'ssh_option' },
                                   "ssh-path:s"              => { name => 'ssh_path' },
                                   "ssh-command:s"           => { name => 'ssh_command', default => 'ssh' },
-                                  "warning:s"				=> { name => 'warning' },
-								  "critical:s"				=> { name => 'critical' },
-								  "filter-type:s"			=> { name => 'filter_type' },
-								  "disk:s"					=> { name => 'disk' },
-								  "regexp"					=> { name => 'regexp' },
+                                  "warning:s"               => { name => 'warning' },
+                                  "critical:s"              => { name => 'critical' },
+                                  "filter-type:s"           => { name => 'filter_type' },
+                                  "disk:s"                  => { name => 'disk' },
+                                  "regexp"                  => { name => 'regexp' },
                                 });
     $self->{components} = {};
     $self->{no_components} = undef;
@@ -69,17 +69,17 @@ sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
 
-	if (!defined($self->{option_results}->{hostname})) {
-       $self->{output}->add_option_msg(short_msg => "Need to specify a hostname.");
-       $self->{output}->option_exit(); 
+    if (!defined($self->{option_results}->{hostname})) {
+        $self->{output}->add_option_msg(short_msg => "Need to specify a hostname.");
+        $self->{output}->option_exit(); 
     }
 
-	if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
-		$self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
-		$self->{output}->option_exit();
-	}
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+        $self->{output}->option_exit();
+    }
 
-	if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
         $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
         $self->{output}->option_exit();
     }
@@ -88,8 +88,8 @@ sub check_options {
 sub run {
     my ($self, %options) = @_;
 
-	$self->{option_results}->{remote} = 1;
-	my $stdout = centreon::plugins::misc::execute(output => $self->{output},
+    $self->{option_results}->{remote} = 1;
+    my $stdout = centreon::plugins::misc::execute(output => $self->{output},
                                                   options => $self->{option_results},
                                                   sudo => $self->{option_results}->{sudo},
                                                   command => "showpd",
@@ -97,48 +97,48 @@ sub run {
                                                   command_options => $self->{option_results}->{command_options});
 
 
-	my $total_disks = 0;
-	my @disks = split("\n",$stdout);
-	foreach my $disk (@disks) {
-		if ($disk =~ /(\d+)\s+\S+\s+(\S+)\s+\d+\s+\S+\s+(\d+)\s+(\d+)\s+\S+\s+\S+\s+\d+/) {
-			$total_disks++;
-			my $diskId = $1;
-			my $diskType = $2;
-			my $diskSize = $3 * 1024 * 1024;
-			my $diskFree = $4 * 1024 * 1024;
-			my $diskUsed = $diskSize - $diskFree;
-			my $percentUsed = ($diskUsed / $diskSize) * 100;
-			my $percentFree = ($diskFree / $diskSize) * 100;
+    my $total_disks = 0;
+    my @disks = split("\n",$stdout);
+    foreach my $disk (@disks) {
+        if ($disk =~ /(\d+)\s+\S+\s+(\S+)\s+\d+\s+\S+\s+(\d+)\s+(\d+)\s+\S+\s+\S+\s+\d+/) {
+            $total_disks++;
+            my $diskId = $1;
+            my $diskType = $2;
+            my $diskSize = $3 * 1024 * 1024;
+            my $diskFree = $4 * 1024 * 1024;
+            my $diskUsed = $diskSize - $diskFree;
+            my $percentUsed = ($diskUsed / $diskSize) * 100;
+            my $percentFree = ($diskFree / $diskSize) * 100;
 
-			next if (defined($self->{option_results}->{filter_type}) && ($diskType !~ /$self->{option_results}->{filter_type}/i));
-			next if (defined($self->{option_results}->{disk}) && defined($self->{option_results}->{regexp}) && ($diskId !~ /$self->{option_results}->{disk}/i));
-			next if (defined($self->{option_results}->{disk}) && !defined($self->{option_results}->{regexp}) && ($diskId != $self->{option_results}->{disk}));
+            next if (defined($self->{option_results}->{filter_type}) && ($diskType !~ /$self->{option_results}->{filter_type}/i));
+            next if (defined($self->{option_results}->{disk}) && defined($self->{option_results}->{regexp}) && ($diskId !~ /$self->{option_results}->{disk}/i));
+            next if (defined($self->{option_results}->{disk}) && !defined($self->{option_results}->{regexp}) && ($diskId != $self->{option_results}->{disk}));
 
-			my ($diskSizeValue, $diskSizeUnit) = $self->{perfdata}->change_bytes(value => $diskSize);
-			my ($diskUsedValue, $diskUsedUnit) = $self->{perfdata}->change_bytes(value => $diskUsed);
-			my ($diskFreeValue, $diskFreeUnit) = $self->{perfdata}->change_bytes(value => $diskFree);
+            my ($diskSizeValue, $diskSizeUnit) = $self->{perfdata}->change_bytes(value => $diskSize);
+            my ($diskUsedValue, $diskUsedUnit) = $self->{perfdata}->change_bytes(value => $diskUsed);
+            my ($diskFreeValue, $diskFreeUnit) = $self->{perfdata}->change_bytes(value => $diskFree);
 
-			$self->{output}->output_add(long_msg => sprintf("Disk '%d' Total: %.2f%s Used: %.2f%s (%.2f%%) Free: %.2f%s (%.2f%%)",
-										$diskId, $diskSizeValue, $diskSizeUnit, $diskUsedValue, $diskUsedUnit, $percentUsed, $diskFreeValue, $diskFreeUnit, $percentFree));
-			my $exit = $self->{perfdata}->threshold_check(value => $percentUsed, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-			if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
-				$self->{output}->output_add(severity => $exit,
-                                			short_msg => sprintf("Disk '%d' Total: %.2f%s Used: %.2f%s (%.2f%%) Free: %.2f%s (%.2f%%)",
-                                        	$diskId, $diskSizeValue, $diskSizeUnit, $diskUsedValue, $diskUsedUnit, $percentUsed, $diskFreeValue, $diskFreeUnit, $percentFree));
-			}
-			$self->{output}->perfdata_add(label => 'disk_'.$diskId,
-										  unit => 'B',
-										  value => $diskUsed,
-										  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning', total => $diskSize, cast_int => 1),
-										  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical', total => $diskSize, cast_int => 1),
-										  min => 0,
-										  max => $diskSize);
-		}
-	}
+            $self->{output}->output_add(long_msg => sprintf("Disk '%d' Total: %.2f%s Used: %.2f%s (%.2f%%) Free: %.2f%s (%.2f%%)",
+                                        $diskId, $diskSizeValue, $diskSizeUnit, $diskUsedValue, $diskUsedUnit, $percentUsed, $diskFreeValue, $diskFreeUnit, $percentFree));
+            my $exit = $self->{perfdata}->threshold_check(value => $percentUsed, threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+            if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+                $self->{output}->output_add(severity => $exit,
+                                            short_msg => sprintf("Disk '%d' Total: %.2f%s Used: %.2f%s (%.2f%%) Free: %.2f%s (%.2f%%)",
+                                            $diskId, $diskSizeValue, $diskSizeUnit, $diskUsedValue, $diskUsedUnit, $percentUsed, $diskFreeValue, $diskFreeUnit, $percentFree));
+            }
+            $self->{output}->perfdata_add(label => 'disk_'.$diskId,
+                                          unit => 'B',
+                                          value => $diskUsed,
+                                          warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning', total => $diskSize, cast_int => 1),
+                                          critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical', total => $diskSize, cast_int => 1),
+                                          min => 0,
+                                          max => $diskSize);
+        }
+    }
 
-	$self->{output}->output_add(severity => 'OK',
+    $self->{output}->output_add(severity => 'OK',
                                 short_msg => 'All disks are ok.');
-	 
+     
     $self->{output}->display();
     $self->{output}->exit();
 }
@@ -185,5 +185,3 @@ If total (with skipped) is 0. (Default: 'critical' returns).
 =back
 
 =cut
-    
-
