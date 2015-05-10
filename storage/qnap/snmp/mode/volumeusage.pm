e2e9c809c2c54c32dc61dc065a4aa2b8a181abf0
garnier-quentin <qgarnier@centreon.com>
2015-05-10 21:39:46 +0200

    + qnap: add volume-usage mode (Fix #27)

---
+++ storage/qnap/snmp/mode/volumeusage.pm
@@ -0,0 +1,319 @@
+################################################################################
+# Copyright 2005-2014 MERETHIS
+# Centreon is developped by : Julien Mathis and Romain Le Merlus under
+# GPL Licence 2.0.
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation ; either version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, see <http://www.gnu.org/licenses>.
+# 
+# Linking this program statically or dynamically with other modules is making a 
+# combined work based on this program. Thus, the terms and conditions of the GNU 
+# General Public License cover the whole combination.
+# 
+# As a special exception, the copyright holders of this program give MERETHIS 
+# permission to link this program with independent modules to produce an executable, 
+# regardless of the license terms of these independent modules, and to copy and 
+# distribute the resulting executable under terms of MERETHIS choice, provided that 
+# MERETHIS also meet, for each linked independent module, the terms  and conditions 
+# of the license of that module. An independent module is a module which is not 
+# derived from this program. If you modify this program, you may extend this 
+# exception to your version of the program, but you are not obliged to do so. If you
+# do not wish to do so, delete this exception statement from your version.
+# 
+# For more information : contact@centreon.com
+# Authors : Quentin Garnier <qgarnier@merethis.com>
+#
+####################################################################################
+
+package storage::qnap::snmp::mode::volumeusage;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::values;
+
+my $plugin_options;
+
+my $maps_counters = {
+    '000_usage' => { set => {
+                        key_values => [
+                                        { name => 'display' }, { name => 'free' }, { name => 'used' },
+                                      ],
+                        closure_custom_calc => \&custom_usage_calc,
+                        closure_custom_output => \&custom_usage_output,
+                        closure_custom_perfdata => \&custom_usage_perfdata,
+                        closure_custom_threshold_check => \&custom_usage_threshold,
+                    }
+               },
+};
+
+sub custom_usage_perfdata {
+    my ($self, %options) = @_;
+    
+    my $label = 'used';
+    my $value_perf = $self->{result_values}->{used};
+    if (defined($plugin_options->{free})) {
+        $label = 'free';
+        $value_perf = $self->{result_values}->{free};
+    }
+    my $extra_label = '';
+    $extra_label = '_' . $self->{result_values}->{display} if (!defined($options{extra_instance}) || $options{extra_instance} != 0);
+    my %total_options = ();
+    if ($plugin_options->{units} eq '%') {
+        $total_options{total} = $self->{result_values}->{total};
+        $total_options{cast_int} = 1; 
+    }
+    
+    $self->{output}->perfdata_add(label => $label . $extra_label, unit => 'B',
+                                  value => $value_perf,
+                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{label}, %total_options),
+                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{label}, %total_options),
+                                  min => 0, max => $self->{result_values}->{total});
+}
+
+sub custom_usage_threshold {
+    my ($self, %options) = @_;
+    
+    my ($exit, $threshold_value);
+    $threshold_value = $self->{result_values}->{used};
+    $threshold_value = $self->{result_values}->{free} if (defined($plugin_options->{free}));
+    if ($plugin_options->{units} eq '%') {
+        $threshold_value = $self->{result_values}->{prct_used};
+        $threshold_value = $self->{result_values}->{prct_free} if (defined($plugin_options->{free}));
+    }
+    $exit = $self->{perfdata}->threshold_check(value => $threshold_value, threshold => [ { label => 'critical-' . $self->{label}, exit_litteral => 'critical' }, { label => 'warning-'. $self->{label}, exit_litteral => 'warning' } ]);
+    return $exit;
+}
+
+sub custom_usage_output {
+    my ($self, %options) = @_;
+    
+    my ($total_size_value, $total_size_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{total});
+    my ($total_used_value, $total_used_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{used});
+    my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{free});
+    
+    my $msg = sprintf("Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
+                      $total_size_value . " " . $total_size_unit,
+                      $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used},
+                      $total_free_value . " " . $total_free_unit, $self->{result_values}->{prct_free});
+    return $msg;
+}
+
+sub custom_usage_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{display} = $options{new_datas}->{$self->{instance} . '_display'};
+    $self->{result_values}->{total} = $options{new_datas}->{$self->{instance} . '_used'} + $options{new_datas}->{$self->{instance} . '_free'};
+    $self->{result_values}->{used} = $options{new_datas}->{$self->{instance} . '_used'};
+    $self->{result_values}->{free} = $options{new_datas}->{$self->{instance} . '_free'};    
+    $self->{result_values}->{prct_free} = $self->{result_values}->{free} * 100 / $self->{result_values}->{total};
+    $self->{result_values}->{prct_used} = $self->{result_values}->{used} * 100 / $self->{result_values}->{total};
+    return 0;
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "filter-name:s"     => { name => 'filter_name' },
+                                  "units:s"           => { name => 'units', default => '%' },
+                                  "free"              => { name => 'free' },
+                                });                         
+     
+    foreach (sort keys %{$maps_counters}) {
+        my ($id, $name) = split /_/;
+        if (!defined($maps_counters->{$_}->{threshold}) || $maps_counters->{$_}->{threshold} != 0) {
+            $options{options}->add_options(arguments => {
+                                                        'warning-' . $name . ':s'    => { name => 'warning-' . $name },
+                                                        'critical-' . $name . ':s'    => { name => 'critical-' . $name },
+                                           });
+        }
+        $maps_counters->{$_}->{obj} = centreon::plugins::values->new(output => $self->{output}, perfdata => $self->{perfdata},
+                                                  label => $name);
+        $maps_counters->{$_}->{obj}->set(%{$maps_counters->{$_}->{set}});
+    }
+    
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    foreach (keys %{$maps_counters}) {
+        $maps_counters->{$_}->{obj}->init(option_results => $self->{option_results});
+    }
+    $plugin_options = $self->{option_results};
+}
+
+sub run {
+    my ($self, %options) = @_;
+    # $options{snmp} = snmp object
+    $self->{snmp} = $options{snmp};
+
+    $self->manage_selection();
+    
+    my $multiple = 1;
+    if (scalar(keys %{$self->{volumes_selected}}) == 1) {
+        $multiple = 0;
+    }
+    
+    if ($multiple == 1) {
+        $self->{output}->output_add(severity => 'OK',
+                                    short_msg => 'All volume usages are ok');
+    }
+    
+    foreach my $id (sort keys %{$self->{volumes_selected}}) {     
+        my ($short_msg, $short_msg_append, $long_msg, $long_msg_append) = ('', '', '', '');
+        my @exits;
+        foreach (sort keys %{$maps_counters}) {
+            $maps_counters->{$_}->{obj}->set(instance => $id);
+        
+            my ($value_check) = $maps_counters->{$_}->{obj}->execute(values => $self->{volumes_selected}->{$id});
+
+            if ($value_check != 0) {
+                $long_msg .= $long_msg_append . $maps_counters->{$_}->{obj}->output_error();
+                $long_msg_append = ', ';
+                next;
+            }
+            my $exit2 = $maps_counters->{$_}->{obj}->threshold_check();
+            push @exits, $exit2;
+
+            my $output = $maps_counters->{$_}->{obj}->output();
+            $long_msg .= $long_msg_append . $output;
+            $long_msg_append = ', ';
+            
+            if (!$self->{output}->is_status(litteral => 1, value => $exit2, compare => 'ok')) {
+                $short_msg .= $short_msg_append . $output;
+                $short_msg_append = ', ';
+            }
+            
+            $maps_counters->{$_}->{obj}->perfdata(extra_instance => $multiple);
+        }
+
+        $self->{output}->output_add(long_msg => "Volume '" . $self->{volumes_selected}->{$id}->{display} . "' $long_msg");
+        my $exit = $self->{output}->get_most_critical(status => [ @exits ]);
+        if (!$self->{output}->is_status(litteral => 1, value => $exit, compare => 'ok')) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => "Volume '" . $self->{volumes_selected}->{$id}->{display} . "' $short_msg"
+                                        );
+        }
+        
+        if ($multiple == 0) {
+            $self->{output}->output_add(short_msg => "Volume '" . $self->{volumes_selected}->{$id}->{display} . "' $long_msg");
+        }
+    }
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+my $mapping = {
+    SysVolumeDescr      => { oid => '.1.3.6.1.4.1.24681.1.2.17.1.2' },
+    SysVolumeFS         => { oid => '.1.3.6.1.4.1.24681.1.2.17.1.3' },
+    SysVolumeTotalSize  => { oid => '.1.3.6.1.4.1.24681.1.2.17.1.4' },
+    SysVolumeFreeSize   => { oid => '.1.3.6.1.4.1.24681.1.2.17.1.5' },
+};
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{volumes_selected} = {};
+    my $oid_SysVolumeEntry = '.1.3.6.1.4.1.24681.1.2.17.1';
+    $self->{results} = $self->{snmp}->get_table(oid => $oid_SysVolumeEntry, 
+                                                start => $mapping->{SysVolumeDescr}->{oid}, 
+                                                end => $mapping->{SysVolumeFreeSize}->{oid}, 
+                                                nothing_quit => 1);
+    foreach my $oid (keys %{$self->{results}}) {
+        next if ($oid !~ /^$mapping->{SysVolumeDescr}->{oid}\.(\d+)/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}, instance => $instance);
+        if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' &&
+            $result->{SysVolumeDescr} !~ /$self->{option_results}->{filter_name}/) {
+            $self->{output}->output_add(long_msg => "Skipping '" . $result->{SysVolumeDescr} . "': no matching filter.");
+            next;
+        }
+        
+        my $free = $self->convert_bytes(value => $result->{SysVolumeFreeSize});
+        my $total = $self->convert_bytes(value => $result->{SysVolumeTotalSize});
+        if ($total == 0) {
+            $self->{output}->output_add(long_msg => "Skipping '" . $result->{SysVolumeDescr} . "': total size is 0.");
+            next;
+        }
+        
+        $self->{volumes_selected}->{$instance} = { display => $result->{SysVolumeDescr}, 
+                                                   free => $free, used => $total - $free };
+    }
+    
+    if (scalar(keys %{$self->{volumes_selected}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "No entry found.");
+        $self->{output}->option_exit();
+    }
+}
+
+sub convert_bytes {
+    my ($self, %options) = @_;
+    my $multiple = defined($options{network}) ? 1000 : 1024;
+    my %units = (K => 1, M => 2, G => 3, T => 4);
+    
+    if ($options{value} !~ /^\s*([0-9\.\,]+)\s*(.)/) {
+        $self->{output}->output_add(severity => 'UNKNOWN',
+                                    output => "Cannot convert value '" . $options{value} . "'");
+        $self->{output}->display();
+        $self->{output}->exit();
+    }
+    my ($bytes, $unit) = ($1, uc($2));
+    
+    for (my $i = 0; $i < $units{$unit}; $i++) {
+        $bytes *= $multiple;
+    }
+
+    return $bytes;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check volume usages.
+
+=over 8
+
+=item B<--warning-usage>
+
+Threshold warning.
+
+=item B<--critical-usage>
+
+Threshold critical.
+
+=item B<--units>
+
+Units of thresholds (Default: '%') ('%', 'B').
+
+=item B<--free>
+
+Thresholds are on free space left.
+
+=item B<--filter-name>
+
+Filter volume name (can be a regexp).
+
+=back
+
+=cut
