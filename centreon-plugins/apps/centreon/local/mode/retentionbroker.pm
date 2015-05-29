d03eb79f2d65e7d202de0a70ea1fc5fe4df908ac
garnier-quentin <qgarnier@centreon.com>
2015-05-29 16:27:24 +0200

    + Refactored centreon modes

--- centreon-plugins/apps/centreon/local/mode/retentionbroker.pm
+++ centreon-plugins/apps/centreon/local/mode/retentionbroker.pm
@@ -40,6 +40,7 @@ use base qw(centreon::plugins::mode);
 use strict;
 use warnings;
 use XML::LibXML;
+use File::Basename;
 
 sub new {
     my ($class, %options) = @_;
@@ -49,10 +50,7 @@ sub new {
     $self->{version} = '1.0';
     $options{options}->add_options(arguments =>
                                 {
-                                 "rrd-config-file:s"    => { name => 'rrd_config_file', default => 'central-rrd.xml' },
-                                 "sql-config-file:s"    => { name => 'sql_config_file', default => 'central-broker.xml' },
-                                 "config-path:s"          => { name => 'config_path', default => '/etc/centreon-broker/' },
-                                 "broker-temporary-dir:s" => { name => 'broker_temporary_dir', default => '/var/lib/centreon-broker/' },
+                                 "broker-config:s@"     => { name => 'broker_config' },
                                 });
     return $self;
 }
@@ -60,63 +58,120 @@ sub new {
 sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
-    if ($self->{option_results}->{config_path} !~ /\/$/) {
-        $self->{output}->add_option_msg(short_msg => "Please set the last / the path to your config-path option");
+ 
+    if (!defined($self->{option_results}->{broker_config}) || scalar(@{$self->{option_results}->{broker_config}}) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Please set broker-config option.");
         $self->{output}->option_exit();
     }
-    if (! -e $self->{option_results}->{broker_temporary_dir}) {
-        $self->{output}->add_option_msg(short_msg => "Directory specified with --broker-temporary-dir does not exist !");
-        $self->{output}->option_exit();
+}
+
+sub check_directory {
+    my ($self, %options) = @_;
+    
+    my ($current_total, $current_size) = (0, 0);
+    my $dirname = dirname($options{path});
+    my $basename = basename($options{path});
+    my $dh;
+    if (!opendir($dh, $dirname)) {
+        $self->{output}->output_add(severity => 'UNKNOWN',
+                                    short_msg => "'$options{config}': cannot open directory '$dirname'");
+        return 0;
     }
+    while (my $file = readdir($dh)) {
+        if ($file =~ /^$basename\d*$/) {
+            $current_total++;
+            $current_size += -s $dirname . '/' . $file;
+        }
+    }
+    closedir $dh;
+    return (1, $current_total, $current_size);
 }
 
 sub run {
     my ($self, %options) = @_;
     
-    my $parser = XML::LibXML->new();
-    my $count = 0;
-    my $filepath;
-    my @fileList;
-    my @configFiles;
-    my @retentionFiles;
-
-    push @configFiles, $self->{option_results}->{config_path}.$self->{option_results}->{rrd_config_file}, $self->{option_results}->{config_path}.$self->{option_results}->{sql_config_file};
-    
     $self->{output}->output_add(severity => 'OK',
-                                short_msg => 'No retention files, centreon-broker outputs are OK');
-   
-    foreach my $file (@configFiles) {
-        my $config = $parser->parse_file($file);
+                                short_msg => 'centreon-broker failover files and tempory are ok');
+    
+    my $total_size = 0;
+    foreach my $config (@{$self->{option_results}->{broker_config}}) {
+        $self->{output}->output_add(long_msg => "Checking config '$config'");
         
-        foreach my $data ($config->findnodes('/centreonBroker/output/path')) {    
+        if (! -f $config or ! -r $config) {
+            $self->{output}->output_add(severity => 'UNKNOWN',
+                                        short_msg => "'$config': not a file or cannot be read");
+            next;
+        }
         
-            $filepath = $data->to_literal;
-            $filepath =~ s/^.*\/(.*)$/$1/;
+        my $parser = XML::LibXML->new();
+        my $xml;
+        eval {
+            $xml = $parser->parse_file($config);
+        };
+        if ($@) {
+            $self->{output}->output_add(severity => 'UNKNOWN',
+                                        short_msg => "'$config': cannot parse xml");
+            next;
+        }
+        my %failover_finded = ();
+        my %file_finded = ();
+        my $temporary;
+        foreach my $node ($xml->findnodes('/centreonBroker/output')) {
+            my %load = ();
+            foreach my $element ($node->getChildrenByTagName('*')) {
+                if ($element->nodeName eq 'failover') {
+                    $failover_finded{$element->textContent} = 1;
+                } elsif ($element->nodeName =~ /^(name|type|path)$/) {
+                    $load{$element->nodeName} = $element->textContent;
+                }
+            }
+            if (defined($load{type}) && $load{type} eq 'file') {
+                $file_finded{$load{name}} = {%load};
+            }
+        }
+        
+        foreach my $node ($xml->findnodes('/centreonBroker/temporary')) {
+            foreach my $element ($node->getChildrenByTagName('path')) {
+                $temporary = $element->textContent;
+            }
+        }
+        
+        # Check failovers
+        my $current_total = 0;
+        foreach my $failover (sort keys %failover_finded) {
+            next if (!defined($file_finded{$failover}));
             
-            next if $filepath =~ m/(sock$|^ )/;
+            my ($status, $total, $size) = $self->check_directory(config => $config, path => $file_finded{$failover}->{path});
+            next if (!$status);
             
-            push @retentionFiles, $filepath;
+            $current_total += $total;
+            $total_size += $size;            
+            my ($size_value, $size_unit) = $self->{perfdata}->change_bytes(value => $size);
+            $self->{output}->output_add(long_msg => sprintf("failover '%s': %d file(s) finded (%s)", 
+                                                            $failover, $total, $size_value . ' ' . $size_unit));
+        }
         
-        }    
-   } 
-   
-   opendir(my $dh, $self->{option_results}->{broker_temporary_dir}) or die "Petit chaton mort\n";
-   
-   while (my $file = readdir($dh)) {
-       push @fileList, $file;
-   }
-   
-   foreach my $file (@retentionFiles) {
-       my $count = grep /$file/,@fileList;
-       if ($count > 0) {
-           my $size = -s $self->{option_results}->{broker_temporary_dir}.$file;
-           $size = $size * $count;
-           my ($size_value, $size_unit) = $self->{perfdata}->change_bytes(value => $size);
-           $self->{output}->output_add(long_msg => sprintf("%s (%d files for an average size of %2f %s)", $file, $count, $size_value, $size_unit));
-           $self->{output}->output_add(severity => 'CRITICAL',
-                                                    short_msg => sprintf("[%s (count: %d) (size:%2f %s)] ", $file, $count, $size_value, $size_unit));
-       }
-   }
+        if ($current_total > 0) {
+            $self->{output}->output_add(severity => 'CRITICAL',
+                                        short_msg => sprintf("Some failover(s) are active"));
+        }
+        
+        # Check temporary
+        if (!defined($temporary)) {
+            $self->{output}->output_add(long_msg => "skipping temporary: no configuration set");
+            next;
+        }
+        my ($status, $total, $size) = $self->check_directory(config => $config, path => $temporary);
+        if ($status) {
+            my ($size_value, $size_unit) = $self->{perfdata}->change_bytes(value => $size);
+            $self->{output}->output_add(long_msg => sprintf("temporary: %d file(s) finded (%s)", 
+                                                            $total, $size_value . ' ' . $size_unit));
+            if ($total > 0) {
+                $self->{output}->output_add(severity => 'CRITICAL',
+                                            short_msg => sprintf("Temporary is active"));
+            }
+        }
+    }
 
     $self->{output}->display();
     $self->{output}->exit();
@@ -129,25 +184,13 @@ __END__
 
 =head1 MODE
 
-Check if one of centreon-broker output is slow and temporary files are presents. CRITICAL STATE ONLY
+Check failover file retention or temporary is active.
 
 =over 8
 
-=item B<--rrd-config-file>
+=item B<--broker-config>
 
-Specify the name of your master rrd config-file (default: central-rrd.xml)
-
-=item B<--sql-config-file>
-
-Specify the name of your master sql config file (default: central-broker.xml)
-
-=item B<--config-path>
-
-Specify the path to your broker config files (defaut: /etc/centreon-broker/)
-
-=item B<--broker-temporary-dir>
-
-Specify the path to your broker retention directory (defaut: /var/lib/centreon-broker/)
+Specify the centreon-broker config (Required). Can be multiple.
 
 =back
 
