32b413b6478687b96fa86a620581667a23afe1e9
garnier-quentin <qgarnier@centreon.com>
2015-07-01 13:09:43 +0200

    + Add hsrp mode

--- centreon-plugins/apps/java/peoplesoft/jmx/mode/queuelength.pm
+++ centreon-plugins/apps/java/peoplesoft/jmx/mode/queuelength.pm
@@ -70,7 +70,6 @@ sub check_options {
 
 sub run {
     my ($self, %options) = @_;
-    # $options{snmp} = snmp object
     $self->{connector} = $options{custom};
 
     my $mbean = "com.bea:Name=weblogic.kernel.Default,Server=PIA,Type=ExecuteQueue";
