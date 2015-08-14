23cd2ef4a6187d4e264cd40f6414b4b2931ba909
Stéphane Duret <sduret@merethis.com>
2015-08-14 16:10:49 +0200

    Add Database Option

--- apps/kayako/sql/mode/liststatus.pm
+++ apps/kayako/sql/mode/liststatus.pm
@@ -30,6 +30,10 @@ sub new {
     my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
     $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+         {
+           "database:s"             => { name => 'db_name' },
+         });
     return $self;
 }
 
@@ -45,7 +49,7 @@ sub run {
 
     $self->{sql}->connect();
 
-    $self->{sql}->query(query => "SELECT ticketstatusid, title FROM swticketstatus");
+    $self->{sql}->query(query => "SELECT ticketstatusid, title FROM " . $self->{option_results}->{'db_name'} . ".swticketstatus");
     while ((my $row = $self->{sql}->fetchrow_hashref())) {
         $self->{output}->output_add(long_msg => "'" . $row->{title} . "' [id = " . $row->{ticketstatusid} . "]");
     }
