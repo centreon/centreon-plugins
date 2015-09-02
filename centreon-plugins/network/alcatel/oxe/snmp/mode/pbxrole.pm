75de660e738ce8c9ce6e8bc330e418fd2ec85ebc
garnier-quentin <qgarnier@centreon.com>
2015-09-02 17:35:32 +0200

    + fix alcatel oxe pbx-role typo

--- centreon-plugins/network/alcatel/oxe/snmp/mode/pbxrole.pm
+++ centreon-plugins/network/alcatel/oxe/snmp/mode/pbxrole.pm
@@ -129,7 +129,7 @@ Check PBX Role.
 
 Set to overload default threshold values (syntax: section,status,regexp)
 It used before default thresholds (order stays).
-Example: --threshold-overload='state,CRITICAL,^(?!(normal)$)'
+Example: --threshold-overload='state,CRITICAL,^(?!(main)$)'
 
 =back
 
