a60bc739af5ad7ebd1505786d6744277dc684fde
Sims24 <Sims24@users.noreply.github.com>
2015-09-30 17:28:24 +0200

    Update globalstatus.pm
    fix copy/paste dumb things

--- storage/ibm/TS3200/mode/globalstatus.pm
+++ storage/ibm/TS3200/mode/globalstatus.pm
@@ -88,15 +88,15 @@ sub run {
     # $options{snmp} = snmp object
     $self->{snmp} = $options{snmp};
     
-    my $oid_ibm3100FaultError = '.1.3.6.1.4.1.2.6.211.3.1.1.22.1';
-    my $oid_ibm3100FaultDesc = '.1.3.6.1.4.1.2.6.211.3.1.1.24.1';
+    my $oid_ibm3200FaultError = '.1.3.6.1.4.1.2.6.211.3.1.1.22.1';
+    my $oid_ibm3200FaultDesc = '.1.3.6.1.4.1.2.6.211.3.1.1.24.1';
     my $oid_ibm3200StatusGlobalStatus = '.1.3.6.1.4.1.2.6.211.2.1.0';
-    my $result = $self->{snmp}->get_leef(oids => [$oid_ibm3200StatusGlobalStatus, $oid_ibm3100FaultError, $oid_ibm3100FaultDesc], nothing_quit => 1);
+    my $result = $self->{snmp}->get_leef(oids => [$oid_ibm3200StatusGlobalStatus, $oid_ibm3200FaultError, $oid_ibm3200FaultDesc], nothing_quit => 1);
     
     $self->{output}->output_add(severity => $self->get_severity(value => $result->{$oid_ibm3200StatusGlobalStatus}),
                                 short_msg => sprintf("Overall global status is '%s'.", 
                                                 ${$states{$result->{$oid_ibm3200StatusGlobalStatus}}}[0]));
-    $self->{output}->output_add(long_msg => sprintf("Error state: %s Error code: %i", $result->{$oid_ibm3100FaultDesc}, $result->{$oid_ibm3100FaultError}));
+    $self->{output}->output_add(long_msg => sprintf("Error state: '%s' Error code: '%i'", $result->{$oid_ibm3200FaultDesc}, $result->{$oid_ibm3200FaultError}));
     $self->{output}->display();
     $self->{output}->exit();
 }
