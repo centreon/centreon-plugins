6af37d506e9e39bd8873dc1e19ad2a614b095dc3
garnier-quentin <qgarnier@centreon.com>
2015-11-10 10:44:19 +0100

    + Set sanity mode by default

--- centreon-plugins/centreon/plugins/script_custom.pm
+++ centreon-plugins/centreon/plugins/script_custom.pm
@@ -42,7 +42,7 @@ sub new {
                                                 'custommode:s'    => { name => 'custommode_name' },
                                                 'list-custommode' => { name => 'list_custommode' },
                                                 'multiple'        => { name => 'multiple' },
-                                                'sanity-options'  => { name => 'sanity_options' },
+                                                'sanity-options'  => { name => 'sanity_options' }, # keep it for 6 month before remove it
                                                 }
                                   );
     $self->{version} = '1.0';
@@ -93,9 +93,7 @@ sub init {
     if (defined($self->{list_custommode})) {
         $self->list_custommode();
     }
-    if (defined($self->{sanity_options})) {
-        $self->{options}->set_sanity();
-    }
+    $self->{options}->set_sanity();
 
     # Output HELP
     $self->{options}->add_help(package => 'centreon::plugins::output', sections => 'OUTPUT OPTIONS');
