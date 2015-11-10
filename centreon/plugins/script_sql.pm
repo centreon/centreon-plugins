fca3e08804a6c441d58a688ab7002113e288a7c1
garnier-quentin <qgarnier@centreon.com>
2015-11-10 10:44:19 +0100

    + Set sanity mode by default

--- centreon/plugins/script_sql.pm
+++ centreon/plugins/script_sql.pm
@@ -43,7 +43,7 @@ sub new {
                                                 'sqlmode:s'      => { name => 'sqlmode_name', default => 'dbi' },
                                                 'list-sqlmode'   => { name => 'list_sqlmode' },
                                                 'multiple'       => { name => 'multiple' },
-                                                'sanity-options' => { name => 'sanity_options' },
+                                                'sanity-options' => { name => 'sanity_options' }, # keep it for 6 month before remove it
                                                 }
                                   );
     $self->{version} = '1.0';
@@ -85,9 +85,7 @@ sub init {
     if (defined($self->{list_sqlmode})) {
         $self->list_sqlmode();
     }
-    if (defined($self->{sanity_options})) {
-        $self->{options}->set_sanity();
-    }
+    $self->{options}->set_sanity();
 
     # Output HELP
     $self->{options}->add_help(package => 'centreon::plugins::output', sections => 'OUTPUT OPTIONS');
