69c1818370cf21ef20a4023cb535f689b44a63ed
garnier-quentin <qgarnier@centreon.com>
2015-12-18 10:15:39 +0100

    + enhance f5 bigip plugin mode hardware

--- network/f5/bigip/mode/components/psu.pm
+++ network/f5/bigip/mode/components/psu.pm
@@ -29,33 +29,42 @@ my %map_status = (
     2 => 'notPresent',
 );
 
+my $mapping = {
+    sysChassisPowerSupplyStatus => { oid => '.1.3.6.1.4.1.3375.2.1.3.2.2.2.1.2', map => \%map_status },
+};
+
+sub load {
+    my (%options) = @_;
+    
+    push @{$options{request}}, { oid => $mapping->{sysChassisPowerSupplyStatus}->{oid} };
+}
+
 sub check {
     my ($self) = @_;
 
-    $self->{components}->{psu} = {name => 'psus', total => 0, skip => 0};
     $self->{output}->output_add(long_msg => "Checking power supplies");
+    $self->{components}->{psu} = {name => 'psus', total => 0, skip => 0};
     return if ($self->check_exclude(section => 'psu'));
-    
-    my $oid_sysChassisPowerSupplyEntry = '.1.3.6.1.4.1.3375.2.1.3.2.2.2.1';
-    my $oid_sysChassisPowerSupplyStatus = '.1.3.6.1.4.1.3375.2.1.3.2.2.2.1.2';
-    
-    my $result = $self->{snmp}->get_table(oid => $oid_sysChassisPowerSupplyEntry);
-    return if (scalar(keys %$result) <= 0);
 
-    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$result)) {
-        next if ($key !~ /^$oid_sysChassisPowerSupplyStatus\.(\d+)$/);
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$mapping->{sysChassisPowerSupplyStatus}->{oid}}})) {
+        $oid =~ /^$mapping->{sysChassisPowerSupplyStatus}->{oid}\.(.*)$/;
         my $instance = $1;
-        next if ($self->check_exclude(section => 'psu', instance => $instance));
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$mapping->{sysChassisPowerSupplyStatus}->{oid}}, instance => $instance);
     
-        my $status = $result->{$oid_sysChassisPowerSupplyStatus . '.' . $instance};
-     
+        next if ($result->{sysChassisPowerSupplyStatus} =~ /notPresent/i && 
+                 $self->absent_problem(section => 'psu', instance => $instance));
+        next if ($self->check_exclude(section => 'psu', instance => $instance));
+        
         $self->{components}->{psu}->{total}++;
-        $self->{output}->output_add(long_msg => sprintf("Power Supply '%s' status is %s.", 
-                                                        $instance, $map_status{$status}));
-        if ($status < 1) {
-            $self->{output}->output_add(severity =>  'CRITICAL',
-                                        short_msg => sprintf("Power Supply '%s' status is %s", 
-                                                             $instance, $map_status{$status}));
+        
+        $self->{output}->output_add(long_msg => sprintf("power supply '%s' status is '%s' [instance: %s].", 
+                                    $instance, $result->{sysChassisPowerSupplyStatus}, $instance
+                                    ));
+        my $exit = $self->get_severity(section => 'psu', value => $result->{sysChassisPowerSupplyStatus});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Power supply '%s' status is '%s'", 
+                                            $instance, $result->{sysChassisPowerSupplyStatus}));
         }
     }
 }
