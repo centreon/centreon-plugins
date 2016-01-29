b68f4b0d00f44d6c2feacbfd7f0b753801020abe
garnier-quentin <qgarnier@centreon.com>
2016-01-29 15:16:26 +0100

    + Fix indent

--- centreon/plugins/statefile.pm
+++ centreon/plugins/statefile.pm
@@ -82,12 +82,12 @@ sub check_options {
 }
 
 sub error {
-     my ($self) = shift;
+    my ($self) = shift;
      
-     if (@_) {
+    if (@_) {
         $self->{error} = $_[0];
-     }
-     return $self->{error};
+    }
+    return $self->{error};
 }
 
 sub read {
