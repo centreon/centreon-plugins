f8d4309cfe30da16557647611c329d7ed213ef9c
Sims24 <Sims24@users.noreply.github.com>
2016-05-23 14:24:26 +0200

    + fix typo threshold

--- centreon/common/emc/navisphere/mode/cache.pm
+++ centreon/common/emc/navisphere/mode/cache.pm
@@ -81,13 +81,13 @@ sub check_options {
     $self->{overload_th} = {};
     foreach my $val (@{$self->{option_results}->{threshold_overload}}) {
         if ($val !~ /(.*?):(.*?)=(.*)/) {
-            $self->{output}->add_option_msg(short_msg => "Wrong treshold-overload option '" . $val . "'.");
+            $self->{output}->add_option_msg(short_msg => "Wrong threshold-overload option '" . $val . "'.");
             $self->{output}->option_exit();
         }
 
         my ($label, $filter, $threshold) = ($1, $2, $3);
         if ($self->{output}->is_litteral_status(status => $threshold) == 0) {
-            $self->{output}->add_option_msg(short_msg => "Wrong treshold-overload status '" . $val . "'.");
+            $self->{output}->add_option_msg(short_msg => "Wrong threshold-overload status '" . $val . "'.");
             $self->{output}->option_exit();
         }
         $self->{overload_th}->{$label} = {} if (!defined($self->{overload_th}->{$label}));
