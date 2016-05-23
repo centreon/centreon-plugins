50032627744be4ddaef27fce78062ba8296d66f3
Sims24 <Sims24@users.noreply.github.com>
2016-05-23 14:44:49 +0200

    + fix typo threshold (help)

--- network/hirschmann/standard/snmp/mode/hardware.pm
+++ network/hirschmann/standard/snmp/mode/hardware.pm
@@ -114,12 +114,12 @@ Example: --threshold-overload='psu,CRITICAL,^(?!(ok)$)'
 
 =item B<--warning>
 
-Set warning threshold for temperatures (syntax: type,regexp,treshold)
+Set warning threshold for temperatures (syntax: type,regexp,threshold)
 Example: --warning='temperature,.*,30'
 
 =item B<--critical>
 
-Set critical threshold for temperatures (syntax: type,regexp,treshold)
+Set critical threshold for temperatures (syntax: type,regexp,threshold)
 Example: --critical='temperature,.*,40'
 
 =back
