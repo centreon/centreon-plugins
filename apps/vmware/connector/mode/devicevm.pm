2fd41a70eb61147dce66f7ee99ceb724a1ea5920
garnier-quentin <qgarnier@centreon.com>
2016-08-04 11:33:08 +0200

    + add device-vm mode for vmware connector

---
+++ apps/vmware/connector/mode/devicevm.pm
@@ -0,0 +1,149 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::vmware::connector::mode::devicevm;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                  "vm-hostname:s"           => { name => 'vm_hostname' },
+                                  "filter"                  => { name => 'filter' },
+                                  "scope-datacenter:s"      => { name => 'scope_datacenter' },
+                                  "scope-cluster:s"         => { name => 'scope_cluster' },
+                                  "scope-host:s"            => { name => 'scope_host' },
+                                  "filter-description:s"    => { name => 'filter_description' },
+                                  "disconnect-status:s"     => { name => 'disconnect_status', default => 'unknown' },
+                                  "nopoweredon-status:s"    => { name => 'nopoweredon_status', default => 'unknown' },
+                                  "display-description"     => { name => 'display_description' },
+                                  "warning:s"               => { name => 'warning' },
+                                  "critical:s"              => { name => 'critical' },
+                                  "device:s"                => { name => 'device' },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+    
+    foreach my $label (('warning', 'critical')) {
+        if (($self->{perfdata}->threshold_validate(label => $label, value => $self->{option_results}->{$label})) == 0) {
+            my ($label_opt) = $label;
+            $label_opt =~ tr/_/-/;
+            $self->{output}->add_option_msg(short_msg => "Wrong " . $label_opt . " threshold '" . $self->{option_results}->{$label} . "'.");
+            $self->{output}->option_exit();
+        }
+    }
+
+    if (!defined($self->{option_results}->{device}) || $self->{option_results}->{device} eq '') {
+        $self->{output}->add_option_msg(short_msg => "Please set device option.");
+        $self->{output}->option_exit();
+    }
+    if ($self->{output}->is_litteral_status(status => $self->{option_results}->{disconnect_status}) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong disconnect-status option '" . $self->{option_results}->{disconnect_status} . "'.");
+        $self->{output}->option_exit();
+    }
+    if ($self->{output}->is_litteral_status(status => $self->{option_results}->{nopoweredon_status}) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong nopoweredon-status option '" . $self->{option_results}->{nopoweredon_status} . "'.");
+        $self->{output}->option_exit();
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+    $self->{connector} = $options{custom};
+
+    $self->{connector}->add_params(params => $self->{option_results},
+                                   command => 'devicevm');
+    $self->{connector}->run();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check virtual machine device connected.
+
+=over 8
+
+=item B<--vm-hostname>
+
+VM hostname to check.
+If not set, we check all VMs.
+
+=item B<--filter>
+
+VM hostname is a regexp.
+
+=item B<--filter-description>
+
+Filter also virtual machines description (can be a regexp).
+
+=item B<--scope-datacenter>
+
+Search in following datacenter(s) (can be a regexp).
+
+=item B<--scope-cluster>
+
+Search in following cluster(s) (can be a regexp).
+
+=item B<--scope-host>
+
+Search in following host(s) (can be a regexp).
+
+=item B<--disconnect-status>
+
+Status if VM disconnected (default: 'unknown').
+
+=item B<--nopoweredon-status>
+
+Status if VM is not poweredOn (default: 'unknown').
+
+=item B<--display-description>
+
+Display virtual machine description.
+
+=item B<--warning>
+
+Threshold warning in bytes per seconds.
+
+=item B<--critical>
+
+Threshold critical in bytes per seconds.
+
+=item B<--device>
+
+Device to check (Required) (Example: --device='VirtualCdrom').
+
+=back
+
+=cut
