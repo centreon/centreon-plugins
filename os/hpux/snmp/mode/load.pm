822decba69ab9cfc57cb9d766a74d44431e4886d
garnier-quentin <qgarnier@centreon.com>
2016-09-01 16:26:48 +0200

    + add plugin for hp-ux in snmp (Fix #13)

---
+++ os/hpux/snmp/mode/load.pm
@@ -0,0 +1,139 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package os::hpux::snmp::mode::load;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "warning:s"   => { name => 'warning', default => '' },
+                                  "critical:s"  => { name => 'critical', default => '' },
+                                });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+
+    ($self->{warn1}, $self->{warn5}, $self->{warn15}) = split /,/, $self->{option_results}->{warning};
+    ($self->{crit1}, $self->{crit5}, $self->{crit15}) = split /,/, $self->{option_results}->{critical};
+    
+    if (($self->{perfdata}->threshold_validate(label => 'warn1', value => $self->{warn1})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning (1min) threshold '" . $self->{warn1} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'warn5', value => $self->{warn5})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning (5min) threshold '" . $self->{warn5} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'warn15', value => $self->{warn15})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning (15min) threshold '" . $self->{warn15} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'crit1', value => $self->{crit1})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical (1min) threshold '" . $self->{crit1} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'crit5', value => $self->{crit5})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical (5min) threshold '" . $self->{crit5} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'crit15', value => $self->{crit15})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical (15min) threshold '" . $self->{crit15} . "'.");
+        $self->{output}->option_exit();
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+   
+    my $oid_computerSystemAvgJobs1 = '.1.3.6.1.4.1.11.2.3.1.1.3.0';
+    my $oid_computerSystemAvgJobs5 = '.1.3.6.1.4.1.11.2.3.1.1.4.0';
+    my $oid_computerSystemAvgJobs15 = '.1.3.6.1.4.1.11.2.3.1.1.5.0';
+
+    my $result = $options{snmp}->get_leef(oids => [$oid_computerSystemAvgJobs1, $oid_computerSystemAvgJobs5, $oid_computerSystemAvgJobs15], nothing_quit => 1);
+
+    my $cpu_load1 = $result->{$oid_computerSystemAvgJobs1} / 100;
+    my $cpu_load5 = $result->{$oid_computerSystemAvgJobs5} / 100;
+    my $cpu_load15 = $result->{$oid_computerSystemAvgJobs15} / 100;
+
+    $self->{output}->perfdata_add(label => 'load1',
+                              value => $cpu_load1,
+                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn1'),
+                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit1'),
+                              min => 0);
+    $self->{output}->perfdata_add(label => 'load5',
+                              value => $cpu_load5,
+                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn5'),
+                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit5'),
+                              min => 0);
+    $self->{output}->perfdata_add(label => 'load15',
+                              value => $cpu_load15,
+                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn15'),
+                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit15'),
+                              min => 0);
+    
+    my $exit1 = $self->{perfdata}->threshold_check(value => $cpu_load1,
+                                                   threshold => [ { label => 'crit1', exit_litteral => 'critical' }, { label => 'warn1', exit_litteral => 'warning' } ]);
+    my $exit2 = $self->{perfdata}->threshold_check(value => $cpu_load5,
+                                                   threshold => [ { label => 'crit5', exit_litteral => 'critical' }, { label => 'warn5', exit_litteral => 'warning' } ]);
+    my $exit3 = $self->{perfdata}->threshold_check(value => $cpu_load15,
+                                                   threshold => [ { label => 'crit15', exit_litteral => 'critical' }, { label => 'warn15', exit_litteral => 'warning' } ]);
+    my $exit = $self->{output}->get_most_critical(status => [ $exit1, $exit2, $exit3 ]);
+    $self->{output}->output_add(severity => $exit,
+                                short_msg => sprintf("Load average: %s, %s, %s", $cpu_load1, $cpu_load5, $cpu_load15));
+
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check system load-average.
+
+=over 8
+
+=item B<--warning>
+
+Threshold warning (1min,5min,15min).
+
+=item B<--critical>
+
+Threshold critical (1min,5min,15min).
+
+=back
+
+=cut
---
+++ os/hpux/snmp/mode/process.pm
@@ -0,0 +1,199 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package os::hpux::snmp::mode::process;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+my %map_process_status = (
+    1 => 'sleep',
+    2 => 'run',
+    3 => 'stop',
+    4 => 'zombie',
+    5 => 'other',
+    6 => 'idle',
+);
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "warning:s"               => { name => 'warning', },
+                                  "critical:s"              => { name => 'critical', },
+                                  "process-cmd:s"           => { name => 'process_cmd', },
+                                  "regexp-cmd"              => { name => 'regexp_cmd', },
+                                  "process-status:s"        => { name => 'process_status', default => 'run' },
+                                });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+        $self->{output}->option_exit();
+    }
+}
+
+my $oids = {
+    status => '.1.3.6.1.4.1.11.2.3.1.4.2.1.19', # processStatus
+    cmd => '.1.3.6.1.4.1.11.2.3.1.4.2.1.22', # processCmd
+};
+
+sub run {
+    my ($self, %options) = @_;
+    $self->{snmp} = $options{snmp};
+
+    my $oid2check_filter = 'status';
+    # To have a better order
+    foreach (('cmd', 'status')) {
+        if (defined($self->{option_results}->{'process_' . $_}) && $self->{option_results}->{'process_' . $_} ne '') {
+            $oid2check_filter = $_;
+            last;
+        }
+    }
+    # Build other
+    my $mores_filters = {};
+    my $more_oids = [];
+    foreach (keys %$oids) {
+        if ($_ ne $oid2check_filter && defined($self->{option_results}->{'process_' . $_}) && $self->{option_results}->{'process_' . $_} ne '') {
+            push @{$more_oids}, $oids->{$_};
+            $mores_filters->{$_} = 1;
+        }
+    }
+
+    my $oids_multiple_table = [ { oid => $oids->{$oid2check_filter} } ];
+
+    $self->{results} = $self->{snmp}->get_multiple_table(oids => $oids_multiple_table);
+    my $result = $self->{results}->{$oids->{$oid2check_filter}};
+    my $instances_keep = {};
+    foreach my $key ($self->{snmp}->oid_lex_sort(keys %{$result})) {
+        my $option_val = $self->{option_results}->{'process_' . $oid2check_filter};
+        
+        if ($oid2check_filter eq 'status') {
+            if ($map_process_status{$result->{$key}} =~ /$option_val/) {
+                $key =~ /\.([0-9]+)$/;
+                $instances_keep->{$1} = 1;
+            }
+        } elsif ((defined($self->{option_results}->{'regexp_' . $oid2check_filter}) && $result->{$key} =~ /$option_val/)
+                 || (!defined($self->{option_results}->{'regexp_' . $oid2check_filter}) && $result->{$key} eq $option_val)) {
+            $key =~ /\.([0-9]+)$/;
+            $instances_keep->{$1} = 1;
+        }
+    }
+
+    my $result2;
+    if (scalar(keys %$instances_keep) > 0) {
+        if (scalar(@$more_oids) > 0) {
+            $self->{snmp}->load(oids => $more_oids, instances => [ keys %$instances_keep ]);
+            $result2 = $self->{snmp}->get_leef();
+        }
+    
+        foreach my $key (keys %$instances_keep) {
+            my $value = ($oid2check_filter eq 'status') ? $map_process_status{$result->{$oids->{$oid2check_filter} . '.' . $key}} : $result->{$oids->{$oid2check_filter} . '.' . $key};       
+            my $long_value = '[ ' . $oid2check_filter . ' => ' . $value . ' ]';
+            my $deleted = 0;
+            foreach (keys %$mores_filters) {
+                my $opt_val = $self->{option_results}->{'process_' . $_};
+                $value = ($_ eq 'status') ? $map_process_status{$result2->{$oids->{$_} . '.' . $key}} : $result2->{$oids->{$_} . '.' . $key};
+                
+                if ($_ eq 'status') {
+                    if ($value !~ /$opt_val/) {
+                        delete $instances_keep->{$key};
+                        $deleted = 1;
+                        last;
+                    }
+                } elsif ((defined($self->{option_results}->{'regexp_' . $_}) && $value !~ /$opt_val/)
+                    || (!defined($self->{option_results}->{'regexp_' . $_}) && $value ne $opt_val)) {
+                    delete $instances_keep->{$key};
+                    $deleted = 1;
+                    last;
+                }
+                
+                $long_value .= ' [ ' . $_ . ' => ' . $value . ' ]';
+            }
+            
+            if ($deleted == 0) {
+                $self->{output}->output_add(long_msg => 'Process: ' . $long_value);
+            }
+        }
+    }
+    
+    my $num_processes_match = scalar(keys(%$instances_keep));
+    my $exit = $self->{perfdata}->threshold_check(value => $num_processes_match, threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+    $self->{output}->output_add(severity => $exit,
+                                short_msg => "Number of current processes running: $num_processes_match");
+    $self->{output}->perfdata_add(label => 'nbproc',
+                                  value => $num_processes_match,
+                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
+                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
+                                  min => 0);
+    
+    $self->{output}->display();
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check system number of processes.
+
+=over 8
+
+=item B<--warning>
+
+Threshold warning (process count).
+
+=item B<--critical>
+
+Threshold critical (process count).
+
+=item B<--process-cmd>
+
+Check process command.
+
+=item B<--regexp-cmd>
+
+Allows to use regexp to filter process command (with option --process-cmd).
+
+=item B<--process-status>
+
+Check process status (Default: 'run'). Can be a regexp.
+
+=back
+
+=cut
