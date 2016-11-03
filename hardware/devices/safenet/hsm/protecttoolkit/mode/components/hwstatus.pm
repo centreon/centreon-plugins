78d11bb23d69d32d6b80b0bef0c81c0de39088a7
garnier-quentin <qgarnier@centreon.com>
2016-11-03 11:02:26 +0100

    + add hsm safenet plugin

---
+++ hardware/devices/safenet/hsm/protecttoolkit/mode/components/hwstatus.pm
@@ -0,0 +1,47 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package hardware::devices::safenet::hsm::protecttoolkit::mode::components::hwstatus;
+
+use strict;
+use warnings;
+
+sub load { }
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking hardware status");
+    return if ($self->check_filter(section => 'hwstatus'));
+
+    return if ($self->{stdout} !~ /^Hardware Status\s+:\s+(.*?)\n/msi);
+    my $status = $1;
+        
+    $self->{output}->output_add(long_msg => sprintf("hardware status is '%s'",
+                                                    $status));
+    my $exit = $self->get_severity(section => 'hwstatus', value => $status);
+    if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+        $self->{output}->output_add(severity => $exit,
+                                    short_msg => sprintf("Hardware status is '%s'",
+                                                         $status));
+    }
+}
+
+1;
