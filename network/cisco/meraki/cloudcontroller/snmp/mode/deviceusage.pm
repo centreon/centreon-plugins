d70d564d43e2f6843d364a0eeae76dd6087a1f23
garnier-quentin <qgarnier@centreon.com>
2016-12-08 10:42:31 +0100

    + Fix #38

---
+++ network/cisco/meraki/cloudcontroller/snmp/mode/deviceusage.pm
@@ -0,0 +1,223 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::cisco::meraki::cloudcontroller::snmp::mode::deviceusage;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+my $instance_mode;
+
+sub custom_threshold_output {
+    my ($self, %options) = @_; 
+    my $status = 'ok';
+    my $message;
+    
+    eval {
+        local $SIG{__WARN__} = sub { $message = $_[0]; };
+        local $SIG{__DIE__} = sub { $message = $_[0]; };
+        
+        if (defined($instance_mode->{option_results}->{critical_status}) && $instance_mode->{option_results}->{critical_status} ne '' &&
+            eval "$instance_mode->{option_results}->{critical_status}") {
+            $status = 'critical';
+        } elsif (defined($instance_mode->{option_results}->{warning_status}) && $instance_mode->{option_results}->{warning_status} ne '' &&
+                 eval "$instance_mode->{option_results}->{warning_status}") {
+            $status = 'warning';
+        }
+    };
+    if (defined($message)) {
+        $self->{output}->output_add(long_msg => 'filter status issue: ' . $message);
+    }
+
+    return $status;
+}
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+    my $msg = 'Status : ' . $self->{result_values}->{status};
+
+    return $msg;
+}
+
+sub custom_status_calc {
+    my ($self, %options) = @_;
+    
+    $self->{result_values}->{status} = $options{new_datas}->{$self->{instance} . '_status'};
+    $self->{result_values}->{display} = $options{new_datas}->{$self->{instance} . '_display'};
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 },
+        { name => 'device', type => 1, cb_prefix_output => 'prefix_device_output', message_multiple => 'All devices are ok' }
+    ];
+    $self->{maps_counters}->{global} = [
+        { label => 'total-devices', set => {
+                key_values => [ { name => 'total' } ],
+                output_template => 'Total devices : %s',
+                perfdatas => [
+                    { label => 'total', value => 'total_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+    ];
+    
+    $self->{maps_counters}->{device} = [
+        { label => 'status', threshold => 0, set => {
+                key_values => [ { name => 'status' }, { name => 'display' } ],
+                closure_custom_calc => $self->can('custom_status_calc'),
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => $self->can('custom_threshold_output'),
+            }
+        },
+        { label => 'clients', set => {
+                key_values => [ { name => 'clients' }, { name => 'display' } ],
+                output_template => 'Clients : %s',
+                perfdatas => [
+                    { label => 'clients', value => 'clients_absolute', template => '%s',
+                      min => 0, label_extra_instance => 1, instance_use => 'display_absolute' },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "filter-name:s"           => { name => 'filter_name' },
+                                  "warning-status:s"        => { name => 'warning_status', default => '' },
+                                  "critical-status:s"       => { name => 'critical_status', default => '%{status} =~ /offline/' },
+                                });
+    
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+
+    $instance_mode = $self;
+    $self->change_macros();
+}
+
+sub prefix_device_output {
+    my ($self, %options) = @_;
+    
+    return "Device '" . $options{instance_value}->{display} . "' ";
+}
+
+sub change_macros {
+    my ($self, %options) = @_;
+    
+    foreach (('warning_status', 'critical_status')) {
+        if (defined($self->{option_results}->{$_})) {
+            $self->{option_results}->{$_} =~ s/%\{(.*?)\}/\$self->{result_values}->{$1}/g;
+        }
+    }
+}
+
+my %map_status = (
+    0 => 'offline',
+    1 => 'online',
+);
+my $mapping = {
+    devName         => { oid => '.1.3.6.1.4.1.29671.1.1.4.1.2' },
+    devStatus       => { oid => '.1.3.6.1.4.1.29671.1.1.4.1.3', map => \%map_status },
+    devClientCount  => { oid => '.1.3.6.1.4.1.29671.1.1.4.1.5' },
+};
+my $oid_devEntry = '.1.3.6.1.4.1.29671.1.1.4.1';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{device} = {};
+    $self->{global} = { total => 0 };
+    my $results = $options{snmp}->get_table(oid => $oid_devEntry, nothing_quit => 1);
+    foreach my $oid (keys %{$results}) {
+        next if ($oid !~ /^$mapping->{devName}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $options{snmp}->map_instance(mapping => $mapping, results => $results, instance => $instance);
+        if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' &&
+            $result->{devName} !~ /$self->{option_results}->{filter_name}/) {
+            $self->{output}->output_add(long_msg => "skipping  '" . $result->{devName} . "': no matching filter.", debug => 1);
+            next;
+        }
+        
+        $self->{global}->{total}++;
+        $self->{device}->{$instance} = { display => $result->{devName}, 
+                                         status => $result->{devStatus}, 
+                                         clients => $result->{devClientCount}};
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check device usages.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: --filter-counters='^clients$'
+
+=item B<--filter-name>
+
+Filter device name (can be a regexp).
+
+=item B<--warning-status>
+
+Set warning threshold for status.
+Can used special variables like: %{status}, %{display}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '%{status} =~ /offline/').
+Can used special variables like: %{status}, %{display}
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'total-devices', 'clients'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'total-devices', 'clients'.
+
+=back
+
+=cut
