dda01d4bbbe90e7bd18f73a8442368543b1f19cc
garnier-quentin <qgarnier@centreon.com>
2016-12-15 11:35:09 +0100

    + update synology hardware mode

---
+++ centreon-plugins/storage/synology/snmp/mode/components/disk.pm
@@ -0,0 +1,70 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::synology::snmp::mode::components::disk;
+
+use strict;
+use warnings;
+
+my %map_disk_status = (
+    1 => 'Normal',
+    2 => 'Initialized',
+    3 => 'NotInitialized',
+    4 => 'SystemPartitionFailed',
+    5 => 'Crashed',
+);
+
+my $mapping = {
+    synoDiskdiskStatus => { oid => '.1.3.6.1.4.1.6574.2.1.1.5', map => \%map_disk_status },
+};
+my $oid_synoDisk = '.1.3.6.1.4.1.6574.2.1';
+
+sub load {
+    my ($self) = @_;
+    
+    push @{$self->{request}}, { oid => $oid_synoDisk };
+}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking disk");
+    $self->{components}->{disk} = {name => 'disk', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'disk'));
+
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$oid_synoDisk}})) {
+        next if ($oid !~ /^$mapping->{synoDiskdiskStatus}->{oid}\.(\d+)/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$oid_synoDisk}, instance => $instance);
+
+        next if ($self->check_filter(section => 'disk', instance => $instance));
+        $self->{components}->{disk}->{total}++;
+
+        $self->{output}->output_add(long_msg => sprintf("disk '%s' status is %s.",
+                                    $instance, $result->{synoDiskdiskStatus}));
+        my $exit = $self->get_severity(section => 'disk', value => $result->{synoDiskdiskStatus});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Disk '%s' status is %s", $instance, $result->{synoDiskdiskStatus}));
+        }
+    }
+}
+
+1;
---
+++ centreon-plugins/storage/synology/snmp/mode/components/raid.pm
@@ -0,0 +1,79 @@
+#
+# Copyright 2016 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::synology::snmp::mode::components::raid;
+
+use strict;
+use warnings;
+
+my %map_raid_status = (
+    1 => 'Normal',
+    2 => 'Repairing',
+    3 => 'Migrating',
+    4 => 'Expanding',
+    5 => 'Deleting',
+    6 => 'Creating',
+    7 => 'RaidSyncing',
+    8 => 'RaidParityChecking',
+    9 => 'RaidAssembling',
+    10 => 'Canceling',
+    11 => 'Degrade',
+    12 => 'Crashed',
+);
+
+my $mapping = {
+    synoRaidraidName    => { oid => '.1.3.6.1.4.1.6574.3.1.1.2' },
+    synoRaidraidStatus  => { oid => '.1.3.6.1.4.1.6574.3.1.1.3', map => \%map_raid_status },
+};
+my $oid_synoRaid = '.1.3.6.1.4.1.6574.3.1.1';
+
+sub load {
+    my ($self) = @_;
+    
+    push @{$self->{request}}, { oid => $oid_synoRaid };
+}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking raid");
+    $self->{components}->{raid} = {name => 'raid', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'raid'));
+
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$oid_synoRaid}})) {
+        next if ($oid !~ /^$mapping->{synoRaidraidStatus}->{oid}\.(\d+)/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$oid_synoRaid}, instance => $instance);
+
+        next if ($self->check_filter(section => 'raid', instance => $instance));
+        $self->{components}->{raid}->{total}++;
+
+        $self->{output}->output_add(long_msg => sprintf("raid '%s' status is %s [instance: %s]",
+                                    $result->{synoRaidraidName}, $result->{synoRaidraidStatus}, $instance));
+        my $exit = $self->get_severity(section => 'raid', value => $result->{synoRaidraidStatus});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Raid '%s' status is %s", 
+                                                             $result->{synoRaidraidName}, $result->{synoRaidraidStatus}));
+        }
+    }
+}
+
+1;
