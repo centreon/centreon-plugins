1d710f93b85c944df98c14a0e53539781a99126a
garnier-quentin <qgarnier@centreon.com>
2017-01-12 11:28:52 +0100

    + add sap hana db plugin

---
+++ database/sap/hana/mode/connectedusers.pm
@@ -0,0 +1,123 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package database::sap::hana::mode::connectedusers;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'host', type => 1, cb_prefix_output => 'prefix_output', message_multiple => 'All connected users are ok' },
+    ];
+    $self->{maps_counters}->{host} = [
+        { label => 'users', set => {
+                key_values => [ { name => 'total' }, { name => 'display' } ],
+                output_template => 'Connected Users : %s',
+                perfdatas => [
+                    { label => 'users', value => 'total_absolute', template => '%s', min => 0,
+                      label_extra_instance => 1, instance_use => 'display_absolute' },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_output {
+    my ($self, %options) = @_;
+
+    return "Host '" . $options{instance_value}->{display} . "' ";
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                "filter-username:s"     => { name => 'filter_username' },
+                                });
+                                
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+    $options{sql}->connect();
+
+    my $query = q{
+        SELECT * FROM SYS.M_CONNECTIONS WHERE CONNECTION_TYPE IN ('Remote')
+    };
+    $options{sql}->query(query => $query);
+
+    $self->{host} = {};
+    while ((my $row = $options{sql}->fetchrow_hashref())) {
+        my $name = $row->{HOST} . ':' . $row->{PORT};
+        
+        $self->{host}->{$name} = { total => 0, display => $name } if (!defined($self->{host}->{$name}));
+        
+        if (defined($self->{option_results}->{filter_username}) && $self->{option_results}->{filter_username} ne '' &&
+            $row->{USER_NAME} !~ /$self->{option_results}->{filter_username}/) {
+            $self->{output}->output_add(long_msg => "skipping  '" . $name . '/' . $row->{USER_NAME} . "': no matching filter.", debug => 1);
+            next;
+        }
+        
+        $self->{host}->{$name}->{total}++;
+    }
+    
+    if (scalar(keys %{$self->{host}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "No host found.");
+        $self->{output}->option_exit();
+    }
+}
+    
+1;
+
+__END__
+
+=head1 MODE
+
+Check connected users.
+
+=over 8
+
+=item B<--filter-username>
+
+Filter connected username. (Can be a regex)
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'users'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'users'.
+
+=back
+
+=cut
---
+++ database/sap/hana/plugin.pm
@@ -0,0 +1,120 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package database::sap::hana::plugin;
+
+use strict;
+use warnings;
+use base qw(centreon::plugins::script_sql);
+
+sub new {
+    my ($class, %options) = @_;
+    
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '0.1';
+    %{$self->{modes}} = (
+                         'blocked-transactions' => 'database::sap::hana::mode::blockedtransactions',
+                         'connected-users'      => 'database::sap::hana::mode::connectedusers',
+                         'connection-time'      => 'centreon::common::protocols::sql::mode::connectiontime',
+                         'disk-usage'           => 'database::sap::hana::mode::diskusage',
+                         'host-memory'          => 'database::sap::hana::mode::hostmemory',
+                         'host-cpu'             => 'database::sap::hana::mode::hostcpu',
+                         'sql'                  => 'centreon::common::protocols::sql::mode::sql',
+                         'volume-usage'         => 'database::sap::hana::mode::volumeusage',
+                         );
+
+    return $self;
+}
+
+sub init {
+    my ($self, %options) = @_;
+
+    $self->{options}->add_options(
+                                   arguments => {
+                                                'servernode:s@' => { name => 'servernode' },
+                                                'port:s@'       => { name => 'port' },
+                                                'database:s'    => { name => 'database' },
+                                                }
+                                  );
+    $self->{options}->parse_options();
+    my $options_result = $self->{options}->get_options();
+    $self->{options}->clean();
+
+    if (defined($options_result->{servernode})) {
+        @{$self->{sqldefault}->{dbi}} = ();
+        for (my $i = 0; $i < scalar(@{$options_result->{servernode}}); $i++) {
+            $self->{sqldefault}->{dbi}[$i] = { data_source => 'ODBC:DRIVER={HDBODBC};SERVERNODE=' . $options_result->{servernode}[$i] };
+            if (defined($options_result->{port}[$i])) {
+                $self->{sqldefault}->{dbi}[$i]->{data_source} .= ':' . $options_result->{port}[$i];
+            } else {
+                $self->{sqldefault}->{dbi}[$i]->{data_source} .= ':30013';
+            }
+            if ((defined($options_result->{database})) && ($options_result->{database} ne '')) {
+                $self->{sqldefault}->{dbi}[$i]->{data_source} .= ';DATABASENAME=' . $options_result->{database};
+            }
+        }
+    }
+    $self->SUPER::init(%options);    
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check SAP Hana DB Server.
+Prerequisite on the system:
+
+=over 8
+
+=item * SAP Hana client for Linux/Unix
+
+=item * unixODBC and perl DBD::ODBC
+
+=item * Add in file /etc/odbcinst.ini
+
+[HDBODBC]
+Description = "SmartCloudPT HANA"
+Driver=/usr/sap/hdbclient/libodbcHDB.so
+
+=item * Use option --connect-options="LongReadLen=1000000,LongTruncOk=1"
+
+=back 
+
+=over 8
+
+=item B<--servernode>
+
+Hostname to query.
+
+=item B<--port>
+
+Database Server Port (default: 30013).
+
+=item B<--database>
+
+Database name to connect.
+
+=back
+
+=cut
