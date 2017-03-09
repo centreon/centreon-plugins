b5f0cde0d782fccfb5ca72981bd0afbb57768c25
garnier-quentin <qgarnier@centreon.com>
2017-03-09 16:24:38 +0100

    add audiocodes snmp plugin (WIP)

---
+++ network/audiocodes/snmp/mode/hardware.pm
@@ -0,0 +1,117 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::audiocodes::snmp::mode::hardware;
+
+use base qw(centreon::plugins::templates::hardware);
+
+use strict;
+use warnings;
+
+sub set_system {
+    my ($self, %options) = @_;
+    
+    $self->{regexp_threshold_overload_check_section_option} = '^(module|fantray|psu)$';
+    
+    $self->{cb_hook2} = 'snmp_execute';
+    
+    $self->{thresholds} = {
+        default => [
+            ['cleared', 'OK'],
+            ['indeterminate', 'UNKNOWN'],
+            ['warning', 'WARNING'],
+            ['minor', 'WARNING'],
+            ['major', 'CRITICAL'],
+            ['critical', 'CRITICAL'],
+        ],
+        module => [
+            ['moduleNotExist', 'OK'],
+            ['moduleExistOk', 'OK'],
+            ['moduleOutOfService', 'CRITICAL'],
+            ['moduleBackToServiceStart', 'WARNING'],
+            ['moduleMismatch', 'WARNING'],
+            ['moduleFaulty', 'CRITICAL'],
+            ['notApplicable', 'OK'],
+        ],
+    };
+    
+    $self->{components_path} = 'network::audiocodes::snmp::mode::components';
+    $self->{components_module} = ['module', 'fantray', 'psu'];
+}
+
+sub snmp_execute {
+    my ($self, %options) = @_;
+    
+    $self->{snmp} = $options{snmp};
+    $self->{results} = $self->{snmp}->get_multiple_table(oids => $self->{request});
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, no_performance => 1);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                });
+    
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check Hardware.
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: '.*').
+Can be: 'fantray', 'module', 'psu'.
+
+=item B<--filter>
+
+Exclude some parts (comma seperated list) (Example: --filter=fan --filter=psu)
+Can also exclude specific instance: --filter=fantray,1
+
+=item B<--absent-problem>
+
+Return an error if an entity is not 'present' (default is skipping) (comma seperated list)
+Can be specific or global: --absent-problem=psu,1
+
+=item B<--no-component>
+
+Return an error if no compenents are checked.
+If total (with skipped) is 0. (Default: 'critical' returns).
+
+=item B<--threshold-overload>
+
+Set to overload default threshold values (syntax: section,[instance,]status,regexp)
+It used before default thresholds (order stays).
+Example: --threshold-overload='psu,CRITICAL,^(?!(cleared)$)'
+
+=back
+
+=cut
