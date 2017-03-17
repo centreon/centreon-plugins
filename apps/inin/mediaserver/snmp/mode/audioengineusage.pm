ac4c5eb1075e85d20c91a5b071930396ba48f655
garnier-quentin <qgarnier@centreon.com>
2017-03-17 14:07:08 +0100

    + add inin plugin in snmp

---
+++ apps/inin/mediaserver/snmp/mode/audioengineusage.pm
@@ -0,0 +1,146 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::inin::mediaserver::snmp::mode::audioengineusage;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'engine', type => 1, cb_prefix_output => 'prefix_engine_output', message_multiple => 'All audio engines are ok' }
+    ];
+    
+    $self->{maps_counters}->{engine} = [
+        { label => 'avg-load', set => {
+                key_values => [ { name => 'i3MsAudioEngineAverageLoad' }, { name => 'display' } ],
+                output_template => 'Average Load : %s',
+                perfdatas => [
+                    { label => 'load_avg', value => 'i3MsAudioEngineAverageLoad_absolute', template => '%s', 
+                      min => 0, label_extra_instance => 1, instance_use => 'display_absolute' },
+                ],
+            }
+        },
+        { label => 'elem-count', set => {
+                key_values => [ { name => 'i3MsAudioEngineElementCount' }, { name => 'display' } ],
+                output_template => 'Total active graph elements : %s',
+                perfdatas => [
+                    { label => 'elem_count', value => 'i3MsAudioEngineElementCount_absolute', template => '%s', 
+                      min => 0, label_extra_instance => 1, instance_use => 'display_absolute' },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "filter-location:s"   => { name => 'filter_location' },
+                                });
+    
+    return $self;
+}
+
+sub prefix_engine_output {
+    my ($self, %options) = @_;
+    
+    return "Audio Engine '" . $options{instance_value}->{display} . "' ";
+}
+
+my $mapping = {
+    i3MsAudioEngineLocation     => { oid => '.1.3.6.1.4.1.2793.8227.2.1.1.4' },
+    i3MsAudioEngineAverageLoad  => { oid => '.1.3.6.1.4.1.2793.8227.2.1.1.6' },
+    i3MsAudioEngineElementCount => { oid => '.1.3.6.1.4.1.2793.8227.2.1.1.8' },
+};
+
+my $oid_i3MsAudioEngineInfoTableEntry = '.1.3.6.1.4.1.2793.8227.2.1.1';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{engine} = {};
+    my $snmp_result = $options{snmp}->get_table(oid => $oid_i3MsAudioEngineInfoTableEntry,
+                                                nothing_quit => 1);
+
+
+    foreach my $oid (keys %{$snmp_result}) {
+        next if ($oid !~ /^$mapping->{i3MsAudioEngineLocation}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => $instance);
+
+        if (defined($self->{option_results}->{filter_location}) && $self->{option_results}->{filter_location} ne '' &&
+            $result->{i3MsAudioEngineLocation} !~ /$self->{option_results}->{filter_location}/) {
+            $self->{output}->output_add(long_msg => "skipping '" . $result->{i3MsAudioEngineLocation} . "': no matching filter.", debug => 1);
+            next;
+        }
+        
+        $self->{engine}->{$instance} = { display => $result->{i3MsAudioEngineLocation}, 
+            %$result
+        };
+    }
+    
+    if (scalar(keys %{$self->{engine}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "No audio engine found.");
+        $self->{output}->option_exit();
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check audio engine usage.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: --filter-counters='^elem-count$'
+
+=item B<--filter-location>
+
+Filter location name (can be a regexp).
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'elem-count', 'avg-load'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'elem-count', 'avg-load'.
+
+=back
+
+=cut
---
+++ apps/inin/mediaserver/snmp/mode/diskusage.pm
@@ -0,0 +1,200 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::inin::mediaserver::snmp::mode::diskusage;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+my $instance_mode;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'disktracelog', type => 0 },
+        { name => 'diskhttpcache', type => 0 },
+    ];
+
+    $self->{maps_counters}->{disktracelog} = [
+        { label => 'tracelog-usage', set => {
+                key_values => [ { name => 'free' }, { name => 'total' }, { name => 'display' } ],
+                closure_custom_calc => $self->can('custom_usage_calc'),
+                closure_custom_output => $self->can('custom_usage_output'),
+                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_usage_threshold'),
+            }
+        },
+    ];
+    $self->{maps_counters}->{diskhttpcache} = [
+        { label => 'httpcache-usage', set => {
+                key_values => [ { name => 'free' }, { name => 'total' }, { name => 'display' } ],
+                closure_custom_calc => $self->can('custom_usage_calc'),
+                closure_custom_output => $self->can('custom_usage_output'),
+                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_usage_threshold'),
+            }
+        },
+    ];
+}
+
+sub custom_usage_perfdata {
+    my ($self, %options) = @_;
+
+    my $label = 'used_' . $self->{result_values}->{display};
+    my $value_perf = $self->{result_values}->{used};
+    if (defined($instance_mode->{option_results}->{free})) {
+        $label = 'free_' . $self->{result_values}->{display};
+        $value_perf = $self->{result_values}->{free};
+    }
+    my %total_options = ();
+    if ($instance_mode->{option_results}->{units} eq '%') {
+        $total_options{total} = $self->{result_values}->{total};
+        $total_options{cast_int} = 1;
+    }
+
+    $self->{output}->perfdata_add(label => $label, unit => 'B',
+                                  value => $value_perf,
+                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{label}, %total_options),
+                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{label}, %total_options),
+                                  min => 0, max => $self->{result_values}->{total});
+}
+
+sub custom_usage_threshold {
+    my ($self, %options) = @_;
+
+    my ($exit, $threshold_value);
+    $threshold_value = $self->{result_values}->{used};
+    $threshold_value = $self->{result_values}->{free} if (defined($instance_mode->{option_results}->{free}));
+    if ($instance_mode->{option_results}->{units} eq '%') {
+        $threshold_value = $self->{result_values}->{prct_used};
+        $threshold_value = $self->{result_values}->{prct_free} if (defined($instance_mode->{option_results}->{free}));
+    }
+    $exit = $self->{perfdata}->threshold_check(value => $threshold_value, threshold => [ { label => 'critical-' . $self->{label}, exit_litteral => 'critical' }, { label => 'warning-'. $self->{label}, exit_litteral => 'warning' } ]);
+    return $exit;
+}
+
+sub custom_usage_output {
+    my ($self, %options) = @_;
+
+    my ($total_size_value, $total_size_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{total});
+    my ($total_used_value, $total_used_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{used});
+    my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{free});
+    my $msg = sprintf("Disk '%s' Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
+                    $self->{result_values}->{display},
+                    $total_size_value . " " . $total_size_unit,
+                    $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used},
+                    $total_free_value . " " . $total_free_unit, $self->{result_values}->{prct_free});
+    return $msg;
+}
+
+sub custom_usage_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{display} = $options{new_datas}->{$self->{instance} . '_display'};
+    $self->{result_values}->{total} = $options{new_datas}->{$self->{instance} . '_total'};
+    $self->{result_values}->{free} = $options{new_datas}->{$self->{instance} . '_free'};
+    $self->{result_values}->{used} = $self->{result_values}->{total} - $self->{result_values}->{free};
+    
+    $self->{result_values}->{prct_used} = $self->{result_values}->{used} * 100 / $self->{result_values}->{total};
+    $self->{result_values}->{prct_free} = 100 - $self->{result_values}->{prct_used};
+    
+    return 0;
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                "units:s"           => { name => 'units', default => '%' },
+                                "free"              => { name => 'free' },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+
+    $instance_mode = $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+    
+    my $oid_i3MsGeneralInfoTracelogFreeDiskSpace = '.1.3.6.1.4.1.2793.8227.1.12.0'; # MiB
+    my $oid_i3MsGeneralInfoTracelogTotalDiskSpace = '.1.3.6.1.4.1.2793.8227.1.13.0'; # MiB
+    my $oid_i3MsGeneralInfoHttpCacheFreeDiskSpace = '.1.3.6.1.4.1.2793.8227.1.15.0'; # MiB
+    my $oid_i3MsGeneralInfoHttpCacheTotalDiskSpace = '.1.3.6.1.4.1.2793.8227.1.16.0'; # MiB
+
+    my $snmp_result = $options{snmp}->get_leef(oids => [
+            $oid_i3MsGeneralInfoTracelogFreeDiskSpace, $oid_i3MsGeneralInfoTracelogTotalDiskSpace,
+            $oid_i3MsGeneralInfoHttpCacheFreeDiskSpace, $oid_i3MsGeneralInfoHttpCacheTotalDiskSpace
+        ], nothing_quit => 1);
+
+    $self->{disktracelog} = { 
+        free => $snmp_result->{$oid_i3MsGeneralInfoTracelogFreeDiskSpace} * 1024 * 1024,
+        total => $snmp_result->{$oid_i3MsGeneralInfoTracelogTotalDiskSpace} * 1024 * 1024,
+        display => 'tracelog'
+    };
+    $self->{diskhttpcache} = {
+        free => $snmp_result->{$oid_i3MsGeneralInfoHttpCacheFreeDiskSpace} * 1024 * 1024,
+        total => $snmp_result->{$oid_i3MsGeneralInfoHttpCacheTotalDiskSpace} * 1024 * 1024,
+        display => 'httpcache'
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check disk usages.
+
+=over 8
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'httpcache-usage', 'tracelog-usage'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'httpcache-usage', 'tracelog-usage'.
+
+=item B<--units>
+
+Default is '%', can be 'B'
+
+=item B<--free>
+
+Thresholds are on free space left.
+
+=back
+
+=cut
