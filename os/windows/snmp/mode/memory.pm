9379dcba8e6cd045237e5335b007a5b1077eee55
garnier-quentin <qgarnier@centreon.com>
2017-03-20 13:17:49 +0100

    + Fix windows help

--- os/windows/snmp/mode/memory.pm
+++ os/windows/snmp/mode/memory.pm
@@ -182,9 +182,11 @@ Check memory usage
 =over 8
 
 =item B<--units>
+
 Units of thresholds (Default: '%') ('%', 'absolute').
 
 =item B<--free>
+
 Thresholds are on free tape left.
 
 =item B<--warning-*>
