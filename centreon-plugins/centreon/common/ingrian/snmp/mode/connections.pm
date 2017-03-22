a0533cbb61b6c619b05dfa07c80d4f1c291010aa
garnier-quentin <qgarnier@centreon.com>
2017-03-22 14:44:18 +0100

    + add plugin for safenet keysecure in snmp

---
+++ centreon-plugins/centreon/common/ingrian/snmp/mode/connections.pm
@@ -0,0 +1,119 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package centreon::common::ingrian::snmp::mode::connections;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use Digest::MD5 qw(md5_hex);
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, message_separator => ' - ' }
+    ];
+    
+    $self->{maps_counters}->{global} = [
+        { label => 'total', set => {
+                key_values => [ { name => 'naeTotalTotalConnections', diff => 1 } ],
+                output_template => 'Total Connections : %s',
+                perfdatas => [
+                    { label => 'total', value => 'naeTotalTotalConnections_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'ssl', set => {
+                key_values => [ { name => 'naeTotalSSLConnections', diff => 1 } ],
+                output_template => 'Total SSL Connections : %s',
+                perfdatas => [
+                    { label => 'total_ssl', value => 'naeTotalSSLConnections_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'non-ssl', set => {
+                key_values => [ { name => 'naeTotalNonSSLConnections', diff => 1 } ],
+                output_template => 'Total non-SSL Connections : %s',
+                perfdatas => [
+                    { label => 'total_non_ssl', value => 'naeTotalNonSSLConnections_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                });
+    
+    return $self;
+}
+
+my $mapping = {
+    naeTotalTotalConnections    => { oid => '.1.3.6.1.4.1.5595.3.5.1.4' },
+    naeTotalNonSSLConnections   => { oid => '.1.3.6.1.4.1.5595.3.5.1.5.4' },
+    naeTotalSSLConnections      => { oid => '.1.3.6.1.4.1.5595.3.5.1.6.4' },
+};
+my $oid_naeConnectionStats = '.1.3.6.1.4.1.5595.3.5';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    
+    my $snmp_result = $options{snmp}->get_table(oid => $oid_naeConnectionStats,
+                                                nothing_quit => 1);
+    my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => '0');
+    $self->{global} = { %$result };
+    
+    $self->{cache_name} = "ingrian_" . $options{snmp}->get_hostname()  . '_' . $options{snmp}->get_port() . '_' . $self->{mode} . '_' . 
+        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all'));
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check connections.
+
+=over 8
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'total', 'ssl', 'non-ssl'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'total', 'ssl', 'non-ssl'.
+
+=back
+
+=cut
---
+++ centreon-plugins/centreon/common/ingrian/snmp/mode/cpu.pm
@@ -0,0 +1,138 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package centreon::common::ingrian::snmp::mode::cpu;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, cb_init => 'skip_global', },
+        { name => 'cpu', type => 1, cb_prefix_output => 'prefix_cpu_output', message_multiple => 'All CPU usages are ok' }
+    ];
+    $self->{maps_counters}->{global} = [
+        { label => 'total', set => {
+                key_values => [ { name => 'total' } ],
+                output_template => 'Total CPU Usage : %.2f %%',
+                perfdatas => [
+                    { label => 'total_cpu_avg', value => 'total_absolute', template => '%.2f', min => 0, max => 100, unit => '%' },
+                ],
+            }
+        },
+    ];
+    
+    $self->{maps_counters}->{cpu} = [
+        { label => 'usage', set => {
+                key_values => [ { name => 'usage' }, { name => 'display' }, ],
+                output_template => 'Usage : %.2f %%',
+                perfdatas => [
+                    { label => 'cpu', value => 'usage_absolute', template => '%.2f',
+                      min => 0, max => 100, unit => '%', label_extra_instance => 1, instance_use => 'display_absolute' },
+                ],
+            }
+        },
+    ];
+}
+
+sub skip_global {
+    my ($self, %options) = @_;
+    
+    scalar(keys %{$self->{cpu}}) > 1 ? return(0) : return(1);
+}
+
+sub prefix_cpu_output {
+    my ($self, %options) = @_;
+    
+    return "CPU '" . $options{instance_value}->{display} . "' ";
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                });
+    
+    return $self;
+}
+
+my $mapping = {
+    naeSystemCPUDescr       => { oid => '.1.3.6.1.4.1.5595.3.2.4.1.2' },
+    naeSystemCPUUtilization => { oid => '.1.3.6.1.4.1.5595.3.2.4.1.3' },
+};
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $oid_naeSystemStatCPUEntry = '.1.3.6.1.4.1.5595.3.2.4.1';
+    my $oid_naeSystemStatCPU = '.1.3.6.1.4.1.5595.3.2.2'; # without .0
+    my $snmp_result = $options{snmp}->get_multiple_table(oids => [
+                                                            { oid => $oid_naeSystemStatCPU },
+                                                            { oid => $oid_naeSystemStatCPUEntry },
+                                                          ], nothing_quit => 1);
+    
+    $self->{cpu} = {};
+    foreach my $oid (keys %{$snmp_result->{$oid_naeSystemStatCPUEntry}}) {
+        next if ($oid !~ /^$mapping->{naeSystemCPUUtilization}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result->{$oid_naeSystemStatCPUEntry}, instance => $instance);
+        
+        $self->{cpu}->{$instance} = { display => $instance - 1, usage => $result->{naeSystemCPUUtilization} };
+    }
+
+    $self->{global} = { total => $snmp_result->{$oid_naeSystemStatCPU}->{$oid_naeSystemStatCPU . '.0'} };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check CPU usages.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: --filter-counters='^total$'
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'total', 'usage'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'total', 'usage'.
+
+=back
+
+=cut
