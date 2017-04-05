2eae46350b0829b0c3178fe45ffee62590423025
Fabien THEPAUT <fthepaut@centreon.com>
2017-04-05 11:37:47 +0200

    add HWG STE2

---
+++ hardware/sensors/hwgste2/snmp/mode/sensors.pm
@@ -0,0 +1,111 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+package hardware::sensors::hwgste2::snmp::mode::sensors;
+
+use base qw(centreon::plugins::templates::hardware);
+
+use strict;
+use warnings;
+
+sub set_system {
+    my ($self, %options) = @_;
+    
+    $self->{regexp_threshold_overload_check_section_option} = '^(temperature|humidity)$';
+    $self->{regexp_threshold_numeric_check_section_option} = '^(temperature|humidity)$';
+    
+    $self->{cb_hook2} = 'snmp_execute';
+    
+    $self->{thresholds} = {
+        default => [
+            ['invalid', 'UNKNOWN'],
+            ['normal', 'OK'],
+            ['outOfRangeLo', 'WARNING'],
+            ['outOfRangeHi', 'WARNING'],
+            ['alarmLo', 'CRITICAL'],
+            ['alarmHi', 'CRITICAL'],
+        ],
+    };
+    
+    $self->{components_path} = 'hardware::sensors::hwgste2::snmp::mode::components';
+    $self->{components_module} = ['temperature', 'humidity'];
+}
+
+sub snmp_execute {
+    my ($self, %options) = @_;
+    
+    $self->{snmp} = $options{snmp};
+    my $oid_sensEntry = '.1.3.6.1.4.1.21796.4.9';
+    push @{$self->{request}}, { oid => $oid_sensEntry };
+    $self->{results} = $self->{snmp}->get_multiple_table(oids => $self->{request});
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, no_absent => 1);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                });
+    
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check HWg-STE sensors.
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: '.*').
+Can be: 'temperature', 'humidity'.
+
+=item B<--filter>
+
+Exclude some parts.
+Can also exclude specific instance: --filter=sensor,10
+
+=item B<--threshold-overload>
+
+Set to overload default threshold values (syntax: section,[instance,]status,regexp)
+It used before default thresholds (order stays).
+Example: --threshold-overload='sensor,CRITICAL,^(?!(normal)$)'
+
+=item B<--warning>
+
+Set warning threshold for temperature, humidity (syntax: type,instance,threshold)
+Example: --warning='temperature,.*,30'
+
+=item B<--critical>
+
+Set critical threshold for temperature, humidity (syntax: type,instance,threshold)
+Example: --critical='humidty,.*,40'
+
+=back
+
+=cut
