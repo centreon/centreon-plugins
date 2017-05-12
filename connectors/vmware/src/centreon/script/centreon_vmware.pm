ce02ce6b118b2a04dad501286b058a56c1f465c9
garnier-quentin <qgarnier@centreon.com>
2017-05-12 17:28:55 +0200

    + prepare new version

--- connectors/vmware/src/centreon/script/centreon_vmware.pm
+++ connectors/vmware/src/centreon/script/centreon_vmware.pm
@@ -54,7 +54,7 @@ BEGIN {
 use base qw(centreon::script);
 use vars qw(%centreon_vmware_config);
 
-my $VERSION = "2.3.1";
+my $VERSION = "2.3.2";
 my %handlers = (TERM => {}, HUP => {}, CHLD => {});
 
 my @load_modules = (
