ec6b33165380bd77663ff7665316f298ba272f73
garnier-quentin <qgarnier@centreon.com>
2017-05-23 10:52:07 +0200

    + update hyperv modes

---
+++ centreon-plugins/apps/hyperv/2012/local/mode/listnodevms.pm
@@ -0,0 +1,133 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::hyperv::2012::local::mode::listnodevms;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+use centreon::common::powershell::hyperv::2012::listnodevms;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {                                  
+                                  "timeout:s"           => { name => 'timeout', default => 50 },
+                                  "command:s"           => { name => 'command', default => 'powershell.exe' },
+                                  "command-path:s"      => { name => 'command_path' },
+                                  "command-options:s"   => { name => 'command_options', default => '-InputFormat none -NoLogo -EncodedCommand' },
+                                  "no-ps"               => { name => 'no_ps', },
+                                  "ps-exec-only"        => { name => 'ps_exec_only', },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub run {
+    my ($self, %options) = @_;
+    
+    my $ps = centreon::common::powershell::hyperv::2012::listnodevms::get_powershell(no_ps => $self->{option_results}->{no_ps});
+    
+    $self->{option_results}->{command_options} .= " " . $ps;
+    my ($stdout) = centreon::plugins::misc::execute(output => $self->{output},
+                                                    options => $self->{option_results},
+                                                    command => $self->{option_results}->{command},
+                                                    command_path => $self->{option_results}->{command_path},
+                                                    command_options => $self->{option_results}->{command_options});
+    if (defined($self->{option_results}->{ps_exec_only})) {
+        $self->{output}->output_add(severity => 'OK',
+                                    short_msg => $stdout);
+    } else {
+        $self->{output}->output_add(severity => 'OK',
+                                    short_msg => 'List Virtual Machines:');
+        centreon::common::powershell::hyperv::2012::listnodevms::list($self, stdout => $stdout);
+    }
+    
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
+    $self->{output}->exit();
+}
+
+sub disco_format {
+    my ($self, %options) = @_;
+    
+    $self->{output}->add_disco_format(elements => ['name', 'state', 'status', 'is_clustered', 'note']);
+}
+
+sub disco_show {
+    my ($self, %options) = @_;
+    
+    my $ps = centreon::common::powershell::hyperv::2012::listnodevms::get_powershell(no_ps => $self->{option_results}->{no_ps});
+    $self->{option_results}->{command_options} .= " " . $ps;
+    my ($stdout) = centreon::plugins::misc::execute(output => $self->{output},
+                                                    options => $self->{option_results},
+                                                    command => $self->{option_results}->{command},
+                                                    command_path => $self->{option_results}->{command_path},
+                                                    command_options => $self->{option_results}->{command_options});
+    centreon::common::powershell::hyperv::2012::listnodevms::disco_show($self, stdout => $stdout);
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+List virtual machines on hyper-v node.
+
+=over 8
+
+=item B<--timeout>
+
+Set timeout time for command execution (Default: 50 sec)
+
+=item B<--no-ps>
+
+Don't encode powershell. To be used with --command and 'type' command.
+
+=item B<--command>
+
+Command to get information (Default: 'powershell.exe').
+Can be changed if you have output in a file. To be used with --no-ps option!!!
+
+=item B<--command-path>
+
+Command path (Default: none).
+
+=item B<--command-options>
+
+Command options (Default: '-InputFormat none -NoLogo -EncodedCommand').
+
+=item B<--ps-exec-only>
+
+Print powershell output.
+
+=back
+
+=cut
