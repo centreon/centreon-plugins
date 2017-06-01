14dcdf67d796b31ab5c0c312cb145897ce7e1edf
qgarnier <qgarnier@centreon.com>
2017-06-01 17:00:02 +0200

    add plugins for evertz in snmp

---
+++ network/evertz/AEA47721/snmp/mode/streamstatus.pm
@@ -0,0 +1,231 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::evertz::AEA47721::snmp::mode::streamstatus;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+my $instance_mode;
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'video', type => 1, cb_prefix_output => 'prefix_video_output', message_multiple => 'All videos are ok' },
+        { name => 'audio', type => 1, cb_prefix_output => 'prefix_audio_output', message_multiple => 'All audios are ok' },
+    ];
+    
+    $self->{maps_counters}->{video} = [
+        { label => 'video-status', threshold => 0, set => {
+                key_values => [ { name => 'videoInputStatus' }, { name => 'display' } ],
+                closure_custom_calc => $self->can('custom_status_calc'), 
+                closure_custom_calc_extra_options => { output_label => 'Status', name_status => 'videoInputStatus' },
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => $self->can('custom_status_threshold'),
+            }
+        },
+    ];
+    $self->{maps_counters}->{audio} = [
+        { label => 'audio-status', threshold => 0, set => {
+                key_values => [ { name => 'videoInputGroupStatus' }, { name => 'display' } ],
+                closure_custom_calc => $self->can('custom_status_calc'), 
+                closure_custom_calc_extra_options => { output_label => 'Status', name_status => 'videoInputGroupStatus' },
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => $self->can('custom_status_threshold'),
+            }
+        },
+    ];
+}
+
+sub custom_status_threshold {
+    my ($self, %options) = @_;
+    my $status = 'ok';
+    my $message;
+
+    eval {
+        local $SIG{__WARN__} = sub { $message = $_[0]; };
+        local $SIG{__DIE__} = sub { $message = $_[0]; };
+
+        my $label = $self->{label};
+        $label =~ s/-/_/g;
+        if (defined($instance_mode->{option_results}->{'critical_' . $label}) && $instance_mode->{option_results}->{'critical_' . $label} ne '' &&
+            eval "$instance_mode->{option_results}->{'critical_' . $label}") {
+            $status = 'critical';
+        } elsif (defined($instance_mode->{option_results}->{'warning_' . $label}) && $instance_mode->{option_results}->{'warning_' . $label} ne '' &&
+                 eval "$instance_mode->{option_results}->{'warning_' . $label}") {
+            $status = 'warning';
+        }
+    };
+    if (defined($message)) {
+        $self->{output}->output_add(long_msg => 'filter status issue: ' . $message);
+    }
+
+    return $status;
+}
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+    my $msg = $self->{result_values}->{output_label} . ' : ' . $self->{result_values}->{status};
+
+    return $msg;
+}
+
+sub custom_status_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{output_label} = $options{extra_options}->{output_label};
+    $self->{result_values}->{status} = $options{new_datas}->{$self->{instance} . '_' . $options{extra_options}->{name_status}};
+    $self->{result_values}->{display} = $options{new_datas}->{$self->{instance} . '_display'};
+    return 0;
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "warning-audio-status:s"  => { name => 'warning_audio_status', default => '' },
+                                  "critical-audio-status:s" => { name => 'critical_audio_status', default => '%{status} =~ /loss/i' },
+                                  "warning-video-status:s"  => { name => 'warning_video_status', default => '' },
+                                  "critical-video-status:s" => { name => 'critical_video_status', default => '%{status} =~ /loss|unknown/i' },
+                                });
+    
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+    
+    $instance_mode = $self;
+    $self->change_macros();
+}
+
+sub change_macros {
+    my ($self, %options) = @_;
+    
+    foreach (('warning_audio_status', 'critical_audio_status', 'warning_video_status', 'critical_video_status')) {
+        if (defined($self->{option_results}->{$_})) {
+            $self->{option_results}->{$_} =~ s/%\{(.*?)\}/\$self->{result_values}->{$1}/g;
+        }
+    }
+}
+
+sub prefix_video_output {
+    my ($self, %options) = @_;
+    
+    return "Video '" . $options{instance_value}->{display} . "' ";
+}
+
+sub prefix_audio_output {
+    my ($self, %options) = @_;
+    
+    return "Audio '" . $options{instance_value}->{display} . "' ";
+}
+
+my %map_video_input_status = (1 => 'loss', 2 => 'unknown', 3 => 'p270', 4 => 'n270', 5 => 'sdtiP270',
+    6 => 'sdtiN270', 7 => 'std1080ix60', 8 => 'std1080ix5994', 9 => 'std1080ix50', 10 => 'std1035ix60',
+    11 => 'std1035ix5994', 12 => 'std1080ix48', 13 => 'std1080ix4796', 14 => 'std1080px24',
+    15 => 'std1080px2398', 16 => 'std1080px25', 17 => 'std1080px30', 18 => 'std1080px2997',
+    19 => 'std720px60', 20 => 'std720px5994',
+);
+my %map_video_input_group_status = (1 => 'free', 2 => 'used', 3 => 'clean', 4 => 'loss');
+
+my $mapping = {
+    videoInputStatus        => { oid => '.1.3.6.1.4.1.6827.10.138.4.1.1.1', map => \%map_video_input_status },
+};
+my $mapping2 = {
+    videoInputGroupStatus   => { oid => '.1.3.6.1.4.1.6827.10.138.4.2.1.2', map => \%map_video_input_group_status },
+};
+
+my $oid_monitorEntry = '.1.3.6.1.4.1.6827.10.138.4.1.1';
+my $oid_audioMonitorEntry = '.1.3.6.1.4.1.6827.10.138.4.2.1';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+    
+    my $snmp_result = $options{snmp}->get_multiple_table(oids => [ { oid => $oid_monitorEntry },
+                                                                   { oid => $oid_audioMonitorEntry },
+                                                                 ],
+                                                         nothing_quit => 1);
+    $self->{video} = {};
+    foreach my $oid (keys %{$snmp_result->{ $oid_monitorEntry }}) {
+        next if ($oid !~ /^$mapping->{videoInputStatus}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result->{ $oid_monitorEntry }, instance => $instance);
+        
+        $self->{video}->{$instance} = { 
+            display => $instance, %$result
+        };
+    }
+    
+    $self->{audio} = {};
+    foreach my $oid (keys %{$snmp_result->{ $oid_audioMonitorEntry }}) {
+        next if ($oid !~ /^$mapping2->{videoInputGroupStatus}->{oid}\.(.*?)\.(.*?)$/);
+        my ($audio_group_id, $instance) = ($1, $2);
+        my $result = $options{snmp}->map_instance(mapping => $mapping2, results => $snmp_result->{ $oid_audioMonitorEntry }, instance => $audio_group_id . '.' . $instance);
+        
+        $self->{audio}->{$audio_group_id . '.' . $instance} = { 
+            display => $instance . '.' . $audio_group_id, %$result
+        };
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check video/audio stream status.
+
+=over 8
+
+=item B<--warning-audio-status>
+
+Set warning threshold for device status.
+Can used special variables like: %{status}, %{display}
+
+=item B<--critical-audio-status>
+
+Set critical threshold for device status (Default: '%{status} =~ /loss/i').
+Can used special variables like: %{status}, %{display}
+
+=item B<--warning-video-status>
+
+Set warning threshold for device connection status.
+Can used special variables like: %{status}, %{display}
+
+=item B<--critical-video-status>
+
+Set critical threshold for device connection status (Default: '%{status} =~ /loss|unknown/i').
+Can used special variables like: %{status}, %{display}
+
+=back
+
+=cut
---
+++ network/evertz/FC7800/snmp/mode/hardware.pm
@@ -0,0 +1,188 @@
+#
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::evertz::FC7800::snmp::mode::hardware;
+
+use base qw(centreon::plugins::templates::hardware);
+
+sub set_system {
+    my ($self, %options) = @_;
+    
+    $self->{regexp_threshold_overload_check_section_option} = '^(psu|frameline)$';
+    
+    $self->{cb_hook2} = 'snmp_execute';
+    
+    $self->{thresholds} = {
+        psu => [
+            ['false', 'CRITICAL'],
+            ['true', 'OK'],
+            ['notAvailable', 'UNKNOWN'],
+        ],
+        frameline => [
+            ['false', 'OK'],
+            ['true', 'CRITICAL'],
+            ['notAvailable', 'UNKNOWN'],
+        ],
+    };
+    
+    $self->{components_path} = 'network::evertz::FC7800::snmp::mode::components';
+    $self->{components_module} = ['frameline', 'psu'];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, no_absent => 1, no_performance => 1, no_load_components => 1);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                });
+
+    return $self;
+}
+
+sub snmp_execute {
+    my ($self, %options) = @_;
+    
+    $self->{snmp} = $options{snmp};
+    $self->{results} = $self->{snmp}->get_leef(oids => $self->{request});
+}
+
+1;
+
+=head1 MODE
+
+Check hardware.
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: '.*').
+Can be: 'psu', 'frameline'.
+
+=item B<--filter>
+
+Exclude some parts (comma seperated list)
+Can also exclude specific instance: --filter=psu,1
+
+=item B<--no-component>
+
+Return an error if no compenents are checked.
+If total (with skipped) is 0. (Default: 'critical' returns).
+
+=item B<--threshold-overload>
+
+Set to overload default threshold values (syntax: section,[instance,]status,regexp)
+It used before default thresholds (order stays).
+Example: --threshold-overload='psu,OK,notAvailable'
+
+=back
+
+=cut
+
+package network::evertz::FC7800::snmp::mode::components::psu;
+
+use strict;
+use warnings;
+
+my %map_psu_status = (1 => 'false', 2 => 'true', 3 => 'notAvailable');
+
+my $mapping_psu = {
+    powerSupply1Status    => { oid => '.1.3.6.1.4.1.6827.10.232.4.3', map => \%map_psu_status },
+    powerSupply2Status    => { oid => '.1.3.6.1.4.1.6827.10.232.4.4', map => \%map_psu_status },
+};
+
+sub load {
+    my ($self) = @_;
+
+    push @{$self->{request}}, $mapping_psu->{powerSupply1Status}->{oid} . '.0', $mapping_psu->{powerSupply2Status}->{oid} . '.0';
+}
+
+sub check_psu {
+    my ($self, %options) = @_;
+    
+    return if (!defined($options{status}));
+    return if ($self->check_filter(section => 'psu', instance => $options{instance}));
+    
+    $self->{components}->{psu}->{total}++;
+    $self->{output}->output_add(long_msg => sprintf("power supply '%s' status is '%s' [instance = %s]",
+                                                    $options{instance}, $options{status}, $options{instance}));
+    my $exit = $self->get_severity(section => 'psu', value => $options{status});
+    if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+        $self->{output}->output_add(severity => $exit,
+                                    short_msg => sprintf("Power supply '%s' status is '%s'", $options{instance}, $options{status}));
+    }
+}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking poer supplies");
+    $self->{components}->{psu} = {name => 'psus', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'psu'));
+
+    my $result = $self->{snmp}->map_instance(mapping => $mapping_psu, results => $self->{results}, instance => '0');
+
+    check_psu($self, status => $result->{powerSupply1Status}, instance => 1);
+    check_psu($self, status => $result->{powerSupply2Status}, instance => 2);
+}
+
+1;
+
+package network::evertz::FC7800::snmp::mode::components::frameline;
+
+use strict;
+use warnings;
+
+my %map_frameline_status = (1 => 'false', 2 => 'true', 3 => 'notAvailable');
+
+my $mapping_frameline = {
+    frameStatusLine => { oid => '.1.3.6.1.4.1.6827.10.232.4.2', map => \%map_frameline_status },
+};
+
+sub load {
+    my ($self) = @_;
+
+    push @{$self->{request}}, $mapping_frameline->{frameStatusLine}->{oid} . '.0';
+}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking frame line");
+    $self->{components}->{frameline} = {name => 'frameline', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'frameline'));
+
+    my $result = $self->{snmp}->map_instance(mapping => $mapping_frameline, results => $self->{results}, instance => '0');
+
+    return if (!defined($result->{frameStatusLine}));
+    $self->{components}->{frameline}->{total}++;
+    $self->{output}->output_add(long_msg => sprintf("frame line status is '%s' [instance = %s]",
+                                                    $result->{frameStatusLine}, '0'));
+    my $exit = $self->get_severity(section => 'frameline', value => $result->{frameStatusLine});
+    if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+        $self->{output}->output_add(severity => $exit,
+                                    short_msg => sprintf("Frame line status is '%s'", $result->{frameStatusLine}));
+    }
+}
+
+1;
