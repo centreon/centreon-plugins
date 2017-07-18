c46a5558aee623a299ea2b0809279350d889c30d
qgarnier <qgarnier@centreon.com>
2017-07-18 16:49:49 +0200

    add list-interfaces mode

--- centreon-plugins/network/adva/fsp3000/snmp/plugin.pm
+++ centreon-plugins/network/adva/fsp3000/snmp/plugin.pm
@@ -31,8 +31,9 @@ sub new {
 
     $self->{version} = '1.0';
     %{$self->{modes}} = (
-                         'alarms'       => 'network::adva::fsp3000::snmp::mode::alarms',
-                         'interfaces'   => 'network::adva::fsp3000::snmp::mode::interfaces',
+                         'alarms'           => 'network::adva::fsp3000::snmp::mode::alarms',
+                         'interfaces'       => 'network::adva::fsp3000::snmp::mode::interfaces',
+                         'list-interfaces'  => 'snmp_standard::mode::listinterfaces',
                          );
 
     return $self;
