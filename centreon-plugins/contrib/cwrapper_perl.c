ddc754f2b7dd757b77f15fe0bda0387471f23744
qgarnier <qgarnier@centreon.com>
2017-08-11 16:44:20 +0200

    fix exit status

--- centreon-plugins/contrib/cwrapper_perl.c
+++ centreon-plugins/contrib/cwrapper_perl.c
@@ -13,14 +13,17 @@ EXTERN_C void xs_init(pTHX)
 }
 
 int main (int argc, char **argv, char **env)
-{    
+{
+    int exitstatus = 0;
+
     PERL_SYS_INIT3(&argc, &argv, &env);
     my_perl = perl_alloc();
     perl_construct(my_perl);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
     perl_parse(my_perl, xs_init, argc, argv, (char **)NULL);
-    perl_run(my_perl);
+    exitstatus = perl_run(my_perl);
     perl_destruct(my_perl);
     perl_free(my_perl);
     PERL_SYS_TERM();
+    exit(exitstatus);
 }
