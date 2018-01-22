c8e92fe9802493ea410e2e8ec13d03479bb71496
qgarnier <qgarnier@centreon.com>
2018-01-22 17:41:54 +0100

    Fix #863

--- centreon-plugins/apps/backup/veeam/local/mode/jobstatus.pm
+++ centreon-plugins/apps/backup/veeam/local/mode/jobstatus.pm
@@ -243,6 +243,8 @@ sub manage_selection {
         my ($name, $type, $is_running, $result, $start_time, $end_time) = (centreon::plugins::misc::trim($1), 
             centreon::plugins::misc::trim($2), centreon::plugins::misc::trim($3), centreon::plugins::misc::trim($4), 
             centreon::plugins::misc::trim($5), centreon::plugins::misc::trim($6));
+        $start_time =~ s/,/\./;
+        $end_time =~ s/,/\./;
         
         if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' &&
             $name !~ /$self->{option_results}->{filter_name}/) {
