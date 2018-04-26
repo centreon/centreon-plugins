baa8b8602c080ce7445f2ac78058f499dda776b4
cgagnaire <cgagnaire@centreon.com>
2018-04-26 13:32:17 +0000

    refactor riverbed steelhead (#971)
    * Closes https://github.com/centreon/centreon-plugins/issues/970

--- network/riverbed/steelhead/snmp/mode/loadaverage.pm
+++ network/riverbed/steelhead/snmp/mode/loadaverage.pm
@@ -18,105 +18,130 @@
 # limitations under the License.
 #
 
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 package network::riverbed::steelhead::snmp::mode::loadaverage;
 
-use base qw(centreon::plugins::mode);
+use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'load', type => 0, cb_prefix_output => 'prefix_load_output' },
+    ];
+
+    $self->{maps_counters}->{load} = [
+        { label => 'average', set => {
+                key_values => [ { name => 'cpuUtil1' } ],
+                output_template => 'average: %d%%',
+                perfdatas => [
+                    { label => 'total_cpu_avg', value => 'cpuUtil1_absolute', template => '%d',
+                      min => 0, max => 100, unit => '%' },
+                ],
+            }
+        },
+        { label => '1min', set => {
+                key_values => [ { name => 'cpuLoad1' } ],
+                output_template => '1 min: %d%%',
+                perfdatas => [
+                    { label => 'load1', value => 'cpuLoad1_absolute', template => '%d',
+                      min => 0, max => 100, unit => '%' },
+                ],
+            }
+        },
+        { label => '5min', set => {
+                key_values => [ { name => 'cpuLoad5' } ],
+                output_template => '5 min: %d%%',
+                perfdatas => [
+                    { label => 'load5', value => 'cpuLoad5_absolute', template => '%d',
+                       min => 0, max => 100, unit => "%" },
+                ],
+            }
+        },
+        { label => '15min', set => {
+                key_values => [ { name => 'cpuLoad15' } ],
+                output_template => '15 min: %d%%',
+                perfdatas => [
+                    { label => 'load15', value => 'cpuLoad15_absolute', template => '%d',
+                       min => 0, max => 100, unit => '%' },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_load_output {
+    my ($self, %options) = @_;
+
+    return "Load ";
+}
 
 sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
 
-    $self->{version} = '1.0';
+    $self->{version} = '0.1';
     $options{options}->add_options(arguments =>
                                 {
-                                  "warning:s"   => { name => 'warning', default => '' },
-                                  "critical:s"  => { name => 'critical', default => '' }
                                 });
-
     return $self;
 }
 
-sub check_options {
+sub manage_selection {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
 
-    ($self->{warn1}, $self->{warn5}, $self->{warn15}) = split /,/, $self->{option_results}->{warning};
-    ($self->{crit1}, $self->{crit5}, $self->{crit15}) = split /,/, $self->{option_results}->{critical};
+    # STEELHEAD-MIB
+    my $oids = {
+        cpuUtil1  => '.1.3.6.1.4.1.17163.1.1.5.1.1.0',
+        cpuLoad1  => '.1.3.6.1.4.1.17163.1.1.5.1.2.0',
+        cpuLoad5  => '.1.3.6.1.4.1.17163.1.1.5.1.3.0',
+        cpuLoad15 => '.1.3.6.1.4.1.17163.1.1.5.1.4.0',
+    };
 
-    if (($self->{perfdata}->threshold_validate(label => 'warn1', value => $self->{warn1})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning (1min) threshold '" . $self->{warn1} . "'.");
-        $self->{output}->option_exit();
+    # STEELHEAD-EX-MIB
+    my $oids_ex = {
+        cpuUtil1  => '.1.3.6.1.4.1.17163.1.51.5.1.1.0',
+        cpuLoad1  => '.1.3.6.1.4.1.17163.1.51.5.1.2.0',
+        cpuLoad5  => '.1.3.6.1.4.1.17163.1.51.5.1.3.0',
+        cpuLoad15 => '.1.3.6.1.4.1.17163.1.51.5.1.4.0',
+    };
+
+    my $snmp_result = $options{snmp}->get_leef(oids => [ values %{$oids}, values %{$oids_ex} ], nothing_quit => 1);
+
+    $self->{load} = {};
+    
+    if (defined($snmp_result->{$oids->{cpuUtil1}})) {
+        foreach (keys %{$oids}) {
+            $self->{load}->{$_} = $snmp_result->{$oids->{$_}};
+        }
+    } else {
+        foreach (keys %{$oids}) {
+            $self->{load}->{$_} = $snmp_result->{$oids_ex->{$_}};
+        }
     }
-    if (($self->{perfdata}->threshold_validate(label => 'warn5', value => $self->{warn5})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning (5min) threshold '" . $self->{warn5} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'warn15', value => $self->{warn15})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning (15min) threshold '" . $self->{warn15} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'crit1', value => $self->{crit1})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical (1min) threshold '" . $self->{crit1} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'crit5', value => $self->{crit5})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical (5min) threshold '" . $self->{crit5} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'crit15', value => $self->{crit15})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical (15min) threshold '" . $self->{crit15} . "'.");
-        $self->{output}->option_exit();
-    }
-}
-
-sub run {
-    my ($self, %options) = @_;
-    $self->{snmp} = $options{snmp};
-
-    my $oid_cpuLoad1 = '.1.3.6.1.4.1.17163.1.1.5.1.1.0';
-    my $oid_cpuLoad5 = '.1.3.6.1.4.1.17163.1.1.5.1.2.0';
-    my $oid_cpuLoad15 = '.1.3.6.1.4.1.17163.1.1.5.1.3.0';
-
-    my $result = $self->{snmp}->get_leef(oids => [$oid_cpuLoad1, $oid_cpuLoad5, $oid_cpuLoad15], nothing_quit => 1);
-
-    my $cpu_load1 = $result->{$oid_cpuLoad1} / 100;
-    my $cpu_load5 = $result->{$oid_cpuLoad5} / 100;
-    my $cpu_load15 = $result->{$oid_cpuLoad15} / 100;
-
-    my $msg = sprintf("Load average: %.2f (1min), %.2f (5min), %.2f (15min)", $cpu_load1, $cpu_load5, $cpu_load15);
-
-    $self->{output}->perfdata_add(label => 'load1',
-                              value => sprintf("%.2f", $cpu_load1),
-                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn1'),
-                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit1'),
-                              min => 0);
-    $self->{output}->perfdata_add(label => 'load5',
-                              value => sprintf("%.2f", $cpu_load5),
-                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn5'),
-                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit5'),
-                              min => 0);
-    $self->{output}->perfdata_add(label => 'load15',
-                              value => sprintf("%.2f", $cpu_load15),
-                              warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn15'),
-                              critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit15'),
-                              min => 0);
-
-    my $exit1 = $self->{perfdata}->threshold_check(value => $cpu_load1,
-                                                   threshold => [ { label => 'crit1', exit_litteral => 'critical' }, { label => 'warn1', exit_litteral => 'warning' } ]);
-    my $exit2 = $self->{perfdata}->threshold_check(value => $cpu_load5,
-                                                   threshold => [ { label => 'crit5', exit_litteral => 'critical' }, { label => 'warn5', exit_litteral => 'warning' } ]);
-    my $exit3 = $self->{perfdata}->threshold_check(value => $cpu_load15,
-                                                   threshold => [ { label => 'crit15', exit_litteral => 'critical' }, { label => 'warn15', exit_litteral => 'warning' } ]);
-    my $exit = $self->{output}->get_most_critical(status => [ $exit1, $exit2, $exit3 ]);
-    $self->{output}->output_add(severity => $exit,
-                                short_msg => $msg);
-
-    $self->{output}->display();
-    $self->{output}->exit();
 }
 
 1;
@@ -129,13 +154,15 @@ Check system load-average.
 
 =over 8
 
-=item B<--warning>
+=item B<--warning-*>
 
-Threshold warning (1min,5min,15min).
+Warning thresholds
+Can be --warning-(average|1m|5m|15m) 
 
-=item B<--critical>
+=item B<--critical-*>
 
-Threshold critical (1min,5min,15min).
+Critical thresholds
+Can be --critical-(average|1m|5m|15m)
 
 =back
 
