acf4c36ac64c69479f297f2b6ad9442b89c86267
Laurent Kappel <lkappel@centreon.com>
2018-08-13 19:25:46 +0200

    use the /ReceivingMember option if the value is not empty, otherwise continue without

--- apps/activedirectory/local/mode/dfsrbacklog.pm
+++ apps/activedirectory/local/mode/dfsrbacklog.pm
@@ -76,15 +76,10 @@ sub check_options {
     
     $self->{option_results}->{command_options} .= '/SendingMember:' . $self->{option_results}->{sending_member} . ' ';
     
-    if (defined($self->{option_results}->{receiving_member})) {
-        if ($self->{option_results}->{receiving_member} eq '') {
-            $self->{output}->add_option_msg(short_msg => "Need to specify receiving-member value.");
-            $self->{output}->option_exit();
-        }
-        else {
-            $self->{option_results}->{command_options} .= '/ReceivingMember:' . $self->{option_results}->{receiving_member} . ' ';
-        }
-    }
+    $self->{option_results}->{command_options} .= '/ReceivingMember:' . $self->{option_results}->{receiving_member} . ' '
+        if ((defined($self->{option_results}->{receiving_member}))
+            and ($self->{option_results}->{receiving_member} ne '')
+        );
     
     if (!defined($self->{option_results}->{replication_group}) || $self->{option_results}->{replication_group} eq '') {
         $self->{output}->add_option_msg(short_msg => "Need to specify replication-group option.");
