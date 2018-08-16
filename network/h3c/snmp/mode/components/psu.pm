6e433f98fb05aa55e0f0511b3f0642102b774278
Colin Gagnaire <cgagnaire@centreon.com>
2018-08-16 15:14:10 +0000

    enh h3c hardware mode to allow count thresholds (#1080)
    * enh h3c hardware mode to allow count thresholds
    
    * + use ternary operator

--- network/h3c/snmp/mode/components/psu.pm
+++ network/h3c/snmp/mode/components/psu.pm
@@ -52,7 +52,9 @@ sub check {
             next;
         }
         
-        my $name = $self->get_long_name(instance => $instance);
+        my $name = '';
+        $name = $self->get_short_name(instance => $instance) if (defined($self->{short_name}) && $self->{short_name} == 1);
+        $name = $self->get_long_name(instance => $instance) unless (defined($self->{short_name}) && $self->{short_name} == 1 && defined($name) && $name ne '');
         $self->{components}->{psu}->{total}++;
         $self->{output}->output_add(long_msg => sprintf("Power supply '%s' status is '%s' [instance = %s]",
                                                         $name, $result->{EntityExtErrorStatus}, $instance));
