595e2464521c3bf63a8042d08f4fa407a4ee6417
Colin Gagnaire <cgagnaire@centreon.com>
2018-08-23 13:30:47 +0200

    refactor huawei ibmc

--- hardware/server/huawei/ibmc/snmp/mode/components/memory.pm
+++ hardware/server/huawei/ibmc/snmp/mode/components/memory.pm
@@ -1,63 +1,75 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 package hardware::server::huawei::ibmc::snmp::mode::components::memory;
 
 use strict;
 use warnings;
 
-sub check {
+my %map_status = (
+    1 => 'ok',
+    2 => 'minor',
+    3 => 'major',
+    4 => 'critical',
+    5 => 'absence',
+    6 => 'unknown',
+);
+
+my $mapping = {
+    memoryStatus               => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.16.50.1.6', map => \%map_status },
+    memoryDevicename           => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.16.50.1.10' },
+};
+my $oid_memoryDescriptionEntry = '.1.3.6.1.4.1.2011.2.235.1.1.16.50.1';
+
+sub load {
     my ($self) = @_;
     
-    my %statusHash = ("1"=>"ok","2"=>"warning","3"=>"warning","4"=>"critical","5"=>"unknown","6"=>"unknown");
-    my %eachStatus = ("1"=>"ok","2"=>"minor","3"=>"major","4"=>"critical","5"=>"absence","6"=>"unknown");     
-     
-    my $memoryStatusOid = ".1.3.6.1.4.1.2011.2.235.1.1.16.1.0";        
-    my $statusTableOid = ".1.3.6.1.4.1.2011.2.235.1.1.16.50.1.6";
-    my $deviceTableOid = ".1.3.6.1.4.1.2011.2.235.1.1.16.50.1.10";
-    
-    my $tmpShortMessage = "";
-    my $totalPresent = 0;
-    my $totalComponent = 0;
-    my $result = $self->{snmp}->get_table(oid => $statusTableOid);
-    if (scalar(keys %$result) <= 0)
-    {
-        $tmpShortMessage = $tmpShortMessage."No memory presence.";
-    }else 
-    {   
-        my $endKey;
-        my $temnameOid;
-        my $statusOid;
-        my $tmpMsg;
-        my $tmpresult;
-        my $v;
-        foreach my $k ($self->{snmp}->oid_lex_sort(keys %$result))
-        {
-            $v = $result->{$k};
-            $endKey = "0";
-            $temnameOid = "";
-            $statusOid = "";
-            $tmpMsg = "";
-            $totalComponent++;
-            if ($v ne "5")  # memory present  
-            {   
-                $totalPresent++;
-                $k =~ /\.([0-9]+)$/;
-                $endKey = $1 ;
-                $temnameOid = $deviceTableOid.".".$endKey;
-                $statusOid = $statusTableOid.".".$endKey;
-                $tmpresult = $self->{snmp}->get_leef(oids =>[$temnameOid]);
-                $tmpMsg = $tmpresult->{$temnameOid}.":";
-                $tmpresult = $self->{snmp}->get_leef(oids =>[$statusOid]);
-                $tmpMsg = $tmpMsg.$eachStatus{$tmpresult->{$statusOid}}." ";
-                $tmpShortMessage = $tmpShortMessage.$tmpMsg;
-            }
-        }       
-    } 
-    $result = $self->{snmp}->get_leef(oids =>[$memoryStatusOid]);
-    $tmpShortMessage = "memoryPresence:".$totalPresent."\/".$totalComponent." ".$tmpShortMessage;
-
-    $self->{output}->output_add(severity => $statusHash{$result-> {$memoryStatusOid}},
-                                short_msg => $tmpShortMessage );
-    
-    $self->{output}->display(); 
-    $self->{output}->exit(); 
+    push @{$self->{request}}, { oid => $oid_memoryDescriptionEntry };
 }
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking memory");
+    $self->{components}->{memory} = {name => 'memorys', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'memory'));
+
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$oid_memoryDescriptionEntry}})) {
+        next if ($oid !~ /^$mapping->{memoryStatus}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$oid_memoryDescriptionEntry}, instance => $instance);
+
+        next if ($self->check_filter(section => 'memory', instance => $instance));
+        next if ($result->{memoryStatus} =~ /absence/);
+        $self->{components}->{memory}->{total}++;
+        
+        $self->{output}->output_add(long_msg => sprintf("Memory '%s' status is '%s' [instance = %s]",
+                                    $result->{memoryDevicename}, $result->{memoryStatus}, $instance, 
+                                    ));
+   
+        my $exit = $self->get_severity(label => 'default', section => 'memory', value => $result->{memoryStatus});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Memory '%s' status is '%s'", $result->{memoryDevicename}, $result->{memoryStatus}));
+        }
+    }
+}
+
 1;
--- hardware/server/huawei/ibmc/snmp/mode/components/psu.pm
+++ hardware/server/huawei/ibmc/snmp/mode/components/psu.pm
@@ -1,64 +1,103 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 package hardware::server::huawei::ibmc::snmp::mode::components::psu;
 
 use strict;
 use warnings;
 
-sub check {
+my %map_status = (
+    1 => 'ok',
+    2 => 'minor',
+    3 => 'major',
+    4 => 'critical',
+    5 => 'absence',
+    6 => 'unknown',
+);
+
+my %map_installation_status = (
+    1 => 'absence',
+    2 => 'presence',
+    3 => 'unknown',
+);
+
+my $mapping = {
+    powerSupplyPowerRating  => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1.6' },
+    powerSupplyStatus       => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1.7', map => \%map_status },
+    powerSupplyInputPower   => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1.8' },
+    powerSupplyPresence     => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1.9', map => \%map_installation_status },
+    powerSupplyDevicename   => { oid => '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1.13' },
+};
+my $oid_powerSupplyDescriptionEntry = '.1.3.6.1.4.1.2011.2.235.1.1.6.50.1';
+
+sub load {
     my ($self) = @_;
     
-    my %statusHash = ("1"=>"ok","2"=>"warning","3"=>"warning","4"=>"critical","5"=>"unknown","6"=>"unknown");
-    my %eachStatus = ("1"=>"ok","2"=>"minor","3"=>"major","4"=>"critical","5"=>"absence","6"=>"unknown");     
-     
-    my $psuStatueOid = ".1.3.6.1.4.1.2011.2.235.1.1.6.1.0";        
-    my $presentTableOid = ".1.3.6.1.4.1.2011.2.235.1.1.6.50.1.9";
-    my $statueTableOid = ".1.3.6.1.4.1.2011.2.235.1.1.6.50.1.7";
-    my $deviceTableOid = ".1.3.6.1.4.1.2011.2.235.1.1.6.50.1.13";
-    
-    my $tmpShortMessage = "";
-    my $totalPresent = 0;
-    my $totalComponent = 0;
-    my $result = $self->{snmp}->get_table(oid => $presentTableOid);
-    if (scalar(keys %$result) <= 0)
-    {
-        $tmpShortMessage = $tmpShortMessage."No psu presence.";
-    }else 
-    {   
-        my $endKey;
-        my $temnameOid;
-        my $statusOid;
-        my $tmpMsg;
-        my $tmpresult;
-        my $v;
-        foreach my $k ($self->{snmp}->oid_lex_sort(keys %$result))
-        {
-            $v = $result->{$k};
-            $endKey = "0";
-            $temnameOid = "";
-            $statusOid = "";
-            $tmpMsg = "";
-            $totalComponent++;
-            if ($v eq "2")  # psu present  
-            {                
-                $totalPresent++;
-                $k =~ /\.([0-9]+)$/;
-                $endKey = $1;
-                $temnameOid = $deviceTableOid.".".$endKey;
-                $statusOid = $statueTableOid.".".$endKey;
-                $tmpresult = $self->{snmp}->get_leef(oids =>[$temnameOid]);
-                $tmpMsg = $tmpresult->{$temnameOid}.":";
-                $tmpresult = $self->{snmp}->get_leef(oids =>[$statusOid]);
-                $tmpMsg = $tmpMsg.$eachStatus{$tmpresult->{$statusOid}}." ";
-                $tmpShortMessage=$tmpShortMessage.$tmpMsg;
-            }
-        }       
-    } 
-    $result = $self->{snmp}->get_leef(oids =>[$psuStatueOid]);
-    $tmpShortMessage = "psuPresence:".$totalPresent."\/".$totalComponent." ".$tmpShortMessage;
-    
-    $self->{output}->output_add(severity => $statusHash{$result-> {$psuStatueOid}},
-                                short_msg => $tmpShortMessage );
-    
-    $self->{output}->display(); 
-    $self->{output}->exit();    
+    push @{$self->{request}}, { oid => $oid_powerSupplyDescriptionEntry };
 }
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking power supplies");
+    $self->{components}->{psu} = {name => 'power supplies', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'psu'));
+
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$oid_powerSupplyDescriptionEntry}})) {
+        next if ($oid !~ /^$mapping->{powerSupplyStatus}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$oid_powerSupplyDescriptionEntry}, instance => $instance);
+
+        next if ($self->check_filter(section => 'psu', instance => $instance));
+        next if ($result->{powerSupplyPresence} !~ /presence/);
+        $self->{components}->{psu}->{total}++;
+
+        if (defined($result->{powerSupplyInputPower}) && $result->{powerSupplyInputPower} =~ /[0-9]/ &&
+            defined($result->{powerSupplyPowerRating}) && $result->{powerSupplyPowerRating} =~ /[0-9]/) {
+            my ($exit, $warn, $crit, $checked) = $self->get_severity_numeric(section => 'psu', instance => $instance, value => $result->{powerSupplyInputPower});
+            
+            if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+                $self->{output}->output_add(severity => $exit,
+                                            short_msg => sprintf("Power supply '%s' power is %s watts", $result->{powerSupplyDevicename}, $result->{powerSupplyInputPower}));
+            }
+            my $perf_label = $result->{powerSupplyDevicename};
+            $perf_label =~ s/ /_/g;
+            $self->{output}->perfdata_add(label => 'power_' . $perf_label, unit => 'W', 
+                                          value => $result->{powerSupplyInputPower},
+                                          warning => $warn,
+                                          critical => $crit,
+                                          min => 0,
+                                          max => $result->{powerSupplyPowerRating}
+                                          );
+        }
+        
+        $self->{output}->output_add(long_msg => sprintf("Power supply '%s' status is '%s' [instance = %s]",
+                                    $result->{powerSupplyDevicename}, $result->{powerSupplyStatus}, $instance, 
+                                    ));
+   
+        my $exit = $self->get_severity(label => 'default', section => 'psu', value => $result->{powerSupplyStatus});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Power supply '%s' status is '%s'", $result->{powerSupplyDevicename}, $result->{powerSupplyStatus}));
+        }
+    }
+}
+
 1;
