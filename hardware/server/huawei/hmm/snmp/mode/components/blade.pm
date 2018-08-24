729eec5c3e1362f430046751ec8b9a902f36a7be
Colin Gagnaire <cgagnaire@centreon.com>
2018-08-24 16:05:51 +0000

    refactor huawei hmm (#1113)

--- hardware/server/huawei/hmm/snmp/mode/components/blade.pm
+++ hardware/server/huawei/hmm/snmp/mode/components/blade.pm
@@ -1,51 +1,72 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 package hardware::server::huawei::hmm::snmp::mode::components::blade;
 
 use strict;
 use warnings;
 
-sub check {
+my %map_status = (
+    1 => 'normal',
+    2 => 'minor',
+    3 => 'major',
+    4 => 'critical',
+);
+
+my $mapping = {
+    mBladeLocation            => { oid => '.1.3.6.1.4.1.2011.2.82.1.82.100.3.2001.1.2' },
+    mBladeHealth              => { oid => '.1.3.6.1.4.1.2011.2.82.1.82.100.3.2001.1.3', map => \%map_status },
+};
+my $oid_bladeManagementEntry = '.1.3.6.1.4.1.2011.2.82.1.82.100.3.2001.1';
+
+sub load {
     my ($self) = @_;
     
-    my %statusHash = ("0"=>"ok","1"=>"warning","2"=>"warning","3"=>"warning","4"=>"critical","5"=>"critical","6"=>"critical","7"=>"critical");
-    my %eachStatus = ("0"=>"ok","1"=>"minor","2"=>"major","3"=>"major","4"=>"critical","5"=>"critical","6"=>"critical","7"=>"critical");
-    
-    my $componentPresentOid = sprintf ".1.3.6.1.4.1.2011.2.82.1.82.4.%s.6.0", $self->{option_results}->{bladeNum};
-    my $componentStatusOid = sprintf ".1.3.6.1.4.1.2011.2.82.1.82.4.%s.8.0", $self->{option_results}->{bladeNum};
-    my $componentTemperatureOid = sprintf ".1.3.6.1.4.1.2011.2.82.1.82.4.%s.2012.1.2.1", $self->{option_results}->{bladeNum};
-    my $tmpShortMessage = "";
-    my $tmpSeverity = "unknown";
-       
-    my $result= $self->{snmp}->get_leef(oids =>[$componentPresentOid]);
-    if (scalar(keys %$result) <= 0)
-    {
-        $tmpShortMessage = $tmpShortMessage."Get blade info error.";
-    }else 
-    {
-        if ($result->{$componentPresentOid} eq "0")
-        {
-            $tmpShortMessage = sprintf $tmpShortMessage."Blade%s not presence.", $self->{option_results}->{bladeNum};
-        }elsif ($result->{$componentPresentOid} eq "2")
-        {
-            $tmpShortMessage = sprintf $tmpShortMessage."Blade%s status indeterminate.", $self->{option_results}->{bladeNum};
-        }elsif ($result->{$componentPresentOid} eq "1") 
-        {   
-            $result= $self->{snmp}->get_leef(oids =>[$componentStatusOid]);
-            $tmpSeverity = $statusHash{$result->{$componentStatusOid}};
-            $tmpShortMessage = $tmpShortMessage. "Blade".$self->{option_results}->{bladeNum}.":".$eachStatus{$result->{$componentStatusOid}}; 
-            $result = $self->{snmp}->get_leef(oids =>[$componentTemperatureOid] );
-            $tmpShortMessage = $tmpShortMessage." Temperature:".$result->{$componentTemperatureOid}."℃"; 
-        }else
-        {
-            $tmpShortMessage = sprintf $tmpShortMessage."Blade%s status unknown.", $self->{option_results}->{bladeNum};
-        }
-    }
-    
-    $self->{output}->output_add(severity => $tmpSeverity,
-                                short_msg => $tmpShortMessage);
-    $self->{output}->display(); 
-    $self->{output}->exit();
+    push @{$self->{request}}, { oid => $oid_bladeManagementEntry };
 }
 
-1;    
-    
-    
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking blades");
+    $self->{components}->{blade} = {name => 'blades', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'blade'));
+
+    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %{$self->{results}->{$oid_bladeManagementEntry}})) {
+        next if ($oid !~ /^$mapping->{mBladeHealth}->{oid}\.(.*)$/);
+        my $instance = $1;
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $self->{results}->{$oid_bladeManagementEntry}, instance => $instance);
+
+        next if ($self->check_filter(section => 'blade', instance => $instance));
+        $self->{components}->{blade}->{total}++;
+        
+        $self->{output}->output_add(long_msg => sprintf("Blade '%s' status is '%s' [instance = %s]",
+                                    $result->{mBladeLocation}, $result->{mBladeHealth}, $instance, 
+                                    ));
+   
+        my $exit = $self->get_severity(label => 'default', section => 'blade', value => $result->{mBladeHealth});
+        if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+            $self->{output}->output_add(severity => $exit,
+                                        short_msg => sprintf("Blade '%s' status is '%s'", $result->{mBladeLocation}, $result->{mBladeHealth}));
+        }
+    }
+}
+
+1;
