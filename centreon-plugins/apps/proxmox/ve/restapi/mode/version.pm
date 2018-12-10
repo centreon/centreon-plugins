11142d458edea16e01f2f49f185bbe4f704288b4
Colin Gagnaire <cgagnaire@centreon.com>
2018-12-10 18:48:18 +0100

    add proxmox ve plugin (#1269)
    * First Release of Proxmox VE Plugins
    
    Discovery for Nodes, VMs (LXC & QEMU) and Storage
    Node Usage, VM Usage and Storage Usage.
    
    * Add Doc Specification about Realm and Privileges
    
    * Added API Ticket Cache Management
    
    * changes after code review

---
+++ centreon-plugins/apps/proxmox/ve/restapi/mode/version.pm
@@ -0,0 +1,64 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::proxmox::ve::restapi::mode::version;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub run {
+    my ($self, %options) = @_;
+    my $result = $options{custom}->get_version();
+        $self->{output}->output_add(severity => 'OK',
+                                    short_msg => "Version is '" . $result . "'");
+        $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
+        $self->{output}->exit();
+}
+
+1;
+
+=head1 MODE
+
+Get Proxmox VE Version
+
+=over 8
+
+=back
+
+=cut
---
+++ centreon-plugins/apps/proxmox/ve/restapi/plugin.pm
@@ -0,0 +1,71 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::proxmox::ve::restapi::plugin;
+
+use strict;
+use warnings;
+use base qw(centreon::plugins::script_custom);
+
+sub new {
+    my ($class, %options) = @_;
+
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    %{$self->{modes}} = (
+                            'list-nodes'        => 'apps::proxmox::ve::restapi::mode::listnodes',
+                            'list-storages'     => 'apps::proxmox::ve::restapi::mode::liststorages',
+                            'list-vms'          => 'apps::proxmox::ve::restapi::mode::listvms',
+                            'storage-usage'     => 'apps::proxmox::ve::restapi::mode::storageusage',
+                            'node-usage'        => 'apps::proxmox::ve::restapi::mode::nodeusage',
+                            'version'           => 'apps::proxmox::ve::restapi::mode::version',
+                            'vm-usage'          => 'apps::proxmox::ve::restapi::mode::vmusage',
+                         );
+
+    $self->{custom_modes}{api} = 'apps::proxmox::ve::restapi::custom::api';
+
+    return $self;
+}
+
+sub init {
+    my ($self, %options) = @_;
+
+    $self->SUPER::init(%options);
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check Proxmox VE ressources through its HTTPS remote API.
+
+API documentation can be checked at https://pve.proxmox.com/pve-docs/api-viewer/.
+
+Requirements: Proxmox VE 5.x.
+
+=over 8
+
+=back
+
+=cut
