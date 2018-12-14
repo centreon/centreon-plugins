a17cbf295dd8d3073c4e09931e3deb205b98a59e
Colin Gagnaire <cgagnaire@centreon.com>
2018-12-14 10:39:58 +0100

    add azure api options to azcli custommode

--- cloud/azure/custom/api.pm
+++ cloud/azure/custom/api.pm
@@ -151,7 +151,6 @@ sub build_options_for_httplib {
     $self->{option_results}->{ssl_opt} = $self->{ssl_opt};
     $self->{option_results}->{warning_status} = '';
     $self->{option_results}->{critical_status} = '';
-    $self->{option_results}->{unknown_status} = '';
 }
 
 sub settings {
