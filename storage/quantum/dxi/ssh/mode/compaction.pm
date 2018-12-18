b3bacfacbc2d610102268b02fbef3ffa955ef63d
Colin Gagnaire <cgagnaire@centreon.com>
2018-12-18 18:50:49 +0100

    add quantum dxi plugin

---
+++ storage/quantum/dxi/ssh/mode/compaction.pm
@@ -0,0 +1,314 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::quantum::dxi::ssh::mode::compaction;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+my $instance_mode;
+
+sub custom_status_threshold {
+    my ($self, %options) = @_; 
+    my $status = 'ok';
+    my $message;
+    
+    eval {
+        local $SIG{__WARN__} = sub { $message = $_[0]; };
+        local $SIG{__DIE__} = sub { $message = $_[0]; };
+        
+        if (defined($instance_mode->{option_results}->{critical_status}) && $instance_mode->{option_results}->{critical_status} ne '' &&
+            eval "$instance_mode->{option_results}->{critical_status}") {
+            $status = 'critical';
+        } elsif (defined($instance_mode->{option_results}->{warning_status}) && $instance_mode->{option_results}->{warning_status} ne '' &&
+            eval "$instance_mode->{option_results}->{warning_status}") {
+            $status = 'warning';
+        }
+    };
+    if (defined($message)) {
+        $self->{output}->output_add(long_msg => 'filter status issue: ' . $message);
+    }
+
+    return $status;
+}
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+    
+    my $msg = 'Compaction status: ' . $self->{result_values}->{compaction_status};
+    return $msg;
+}
+
+sub custom_status_calc {
+    my ($self, %options) = @_;
+    
+    $self->{result_values}->{compaction_status} = $options{new_datas}->{$self->{instance} . '_compaction_status'};
+    return 0;
+}
+
+sub custom_volume_perfdata {
+    my ($self, %options) = @_;
+    
+    $self->{output}->perfdata_add(label => $self->{result_values}->{label}, unit => 'B',
+                                  value => $self->{result_values}->{volume},
+                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{label}),
+                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{label})
+                                  );
+}
+
+sub custom_volume_threshold {
+    my ($self, %options) = @_;
+    
+    my $exit = $self->{perfdata}->threshold_check(value => $self->{result_values}->{volume},
+                                               threshold => [ { label => 'critical-' . $self->{label}, exit_litteral => 'critical' }, { label => 'warning-'. $self->{label}, exit_litteral => 'warning' } ]);
+    return $exit;
+}
+
+sub custom_volume_output {
+    my ($self, %options) = @_;
+    
+    my ($volume_value, $volume_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{volume});
+    my $msg = sprintf("%s volume: %s %s", $self->{result_values}->{display}, $volume_value, $volume_unit);
+    return $msg;
+}
+
+sub custom_volume_calc {
+    my ($self, %options) = @_;
+
+    my $raw_value = $options{new_datas}->{$self->{instance} . '_' . $options{extra_options}->{label_ref}};
+    my ($value, $unit) = split(/\s+/, $raw_value);
+    if ($unit =~ /kb*/i) {
+        $value = $value * 1024;
+    } elsif ($unit =~ /mb*/i) {
+        $value = $value * 1024 * 1024;
+    } elsif ($unit =~ /gb*/i) {
+        $value = $value * 1024 * 1024 * 1024;
+    } elsif ($unit =~ /tb*/i) {
+        $value = $value * 1024 * 1024 * 1024 * 1024;
+    }
+    $self->{result_values}->{volume} = $value;
+    $self->{result_values}->{display} = $options{extra_options}->{display_ref};
+    $self->{result_values}->{label} = $options{extra_options}->{label_ref};
+    
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 }
+    ];
+    
+    $self->{maps_counters}->{global} = [
+        { label => 'status', threshold => 0, set => {
+                key_values => [ { name => 'compaction_status' } ],
+                closure_custom_calc => $self->can('custom_status_calc'),
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => $self->can('custom_status_threshold'),
+            }
+        },
+        { label => 'status-progress', set => {
+                key_values => [ { name => 'status_progress' } ],
+                output_template => 'Status progress: %.2f %%',
+                perfdatas => [
+                    { label => 'status_progress', value => 'status_progress_absolute', template => '%.2f',
+                      unit => '%', min => 0, max => 100 },
+                ],
+            }
+        },
+        { label => 'compacted', set => {
+                key_values => [ { name => 'compacted' } ],
+                closure_custom_calc => $self->can('custom_volume_calc'),
+                closure_custom_calc_extra_options => { label_ref => 'compacted', display_ref => 'Compacted' },
+                closure_custom_output => $self->can('custom_volume_output'),
+                closure_custom_perfdata => $self->can('custom_volume_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_volume_threshold'),
+            }
+        },
+        { label => 'still-to-compact', set => {
+                key_values => [ { name => 'still_to_compact' } ],
+                closure_custom_calc => $self->can('custom_volume_calc'),
+                closure_custom_calc_extra_options => { label_ref => 'still_to_compact', display_ref => 'Still to compact' },
+                closure_custom_output => $self->can('custom_volume_output'),
+                closure_custom_perfdata => $self->can('custom_volume_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_volume_threshold'),
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "hostname:s"          => { name => 'hostname' },
+                                  "filter-name:s"       => { name => 'filter_name' },
+                                  "warning-status:s"    => { name => 'warning_status', default => '' },
+                                  "critical-status:s"   => { name => 'critical_status', default => '%{compaction_status} !~ /ready/i' },
+                                  "units:s"             => { name => 'units', default => '%' },
+                                  "free"                => { name => 'free' },
+                                  "ssh-option:s@"       => { name => 'ssh_option' },
+                                  "ssh-path:s"          => { name => 'ssh_path' },
+                                  "ssh-command:s"       => { name => 'ssh_command', default => 'ssh' },
+                                  "timeout:s"           => { name => 'timeout', default => 30 },
+                                  "sudo"                => { name => 'sudo' },
+                                  "command:s"           => { name => 'command', default => 'syscli' },
+                                  "command-path:s"      => { name => 'command_path' },
+                                  "command-options:s"   => { name => 'command_options', default => '--getstatus compaction' },
+                                });
+    
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+
+    if (defined($self->{option_results}->{hostname}) && $self->{option_results}->{hostname} ne '') {
+       $self->{output}->add_option_msg(short_msg => "Need to specify --hostname.");
+       $self->{output}->option_exit();
+    }
+
+    $instance_mode = $self;
+    $self->change_macros();
+}
+
+sub change_macros {
+    my ($self, %options) = @_;
+    
+    foreach (('warning_status', 'critical_status')) {
+        if (defined($self->{option_results}->{$_})) {
+            $self->{option_results}->{$_} =~ s/%\{(.*?)\}/\$self->{result_values}->{$1}/g;
+        }
+    }
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{global} = {};
+
+    my ($stdout, $exit_code) = centreon::plugins::misc::execute(output => $self->{output},
+                                                                options => $self->{option_results},
+                                                                sudo => $self->{option_results}->{sudo},
+                                                                command => $self->{option_results}->{command},
+                                                                command_path => $self->{option_results}->{command_path},
+                                                                command_options => $self->{option_results}->{command_options},
+                                                                );
+    # Output data:
+    #    Compaction Status = Ready
+    #    Status Progress = 0 %
+    #    Start Time =
+    #    End Time =
+    #    Compacted = 12.00 MB
+    #    Still to compact = 123.00 MB
+    #
+    # Command completed successfully.
+
+    foreach (split(/\n/, $stdout)) {
+        $self->{global}->{compaction_status} = $1 if ($_ =~ /^\s+Compaction\sStatus\s=\s(.*)$/i);
+        $self->{global}->{status_progress} = $1 if ($_ =~ /^\s+Status\sProgress\s=\s(.*)\s%$/i);
+        $self->{global}->{compacted} = $1 if ($_ =~ /^\s+Compacted\s=\s(.*)$/i);
+        $self->{global}->{still_to_compact} = $1 if ($_ =~ /^\s+Still\sto\scompact\s=\s(.*)$/i);
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check compaction status and volumes.
+
+=over 8
+
+=item B<--hostname>
+
+Hostname to query.
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: --filter-counters='status'
+
+=item B<--warning-status>
+
+Set warning threshold for status (Default: '').
+Can used special variables like: %{compaction_status}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '%{compaction_status} !~ /ready/i').
+Can used special variables like: %{compaction_status}
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'status-progress', 'compacted', 'still-to-compact'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'status-progress', 'compacted', 'still-to-compact'.
+
+=item B<--ssh-option>
+
+Specify multiple options like the user (example: --ssh-option='-l=centreon-engine' --ssh-option='-p=52').
+
+=item B<--ssh-path>
+
+Specify ssh command path (default: none)
+
+=item B<--ssh-command>
+
+Specify ssh command (default: 'ssh'). Useful to use 'plink'.
+
+=item B<--timeout>
+
+Timeout in seconds for the command (Default: 30).
+
+=item B<--sudo>
+
+Use 'sudo' to execute the command.
+
+=item B<--command>
+
+Command to get information (Default: 'syscli').
+
+=item B<--command-path>
+
+Command path.
+
+=item B<--command-options>
+
+Command options (Default: '--getstatus compaction').
+
+=back
+
+=cut
