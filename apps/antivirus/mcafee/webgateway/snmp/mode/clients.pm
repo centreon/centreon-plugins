2874e21a49d2f11e614d688b6a0faca7d57e92a0
Colin Gagnaire <cgagnaire@centreon.com>
2019-01-02 12:06:24 +0100

    add mcafee web gateway plugin

---
+++ apps/antivirus/mcafee/webgateway/snmp/mode/clients.pm
@@ -0,0 +1,120 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::antivirus::mcafee::webgateway::snmp::mode::clients;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use Digest::MD5 qw(md5_hex);
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 },
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'clients', set => {
+                key_values => [ { name => 'stClientCount' } ],
+                output_template => 'Connected clients: %d',
+                perfdatas => [
+                    { label => 'connected_clients', value => 'stClientCount_absolute', template => '%d',
+                      min => 0, unit => 'clients' },
+                ],
+            }
+        },
+        { label => 'sockets', set => {
+                key_values => [ { name => 'stConnectedSockets' } ],
+                output_template => 'Open network sockets: %d',
+                perfdatas => [
+                    { label => 'open_sockets', value => 'stConnectedSockets_absolute', template => '%d',
+                      min => 0, unit => 'sockets' },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                    "filter-counters:s" => { name => 'filter_counters', default => '' },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+}
+
+my $oid_stClientCount = '.1.3.6.1.4.1.1230.2.7.2.5.2.0';
+my $oid_stConnectedSockets = '.1.3.6.1.4.1.1230.2.7.2.5.3.0';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $results = $options{snmp}->get_leef(oids => [ $oid_stClientCount, $oid_stConnectedSockets ], 
+                                           nothing_quit => 1);
+    
+    $self->{global} = {};
+
+    $self->{global} = {
+        stClientCount => $results->{$oid_stClientCount},
+        stConnectedSockets => $results->{$oid_stConnectedSockets},
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check connected clients and open network sockets.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+(Example: --filter-counters='clients')
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'clients', 'sockets'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'clients', 'sockets'.
+
+=back
+
+=cut
---
+++ apps/antivirus/mcafee/webgateway/snmp/mode/httpsstatistics.pm
@@ -0,0 +1,182 @@
+#
+# Copyright 2018 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::antivirus::mcafee::webgateway::snmp::mode::httpsstatistics;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use Digest::MD5 qw(md5_hex);
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 },
+        { name => 'traffics', type => 0, cb_prefix_output => 'prefix_traffic_output' },
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'requests', set => {
+                key_values => [ { name => 'stHttpsRequests', diff => 1 } ],
+                output_template => 'HTTPS Requests (per sec): %d',
+                per_second => 1,
+                perfdatas => [
+                    { label => 'https_requests', value => 'stHttpsRequests_per_second', template => '%d',
+                      min => 0, unit => 'requests/s' },
+                ],
+            }
+        },
+    ];
+    $self->{maps_counters}->{traffics} = [
+        { label => 'client-to-proxy', set => {
+                key_values => [ { name => 'stHttpsBytesFromClient', diff => 1 } ],
+                output_template => 'from client to proxy: %s %s/s',
+                output_change_bytes => 2,
+                per_second => 1,
+                perfdatas => [
+                    { label => 'https_traffic_client_to_proxy', value => 'stHttpsBytesFromClient_per_second', template => '%d',
+                      min => 0, unit => 'b/s' },
+                ],
+            }
+        },
+        { label => 'server-to-proxy', set => {
+                key_values => [ { name => 'stHttpsBytesFromServer', diff => 1 } ],
+                output_template => 'from server to proxy: %s %s/s',
+                output_change_bytes => 2,
+                per_second => 1,
+                perfdatas => [
+                    { label => 'https_traffic_server_to_proxy', value => 'stHttpsBytesFromServer_per_second', template => '%d',
+                      min => 0, unit => 'b/s' },
+                ],
+            }
+        },
+        { label => 'proxy-to-client', set => {
+                key_values => [ { name => 'stHttpsBytesToClient', diff => 1 } ],
+                output_template => 'from proxy to client: %s %s/s',
+                output_change_bytes => 2,
+                per_second => 1,
+                perfdatas => [
+                    { label => 'https_traffic_proxy_to_client', value => 'stHttpsBytesToClient_per_second', template => '%d',
+                      min => 0, unit => 'b/s' },
+                ],
+            }
+        },
+        { label => 'proxy-to-server', set => {
+                key_values => [ { name => 'stHttpsBytesToServer', diff => 1 } ],
+                output_template => 'from proxy to server: %s %s/s',
+                output_change_bytes => 2,
+                per_second => 1,
+                perfdatas => [
+                    { label => 'https_traffic_proxy_to_server', value => 'stHttpsBytesToServer_per_second', template => '%d',
+                      min => 0, unit => 'b/s' },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_traffic_output {
+    my ($self, %options) = @_;
+
+    return "HTTPS Traffic ";
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    $options{options}->add_options(arguments =>
+                                {
+                                    "filter-counters:s" => { name => 'filter_counters', default => '' },
+                                });
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+}
+
+my $oid_stHttpsRequests = '.1.3.6.1.4.1.1230.2.7.2.3.1.0';
+my $oid_stHttpsBytesFromClient = '.1.3.6.1.4.1.1230.2.7.2.3.3.0';
+my $oid_stHttpsBytesFromServer = '.1.3.6.1.4.1.1230.2.7.2.3.4.0';
+my $oid_stHttpsBytesToClient = '.1.3.6.1.4.1.1230.2.7.2.3.5.0';
+my $oid_stHttpsBytesToServer = '.1.3.6.1.4.1.1230.2.7.2.3.6.0';
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{cache_name} = "mcafee_" . $options{snmp}->get_hostname()  . '_' . $options{snmp}->get_port() . '_' . $self->{mode} . '_' .
+        (defined($self->{option_results}->{filter_name}) ? md5_hex($self->{option_results}->{filter_name}) : md5_hex('all')) . '_' .
+        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all'));
+
+    my $results = $options{snmp}->get_leef(oids => [ $oid_stHttpsRequests, $oid_stHttpsBytesFromClient,
+                                                     $oid_stHttpsBytesFromServer, $oid_stHttpsBytesToClient,
+                                                     $oid_stHttpsBytesToServer ], 
+                                           nothing_quit => 1);
+    
+    $self->{global} = {};
+    $self->{traffics} = {};
+
+    $self->{global} = {
+        stHttpsRequests => $results->{$oid_stHttpsRequests},
+    };
+    $self->{traffics} = {
+        stHttpsBytesFromClient => $results->{$oid_stHttpsBytesFromClient} * 8,
+        stHttpsBytesFromServer => $results->{$oid_stHttpsBytesFromServer} * 8,
+        stHttpsBytesToClient => $results->{$oid_stHttpsBytesToClient} * 8,
+        stHttpsBytesToServer => $results->{$oid_stHttpsBytesToServer} * 8,
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check HTTPS statistics.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+(Example: --filter-counters='^proxy')
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'request', 'client-to-proxy', 'server-to-proxy',
+'proxy-to-client', 'proxy-to-server'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'request', 'client-to-proxy', 'server-to-proxy',
+'proxy-to-client', 'proxy-to-server'.
+
+=back
+
+=cut
