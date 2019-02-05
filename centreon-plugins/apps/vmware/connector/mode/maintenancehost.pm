703a47977067c677b1a036f044f1f5696f1358cf
garnier-quentin <garnier.quentin@gmail.com>
2019-02-05 10:27:05 +0100

    WIP: centreon connector 3.0 vmware

--- centreon-plugins/apps/vmware/connector/mode/maintenancehost.pm
+++ centreon-plugins/apps/vmware/connector/mode/maintenancehost.pm
@@ -20,10 +20,72 @@
 
 package apps::vmware::connector::mode::maintenancehost;
 
-use base qw(centreon::plugins::mode);
+use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold);
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+
+    my $msg = 'status ' . $self->{result_values}->{status};
+    return $msg;
+}
+
+sub custom_status_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{status} = $options{new_datas}->{$self->{instance} . '_state'};
+    return 0;
+}
+
+sub custom_maintenance_output {
+    my ($self, %options) = @_;
+
+    my $msg = 'maintenance mode is ' . $self->{result_values}->{maintenance};
+    return $msg;
+}
+
+sub custom_maintenance_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{maintenance} = $options{new_datas}->{$self->{instance} . '_maintenance'};
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'host', type => 1, cb_prefix_output => 'prefix_host_output', message_multiple => 'All ESX Hosts are ok' },
+    ];
+    
+    $self->{maps_counters}->{host} = [
+        { label => 'status', threshold => 0, set => {
+                key_values => [ { name => 'state' } ],
+                closure_custom_calc => $self->can('custom_status_calc'),
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => \&catalog_status_threshold,
+            }
+        },
+        { label => 'maintenance-status', threshold => 0, set => {
+                key_values => [ { name => 'maintenance' } ],
+                closure_custom_calc => $self->can('custom_maintenance_calc'),
+                closure_custom_output => $self->can('custom_maintenance_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => \&catalog_status_threshold,
+            }
+        },
+    ];
+}
+
+sub prefix_host_output {
+    my ($self, %options) = @_;
+
+    return "Host '" . $options{instance_value}->{display} . "' : ";
+}
 
 sub new {
     my ($class, %options) = @_;
@@ -31,40 +93,44 @@ sub new {
     bless $self, $class;
     
     $self->{version} = '1.0';
-    $options{options}->add_options(arguments =>
-                                {
-                                  "esx-hostname:s"          => { name => 'esx_hostname' },
-                                  "filter"                  => { name => 'filter' },
-                                  "scope-datacenter:s"      => { name => 'scope_datacenter' },
-                                  "scope-cluster:s"         => { name => 'scope_cluster' },
-                                  "disconnect-status:s"     => { name => 'disconnect_status', default => 'unknown' },
-                                  "maintenance-alert:s"     => { name => 'maintenance_alert', default => '^(?!(false))' },
-                                  "maintenance-status:s"    => { name => 'maintenance_status', default => 'critical' },
-                                });
+    $options{options}->add_options(arguments => {
+        "esx-hostname:s"        => { name => 'esx_hostname' },
+        "filter"                => { name => 'filter' },
+        "scope-datacenter:s"    => { name => 'scope_datacenter' },
+        "scope-cluster:s"       => { name => 'scope_cluster' },
+        "unknown-status:s"      => { name => 'unknown_status', default => '%{status} !~ /^connected$/i' },
+        "warning-status:s"      => { name => 'warning_status', default => '' },
+        "critical-status:s"     => { name => 'critical_status', default => '' },
+        "warning-maintenance-status:s"  => { name => 'warning_maintenance_status', default => '' },
+        "critical-maintenance-status:s" => { name => 'critical_maintenance_status', default => '%{maintenance} !~ /false/' },
+    });
+    
     return $self;
 }
 
 sub check_options {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
+    $self->SUPER::check_options(%options);
     
-    if ($self->{output}->is_litteral_status(status => $self->{option_results}->{disconnect_status}) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong disconnect-status status option '" . $self->{option_results}->{disconnect_status} . "'.");
-        $self->{output}->option_exit();
-    }
-    if ($self->{output}->is_litteral_status(status => $self->{option_results}->{maintenance_status}) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong maintenance-status status option '" . $self->{option_results}->{maintenance_status} . "'.");
-        $self->{output}->option_exit();
-    }
+    $self->change_macros(macros => ['unknown_status', 'warning_status', 'critical_status',
+        'warning_maintenance_status', 'critical_maintenance_status']);
 }
 
-sub run {
+sub manage_selection {
     my ($self, %options) = @_;
-    $self->{connector} = $options{custom};
 
-    $self->{connector}->add_params(params => $self->{option_results},
-                                   command => 'maintenancehost');
-    $self->{connector}->run();
+    $self->{host} = {};
+    my $response = $options{custom}->execute(params => $self->{option_results},
+        command => 'maintenancehost');
+
+    foreach my $host_id (keys %{$response->{data}}) {
+        my $host_name = $response->{data}->{$host_id}->{name};
+        $self->{host}->{$host_name} = {
+            display => $host_name, 
+            state => $response->{data}->{$host_id}->{state},
+            maintenance => $response->{data}->{$host_id}->{inMaintenanceMode},
+        };
+    }    
 }
 
 1;
@@ -94,17 +160,31 @@ Search in following datacenter(s) (can be a regexp).
 
 Search in following cluster(s) (can be a regexp).
 
-=item B<--disconnect-status>
+=item B<--unknown-status>
 
-Status if ESX host disconnected (default: 'unknown').
+Set warning threshold for status (Default: '%{status} !~ /^connected$/i').
+Can used special variables like: %{status}
 
-=item B<--maintenance-alert>
+=item B<--warning-status>
 
-Alert if maintenance mode value matches (default: '^(?!(false))').
+Set warning threshold for status (Default: '').
+Can used special variables like: %{status}
 
-=item B<--maintenance-status>
+=item B<--critical-status>
 
-Maintenance alert status (default: 'critical').
+Set critical threshold for status (Default: '').
+Can used special variables like: %{status}
+
+=item B<--warning-maintenance-status>
+
+Set warning threshold for status (Default: '').
+Can used special variables like: %{maintenance}
+
+=item B<--critical-maintenance-status>
+
+Set critical threshold for status (Default: '%{maintenance} !~ /false/').
+Can used special variables like: %{maintenance}
+
 
 =back
 
