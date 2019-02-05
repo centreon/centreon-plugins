027f9d3adb4c9170a5c5208347aceb43d9f0a1cf
garnier-quentin <garnier.quentin@gmail.com>
2019-02-05 15:02:46 +0100

    WIP: centreon connector 3.0 vmware

--- centreon-plugins/apps/vmware/connector/mode/statconnectors.pm
+++ centreon-plugins/apps/vmware/connector/mode/statconnectors.pm
@@ -20,14 +20,54 @@
 
 package apps::vmware::connector::mode::statconnectors;
 
-use base qw(centreon::plugins::mode);
+use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
+use Digest::MD5 qw(md5_hex);
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, skipped_code => { -10 => 1 } },
+        { name => 'container', type => 1, cb_prefix_output => 'prefix_container_output', message_multiple => 'All containers are ok' },
+    ];
+    
+    $self->{maps_counters}->{global} = [
+        { label => 'total-requests', set => {
+                key_values => [ { name => 'requests', diff => 1  } ],
+                output_template => 'Total %s requests',
+                perfdatas => [
+                    { label => 'requests', value => 'requests_absolute', template => '%s',
+                      min => 0, label_extra_instance => 1 },
+                ],
+            }
+        },
+    ];
+    
+    $self->{maps_counters}->{container} = [
+        { label => 'requests', set => {
+                key_values => [ { name => 'requests', diff => 1  } ],
+                output_template => '%s requests',
+                perfdatas => [
+                    { label => 'requests', value => 'requests_absolute', template => '%s',
+                      min => 0, label_extra_instance => 1 },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_container_output {
+    my ($self, %options) = @_;
+
+    return "Container '" . $options{instance_value}->{display} . "' : ";
+}
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
     bless $self, $class;
     
     $self->{version} = '1.0';
@@ -37,18 +77,24 @@ sub new {
     return $self;
 }
 
-sub check_options {
+sub manage_selection {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
-}
 
-sub run {
-    my ($self, %options) = @_;
-    $self->{connector} = $options{custom};
+    $self->{global} = { requests => 0 };
+    $self->{container} = {};
+    my $response = $options{custom}->execute(params => $self->{option_results},
+        command => 'stats');
 
-    $self->{connector}->add_params(params => $self->{option_results},
-                                   command => 'stats');
-    $self->{connector}->run();
+    foreach my $container_name (keys %{$response->{data}}) {
+        $self->{container}->{$container_name} = {
+            display => $container_name,
+            requests => $response->{data}->{$container_name}->{requests}
+        };
+        $self->{global}->{requests} += $response->{data}->{$container_name}->{requests};
+    }
+    
+    $self->{cache_name} = "cache_vmware_" . $options{custom}->get_id() . '_' . $self->{mode} . '_' .
+        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all'));
 }
 
 1;
@@ -60,6 +106,16 @@ __END__
 Get number of requests for each connectors (information from daemon. Not VMWare).
 
 =over 8
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'total-requests', 'requests'.
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'total-requests', 'requests'.
 
 =back
 
