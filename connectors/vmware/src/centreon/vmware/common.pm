a15a25e99694571996ecdbcb7e06c46a96a83fe3
garnier-quentin <garnier.quentin@gmail.com>
2019-02-06 14:39:33 +0100

    add vmware connector version in response

--- connectors/vmware/src/centreon/vmware/common.pm
+++ connectors/vmware/src/centreon/vmware/common.pm
@@ -45,6 +45,7 @@ sub init_response {
     my (%options) = @_;
     
     $manager_response->{code} = 0;
+    $manager_response->{vmware_connector_version} = '3.0.0';
     $manager_response->{short_message} = 'OK';
     $manager_response->{extra_message} = '';
     $manager_response->{identity} = $options{identity} if (defined($options{identity}));
