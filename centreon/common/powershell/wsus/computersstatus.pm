4a9118004d36e8fb7437f0b5ae9ebfd27477533e
Colin Gagnaire <cgagnaire@centreon.com>
2019-03-21 14:12:48 +0100

    add wsus powershell plugin

---
+++ centreon/common/powershell/wsus/computersstatus.pm
@@ -0,0 +1,86 @@
+#
+# Copyright 2019 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package centreon::common::powershell::wsus::computersstatus;
+
+use strict;
+use warnings;
+
+sub get_powershell {
+    my (%options) = @_;
+    my $no_ps = (defined($options{no_ps})) ? 1 : 0;
+    
+    return '' if ($no_ps == 1);
+
+    my $ps = '
+$culture = new-object "System.Globalization.CultureInfo" "en-us"    
+[System.Threading.Thread]::CurrentThread.CurrentUICulture = $culture
+
+$wsusServer = "' . $options{wsus_server} . '"
+$useSsl = ' . $options{secure_connection} . '
+$wsusPort = ' . $options{wsus_port} . '
+$notUpdatedSince = ' . $options{not_updated_since} . '
+
+Try {
+    [void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") 
+} Catch {
+    Write-Host $Error[0].Exception
+    exit 1
+}
+
+$ProgressPreference = "SilentlyContinue"
+
+Try {
+    $ErrorActionPreference = "Stop"
+
+    $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer($wsusServer, $useSsl, $wsusPort)
+
+    $notUpdatedSinceTimespan = new-object TimeSpan($notUpdatedSince, 0, 0, 0)
+    $computersNotContactedSinceCount = $wsus.GetComputersNotContactedSinceCount([DateTime]::UtcNow.Subtract($notUpdatedSinceTimespan))
+
+    $computerTargetScope = new-object Microsoft.UpdateServices.Administration.ComputerTargetScope
+    $unassignedComputersCount = $wsus.GetComputerTargetGroup([Microsoft.UpdateServices.Administration.ComputerTargetGroupId]::UnassignedComputers).GetComputerTargets().Count
+
+    $status = $wsus.GetStatus()
+    Write-Host "[ComputerTargetsNeedingUpdatesCount = "$status.ComputerTargetsNeedingUpdatesCount"]" -NoNewline
+    Write-Host "[ComputerTargetsWithUpdateErrorsCount = "$status.ComputerTargetsWithUpdateErrorsCount"]" -NoNewline
+    Write-Host "[ComputersUpToDateCount = "$status.ComputersUpToDateCount"]" -NoNewline
+    Write-Host "[ComputersNotContactedSinceCount = "$computersNotContactedSinceCount"]" -NoNewline
+    Write-Host "[UnassignedComputersCount = "$unassignedComputersCount"]"
+} Catch {
+    Write-Host $Error[0].Exception
+    exit 1
+}
+
+exit 0
+';
+
+    return centreon::plugins::misc::powershell_encoded($ps);
+}
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+Method to get WSUS computers informations.
+
+=cut
