290492972929f06f16b0a0fd05d35c5747b0481e
Colin Gagnaire <cgagnaire@centreon.com>
2019-04-12 16:43:06 +0200

    add version in centreon_vmware.pm

--- connectors/vmware/src/centreon/script/centreon_vmware.pm
+++ connectors/vmware/src/centreon/script/centreon_vmware.pm
@@ -54,7 +54,7 @@ BEGIN {
 use base qw(centreon::vmware::script);
 use vars qw(%centreon_vmware_config);
 
-my $VERSION = "3.0.0";
+my $VERSION = "3.0.1";
 my %handlers = (TERM => {}, HUP => {}, CHLD => {});
 
 my @load_modules = (
