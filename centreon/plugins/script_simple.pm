a354a0e913f20ef177064356e40e83827a4ba423
garnier-quentin <garnier.quentin@gmail.com>
2019-04-16 17:01:22 +0200

    Fix #1435

--- centreon/plugins/script_simple.pm
+++ centreon/plugins/script_simple.pm
@@ -34,14 +34,14 @@ sub new {
     $self->{output} = $options{output};
     
     $self->{options}->add_options(
-                                   arguments => {
-                                                'mode:s'         => { name => 'mode_name' },
-                                                'dyn-mode:s'     => { name => 'dynmode_name' },
-                                                'list-mode'      => { name => 'list_mode' },
-                                                'mode-version:s' => { name => 'mode_version' },
-                                                'sanity-options' => { name => 'sanity_options' }, # keep it for 6 month before remove it
-                                                }
-                                  );
+    arguments => {
+            'mode:s'         => { name => 'mode_name' },
+            'dyn-mode:s'     => { name => 'dynmode_name' },
+            'list-mode'      => { name => 'list_mode' },
+            'mode-version:s' => { name => 'mode_version' },
+            'sanity-options' => { name => 'sanity_options' }, # keep it for 6 month before remove it
+        }
+    );
     $self->{version} = '1.0';
     %{$self->{modes}} = ();
     $self->{default} = undef;
@@ -55,6 +55,7 @@ sub new {
 
     $self->{options}->add_help(package => $options{package}, sections => 'PLUGIN DESCRIPTION');
     $self->{options}->add_help(package => __PACKAGE__, sections => 'GLOBAL OPTIONS');
+    $self->{output}->mode(name => $self->{mode_name});
 
     return $self;
 }
--- centreon/plugins/script_snmp.pm
+++ centreon/plugins/script_snmp.pm
@@ -36,15 +36,15 @@ sub new {
     $self->{output} = $options{output};
     
     $self->{options}->add_options(
-                                   arguments => {
-                                                'mode:s'         => { name => 'mode_name' },
-                                                'dyn-mode:s'     => { name => 'dynmode_name' },
-                                                'list-mode'      => { name => 'list_mode' },
-                                                'mode-version:s' => { name => 'mode_version' },
-                                                'sanity-options' => { name => 'sanity_options' }, # keep it for 6 month before remove it
-                                                'pass-manager:s' => { name => 'pass_manager' },
-                                                }
-                                  );
+        arguments => {
+            'mode:s'         => { name => 'mode_name' },
+            'dyn-mode:s'     => { name => 'dynmode_name' },
+            'list-mode'      => { name => 'list_mode' },
+            'mode-version:s' => { name => 'mode_version' },
+            'sanity-options' => { name => 'sanity_options' }, # keep it for 6 month before remove it
+            'pass-manager:s' => { name => 'pass_manager' },
+        }
+    );
     $self->{version} = '1.0';
     %{$self->{modes}} = ();
     $self->{default} = undef;
@@ -58,6 +58,7 @@ sub new {
 
     $self->{options}->add_help(package => $options{package}, sections => 'PLUGIN DESCRIPTION');
     $self->{options}->add_help(package => __PACKAGE__, sections => 'GLOBAL OPTIONS');
+    $self->{output}->mode(name => $self->{mode_name});
 
     return $self;
 }
@@ -87,7 +88,6 @@ sub init {
     # SNMP
     $self->{snmp} = centreon::plugins::snmp->new(options => $self->{options}, output => $self->{output});
 
-    
     # Load mode
     if (defined($self->{mode_name}) && $self->{mode_name} ne '') {
         $self->is_mode(mode => $self->{mode_name});
