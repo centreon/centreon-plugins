9ecbd62a24dd14b12efd25bf35646ba6baa63d94
garnier-quentin <garnier.quentin@gmail.com>
2019-07-03 13:44:43 +0200

    enhance hp bladechassis snmp

--- centreon-plugins/hardware/server/hp/bladechassis/snmp/mode/components/blade.pm
+++ centreon-plugins/hardware/server/hp/bladechassis/snmp/mode/components/blade.pm
@@ -37,72 +37,70 @@ my %present_map = (
     4 => 'Weird!!!', # for blades it can return 4, which is NOT spesified in MIB
 );
 
+my $mapping = {
+    blade_name      => { oid => '.1.3.6.1.4.1.232.22.2.4.1.1.1.4' }, # cpqRackServerBladeName
+    blade_part      => { oid => '.1.3.6.1.4.1.232.22.2.4.1.1.1.6' }, # cpqRackServerBladePartNumber
+    blade_status    => { oid => '.1.3.6.1.4.1.232.22.2.3.1.3.1.11', map => \%map_conditions  }, # cpqRackServerBladeStatus (v2)
+    blade_spare     => { oid => '.1.3.6.1.4.1.232.22.2.4.1.1.1.21' }, # cpqRackServerBladeSparePartNumber
+    blade_productid => { oid => '.1.3.6.1.4.1.232.22.2.4.1.1.1.17' }, # cpqRackServerBladeProductId
+    blade_diago     => { oid => '.1.3.6.1.4.1.232.22.2.4.1.1.1.24' }, # cpqRackServerBladeFaultDiagnosticString (v2)
+};
+
 sub check {
     my ($self) = @_;
 
     $self->{components}->{blade} = {name => 'blades', total => 0, skip => 0};
-    $self->{output}->output_add(long_msg => "Checking blades");
-    return if ($self->check_exclude(section => 'blade'));
+    $self->{output}->output_add(long_msg => "checking blades");
+    return if ($self->check_filter(section => 'blade'));
     
     my $oid_cpqRackServerBladePresent = '.1.3.6.1.4.1.232.22.2.4.1.1.1.12';
-    my $oid_cpqRackServerBladeIndex = '.1.3.6.1.4.1.232.22.2.4.1.1.1.3';
-    my $oid_cpqRackServerBladeName = '.1.3.6.1.4.1.232.22.2.4.1.1.1.4';
-    my $oid_cpqRackServerBladePartNumber = '.1.3.6.1.4.1.232.22.2.4.1.1.1.6';
-    my $oid_cpqRackServerBladeSparePartNumber = '.1.3.6.1.4.1.232.22.2.4.1.1.1.7';
-    my $oid_cpqRackServerBladeProductId = '.1.3.6.1.4.1.232.22.2.4.1.1.1.17';
-    my $oid_cpqRackServerBladeStatus = '.1.3.6.1.4.1.232.22.2.4.1.1.1.21'; # v2
-    my $oid_cpqRackServerBladeFaultDiagnosticString = '.1.3.6.1.4.1.232.22.2.4.1.1.1.24'; # v2
     
-    my $result = $self->{snmp}->get_table(oid => $oid_cpqRackServerBladePresent);
-    return if (scalar(keys %$result) <= 0);
+    my $snmp_result = $self->{snmp}->get_table(oid => $oid_cpqRackServerBladePresent);
+    return if (scalar(keys %$snmp_result) <= 0);
     my @get_oids = ();
     my @oids_end = ();
-    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$result)) {       
+    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$snmp_result)) {       
         $key =~ /\.([0-9]+)$/;
         my $oid_end = $1;
         
-        next if ($present_map{$result->{$key}} ne 'present' && 
+        next if ($present_map{$snmp_result->{$key}} ne 'present' && 
                  $self->absent_problem(section => 'blade', instance => $oid_end));
         
         push @oids_end, $oid_end;
-        push @get_oids, $oid_cpqRackServerBladeIndex . "." . $oid_end, $oid_cpqRackServerBladeName . "." . $oid_end,
-                $oid_cpqRackServerBladePartNumber . "." . $oid_end, $oid_cpqRackServerBladeSparePartNumber . "." . $oid_end,
-                $oid_cpqRackServerBladeProductId . "." . $oid_end, 
-                $oid_cpqRackServerBladeStatus . "." . $oid_end, $oid_cpqRackServerBladeFaultDiagnosticString . "." . $oid_end;
+        push @get_oids, map($_->{oid} . '.' . $oid_end, values(%$mapping));
     }
 
-    $result = $self->{snmp}->get_leef(oids => \@get_oids);
+    $snmp_result = $self->{snmp}->get_leef(oids => \@get_oids);
     foreach (@oids_end) {
-        my $blade_index = $result->{$oid_cpqRackServerBladeIndex . '.' . $_};
-        my $blade_status = defined($result->{$oid_cpqRackServerBladeStatus . '.' . $_}) ? $result->{$oid_cpqRackServerBladeStatus . '.' . $_} : '';
-        my $blade_name = $result->{$oid_cpqRackServerBladeName . '.' . $_};
-        my $blade_part = $result->{$oid_cpqRackServerBladePartNumber . '.' . $_};
-        my $blade_spare = $result->{$oid_cpqRackServerBladeSparePartNumber . '.' . $_};
-        my $blade_productid = $result->{$oid_cpqRackServerBladeProductId . '.' . $_};
-        my $blade_diago = defined($result->{$oid_cpqRackServerBladeFaultDiagnosticString . '.' . $_}) ? $result->{$oid_cpqRackServerBladeFaultDiagnosticString . '.' . $_} : '';
-        
-        next if ($self->check_exclude(section => 'blade', instance => $blade_index));
+        my $blade_index = $_;
+
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => $blade_index);
+        next if ($self->check_filter(section => 'blade', instance => $blade_index));
         
         $self->{components}->{blade}->{total}++;
-        if ($blade_status eq '') {
-            $self->{output}->output_add(long_msg => sprintf("Skipping Blade %d (%s, %s). Cant get status.",
-                                        $blade_index, $blade_name, $blade_productid));
+        if (!defined($result->{blade_status})) {
+            $self->{output}->output_add(long_msg => sprintf("skipping blade '%s' (%s, %s). Cant get status.",
+                                        $blade_index, $result->{blade_name}, $result->{blade_productid}));
             next;
         }
         
-        $self->{output}->output_add(long_msg => sprintf("Blade %d (%s, %s) status is %s [part: %s, spare: %s]%s.",
-                                    $blade_index, $blade_name, $blade_productid,
-                                    $map_conditions{$blade_status},
-                                    $blade_part, $blade_spare,
-                                    ($blade_diago ne '') ? " (Diagnostic '$blade_diago')" : ''
-                                    ));
-        my $exit = $self->get_severity(section => 'blade', value => $map_conditions{$blade_status});
+        $self->{output}->output_add(
+            long_msg => sprintf("blade '%s' (%s, %s) status is %s [part: %s, spare: %s]%s.",
+                $blade_index, $result->{blade_name}, $result->{blade_productid},
+                $result->{blade_status},
+                $result->{blade_part}, $result->{blade_spare},
+                defined($result->{blade_diago}) ? " (Diagnostic '$result->{blade_diago}')" : ''
+            )
+        );
+        my $exit = $self->get_severity(label => 'default', section => 'blade', instance => $blade_index, value => $result->{blade_status});
         if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
-            $self->{output}->output_add(severity => $exit,
-                                        short_msg => sprintf("Blade %d (%s, %s) status is %s",
-                                            $blade_index, $blade_name, $blade_productid,
-                                            $map_conditions{$blade_status}
-                                       ));
+            $self->{output}->output_add(
+                severity => $exit,
+                short_msg => sprintf("Blade '%s' (%s, %s) status is %s",
+                    $blade_index, $result->{blade_name}, $result->{blade_productid},
+                    $result->{blade_status}
+                )
+            );
         }
     }
 }
--- centreon-plugins/hardware/server/hp/bladechassis/snmp/mode/components/fan.pm
+++ centreon-plugins/hardware/server/hp/bladechassis/snmp/mode/components/fan.pm
@@ -37,51 +37,51 @@ my %present_map = (
     4 => 'Weird!!!', # for blades it can return 4, which is NOT spesified in MIB
 );
 
+my $mapping = {
+    fan_part        => { oid => '.1.3.6.1.4.1.232.22.2.3.1.3.1.6' }, # cpqRackCommonEnclosureFanPartNumber
+    fan_spare       => { oid => '.1.3.6.1.4.1.232.22.2.3.1.3.1.7' }, # cpqRackCommonEnclosureFanSparePartNumber
+    fan_condition   => { oid => '.1.3.6.1.4.1.232.22.2.3.1.3.1.11', map => \%map_conditions  }, # cpqRackCommonEnclosureFanCondition
+};
+
 sub check {
     my ($self) = @_;
 
     $self->{components}->{fan} = {name => 'fans', total => 0, skip => 0};
-    $self->{output}->output_add(long_msg => "Checking fans");
-    return if ($self->check_exclude(section => 'fan'));
+    $self->{output}->output_add(long_msg => "checking fans");
+    return if ($self->check_filter(section => 'fan'));
     
     my $oid_cpqRackCommonEnclosureFanPresent = '.1.3.6.1.4.1.232.22.2.3.1.3.1.8';
-    my $oid_cpqRackCommonEnclosureFanIndex = '.1.3.6.1.4.1.232.22.2.3.1.3.1.3';
-    my $oid_cpqRackCommonEnclosureFanPartNumber = '.1.3.6.1.4.1.232.22.2.3.1.3.1.6';
-    my $oid_cpqRackCommonEnclosureFanSparePartNumber = '.1.3.6.1.4.1.232.22.2.3.1.3.1.7';
-    my $oid_cpqRackCommonEnclosureFanCondition = '.1.3.6.1.4.1.232.22.2.3.1.3.1.11';
     
-    my $result = $self->{snmp}->get_table(oid => $oid_cpqRackCommonEnclosureFanPresent);
-    return if (scalar(keys %$result) <= 0);
+    my $snmp_result = $self->{snmp}->get_table(oid => $oid_cpqRackCommonEnclosureFanPresent);
+    return if (scalar(keys %$snmp_result) <= 0);
     my @get_oids = ();
     my @oids_end = ();
-    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$result)) {
+    foreach my $key ($self->{snmp}->oid_lex_sort(keys %$snmp_result)) {
         $key =~ /\.([0-9]+)$/;
         my $oid_end = $1;
         
-        next if ($present_map{$result->{$key}} ne 'present' && 
+        next if ($present_map{$snmp_result->{$key}} ne 'present' && 
                  $self->absent_problem(section => 'fan', instance => $oid_end));
         
         push @oids_end, $oid_end;
-        push @get_oids, $oid_cpqRackCommonEnclosureFanIndex . "." . $oid_end, $oid_cpqRackCommonEnclosureFanPartNumber . "." . $oid_end,
-                $oid_cpqRackCommonEnclosureFanSparePartNumber . "." . $oid_end, $oid_cpqRackCommonEnclosureFanCondition . "." . $oid_end;
+        push @get_oids, map($_->{oid} . '.' . $oid_end, values(%$mapping));
     }
 
-    $result = $self->{snmp}->get_leef(oids => \@get_oids);
+    $snmp_result = $self->{snmp}->get_leef(oids => \@get_oids);
     foreach (@oids_end) {
-        my $fan_index = $result->{$oid_cpqRackCommonEnclosureFanIndex . '.' . $_};
-        my $fan_condition = $result->{$oid_cpqRackCommonEnclosureFanCondition . '.' . $_};
-        my $fan_part = $result->{$oid_cpqRackCommonEnclosureFanPartNumber . '.' . $_};
-        my $fan_spare = $result->{$oid_cpqRackCommonEnclosureFanSparePartNumber . '.' . $_};
-        
-        next if ($self->check_exclude(section => 'fan', instance => $fan_index));
+        my $fan_index = $_;
+
+        my $result = $self->{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => $fan_index);
+        next if ($self->check_filter(section => 'fan', instance => $fan_index));
+
         $self->{components}->{fan}->{total}++;
-        $self->{output}->output_add(long_msg => sprintf("Fan %d condition is %s [part: %s, spare: %s].", 
-                                    $fan_index, $map_conditions{$fan_condition},
-                                    $fan_part, $fan_spare));
-        my $exit = $self->get_severity(section => 'fan', value => $map_conditions{$fan_condition});
+        $self->{output}->output_add(long_msg => sprintf("fan '%s' condition is %s [part: %s, spare: %s].", 
+                                    $fan_index, $result->{fan_condition},
+                                    $result->{fan_part}, $result->{fan_spare}));
+        my $exit = $self->get_severity(label => 'default', section => 'fan', instance => $fan_index, value => $result->{fan_condition});
         if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
             $self->{output}->output_add(severity => $exit,
-                                        short_msg => sprintf("Fan %d condition is %s", $fan_index, $map_conditions{$fan_condition}));
+                                        short_msg => sprintf("Fan '%s' condition is %s", $fan_index, $result->{fan_condition}));
         }
     }
 }
