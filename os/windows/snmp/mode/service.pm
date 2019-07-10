a7393fe4b093ed77cae598980ce19623e9b735b8
garnier-quentin <garnier.quentin@gmail.com>
2019-07-10 16:56:25 +0200

    enhance performance snmp service

--- os/windows/snmp/mode/service.pm
+++ os/windows/snmp/mode/service.pm
@@ -82,7 +82,7 @@ sub run {
     my $oid_svSvcName  = '.1.3.6.1.4.1.77.1.2.3.1.1';
     my $oid_svSvcInstalledState  = '.1.3.6.1.4.1.77.1.2.3.1.2';
     my $oid_svSvcOperatingState  = '.1.3.6.1.4.1.77.1.2.3.1.3';
-    my $result = $self->{snmp}->get_table(oid => $oid_svSvcEntry);
+    my $result = $self->{snmp}->get_table(oid => $oid_svSvcEntry, start => $oid_svSvcName, end => $oid_svSvcOperatingState);
     
     my %services_match = ();
     $self->{output}->output_add(severity => 'OK',
