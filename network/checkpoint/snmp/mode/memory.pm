79874167598a4649634c9818f8c61b050b93a3f0
garnier-quentin <garnier.quentin@gmail.com>
2019-07-18 09:12:59 +0200

    Fix  #1589

--- network/checkpoint/snmp/mode/memory.pm
+++ network/checkpoint/snmp/mode/memory.pm
@@ -139,12 +139,14 @@ sub manage_selection {
         total => $results->{$oid_memTotalReal64},
     };
 
-    $self->{swap} = {
-        prct_used => ($results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64}) * 100 / ($results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64}),
-        used => $results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64},
-        free => $results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64} - ($results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64}),
-        total => $results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64}
-    };
+    if ($results->{$oid_memTotalVirtual64} > $results->{$oid_memTotalReal64}) {
+        $self->{swap} = {
+            prct_used => ($results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64}) * 100 / ($results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64}),
+            used => $results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64},
+            free => $results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64} - ($results->{$oid_memActiveVirtual64} - $results->{$oid_memActiveReal64}),
+            total => $results->{$oid_memTotalVirtual64} - $results->{$oid_memTotalReal64}
+        };
+    }
 
     $self->{malloc} = { failed_mallocs => $results->{$oid_fwKmemFailedAlloc} };
 }
