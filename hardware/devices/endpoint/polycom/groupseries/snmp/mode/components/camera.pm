16367857fb6126c5b3168aa33b69f1b38b936cc3
Fabien THEPAUT <fthepaut@centreon.com>
2019-08-07 13:05:21 -0400

    add-groupseries-polycom-endpoint

---
+++ hardware/devices/endpoint/polycom/groupseries/snmp/mode/components/camera.pm
@@ -0,0 +1,47 @@
+#
+# Copyright 2019 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package hardware::devices::endpoint::polycom::groupseries::snmp::mode::components::camera;
+
+use strict;
+use warnings;
+
+sub load {}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking cameras");
+    $self->{components}->{camera} = {name => 'camera', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'camera'));
+
+    return if (!defined($self->{results}->{hardwareCameraStatus}));
+    $self->{components}->{camera}->{total}++;
+    
+    $self->{output}->output_add(long_msg => sprintf("camera status is '%s'",
+                                                    $self->{results}->{hardwareCameraStatus}));
+    my $exit = $self->get_severity(label => 'default', section => 'camera', value => $self->{results}->{hardwareCameraStatus});
+    if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+        $self->{output}->output_add(severity => $exit,
+                                    short_msg => sprintf("Camera status is '%s'", $self->{results}->{hardwareCameraStatus}));
+    }
+}
+
+1;
