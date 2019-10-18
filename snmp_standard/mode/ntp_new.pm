8c9c9bb866007886c9a55f556d744bdfb156afbc
thibaults-centreon <tscheitenberger@centreon.com>
2019-10-18 14:14:12 +0200

    enh(plugin): test ntp_new.pm

---
+++ snmp_standard/mode/ntp_new.pm
@@ -0,0 +1,170 @@
+#
+# Copyright 2019 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package snmp_standard::mode::ntp_new;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::misc;
+use DateTime;
+use Data::Dumper;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'offset', type => 0 }
+    ];
+
+    $self->{maps_counters}->{offset} = [
+        { label => 'offset', nlabel => 'time.offset.seconds', set => {
+                key_values => [ { name => 'offset' } ],
+                output_template => 'Offset: %d second(s)',
+                perfdatas => [
+                    { label => 'offset', value => 'offset_absolute', template => '%d', unit => 's' },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments =>
+                                { 
+                                  "ntp-hostname:s"          => { name => 'ntp_hostname' },
+                                  "ntp-port:s"              => { name => 'ntp_port', default => 123 },
+                                  "warning:s"               => { name => 'warning' },
+                                  "critical:s"              => { name => 'critical' },
+                                  "timezone:s"              => { name => 'timezone' },
+                                });
+    return $self;
+}
+
+sub check_options {
+   my ($self, %options) = @_;
+   $self->SUPER::init(%options);
+   
+   if (defined($self->{option_results}->{ntp_hostname})) {
+        centreon::plugins::misc::mymodule_load(output => $self->{output}, module => 'Net::NTP',
+                                              error_msg => "Cannot load module 'Net::NTP'.");
+    }
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+    $self->{snmp} = $options{snmp};
+    $self->{offset} = {};
+
+    my ($ref_time, $distant_time);
+    my $oid_hrSystemDate = '.1.3.6.1.2.1.25.1.2.0';
+    my $result = $self->{snmp}->get_leef(oids => [ $oid_hrSystemDate ], nothing_quit => 1);
+    if (defined($self->{option_results}->{ntp_hostname}) && $self->{option_results}->{ntp_hostname} ne '') {
+        my %ntp;
+        
+        eval {
+            %ntp = Net::NTP::get_ntp_response($self->{option_results}->{ntp_hostname}, $self->{option_results}->{ntp_port});
+        };
+        if ($@) {
+            $self->{output}->output_add(severity => 'UNKNOWN',
+                                        short_msg => "Couldn't connect to ntp server: " . $@);
+            $self->{output}->display();
+            $self->{output}->exit();
+        }
+        
+        $ref_time = $ntp{'Transmit Timestamp'};
+    } else {
+        $ref_time = time();
+    }
+    
+    my @remote_date = unpack 'n C6 a C2', $result->{$oid_hrSystemDate};
+    my $timezone = 'UTC';
+    if (defined($self->{option_results}->{timezone}) && $self->{option_results}->{timezone} ne '') {
+        $timezone = $self->{option_results}->{timezone};
+    } elsif (defined($remote_date[9])) {
+        $timezone = sprintf("%s%02d%02d", $remote_date[7], $remote_date[8], $remote_date[9]); # format +0630
+    }
+    
+    my $tz = centreon::plugins::misc::set_timezone(name => $timezone);
+    my $dt = DateTime->new(
+      year       => $remote_date[0],
+      month      => $remote_date[1],
+      day        => $remote_date[2],
+      hour       => $remote_date[3],
+      minute     => $remote_date[4],
+      second     => $remote_date[5],
+      %$tz
+    );
+    $distant_time = $dt->epoch;
+    
+    my $offset = $distant_time - $ref_time;
+    my $remote_date_formated = sprintf("Local Time : %02d-%02d-%02dT%02d:%02d:%02d (%s)", $remote_date[0], $remote_date[1], $remote_date[2],
+                                       $remote_date[3], $remote_date[4], $remote_date[5], $timezone);
+    
+   
+    $self->{output}->output_add(short_msg => $remote_date_formated);
+      
+    my $formated_offset = sprintf("%d", $offset);
+    
+    $self->{offset} = { offset => $formated_offset, display => 0 };
+    #print Dumper($self);
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check time offset of server with ntp server. Use local time if ntp-host option is not set. 
+SNMP gives a date with second precision (no milliseconds). Time precision is not very accurate.
+Use threshold with (+-) 2 seconds offset (minimum).
+
+=over 8
+
+=item B<--warning>
+
+Threshold warning.
+
+=item B<--critical>
+
+Threshold critical.
+
+=item B<--ntp-hostname>
+
+Set the ntp hostname (if not set, localtime is used).
+
+=item B<--ntp-port>
+
+Set the ntp port (Default: 123).
+
+=item B<--timezone>
+
+Set the timezone of distant server. For Windows, you need to set it.
+Can use format: 'Europe/London' or '+0100'.
+
+=back
+
+=cut
