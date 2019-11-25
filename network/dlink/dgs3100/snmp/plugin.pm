13ee66243c30cde603cc8852a94cf7763fb119c5
garnier-quentin <garnier.quentin@gmail.com>
2019-11-25 12:48:20 +0100

    add uptime mode for dgs3100

--- network/dlink/dgs3100/snmp/plugin.pm
+++ network/dlink/dgs3100/snmp/plugin.pm
@@ -35,6 +35,7 @@ sub new {
         'hardware'         => 'network::dlink::dgs3100::snmp::mode::hardware',
         'interfaces'       => 'snmp_standard::mode::interfaces',
         'list-interfaces'  => 'snmp_standard::mode::listinterfaces',
+        'uptime'           => 'snmp_standard::mode::uptime',
     );
 
     return $self;
