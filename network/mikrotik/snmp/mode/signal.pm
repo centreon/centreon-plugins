ce2af04191723500f29bd6c611d93dee5f796f98
garnier-quentin <garnier.quentin@gmail.com>
2019-11-26 13:55:58 +0100

    update mikrotik signal code

--- network/mikrotik/snmp/mode/signal.pm
+++ network/mikrotik/snmp/mode/signal.pm
@@ -29,38 +29,34 @@ sub set_counters {
     my ($self, %options) = @_;
     
     $self->{maps_counters_type} = [
-        { name => 'signalstrength', type => 1, cb_prefix_output => 'prefix_mac_output', message_multiple => 'All signals are ok' },
-        { name => 'signalnoise', type => 1, cb_prefix_output => 'prefix_mac_output', message_multiple => 'All signals are ok' }
+        { name => 'wreg', type => 1, cb_prefix_output => 'prefix_wreg_output', message_multiple => 'All wireless registrations are ok', skipped_code => { -10 => 1 } },
     ];
     
-    $self->{maps_counters}->{signalstrength} = [
+    $self->{maps_counters}->{wreg} = [
         { label => 'rx-strength', set => {
-                key_values => [ { name => 'rx' }, { name => 'display' } ],
-                output_template => 'Signal Strength Rx : %s',
+                key_values => [ { name => 'mtxrWlRtabStrength' }, { name => 'display' } ],
+                output_template => 'signal strength Rx: %s dBm',
                 perfdatas => [
-                    { label => 'signal_rx', value => 'rx_absolute', template => '%s', 
-                      min => -90, max => -20, label_extra_instance => 1, instance_use => 'display_absolute' },
+                    { label => 'signal_rx', value => 'mtxrWlRtabStrength_absolute', template => '%s', unit => 'dBm',
+                      label_extra_instance => 1, instance_use => 'display_absolute' },
                 ],
             }
         },
         { label => 'tx-strength', set => {
-                key_values => [ { name => 'tx' }, { name => 'display' } ],
-                output_template => 'Signal Strength Tx : %s',
+                key_values => [ { name => 'mtxrWlRtabTxStrength' }, { name => 'display' } ],
+                output_template => 'signal strength Tx: %s dBm',
                 perfdatas => [
-                    { label => 'signal_tx', value => 'tx_absolute', template => '%s', 
-                      min => -90, max => -20, label_extra_instance => 1, instance_use => 'display_absolute' },
+                    { label => 'signal_tx', value => 'mtxrWlRtabTxStrength_absolute', template => '%s', unit => 'dBm',
+                      label_extra_instance => 1, instance_use => 'display_absolute' },
                 ],
             }
-        }
-    ];
-
-    $self->{maps_counters}->{signalnoise} = [
+        },
         { label => 'signal2noise', set => {
-                key_values => [ { name => 'regs2n' }, { name => 'display' } ],
-                output_template => 'Signal To Noise : %s',
+                key_values => [ { name => 'mtxrWlRtabSignalToNoise', no_value => 0 }, { name => 'display' } ],
+                output_template => 'signal to noise: %s dB',
                 perfdatas => [
-                    { label => 'signal_noise', value => 'regs2n_absolute', template => '%s', 
-                      min => 20, max => 80, label_extra_instance => 1, instance_use => 'display_absolute' },
+                    { label => 'signal_noise', value => 'mtxrWlRtabSignalToNoise_absolute', template => '%s', unit => 'dB',
+                      label_extra_instance => 1, instance_use => 'display_absolute' },
                 ],
             }
         }
@@ -69,98 +65,53 @@ sub set_counters {
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
-    
-    $options{options}->add_options(arguments =>
-                                {
-                                    "strength"  =>  { name => "strength" },
-                                    "noise"     =>  { name => "noise" }
-                                });
-    
+
+    $options{options}->add_options(arguments => {
+    });
+
     return $self;
 }
 
-sub prefix_mac_output {
+sub prefix_wreg_output {
     my ($self, %options) = @_;
     
     return "MAC '" . $options{instance_value}->{display} . "' ";
 }
 
 my $mapping = {
-    regmac => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.1' },
-    rx => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.3' },
-    tx => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.19' },
-    regs2n => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.12' },
+    mtxrWlRtabAddr          => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.1' },
+    mtxrWlRtabStrength      => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.3' }, # dBm
+    mtxrWlRtabSignalToNoise => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.12' }, # dB
+    mtxrWlRtabTxStrength    => { oid => '.1.3.6.1.4.1.14988.1.1.1.2.1.19' }, # dBm
 };
 
 sub manage_selection {
     my ($self, %options) = @_;
 
-    if ($options{snmp}->is_snmpv1()) {
-        $self->{output}->add_option_msg(short_msg => "Need to use SNMP v2c or v3.");
-        $self->{output}->option_exit();
-    }
-    
-    my $oids = [ $mapping->{regmac} ];
-    
-    if(defined($self->{option_results}->{strength})){
-        push @{$oids}, ($mapping->{rx}, $mapping->{tx});
-    }
+    my $snmp_result = $options{snmp}->get_multiple_table(
+        oids => [
+            map({ oid => $_->{oid} }, values(%$mapping))
+        ], 
+        return_type => 1,
+        nothing_quit => 1
+    );
 
-    if(defined($self->{option_results}->{noise})){
-        push @{$oids}, $mapping->{regs2n};
-    }
-
-    $self->{snmp} = $options{snmp};
-
-    $self->{signalstrength} = {};
-    my $interfaceTables = $self->{snmp}->get_multiple_table(oids => $oids);
-
-    my @KeyMac = $self->{snmp}->oid_lex_sort(keys %{$interfaceTables->{ $mapping->{regmac}->{oid} }});
-    my (@KeysRx, @KeysTx, @Keys2n);
-
-    if (defined($self->{option_results}->{strength})) {
-        @KeysRx = $self->{snmp}->oid_lex_sort(keys %{$interfaceTables->{ $mapping->{rx}->{oid} }});
-        @KeysTx = $self->{snmp}->oid_lex_sort(keys %{$interfaceTables->{ $mapping->{tx}->{oid} }});
-
-    } elsif (defined($self->{option_results}->{noise})){
-        @Keys2n = $self->{snmp}->oid_lex_sort(keys %{$interfaceTables->{ $mapping->{regs2n}->{oid} }});
-    }
-
-    foreach my $index (0 .. $#KeyMac) {
-        next if ($KeyMac[$index] !~ /^$mapping->{regmac}->{oid}\.(.*)$/); 
-        next if (defined($self->{option_results}->{strength}) && 
-                 $KeysRx[$index] !~ /^$mapping->{rx}->{oid}\.(.*)$/ && 
-                 $KeysTx[$index] !~ /^$mapping->{tx}->{oid}\.(.*)$/); 
-        next if (defined($self->{option_results}->{noise}) && $Keys2n[$index] !~ /^$mapping->{regs2n}->{oid}\.(.*)$/);
-
+    $self->{wreg} = {};
+    foreach my $oid (keys %$snmp_result) {
+        next if ($oid !~ /^$mapping->{mtxrWlRtabAddr}->{oid}\.(.*)$/);
         my $instance = $1;
-        my $result = $options{snmp}->map_instance(mapping => $mapping, 
-                                                  results => $interfaceTables,  
-                                                  instance => $instance);
-        my $mac = unpack('H*', $interfaceTables->{$mapping->{regmac}->{oid}}->{$KeyMac[$index]});
-        $mac =~ s/..\K\B/:/g;
-        
-        if(defined($self->{option_results}->{strength})) {
-            $self->{signalstrength}->{$instance} = { display => $mac, 
-                %$result
-            };
-        }
+        my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => $instance);
 
-        if(defined($self->{option_results}->{noise})) {
-            $self->{signalnoise}->{$instance} = { display => $mac, 
-                %$result
-            };
-        }
-    }
-    
-    if (scalar(keys %{$self->{signalstrength}}) <= 0 && scalar(keys %{$self->{signalnoise}}) <= 0) {
-        $self->{output}->add_option_msg(short_msg => "No signal found.");
-        $self->{output}->option_exit();
-    }
+        my $macaddress = unpack('H*', $result->{mtxrWlRtabAddr});
+        $macaddress =~ s/(..)(?=.)/$1:/g;
 
-    $self->{cache_name} = "mikrotik_" . $self->{mode} . '_' . $options{snmp}->get_hostname()  . '_' . $options{snmp}->get_port();
+        $self->{wreg}->{$instance} = {
+            display => $macaddress,
+            %$result
+        };
+    }
 }
 
 1;
@@ -169,25 +120,13 @@ __END__
 
 =head1 MODE
 
-Check signal strength and signal to noise.
+Check wireless registration signals.
 
 =over 8
 
-=item B<--noise>
+=item B<--warning-*> B<--critical-*>
 
-Gets values for signal to noise
-
-=item B<--strength>
-
-Gets values for signal strength
-
-=item B<--warning-*>
-
-Can be rx-strength, tx-strength or signal2noise
-
-=item B<--critical-*>
-
-Can be rx-strength, tx-strength or signal2noise
+Can be: 'rx-strength', 'tx-strength', 'signal2noise'
 
 =back
 
