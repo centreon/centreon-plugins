5222d5c0858bd1fde4900ac123a2d7e559f96d44
garnier-quentin <garnier.quentin@gmail.com>
2020-01-16 16:57:40 +0100

    kingdee: wip

--- apps/kingdee/eas/mode/transaction.pm
+++ apps/kingdee/eas/mode/transaction.pm
@@ -21,125 +21,125 @@
 
 package apps::kingdee::eas::mode::transaction;
 
-use base qw(centreon::plugins::mode);
+use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
 
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, cb_prefix_output => 'prefix_global_output', skipped_code => { -10 => 1 } },
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'active', nlabel => 'transactions.active.count', set => {
+                key_values => [ { name => 'active_count' } ],
+                output_template => 'active: %s',
+                perfdatas => [
+                    { value => 'active_count_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'timedout', nlabel => 'transactions.timedout.count', display_ok => 0, set => {
+                key_values => [ { name => 'timedout_count' } ],
+                output_template => 'timed out: %s',
+                perfdatas => [
+                    { value => 'timedout_count_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'committed', nlabel => 'transactions.committed.count', set => {
+                key_values => [ { name => 'committed_count', diff => 1 } ],
+                output_template => 'committed: %s',
+                perfdatas => [
+                    { value => 'committed_count_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'rolledback', nlabel => 'transactions.rolledback.count', set => {
+                key_values => [ { name => 'rolledback_count', diff => 1 } ],
+                output_template => 'rolledback: %s',
+                perfdatas => [
+                    { value => 'rolledback_count_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'created', nlabel => 'transactions.created.count', set => {
+                key_values => [ { name => 'transaction_count', diff => 1 } ],
+                output_template => 'created: %s',
+                perfdatas => [
+                    { value => 'transaction_count_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+        { label => 'time-total', nlabel => 'transactions.time.total.milliseconds', set => {
+                key_values => [ { name => 'total_transaction_time', diff => 1 } ],
+                output_template => 'total time: %s ms',
+                perfdatas => [
+                    { value => 'total_transaction_time_absolute', template => '%s', min => 0, unit => 'ms' },
+                ],
+            }
+        },
+        { label => 'time-max', nlabel => 'transactions.time.max.milliseconds', display_ok => 0, set => {
+                key_values => [ { name => 'max_transaction_time', diff => 1 } ],
+                output_template => 'max time: %s ms',
+                perfdatas => [
+                    { value => 'max_transaction_time_absolute', template => '%s', min => 0, unit => 'ms' },
+                ],
+            }
+        },
+        { label => 'timeout-default', nlabel => 'transactions.timeout.default.count', display_ok => 0, set => {
+                key_values => [ { name => 'default_timeout' } ],
+                output_template => 'default timeout: %s',
+                perfdatas => [
+                    { value => 'default_timeout_absolute', template => '%s', min => 0 },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_global_output {
+    my ($self, %options) = @_;
+
+    return 'transactions ';
+}
+
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1, force_new_perfdata => 1);
     bless $self, $class;
 
-    $options{options}->add_options(arguments =>
-            {
-            "urlpath:s"         => { name => 'url_path', default => "/easportal/tools/nagios/checktransaction.jsp" },
-            "datasource:s"      => { name => 'datasource' },
-            "warning:s"         => { name => 'warning', default => "," },
-            "critical:s"        => { name => 'critical', default => "," },
-            });
+    $options{options}->add_options(arguments => {
+        'urlpath:s' => { name => 'url_path', default => "/easportal/tools/nagios/checktransaction.jsp" },
+    });
 
     return $self;
 }
 
-sub check_options {
+sub manage_selection {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
 
-    ($self->{warn_activecount}, $self->{warn_timeoutcount}) 
-        = split /,/, $self->{option_results}->{"warning"};
-    ($self->{crit_activecount}, $self->{crit_timeoutcount}) 
-        = split /,/, $self->{option_results}->{"critical"};
-
-    # warning
-    if (($self->{perfdata}->threshold_validate(label => 'warn_activecount', value => $self->{warn_activecount})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning activecount threshold '" . $self->{warn_activecount} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'warn_timeoutcount', value => $self->{warn_timeoutcount})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong warning timeoutcount threshold '" . $self->{warn_timeoutcount} . "'.");
-        $self->{output}->option_exit();
-    }
-    # critical
-    if (($self->{perfdata}->threshold_validate(label => 'crit_activecount', value => $self->{crit_activecount})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical activecount threshold '" . $self->{crit_activecount} . "'.");
-        $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'crit_timeoutcount', value => $self->{crit_timeoutcount})) == 0) {
-        $self->{output}->add_option_msg(short_msg => "Wrong critical timeoutcount threshold '" . $self->{crit_timeoutcount} . "'.");
-        $self->{output}->option_exit();
-    }    
-}
-
-sub run {
-    my ($self, %options) = @_;
-    
     my $webcontent = $options{custom}->request(path => $self->{option_results}->{url_path});
     if ($webcontent !~ /TransactionCount=\d+/i) {
-        $self->{output}->output_add(
-            severity  => 'UNKNOWN',
-            short_msg => "Cannot find transaction status."
-        );
+        $self->{output}->add_option_msg(short_msg => 'Cannot find transaction status.');
+        $self->{output}->option_exit();
     }
 
-    my ($transactioncount, $totaltransactiontime, $committedcount, $rolledbackcount, $activecount, $maxtransactiontime, $defaulttimeout, $timeoutcount) = (0, 0, 0, 0, 0, 0, 0, 0);
+    $self->{global} = {};
+    $self->{global}->{transaction_count} = $1 if ($webcontent =~ /TransactionCount=(\d+)\s/i);
+    $self->{global}->{total_transaction_time} = $1 if ($webcontent =~ /TotalTransactionTime=(\d+)\s/i);
+    $self->{global}->{committed_count} = $1 if ($webcontent =~ /CommittedCount=(\d+)\s/i);
+    $self->{global}->{rolledback_count} = $1 if ($webcontent =~ /RolledbackCount=(\d+)\s/i);
+    $self->{global}->{active_count} = $1 if ($webcontent =~ /ActiveCount=(\d+)\s/i);
+    $self->{global}->{max_transaction_time} = $1 if ($webcontent =~ /MaxTransactionTime=(\d+)\s/i);
+    $self->{global}->{default_timeout} = $1 if ($webcontent =~ /DefaultTimeout=(\d+)\s/i);
+    $self->{global}->{timedout_count} = $1 if ($webcontent =~ /TimedOutCount=(\d+)\s/i);
 
-    $transactioncount = $1 if $webcontent =~ /TransactionCount=(\d+)\s/i;
-    $totaltransactiontime = $1 if $webcontent =~ /TotalTransactionTime=(\d+)\s/i;
-    $committedcount = $1 if $webcontent =~ /CommittedCount=(\d+)\s/i;
-    $rolledbackcount = $1 if $webcontent =~ /RolledbackCount=(\d+)\s/i;
-    $activecount = $1 if $webcontent =~ /ActiveCount=(\d+)\s/i;
-    $maxtransactiontime = $1 if $webcontent =~ /MaxTransactionTime=(\d+)\s/i;
-    $defaulttimeout = $1 if $webcontent =~ /DefaultTimeout=(\d+)\s/i;
-    $timeoutcount = $1 if $webcontent =~ /TimedOutCount=(\d+)\s/i;
-
-    my $exit = $self->{perfdata}->threshold_check(value => $activecount, threshold => [ { label => 'crit_activecount', exit_litteral => 'critical' }, 
-                                                                                        { label => 'warn_activecount', exit_litteral => 'warning' } ]);
-    $self->{output}->output_add(severity => $exit, short_msg => sprintf("ActiveCount: %d", $activecount));
-
-    $exit = $self->{perfdata}->threshold_check(value => $timeoutcount, threshold => [ { label => 'crit_timeoutcount', exit_litteral => 'critical' }, 
-                                                                                      { label => 'warn_timeoutcount', exit_litteral => 'warning' } ]);
-    $self->{output}->output_add(severity => $exit, short_msg => sprintf("TimedOutCount: %d", $timeoutcount));
-
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("CommittedCount: %d", $committedcount));
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("RolledbackCount: %d", $rolledbackcount));
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("TransactionCount: %d", $transactioncount));
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("TotalTransactionTime: %dms", $totaltransactiontime));
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("MaxTransactionTime: %dms", $maxtransactiontime));
-    $self->{output}->output_add(severity => "ok", short_msg => sprintf("DefaultTimeout: %ds", $defaulttimeout));
- 
-    
-    $self->{output}->perfdata_add(label => "ActiveCount", unit => '',
-                                  value => sprintf("%d", $activecount),
-                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn_activecount'),
-                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit_activecount'),
-                                  );
-    $self->{output}->perfdata_add(label => "TimedOutCount", unit => '',
-                                  value => sprintf("%d", $timeoutcount),
-                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warn_timeoutcount'),
-                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'crit_timeoutcount'),
-                                  );
-    $self->{output}->perfdata_add(label => "c[CommittedCount]", unit => '',
-                                  value => sprintf("%d", $committedcount),
-                                  );
-    $self->{output}->perfdata_add(label => "c[RolledbackCount]", unit => '',
-                                  value => sprintf("%d", $rolledbackcount),
-                                  );
-    $self->{output}->perfdata_add(label => "c[TransactionCount]", unit => '',
-                                  value => sprintf("%d", $transactioncount),
-                                  );
-    $self->{output}->perfdata_add(label => "c[TotalTransactionTime]", unit => 'ms',
-                                  value => sprintf("%d", $totaltransactiontime),
-                                  );
-    $self->{output}->perfdata_add(label => "MaxTransactionTime", unit => 'ms',
-                                  value => sprintf("%d", $maxtransactiontime),
-                                  );
-    $self->{output}->perfdata_add(label => "DefaultTimeout", unit => 's',
-                                  value => sprintf("%d", $defaulttimeout),
-                                  );
- 
-    $self->{output}->display();
-    $self->{output}->exit();
+    $self->{cache_name} = 'kingdee_' . $self->{mode} . '_' . $options{custom}->get_hostname() . '_' . $options{custom}->get_port() . '_' .
+        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all'));
 }
 
 1;
@@ -156,13 +156,12 @@ Check EAS application EJB transaction status.
 
 Set path to get status page. (Default: '/easportal/tools/nagios/checktransaction.jsp')
 
-=item B<--warning>
+=item B<--warning-*> B<--critical-*>
 
-Warning Threshold for (activecount,timeoutcount).  for example : --warning=100,1
-
-=item B<--critical>
-
-Critical Threshold for (activecount,timeoutcount). for example : --critical=100,1
+Thresholds.
+Can be: 'active', 'timedout', 'committed',
+'rolledback', 'created', 'time-total', 'time-max', 
+'timeout-default'.
 
 =back
 
