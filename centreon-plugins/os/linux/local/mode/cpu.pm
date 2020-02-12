7172d79ed6a07977181c7d3414eca39d6c6ae728
garnier-quentin <garnier.quentin@gmail.com>
2020-02-12 13:56:56 +0100

    break change: os linux local cpu updated counters

--- centreon-plugins/os/linux/local/mode/cpu.pm
+++ centreon-plugins/os/linux/local/mode/cpu.pm
@@ -20,140 +20,191 @@
 
 package os::linux::local::mode::cpu;
 
-use base qw(centreon::plugins::mode);
+use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
 use centreon::plugins::misc;
-use centreon::plugins::statefile;
+use Digest::MD5 qw(md5_hex);
+
+sub custom_cpu_avg_calc {
+    my ($self, %options) = @_;
+
+    my ($skipped, $buffer) = (1, 1);
+    my ($count, $total_cpu) = (0, 0);
+    foreach (keys %{$options{new_datas}}) {
+        if (/^(.*?cpu\d+)_idle/) {
+            my $prefix = $1;
+            $skipped = 0;
+            next if (!defined($options{old_datas}->{$_}));
+            $buffer = 0;
+
+            my ($old_total, $old_cpu_idle) = (0, 0);
+            if ($options{new_datas}->{$_} > $options{old_datas}->{$_}) {
+                $old_total = $options{old_datas}->{$_} + $options{old_datas}->{$prefix . '_system'} + 
+                    $options{old_datas}->{$prefix . '_user'} + $options{old_datas}->{$prefix . '_iowait'};
+                $old_cpu_idle = $options{old_datas}->{$_};
+            }
+            my $total_elapsed = ($options{new_datas}->{$_} + $options{new_datas}->{$prefix . '_system'} + 
+                $options{new_datas}->{$prefix . '_user'} + $options{new_datas}->{$prefix . '_iowait'}) -
+                $old_total;
+            if ($total_elapsed == 0) {
+                $self->{error_msg} = 'no new values for cpu counters';
+                return -12;
+            }
+
+            my $idle_elapsed = $options{new_datas}->{$_} - $old_cpu_idle;
+            $total_cpu += 100 - (100 * $idle_elapsed / $total_elapsed);
+            $count++;
+        }
+    }
+
+    return -10 if ($skipped == 1);
+    if ($buffer == 1) {
+        $self->{error_msg} = "Buffer creation";
+        return -1;
+    }
+
+    $self->{result_values}->{prct_used} = $total_cpu / $count;
+    return 0;
+}
+
+sub custom_cpu_core_calc {
+    my ($self, %options) = @_;
+
+    my ($old_total, $old_cpu_idle) = (0, 0);
+    if ($options{new_datas}->{$self->{instance} . '_idle'} > $options{old_datas}->{$self->{instance} . '_idle'}) {
+        $old_total = $options{old_datas}->{$self->{instance} . '_idle'} + $options{old_datas}->{$self->{instance} . '_system'} + 
+            $options{old_datas}->{$self->{instance} . '_user'} + $options{old_datas}->{$self->{instance} . '_iowait'};
+        $old_cpu_idle = $options{old_datas}->{$self->{instance} . '_idle'};
+    }
+    my $total_elapsed = ($options{new_datas}->{$self->{instance} . '_idle'} + $options{new_datas}->{$self->{instance} . '_system'} + 
+        $options{new_datas}->{$self->{instance} . '_user'} + $options{new_datas}->{$self->{instance} . '_iowait'}) -
+        $old_total;
+    if ($total_elapsed == 0) {
+        $self->{error_msg} = 'no new values for cpu counters';
+        return -12;
+    }
+
+    my $idle_elapsed = $options{new_datas}->{$self->{instance} . '_idle'} - $old_cpu_idle;
+    $self->{result_values}->{prct_used} = 100 - (100 * $idle_elapsed / $total_elapsed);
+
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'cpu_avg', type => 0 },
+        { name => 'cpu_core', type => 1, cb_prefix_output => 'prefix_cpu_core_output' }
+    ];
+
+    $self->{maps_counters}->{cpu_avg} = [
+        { label => 'average', nlabel => 'cpu.utilization.percentage', set => {
+                key_values => [],
+                closure_custom_calc => $self->can('custom_cpu_avg_calc'),
+                manual_keys => 1, 
+                output_template => 'CPU(s) average usage is %.2f %%',
+                output_use => 'prct_used', threshold_use => 'prct_used',
+                perfdatas => [
+                    { label => 'total_cpu_avg', value => 'prct_used', template => '%.2f',
+                      min => 0, max => 100, unit => '%' },
+                ],
+            }
+        },
+    ];
+
+    $self->{maps_counters}->{cpu_core} = [
+        { label => 'core', nlabel => 'core.cpu.utilization.percentage', set => {
+                key_values => [
+                    { name => 'idle', diff => 1 }, { name => 'user', diff => 1 }, 
+                    { name => 'system', diff => 1 }, { name => 'iowait', diff => 1 }, { name => 'display' }
+                ],
+                closure_custom_calc => $self->can('custom_cpu_core_calc'),
+                output_template => 'usage : %.2f %%',
+                output_use => 'prct_used', threshold_use => 'prct_used',
+                perfdatas => [
+                    { label => 'cpu', value => 'prct_used', template => '%.2f',
+                      min => 0, max => 100, unit => '%', label_extra_instance => 1 },
+                ],
+            }
+        },
+    ];
+}
+
+sub prefix_cpu_core_output {
+    my ($self, %options) = @_;
+
+    return "CPU '" . $options{instance_value}->{display} . "' ";
+}
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
     bless $self, $class;
-    
-    $options{options}->add_options(arguments =>
-                                { 
-                                  "hostname:s"        => { name => 'hostname' },
-                                  "remote"            => { name => 'remote' },
-                                  "ssh-option:s@"     => { name => 'ssh_option' },
-                                  "ssh-path:s"        => { name => 'ssh_path' },
-                                  "ssh-command:s"     => { name => 'ssh_command', default => 'ssh' },
-                                  "timeout:s"         => { name => 'timeout', default => 30 },
-                                  "sudo"              => { name => 'sudo' },
-                                  "command:s"         => { name => 'command', default => 'cat' },
-                                  "command-path:s"    => { name => 'command_path' },
-                                  "command-options:s" => { name => 'command_options', default => '/proc/stat 2>&1' },
-                                  "warning:s"         => { name => 'warning', },
-                                  "critical:s"        => { name => 'critical', },
-                                });
-    $self->{statefile_cache} = centreon::plugins::statefile->new(%options);
+
+    $options{options}->add_options(arguments => { 
+        'hostname:s'        => { name => 'hostname' },
+        'remote'            => { name => 'remote' },
+        'ssh-option:s@'     => { name => 'ssh_option' },
+        'ssh-path:s'        => { name => 'ssh_path' },
+        'ssh-command:s'     => { name => 'ssh_command', default => 'ssh' },
+        'timeout:s'         => { name => 'timeout', default => 30 },
+        'sudo'              => { name => 'sudo' },
+        'command:s'         => { name => 'command', default => 'cat' },
+        'command-path:s'    => { name => 'command_path' },
+        'command-options:s' => { name => 'command_options', default => '/proc/stat 2>&1' }
+    });
+
     $self->{hostname} = undef;
     return $self;
 }
 
 sub check_options {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
+    $self->SUPER::check_options(%options);
 
-    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
-       $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
-       $self->{output}->option_exit();
-    }
-    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
-       $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
-       $self->{output}->option_exit();
-    }
-    
-    $self->{statefile_cache}->check_options(%options);
     $self->{hostname} = $self->{option_results}->{hostname};
     if (!defined($self->{hostname})) {
         $self->{hostname} = 'me';
     }
 }
 
-sub run {
+sub manage_selection {
     my ($self, %options) = @_;
 
-    my $stdout = centreon::plugins::misc::execute(output => $self->{output},
-                                                  options => $self->{option_results},
-                                                  sudo => $self->{option_results}->{sudo},
-                                                  command => $self->{option_results}->{command},
-                                                  command_path => $self->{option_results}->{command_path},
-                                                  command_options => $self->{option_results}->{command_options});
-    $self->{statefile_cache}->read(statefile => 'cache_linux_local_' . $self->{hostname}  . '_' .  $self->{mode});
-    my $old_timestamp = $self->{statefile_cache}->get(name => 'last_timestamp');
-    my $datas = {};
-    $datas->{last_timestamp} = time();
-    
-    my ($cpu, $i) = (0, 0);
+    my $stdout = centreon::plugins::misc::execute(
+        output => $self->{output},
+        options => $self->{option_results},
+        sudo => $self->{option_results}->{sudo},
+        command => $self->{option_results}->{command},
+        command_path => $self->{option_results}->{command_path},
+        command_options => $self->{option_results}->{command_options}
+    );
+
+    $self->{cpu_avg} = {};
+    $self->{cpu_core} = {};
     foreach (split(/\n/, $stdout)) {
         next if (!/cpu(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/);
         my $cpu_number = $1;
-        $datas->{'cpu_idle_' . $cpu_number} = $5;
-        $datas->{'cpu_system_' . $cpu_number} = $4;
-        $datas->{'cpu_user_' . $cpu_number} = $2;
-        $datas->{'cpu_iowait_' . $cpu_number} = $6;
         
-        if (!defined($old_timestamp)) {
-            next;
-        }
-        my $old_cpu_idle = $self->{statefile_cache}->get(name => 'cpu_idle_' . $cpu_number);
-        my $old_cpu_system = $self->{statefile_cache}->get(name => 'cpu_system_' . $cpu_number);
-        my $old_cpu_user = $self->{statefile_cache}->get(name => 'cpu_user_' . $cpu_number);
-        my $old_cpu_iowait = $self->{statefile_cache}->get(name => 'cpu_iowait_' . $cpu_number);
-        if (!defined($old_cpu_system) || !defined($old_cpu_idle) || !defined($old_cpu_user) || !defined($old_cpu_iowait)) {
-            next;
-        }
-        
-        if ($datas->{'cpu_idle_' . $cpu_number} < $old_cpu_idle) {
-            # We set 0. Has reboot.
-            $old_cpu_user = 0;
-            $old_cpu_idle = 0;
-            $old_cpu_system = 0;
-            $old_cpu_iowait = 0;
-        }
-        
-        my $total_elapsed = ($datas->{'cpu_idle_' . $cpu_number} + $datas->{'cpu_user_' . $cpu_number} + $datas->{'cpu_system_' . $cpu_number} + $datas->{'cpu_iowait_' . $cpu_number}) - ($old_cpu_user + $old_cpu_idle + $old_cpu_system + $old_cpu_iowait);
-        if ($total_elapsed == 0) {
-            $self->{output}->output_add(severity => 'OK',
-                                        short_msg => "No new values for cpu counters");
-            $self->{output}->display();
-            $self->{output}->exit();
-        }
-        my $idle_elapsed = $datas->{'cpu_idle_' . $cpu_number} - $old_cpu_idle;
-        my $cpu_ratio_usetime = 100 * $idle_elapsed / $total_elapsed;
-        $cpu_ratio_usetime = 100 - $cpu_ratio_usetime;        
-        
-        $cpu += $cpu_ratio_usetime;
-        $i++;
-        $self->{output}->output_add(long_msg => sprintf("CPU %d Usage is %.2f%%", $cpu_number, $cpu_ratio_usetime));
-        $self->{output}->perfdata_add(label => 'cpu' . $cpu_number, unit => '%',
-                                      value => sprintf("%.2f", $cpu_ratio_usetime),
-                                      min => 0, max => 100);
-    }
-    
-    if ($i > 0) {
-        my $avg_cpu = $cpu / $i;
-        my $exit_code = $self->{perfdata}->threshold_check(value => $avg_cpu, 
-                                                           threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-        $self->{output}->output_add(severity => $exit_code,
-                                    short_msg => sprintf("CPU(s) average usage is: %.2f%%", $avg_cpu));
-        $self->{output}->perfdata_add(label => 'total_cpu_avg', unit => '%',
-                                      value => sprintf("%.2f", $avg_cpu),
-                                      warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
-                                      critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
-                                      min => 0, max => 100);
-    }
-
-    $self->{statefile_cache}->write(data => $datas);
-    if (!defined($old_timestamp)) {
-        $self->{output}->output_add(severity => 'OK',
-                                    short_msg => "Buffer creation...");
+        $self->{cpu_core}->{$cpu_number} = {
+            display => $cpu_number,
+            idle => $5,
+            system => $4,
+            user => $2,
+            iowait => $6
+        };
+        $self->{cpu_avg}->{'cpu' . $cpu_number . '_idle'} = $5;
+        $self->{cpu_avg}->{'cpu' . $cpu_number . '_system'} = $4;
+        $self->{cpu_avg}->{'cpu' . $cpu_number . '_user'} = $2;
+        $self->{cpu_avg}->{'cpu' . $cpu_number . '_iowait'} = $6;
     }
  
-    $self->{output}->display();
-    $self->{output}->exit();
+    $self->{cache_name} = "cache_linux_local_" . md5_hex($self->{hostname})  . '_' . $self->{mode} . '_' .
+        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all')) . '_' .
+        (defined($self->{option_results}->{ssh_option}) ? md5_hex(join('', @{$self->{option_results}->{ssh_option}})) : md5_hex('all'));
 }
 
 1;
@@ -165,14 +216,6 @@ __END__
 Check system CPUs (need '/proc/stat' file).
 
 =over 8
-
-=item B<--warning>
-
-Threshold warning in percent.
-
-=item B<--critical>
-
-Threshold critical in percent.
 
 =item B<--remote>
 
@@ -214,6 +257,22 @@ Command path (Default: none).
 =item B<--command-options>
 
 Command options (Default: '/proc/stat 2>&1').
+
+=item B<--warning-average>
+
+Warning threshold average CPU utilization. 
+
+=item B<--critical-average>
+
+Critical  threshold average CPU utilization. 
+
+=item B<--warning-core>
+
+Warning thresholds for each CPU core
+
+=item B<--critical-core>
+
+Critical thresholds for each CPU core
 
 =back
 
