e3d12fd5deae44f72a6d4778a6a1b2460e620bbb
Lotfi zaouche <lotfi.zaouche@gmail.com>
2020-03-26 08:45:23 +0000

    adding parity mode

--- centreon-plugins/blockchain/parity/restapi/plugin.pm
+++ centreon-plugins/blockchain/parity/restapi/plugin.pm
@@ -31,8 +31,9 @@ sub new {
 
     $self->{version} = '0.1';
     %{$self->{modes}} = ( 
-        'net' => 'blockchain::parity::restapi::mode::net',
-        'eth' => 'blockchain::parity::restapi::mode::eth',
+        'net'       => 'blockchain::parity::restapi::mode::net',
+        'eth'       => 'blockchain::parity::restapi::mode::eth',
+        'parity'    => 'blockchain::parity::restapi::mode::parity'
     );
     $self->{custom_modes}{api} = 'blockchain::parity::restapi::custom::api';
     return $self;
