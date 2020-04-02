d749650aba793ff4b946e00ad6b8109779db24cf
garnier-quentin <garnier.quentin@gmail.com>
2020-04-02 16:31:23 +0200

    break change: refacto hddtemp plugin

---
+++ apps/hddtemp/mode/listdrives.pm
@@ -0,0 +1,97 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::hddtemp::mode::listdrives;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {});
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    return $options{custom}->list_drives();
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my $drives = $self->manage_selection(%options);
+    foreach (sort keys %$drives) {
+        $self->{output}->output_add(long_msg =>
+            sprintf(
+                '[name = %s]',
+                $_
+            )
+        );
+    }
+    $self->{output}->output_add(
+        severity => 'OK',
+        short_msg => 'List drives:'
+    );
+
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
+    $self->{output}->exit();
+}
+
+sub disco_format {
+    my ($self, %options) = @_;
+    
+    $self->{output}->add_disco_format(elements => ['name']);
+}
+
+sub disco_show {
+    my ($self, %options) = @_;
+
+    my $drives = $self->manage_selection(%options);
+    foreach (sort keys %$drives) {
+        $self->{output}->add_disco_entry(name => $_);
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+List queues.
+
+=over 8
+
+=back
+
+=cut
