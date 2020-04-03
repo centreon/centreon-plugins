e565cb8008255a702e61ccbec8166df71fb5adcf
garnier-quentin <garnier.quentin@gmail.com>
2020-04-03 14:57:02 +0200

    enhance typo

--- hardware/kvm/avocent/acs/8000/snmp/mode/hardware.pm
+++ hardware/kvm/avocent/acs/8000/snmp/mode/hardware.pm
@@ -27,17 +27,17 @@ use warnings;
 
 sub set_system {
     my ($self, %options) = @_;
-    
+
     $self->{regexp_threshold_overload_check_section_option} = '^(psu)$';
-    
+
     $self->{cb_hook2} = 'snmp_execute';
-    
+
     $self->{thresholds} = {
         psu => [
             ['statePowerOn', 'OK'],
             ['statePowerOff', 'CRITICAL'],
-            ['powerNotInstalled', 'OK'],
-        ],
+            ['powerNotInstalled', 'OK']
+        ]
     };
     
     $self->{components_path} = 'hardware::kvm::avocent::acs::8000::snmp::mode::components';
@@ -55,10 +55,9 @@ sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options, no_performance => 1);
     bless $self, $class;
-    
-    $options{options}->add_options(arguments =>
-                                {
-                                });
+
+    $options{options}->add_options(arguments => {
+    });
 
     return $self;
 }
