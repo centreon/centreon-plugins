b50c9b296a8b015f97886ed1611bbe3fc9fddb96
thibaults-centreon <tscheitenberger@centreon.com>
2020-04-10 14:25:34 +0200

    add(plugin): AWS VPN

---
+++ cloud/aws/vpn/mode/listvpn.pm
@@ -0,0 +1,96 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package cloud::aws::vpn::mode::listvpn;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {});
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    $self->{connection} = $options{custom}->vpn_list_connections(region => $self->{option_results}->{region});
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    $self->manage_selection(%options);
+    foreach (@{$self->{connection}}) {
+        $self->{output}->output_add(
+            long_msg => sprintf("[Id = %s][Name = %s][State = %s]",
+            $_->{id}, $_->{name}, $_->{state} ));
+    }
+
+    $self->{output}->output_add(severity => 'OK',
+                                short_msg => 'List VPN connections:');
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
+    $self->{output}->exit();
+}
+
+sub disco_format {
+    my ($self, %options) = @_;
+
+    $self->{output}->add_disco_format(elements => ['id', 'name', 'state']);
+}
+
+sub disco_show {
+    my ($self, %options) = @_;
+
+    $self->manage_selection(%options);
+    foreach (@{$self->{connection}}) {
+        $self->{output}->add_disco_entry(
+            id => $_->{id},
+            name => $_->{name},
+            state => $_->{state},
+        );
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+List EC2 instances.
+
+=over 8
+
+=back
+
+=cut
