3b6140e991873659bbe27421b6d79f535625d50e
garnier-quentin <garnier.quentin@gmail.com>
2020-05-04 09:58:32 +0200

    Fix #1951

---
+++ centreon-plugins/storage/lenovo/iomega/snmp/mode/hardware.pm
@@ -0,0 +1,117 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::lenovo::iomega::snmp::mode::hardware;
+
+use base qw(centreon::plugins::templates::hardware);
+
+use strict;
+use warnings;
+
+sub set_system {
+    my ($self, %options) = @_;
+    
+    $self->{regexp_threshold_overload_check_section_option} = '^(?:raid|disk)$';
+    $self->{regexp_threshold_numeric_check_section_option} = '^(?:temperature|temperature|voltage)$';
+    
+    $self->{cb_hook2} = 'snmp_execute';
+    
+    $self->{thresholds} = {
+        raid => [
+            ['normal', 'OK'],
+            ['rebuilding', 'OK'],
+            ['degraded', 'WARNING'],
+            ['rebuildfs', 'OK'],
+            ['faulted', 'CRITICAL']
+        ],
+        disk => [
+            ['normal', 'OK'],
+            ['foreign', 'WARNING'],
+            ['faulted', 'CRITICAL'],
+            ['missing', 'OK']
+        ]
+    };
+    
+    $self->{components_path} = 'storage::lenovo::iomega::snmp::mode::components';
+    $self->{components_module} = ['raid', 'disk', 'voltage', 'temperature', 'fan'];
+}
+
+sub snmp_execute {
+    my ($self, %options) = @_;
+    
+    $self->{snmp} = $options{snmp};
+    $self->{results} = $self->{snmp}->get_multiple_table(oids => $self->{request});
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, no_absent => 1, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => { 
+    });
+
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check hardware.
+
+=over 8
+
+=item B<--component>
+
+Which component to check (Default: '.*').
+Can be: 'raid', 'disk', 'voltage', 'temperature', 'fan'.
+
+=item B<--filter>
+
+Exclude some parts (comma seperated list) (Example: --filter=fan)
+Can also exclude specific instance: --filter=fan,1
+
+=item B<--no-component>
+
+Return an error if no compenents are checked.
+If total (with skipped) is 0. (Default: 'critical' returns).
+
+=item B<--threshold-overload>
+
+Set to overload default threshold values (syntax: section,[instance,]status,regexp)
+It used before default thresholds (order stays).
+Example: --threshold-overload='disk,WARNING,missing'
+
+=item B<--warning>
+
+Set warning threshold for 'temperature', 'voltage', 'fan' (syntax: type,regexp,threshold)
+Example: --warning='temperature,.*,40'
+
+=item B<--critical>
+
+Set critical threshold for 'temperature', 'voltage', 'fan' (syntax: type,regexp,threshold)
+Example: --critical='temperature,.*,50'
+
+=back
+
+=cut
---
+++ centreon-plugins/storage/lenovo/iomega/snmp/mode/interfaces.pm
@@ -0,0 +1,187 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::lenovo::iomega::snmp::mode::interfaces;
+
+use base qw(snmp_standard::mode::interfaces);
+
+use strict;
+use warnings;
+
+sub default_oid_filter_name {
+    my ($self, %options) = @_;
+
+    return 'ifdesc';
+}
+
+sub default_oid_display_name {
+    my ($self, %options) = @_;
+
+    return 'ifdesc';
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    return $self;
+}
+
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check interfaces.
+
+=over 8
+
+=item B<--add-global>
+
+Check global port statistics (By default if no --add-* option is set).
+
+=item B<--add-status>
+
+Check interface status.
+
+=item B<--add-duplex-status>
+
+Check duplex status (with --warning-status and --critical-status).
+
+=item B<--add-traffic>
+
+Check interface traffic.
+
+=item B<--add-errors>
+
+Check interface errors.
+
+=item B<--add-cast>
+
+Check interface cast.
+
+=item B<--add-speed>
+
+Check interface speed.
+
+=item B<--add-volume>
+
+Check interface data volume between two checks (not supposed to be graphed, useful for BI reporting).
+
+=item B<--warning-status>
+
+Set warning threshold for status.
+Can used special variables like: %{admstatus}, %{opstatus}, %{duplexstatus}, %{display}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '%{admstatus} eq "up" and %{opstatus} ne "up"').
+Can used special variables like: %{admstatus}, %{opstatus}, %{duplexstatus}, %{display}
+
+=item B<--warning-*>
+
+Threshold warning.
+Can be: 'total-port', 'total-admin-up', 'total-admin-down', 'total-oper-up', 'total-oper-down',
+'in-traffic', 'out-traffic', 'in-error', 'in-discard', 'out-error', 'out-discard',
+'in-ucast' (%), 'in-bcast' (%), 'in-mcast' (%), 'out-ucast' (%), 'out-bcast' (%), 'out-mcast' (%),
+'speed' (b/s).
+
+=item B<--critical-*>
+
+Threshold critical.
+Can be: 'total-port', 'total-admin-up', 'total-admin-down', 'total-oper-up', 'total-oper-down',
+'in-traffic', 'out-traffic', 'in-error', 'in-discard', 'out-error', 'out-discard',
+'in-ucast' (%), 'in-bcast' (%), 'in-mcast' (%), 'out-ucast' (%), 'out-bcast' (%), 'out-mcast' (%),
+'speed' (b/s).
+
+=item B<--units-traffic>
+
+Units of thresholds for the traffic (Default: '%') ('%', 'b/s').
+
+=item B<--units-errors>
+
+Units of thresholds for errors/discards (Default: '%') ('%', 'absolute').
+
+=item B<--nagvis-perfdata>
+
+Display traffic perfdata to be compatible with nagvis widget.
+
+=item B<--interface>
+
+Set the interface (number expected) ex: 1,2,... (empty means 'check all interface').
+
+=item B<--name>
+
+Allows to use interface name with option --interface instead of interface oid index (Can be a regexp)
+
+=item B<--speed>
+
+Set interface speed for incoming/outgoing traffic (in Mb).
+
+=item B<--speed-in>
+
+Set interface speed for incoming traffic (in Mb).
+
+=item B<--speed-out>
+
+Set interface speed for outgoing traffic (in Mb).
+
+=item B<--no-skipped-counters>
+
+Don't skip counters when no change.
+
+=item B<--force-counters32>
+
+Force to use 32 bits counters (even in snmp v2c and v3). Should be used when 64 bits counters are buggy.
+
+=item B<--reload-cache-time>
+
+Time in minutes before reloading cache file (default: 180).
+
+=item B<--oid-filter>
+
+Choose OID used to filter interface (default: ifDesc) (values: ifDesc, ifAlias, ifName, IpAddr).
+
+=item B<--oid-display>
+
+Choose OID used to display interface (default: ifDesc) (values: ifDesc, ifAlias, ifName, IpAddr).
+
+=item B<--oid-extra-display>
+
+Add an OID to display.
+
+=item B<--display-transform-src>
+
+Regexp src to transform display value.
+
+=item B<--display-transform-dst>
+
+Regexp dst to transform display value.
+
+=item B<--show-cache>
+
+Display cache interface datas.
+
+=back
+
+=cut
