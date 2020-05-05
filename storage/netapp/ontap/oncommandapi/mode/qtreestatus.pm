140d43162c17915a338f575d4ab5c77a2dda21ab
garnier-quentin <garnier.quentin@gmail.com>
2020-05-05 14:53:41 +0200

    Break change: move netapp directories - can add some new plugins for netapp

---
+++ storage/netapp/ontap/oncommandapi/mode/qtreestatus.pm
@@ -0,0 +1,158 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::netapp::ontap::oncommandapi::mode::qtreestatus;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold);
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+
+    my $msg = sprintf("Status is '%s' [path: %s] [volume: %s]",
+        $self->{result_values}->{status},
+        $self->{result_values}->{qtree_path},
+        $self->{result_values}->{volume});
+    return $msg;
+}
+
+sub custom_status_calc {
+    my ($self, %options) = @_;
+
+    $self->{result_values}->{display} = $options{new_datas}->{$self->{instance} . '_name'};
+    $self->{result_values}->{status} = $options{new_datas}->{$self->{instance} . '_status'};
+    $self->{result_values}->{qtree_path} = $options{new_datas}->{$self->{instance} . '_qtree_path'};
+    $self->{result_values}->{volume} = $options{new_datas}->{$self->{instance} . '_volume'};
+
+    return 0;
+}
+
+sub prefix_output {
+    my ($self, %options) = @_;
+
+    return "Qtree '" . $options{instance_value}->{name} . "' ";
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'qtrees', type => 1, cb_prefix_output => 'prefix_output', message_multiple => 'All Qtrees status are ok' },
+    ];
+    
+    $self->{maps_counters}->{qtrees} = [
+        { label => 'status', set => {
+                key_values => [ { name => 'status' }, { name => 'qtree_path' }, { name => 'volume' }, { name => 'name' } ],
+                closure_custom_calc => $self->can('custom_status_calc'),
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => \&catalog_status_threshold,
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {
+        'filter-name:s'     => { name => 'filter_name' },
+        'filter-volume:s'   => { name => 'filter_volume' },
+        'warning-status:s'  => { name => 'warning_status' },
+        'critical-status:s' => { name => 'critical_status' }
+    });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+
+    $self->change_macros(macros => ['warning_status', 'critical_status']);
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->get(path => '/qtrees');
+
+    foreach my $qtree (@{$result}) {
+        if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' &&
+            $qtree->{name} !~ /$self->{option_results}->{filter_name}/) {
+            $self->{output}->output_add(long_msg => "skipping  '" . $qtree->{name} . "': no matching filter name.", debug => 1);
+            next;
+        }
+
+        my $volume = $2 if ($qtree->{qtree_path} =~ /^\/(\S+)\/(\S+)\/(\S+)\s*/);
+
+        if (defined($self->{option_results}->{filter_volume}) && $self->{option_results}->{filter_volume} ne '' &&
+            defined($volume) && $volume !~ /$self->{option_results}->{filter_volume}/) {
+            $self->{output}->output_add(long_msg => "skipping '" . $qtree->{name} . "': no matching filter volume '" . $volume . "'", debug => 1);
+            next;
+        }
+
+        $self->{qtrees}->{$qtree->{key}} = {
+            name => $qtree->{name},
+            status => $qtree->{status},
+            qtree_path => $qtree->{qtree_path},
+            volume => $volume,
+        }
+    }
+
+    if (scalar(keys %{$self->{qtrees}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "No entry found.");
+        $self->{output}->option_exit();
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check NetApp Qtrees status.
+
+=over 8
+
+=item B<--filter-*>
+
+Filter qtree.
+Can be: 'name', 'volume' (can be a regexp).
+
+=item B<--warning-status>
+
+Set warning threshold for status (Default: '').
+Can used special variables like: %{state}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '').
+Can used special variables like: %{status}
+
+=back
+
+=cut
---
+++ storage/netapp/ontap/snmp/mode/components/fan.pm
@@ -0,0 +1,95 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::netapp::ontap::snmp::mode::components::fan;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+my $oid_enclChannelShelfAddr = '.1.3.6.1.4.1.789.1.21.1.2.1.3';
+my $oid_enclFansPresent = '.1.3.6.1.4.1.789.1.21.1.2.1.17';
+my $oid_enclFansFailed = '.1.3.6.1.4.1.789.1.21.1.2.1.18';
+my $oid_enclFansSpeed = '.1.3.6.1.4.1.789.1.21.1.2.1.62';
+
+sub load {
+    my ($self) = @_;
+    
+    push @{$self->{request}}, { oid => $oid_enclFansPresent }, { oid => $oid_enclFansFailed },
+        { oid => $oid_enclFansSpeed };
+}
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => "Checking fans");
+    $self->{components}->{fan} = {name => 'fans', total => 0, skip => 0};
+    return if ($self->check_filter(section => 'fan'));
+
+    for (my $i = 1; $i <= $self->{number_shelf}; $i++) {
+        my $shelf_addr = $self->{shelf_addr}->{$oid_enclChannelShelfAddr . '.' . $i};
+        my $present = $self->{results}->{$oid_enclFansPresent}->{$oid_enclFansPresent . '.' . $i};
+        my $failed = $self->{results}->{$oid_enclFansFailed}->{$oid_enclFansFailed . '.' . $i};
+        my @current_speed = defined($self->{results}->{$oid_enclFansSpeed}->{$oid_enclFansSpeed . '.' . $i}) ? split /,/, $self->{results}->{$oid_enclFansSpeed}->{$oid_enclFansSpeed . '.' . $i} : ();
+        
+        foreach my $num (split /,/, $present) {
+            $num = centreon::plugins::misc::trim($num);
+            next if ($num !~ /[0-9]/);
+            my $current_value = (defined($current_speed[$num - 1]) && $current_speed[$num - 1] =~ /(^|\s)([0-9]+)/) ? $2 : '';
+            
+            next if ($self->check_filter(section => 'fan', instance => $shelf_addr . '.' . $num));
+            $self->{components}->{fan}->{total}++;
+
+            my $status = 'ok';
+            if ($failed =~ /(^|,|\s)$num(,|\s|$)/) {
+                $status = 'failed';
+            }
+
+            $self->{output}->output_add(long_msg => sprintf("Shelve '%s' Fan '%s' is '%s'", 
+                                        $shelf_addr, $num, $status));
+            my $exit = $self->get_severity(section => 'fan', value => $status);
+            if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+                $self->{output}->output_add(severity => $exit,
+                                            short_msg => sprintf("Shelve '%s' Fan '%s' is '%s'", $shelf_addr, $num, $status));
+            }
+            
+            if ($current_value ne '') {
+                my ($exit, $warn, $crit) = $self->get_severity_numeric(section => 'fan', instance => $shelf_addr . '.' . $num, value => $current_value);
+                if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+                    $self->{output}->output_add(severity => $exit,
+                                                short_msg => sprintf("Shelve '%s' Fan '%s' speed is '%s'", $shelf_addr, $num, $current_value));
+                }
+                
+                $self->{output}->perfdata_add(
+                    label => "speed", unit => 'rpm',
+                    nlabel => 'hardware.fan.speed.rpm',
+                    instances => [$shelf_addr, $num],
+                    value => $current_value,
+                    warning => $warn,
+                    critical => $crit,
+                    min => 0
+                );
+            }
+        }
+    }
+}
+
+1;
