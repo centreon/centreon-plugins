c31f096929de97a073e405021e70b8beb80bd8cd
garnier-quentin <garnier.quentin@gmail.com>
2020-05-06 16:26:04 +0200

    Fix #1807

--- storage/netapp/santricity/restapi/plugin.pm
+++ storage/netapp/santricity/restapi/plugin.pm
@@ -31,7 +31,10 @@ sub new {
 
     $self->{version} = '0.1';
     %{ $self->{modes} } = (
-        'hardware' => 'storage::netapp::santricity::restapi::mode::hardware'
+        'hardware'        => 'storage::netapp::santricity::restapi::mode::hardware',
+        'storage-pools'   => 'storage::netapp::santricity::restapi::mode::storagepools',
+        'storage-systems' => 'storage::netapp::santricity::restapi::mode::storagesystems',
+        'storage-volumes' => 'storage::netapp::santricity::restapi::mode::storagevolumes'
     );
 
     $self->{custom_modes}{api} = 'storage::netapp::santricity::restapi::custom::api';
