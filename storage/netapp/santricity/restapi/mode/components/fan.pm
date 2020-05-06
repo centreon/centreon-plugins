a614beb98db87234b2770aabfdb41e45a0be92d2
garnier-quentin <garnier.quentin@gmail.com>
2020-05-06 15:19:24 +0200

    wip: netapp santricity

---
+++ storage/netapp/santricity/restapi/mode/components/fan.pm
@@ -0,0 +1,67 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package storage::netapp::santricity::restapi::mode::components::fan;
+
+use strict;
+use warnings;
+
+sub check {
+    my ($self) = @_;
+
+    $self->{output}->output_add(long_msg => 'checking fans');
+    $self->{components}->{fan} = { name => 'fans', total => 0, skip => 0 };
+    return if ($self->check_filter(section => 'fan'));
+
+    return if (!defined($self->{json_results}->{storages}));
+
+    foreach (@{$self->{json_results}->{storages}}) {
+        my $storage_name = $_->{name};
+
+        next if ($self->check_filter(section => 'storage', instance => $_->{chassisSerialNumber}));
+        
+        next if (!defined($_->{'/hardware-inventory'}->{fans}));
+
+        foreach my $entry (@{$_->{'/hardware-inventory'}->{fans}}) {
+            my $instance = $entry->{fanRef};
+            my $name = $storage_name . ':' . $entry->{physicalLocation}->{locationPosition} . ':' . $entry->{physicalLocation}->{slot};
+
+            next if ($self->check_filter(section => 'fan', instance => $instance));
+            $self->{components}->{fan}->{total}++;
+
+            $self->{output}->output_add(
+                long_msg => sprintf(
+                    "fan '%s' status is '%s' [instance = %s]",
+                    $name, $entry->{status}, $instance
+                )
+            );
+
+            my $exit = $self->get_severity(section => 'fan', instance => $instance, value => $entry->{status});
+            if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
+                $self->{output}->output_add(
+                    severity => $exit,
+                    short_msg => sprintf("Fan '%s' status is '%s'", $name, $entry->{status})
+                );
+            }
+        }
+    }
+}
+
+1;
