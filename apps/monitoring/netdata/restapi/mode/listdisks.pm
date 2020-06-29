e6d339f3be8d8d070ffc9c1e05f0eafc76e89924
Thibault S <48209914+thibaults-centreon@users.noreply.github.com>
2020-06-29 17:49:54 +0200

    Update listdisks.pm

--- apps/monitoring/netdata/restapi/mode/listdisks.pm
+++ apps/monitoring/netdata/restapi/mode/listdisks.pm
@@ -57,7 +57,7 @@ sub run {
         );
     }
 
-    $self->{output}->output_add(severity => 'OK', short_msg => 'Server interfaces:');
+    $self->{output}->output_add(severity => 'OK', short_msg => 'Server disks:');
     $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1, force_long_output => 1);
     $self->{output}->exit();
 }
