eb8eee6116c3c84402326570670a56168c2f41ad
Lotfi Zaouche <lotfi.zaouche@gmail.com>
2020-08-05 20:57:08 +0200

    adding new modes: health, version and logspec

---
+++ blockchain/hyperledger/exporter/mode/logspec.pm
@@ -0,0 +1,59 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package blockchain::hyperledger::exporter::mode::logspec;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->request_api();
+
+    if ($result->{spec}) {    
+        $self->{output}->output_add(severity  => 'OK', short_msg => 'Peer/Orderer active logging spec: [' . $result->{spec} . ']');
+    }
+
+    if ($result->{error}) {    
+        $self->{output}->output_add(severity  => 'OK', short_msg => 'Peer/Orderer active logging spec error: [' . $result->{error} . ']');
+    }
+
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check blockchain node status.
+
+
