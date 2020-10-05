280313089aca2bf5d80aa725d126a27a1b442cbb
garnier-quentin <garnier.quentin@gmail.com>
2020-10-05 11:56:20 +0200

    fix  #2237

--- centreon-plugins/storage/netapp/ontap/snmp/mode/aggregatestate.pm
+++ centreon-plugins/storage/netapp/ontap/snmp/mode/aggregatestate.pm
@@ -88,7 +88,7 @@ sub prefix_agg_output {
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
     
     $options{options}->add_options(arguments => { 
