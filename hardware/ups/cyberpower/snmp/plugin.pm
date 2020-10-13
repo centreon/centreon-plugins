56bdf1528b3bd7a16c8ac681b431a74ba6c36f0a
garnier-quentin <garnier.quentin@gmail.com>
2020-10-13 16:37:25 +0200

    add plugin cyberpower

---
+++ hardware/ups/cyberpower/snmp/plugin.pm
@@ -0,0 +1,48 @@
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package hardware::ups::cyberpower::snmp::plugin;
+
+use strict;
+use warnings;
+use base qw(centreon::plugins::script_snmp);
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '0.1';
+    $self->{modes} = { 
+        'battery-status'    => 'centreon::common::cps::ups::snmp::mode::batterystatus',
+        'input-lines'       => 'centreon::common::cps::ups::snmp::mode::inputlines',
+        'output-lines'      => 'centreon::common::cps::ups::snmp::mode::outputlines'
+    };
+
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check UPS CyberPower Systems in SNMP.
+
+=cut
