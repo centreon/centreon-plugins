5bef9e954607576814f447cea7905c159c80af62
garnier-quentin <garnier.quentin@gmail.com>
2020-12-01 15:27:57 +0100

    Fix #2395

--- storage/qnap/snmp/mode/components/fan.pm
+++ storage/qnap/snmp/mode/components/fan.pm
@@ -51,15 +51,23 @@ sub check {
         next if ($self->check_filter(section => 'fan', instance => $instance));
         
         $self->{components}->{fan}->{total}++;
-        $self->{output}->output_add(long_msg => sprintf("fan '%s' [instance: %s] speed is '%s'.",
-                                    $fan_descr, $instance, $fan_speed));
+        $self->{output}->output_add(
+            long_msg => sprintf(
+                "fan '%s' [instance: %s] speed is '%s'.",
+                $fan_descr, $instance, $fan_speed
+            )
+        );
 
         if ($fan_speed =~ /([0-9]+)\s*rpm/i) {
             my $fan_speed_value = $1;
             my ($exit, $warn, $crit) = $self->get_severity_numeric(section => 'fan', instance => $instance, value => $fan_speed_value);
             if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
-                $self->{output}->output_add(severity => $exit,
-                                            short_msg => sprintf("Fan '%s' speed is %s rpm", $fan_descr, $fan_speed_value));
+                $self->{output}->output_add(
+                    severity => $exit,
+                    short_msg => sprintf(
+                        "Fan '%s' speed is %s rpm", $fan_descr, $fan_speed_value
+                    )
+                );
             }
             $self->{output}->perfdata_add(
                 label => 'fan', unit => 'rpm',
