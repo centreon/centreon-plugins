58c726651aa3f0a30270dc68572eb1a2025e9b65
garnier-quentin <garnier.quentin@gmail.com>
2021-01-18 14:49:55 +0100

    wip jasmin sms notif

---
+++ notification/jasminsms/httpapi/custom/api.pm
@@ -0,0 +1,170 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package notification::jasminsms::httpapi::custom::api;
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self  = {};
+    bless $self, $class;
+
+    if (!defined($options{output})) {
+        print "Class Custom: Need to specify 'output' argument.\n";
+        exit 3;
+    }
+    if (!defined($options{options})) {
+        $options{output}->add_option_msg(short_msg => "Class Custom: Need to specify 'options' argument.");
+        $options{output}->option_exit();
+    }
+    
+    if (!defined($options{noptions})) {
+        $options{options}->add_options(arguments => {
+            'hostname:s@'    => { name => 'hostname' },
+            'api-username:s' => { name => 'api_username' },
+            'api-password:s' => { name => 'api_password' },
+            'port:s'         => { name => 'port' },
+            'proto:s'        => { name => 'proto' },
+            'timeout:s'      => { name => 'timeout' }
+        });
+    }
+    $options{options}->add_help(package => __PACKAGE__, sections => 'REST API OPTIONS', once => 1);
+
+    $self->{output} = $options{output};
+
+    return $self;
+}
+
+sub set_options {
+    my ($self, %options) = @_;
+
+    $self->{option_results} = $options{option_results};
+}
+
+sub set_defaults {}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->{hostnames} = [];
+    if (defined($self->{option_results}->{hostname})) {
+        foreach my $hostname (@{$self->{option_results}->{hostname}}) {
+            next if ($hostname eq '');
+            push @{$self->{hostnames}}, $hostname;
+        }
+    }
+
+    $self->{port} = (defined($self->{option_results}->{port})) ? $self->{option_results}->{port} : 1401;
+    $self->{proto} = (defined($self->{option_results}->{proto})) ? $self->{option_results}->{proto} : 'http';
+    $self->{timeout} = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 20;
+    $self->{api_username} = (defined($self->{option_results}->{api_username})) ? $self->{option_results}->{api_username} : '';
+    $self->{api_password} = (defined($self->{option_results}->{api_password})) ? $self->{option_results}->{api_password} : '';
+
+    if (scalar(@{$self->{hostnames}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "Need to specify --hostname option.");
+        $self->{output}->option_exit();
+    }
+    if ($self->{api_username} eq '') {
+        $self->{output}->add_option_msg(short_msg => "Need to specify --api-username option.");
+        $self->{output}->option_exit();
+    }
+    if ($self->{api_password} eq '') {
+        $self->{output}->add_option_msg(short_msg => "Need to specify --api-password option.");
+        $self->{output}->option_exit();
+    }
+
+    return 0;
+}
+
+sub request_api {
+    my ($self, %options) = @_;
+
+    my $http = centreon::plugins::http->new(%options);
+    my $content = $http->request(
+        hostname => $self->{hostname},
+        port => $self->{port},
+        proto => $self->{proto},
+        timeout => $self->{timeout},
+        unknown_status => '',
+        warning_status => '',
+        critical_status => ''
+    );
+
+    if (!defined($content) || $content eq '') {
+        $self->{output}->add_option_msg(short_msg => "API returns empty content [code: '" . $self->{http}->get_code() . "'] [message: '" . $self->{http}->get_message() . "']");
+        $self->{output}->option_exit();
+    }
+    
+    if ($@) {
+        $self->{output}->add_option_msg(short_msg => "Cannot decode response (add --debug option to display returned content)");
+        $self->{output}->option_exit();
+    }
+
+    return $decoded;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Jasmin SMS HTTP-API
+
+=head1 HTTP API OPTIONS
+
+Jasmin SMS HTTP API
+
+=over 8
+
+=item B<--hostname>
+
+Hostname (can be multiple if you want a failover system).
+
+=item B<--port>
+
+Port used (Default: 1401)
+
+=item B<--proto>
+
+Specify https if needed (Default: 'http')
+
+=item B<--api-username>
+
+API username.
+
+=item B<--api-password>
+
+API password.
+
+=item B<--timeout>
+
+Set timeout in seconds (Default: 20).
+
+=back
+
+=head1 DESCRIPTION
+
+B<custom>.
+
+=cut
---
+++ notification/jasminsms/httpapi/mode/alert.pm
@@ -0,0 +1,149 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package notification::jasminsms::httpapi::mode::alert;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'from:s'            => { name => 'from'},
+        'to:s'              => { name => 'to' },
+        'message:s'         => { name => 'message' },
+        'class:s'           => { name => 'class', default => 1 },
+        'nostop:s'          => { name => 'nostop', default => 1 },
+        'smscoding:s'       => { name => 'smscoding', default => 1 }
+    });
+
+    return $self;
+}
+
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->SUPER::init(%options);
+    if (!defined($self->{option_results}->{account})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --account option");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{option_results}->{login})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --login option");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{option_results}->{password})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --password option");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{option_results}->{from})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --from option");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{option_results}->{to})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --to option");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{option_results}->{message})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --message option");
+        $self->{output}->option_exit();
+    }
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my $sms_param = [
+        "account=$self->{option_results}->{account}",
+        "login=$self->{option_results}->{login}",
+        "password=$self->{option_results}->{password}",
+        "to=$self->{option_results}->{to}",
+        "from=$self->{option_results}->{from}",
+        "message=$self->{option_results}->{message}",
+        "class=" . (defined($self->{option_results}->{class}) ? $self->{option_results}->{class} : ''),
+        "noStop=" . (defined($self->{option_results}->{nostop}) ? $self->{option_results}->{nostop} : ''),
+        "contentType=application/json",
+        "smsCoding=" . (defined($self->{option_results}->{smsCoding}) ? $self->{option_results}->{smsCoding} : ''),
+    ];
+    my $response = $self->{http}->request(method => 'GET', get_param => $sms_param);
+
+    my $decoded;
+    eval {
+        $decoded = JSON::XS->new->utf8->decode($response);
+    };
+    if ($@) {
+        $self->{output}->output_add(long_msg => $response, debug => 1);
+        $self->{output}->add_option_msg(short_msg => "Cannot decode json response: $@");
+        $self->{output}->option_exit();
+    }
+    if (defined($decoded->{status}) && ($decoded->{status} < 100 || $decoded->{status} >= 200)) {
+        $self->{output}->add_option_msg(short_msg => "API returned status '" . $decoded->{status} . "' and message '" . $decoded->{message} . "'");
+        $self->{output}->option_exit();
+    }
+
+    $self->{output}->output_add(short_msg => 'smsIds : ' . join(', ', @{$decoded->{smsIds}}));
+    $self->{output}->display(force_ignore_perfdata => 1);
+    $self->{output}->exit();
+}
+
+
+1;
+
+__END__
+
+=head1 MODE
+
+Send SMS with OVH API (https://docs.ovh.com/fr/sms/envoyer_des_sms_depuis_une_url_-_http2sms/)
+
+=over 8
+
+=item B<--from>
+
+Specify sender linked to account.
+
+=item B<--to>
+
+Specify receiver phone number (format 00336xxxx for French Number).
+
+=item B<--message>
+
+Specify the message to send.
+
+=item B<--class>
+
+Specify the class of message. (Default : '1').
+
+=item B<--nostop>
+
+Specify the nostop option. (Default : '1').
+
+=item B<--smsdoding>
+
+Specify the coding of message. (Default : '1').
+
+=back
+
+=cut
