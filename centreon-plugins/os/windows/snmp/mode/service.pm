3a49ec52063eb383b06cc7c7bd2779f6dc47ddde
qgarnier <garnier.quentin@gmail.com>
2021-01-19 10:53:06 +0100

    Notification jasminsms (#2527)

--- centreon-plugins/os/windows/snmp/mode/service.pm
+++ centreon-plugins/os/windows/snmp/mode/service.pm
@@ -48,7 +48,7 @@ sub new {
         'critical:s' => { name => 'critical', },
         'service:s@' => { name => 'service', },
         'regexp'     => { name => 'use_regexp', },
-        'state:s'    => { name => 'state', },
+        'state:s'    => { name => 'state' }
     });
 
     return $self;
@@ -75,64 +75,91 @@ sub check_options {
 
 sub run {
     my ($self, %options) = @_;
-    $self->{snmp} = $options{snmp};
     
     my $oid_svSvcEntry = '.1.3.6.1.4.1.77.1.2.3.1';
     my $oid_svSvcInstalledState  = '.1.3.6.1.4.1.77.1.2.3.1.2';
     my $oid_svSvcOperatingState  = '.1.3.6.1.4.1.77.1.2.3.1.3';
-    my $result = $self->{snmp}->get_table(oid => $oid_svSvcEntry, start => $oid_svSvcInstalledState, end => $oid_svSvcOperatingState);
+    my $result = $options{snmp}->get_table(oid => $oid_svSvcEntry, start => $oid_svSvcInstalledState, end => $oid_svSvcOperatingState);
     
-    my %services_match = ();
-    $self->{output}->output_add(severity => 'OK',
-                                short_msg => 'All service states are ok');
-    foreach my $oid ($self->{snmp}->oid_lex_sort(keys %$result)) {
-        next if ($oid !~ /^$oid_svSvcOperatingState\.(.*?)\.(.*)$/);
+    my $services_match = {};
+    $self->{output}->output_add(
+        severity => 'OK',
+        short_msg => 'All service states are ok'
+    );
+    foreach my $oid ($options{snmp}->oid_lex_sort(keys %$result)) {
+        next if ($oid !~ /^$oid_svSvcOperatingState\.(\d+)\.(.*)$/);
         my $instance = $1 . '.' . $2;
 
         my $svc_name = $self->{output}->to_utf8(join('', map(chr($_), split(/\./, $2))));
         my $svc_installed_state = $result->{$oid_svSvcInstalledState . '.' . $instance};
         my $svc_operating_state = $result->{$oid_svSvcOperatingState . '.' . $instance};        
         for (my $i = 0; $i < scalar(@{$self->{option_results}->{service}}); $i++) {
-            my $filter = ${$self->{option_results}->{service}}[$i];
+            $services_match->{$i} = {} if (!defined($services_match->{$i}));
+            my $filter = $self->{option_results}->{service}->[$i];
             if (defined($self->{option_results}->{use_regexp}) && $svc_name =~ /$filter/) {
-                $services_match{$i}{$svc_name}{operating_state} = $svc_operating_state;
-                $services_match{$i}{$svc_name}{installed_state} = $svc_installed_state;
+                $services_match->{$i}->{$svc_name} = {
+                    operating_state => $svc_operating_state,
+                    installed_state => $svc_installed_state
+                }
             } elsif ($svc_name eq $filter) {
-                $services_match{$i}{$svc_name}{operating_state} = $svc_operating_state;
-                $services_match{$i}{$svc_name}{installed_state} = $svc_installed_state;
+                $services_match->{$i}->{$svc_name} = {
+                    operating_state => $svc_operating_state,
+                    installed_state => $svc_installed_state
+                }
             }
         }
     }
-    
+
     for (my $i = 0; $i < scalar(@{$self->{option_results}->{service}}); $i++) {
         my $numbers = 0;
-        my %svc_name_state_wrong = ();
-        foreach my $svc_name (keys %{$services_match{$i}}) {
-            my $operating_state = $services_match{$i}{$svc_name}{operating_state};
-            my $installed_state = $services_match{$i}{$svc_name}{installed_state};
-            $self->{output}->output_add(long_msg => sprintf("Service '%s' match (pattern: '%s') [operating state = %s, installed state = %s]", 
-                                                            $svc_name, ${$self->{option_results}->{service}}[$i],
-                                                            $map_operating_state{$operating_state}, $map_installed_state{$installed_state}));
+        my $svc_name_state_wrong = {};
+        foreach my $svc_name (keys %{$services_match->{$i}}) {
+            my $operating_state = $services_match->{$i}->{$svc_name}->{operating_state};
+            my $installed_state = $services_match->{$i}->{$svc_name}->{installed_state};
+            $self->{output}->output_add(long_msg => 
+                sprintf(
+                    "Service '%s' match (pattern: '%s') [operating state = %s, installed state = %s]", 
+                    $svc_name, $self->{option_results}->{service}->[$i],
+                    $map_operating_state{$operating_state},
+                    $map_installed_state{$installed_state}
+                )
+            );
             if (defined($self->{option_results}->{state}) && $map_operating_state{$operating_state} !~ /$self->{option_results}->{state}/) {
-                delete $services_match{$i}{$svc_name};
-                $svc_name_state_wrong{$svc_name} = $operating_state;
+                delete $services_match->{$i}->{$svc_name};
+                $svc_name_state_wrong->{$svc_name} = $operating_state;
                 next;
             }
             $numbers++;
         }
         
-        my $exit = $self->{perfdata}->threshold_check(value => $numbers, threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-        $self->{output}->output_add(long_msg => sprintf("Service pattern '%s': service list %s",
-                                       ${$self->{option_results}->{service}}[$i],
-                                       join(', ', keys %{$services_match{$i}})));
+        my $exit = $self->{perfdata}->threshold_check(
+            value => $numbers, threshold => [
+                { label => 'critical', exit_litteral => 'critical' },
+                { label => 'warning', exit_litteral => 'warning' }
+            ]
+        );
+        $self->{output}->output_add(
+            long_msg => sprintf(
+                "Service pattern '%s': service list %s",
+                $self->{option_results}->{service}->[$i],
+                join(', ', keys %{$services_match->{$i}})
+            )
+        );
         if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
-            if (scalar(keys %svc_name_state_wrong) > 0) {
-                $self->{output}->output_add(severity => $exit,
-                                            short_msg => sprintf("Service pattern '%s' problem: %s [following services match but has the wrong state]",
-                                                                 ${$self->{option_results}->{service}}[$i], join(', ', keys %svc_name_state_wrong)));
+            if (scalar(keys %$svc_name_state_wrong) > 0) {
+                $self->{output}->output_add(
+                    severity => $exit,
+                    short_msg => sprintf(
+                        "Service pattern '%s' problem: %s [following services match but has the wrong state]",
+                        $self->{option_results}->{service}->[$i],
+                        join(', ', keys %$svc_name_state_wrong)
+                    )
+                );
             } else {
-                $self->{output}->output_add(severity => $exit,
-                                            short_msg => sprintf("Service problem '%s'", ${$self->{option_results}->{service}}[$i]));
+                $self->{output}->output_add(
+                    severity => $exit,
+                    short_msg => sprintf("Service problem '%s'", $self->{option_results}->{service}->[$i])
+                );
             }
         }
     }
