dde853acdce03ceb043ca5e3c2e0e25be7f9b97f
thibaults-centreon <tscheitenberger@centreon.com>
2021-01-22 10:56:48 +0100

    refacto with custom class

--- cloud/azure/network/loadbalancer/mode/datapath.pm
+++ cloud/azure/network/loadbalancer/mode/datapath.pm
@@ -20,111 +20,27 @@
 
 package cloud::azure::network::loadbalancer::mode::datapath;
 
-use base qw(centreon::plugins::templates::counter);
+use base qw(cloud::azure::custom::mode);
 
 use strict;
 use warnings;
 
-my %metrics_mapping = (
-    'vipavailability' => {
-        'output' => 'Data Path (VIP) Availability',
-        'label'  => 'datapath-availability-percentage',
-        'nlabel' => 'loadbalancer.datapath.availability.percentage',
-        'unit'   => '%'
-    }
-);
-
-sub custom_metric_calc {
+sub get_metrics_mapping {
     my ($self, %options) = @_;
 
-    $self->{result_values}->{timeframe} = $options{new_datas}->{$self->{instance} . '_timeframe'};
-    $self->{result_values}->{value} = $options{new_datas}->{$self->{instance} . '_' . $options{extra_options}->{metric}};
-    $self->{result_values}->{metric} = $options{extra_options}->{metric};
-    return 0;
-}
-
-sub custom_metric_threshold {
-    my ($self, %options) = @_;
-
-    my $exit = $self->{perfdata}->threshold_check(
-        value     => $self->{result_values}->{value},
-        threshold => [
-            { label => 'critical-' . $metrics_mapping{$self->{result_values}->{metric}}->{label} , exit_litteral => 'critical' },
-            { label => 'warning-' . $metrics_mapping{$self->{result_values}->{metric}}->{label}, exit_litteral => 'warning' }
-        ]
-    );
-    return $exit;
-}
-
-sub custom_metric_perfdata {
-    my ($self, %options) = @_;
-
-    $self->{output}->perfdata_add(
-        instances => $self->{instance},
-        nlabel    => $metrics_mapping{$self->{result_values}->{metric}}->{nlabel},
-        unit      => $metrics_mapping{$self->{result_values}->{metric}}->{unit},
-        value     => sprintf('%.2f', $self->{result_values}->{value}),
-        warning   => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $metrics_mapping{$self->{result_values}->{metric}}->{label}),
-        critical  => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $metrics_mapping{$self->{result_values}->{metric}}->{label})
-    );
-}
-
-sub custom_metric_output {
-    my ($self, %options) = @_;
-
-    my ($value, $unit) = ($self->{result_values}->{value}, $metrics_mapping{$self->{result_values}->{metric}}->{unit});
-
-    return sprintf('%s: %.2f %s', $metrics_mapping{$self->{result_values}->{metric}}->{output}, $value, $unit);
-}
-
-sub prefix_metric_output {
-    my ($self, %options) = @_;
-
-    return "Load Balancer '" . $options{instance_value}->{display} . "' ";
-}
-
-sub prefix_statistics_output {
-    my ($self, %options) = @_;
-
-    return "Statistic '" . $options{instance_value}->{display} . "' Metrics ";
-}
-
-sub long_output {
-    my ($self, %options) = @_;
-
-    return "Checking Load Balancer'" . $options{instance_value}->{display} . "' ";
-}
-
-sub set_counters {
-    my ($self, %options) = @_;
-
-    $self->{maps_counters_type} = [
-        { name => 'metrics', type => 3, cb_prefix_output => 'prefix_metric_output', cb_long_output => 'long_output',
-          message_multiple => 'All Data Path metrics are ok', indent_long_output => '    ',
-            group => [
-                { name => 'statistics', display_long => 1, cb_prefix_output => 'prefix_statistics_output',
-                  message_multiple => 'All metrics are ok', type => 1, skipped_code => { -10 => 1 } }
-            ]
+    my $metrics_mapping = {
+        'vipavailability' => {
+            'output' => 'Data Path (VIP) Availability',
+            'label'  => 'datapath-availability-percentage',
+            'nlabel' => 'loadbalancer.datapath.availability.percentage',
+            'unit'   => '%',
+            'min'    => '0',
+            'max'    => '100'
         }
-    ];
+    };
 
-    foreach my $metric (keys %metrics_mapping) {
-        my $entry = {
-            label => $metrics_mapping{$metric}->{label},
-            nlabel => $metrics_mapping{$metric}->{nlabel},
-            set => {
-                key_values                        => [ { name => $metric }, { name => 'timeframe' }, { name => 'display' } ],
-                closure_custom_calc               => $self->can('custom_metric_calc'),
-                closure_custom_calc_extra_options => { metric => $metric },
-                closure_custom_output             => $self->can('custom_metric_output'),
-                closure_custom_perfdata           => $self->can('custom_metric_perfdata'),
-                closure_custom_threshold_check    => $self->can('custom_metric_threshold')
-            }
-        };
-        push @{$self->{maps_counters}->{statistics}}, $entry;
-    }
+    return $metrics_mapping;
 }
-
 
 sub new {
     my ($class, %options) = @_;
@@ -132,9 +48,10 @@ sub new {
     bless $self, $class;
 
     $options{options}->add_options(arguments => {
-        'resource:s@'      => { name => 'resource' },
-        'resource-group:s' => { name => 'resource_group' },
-        'filter-metric:s'  => { name => 'filter_metric' }
+        'filter-metric:s'  => { name => 'filter_metric' },
+        'per-sec'          => { name => 'per_second'},
+        'resource:s'       => { name => 'resource' },
+        'resource-group:s' => { name => 'resource_group' }
     });
 
     return $self;
@@ -148,9 +65,15 @@ sub check_options {
         $self->{output}->add_option_msg(short_msg => 'Need to specify either --resource <name> with --resource-group option or --resource <id>.');
         $self->{output}->option_exit();
     }
+    my $resource = $self->{option_results}->{resource};
+    my $resource_group = defined($self->{option_results}->{resource_group}) ? $self->{option_results}->{resource_group} : '';
+    if ($resource =~ /^\/subscriptions\/.*\/resourceGroups\/(.*)\/providers\/Microsoft\.Network\/loadBalancers\/(.*)$/) {
+        $resource_group = $1;
+        $resource = $2;
+    }
 
-    $self->{az_resource} = $self->{option_results}->{resource};
-    $self->{az_resource_group} = $self->{option_results}->{resource_group} if (defined($self->{option_results}->{resource_group}));
+    $self->{az_resource} = $resource;
+    $self->{az_resource_group} = $resource_group;
     $self->{az_resource_type} = 'loadBalancers';
     $self->{az_resource_namespace} = 'Microsoft.Network';
     $self->{az_timeframe} = defined($self->{option_results}->{timeframe}) ? $self->{option_results}->{timeframe} : 900;
@@ -165,59 +88,11 @@ sub check_options {
         }
     }
 
-    foreach my $metric (keys %metrics_mapping) {
+    foreach my $metric (keys %{$self->{metrics_mapping}}) {
         next if (defined($self->{option_results}->{filter_metric}) && $self->{option_results}->{filter_metric} ne ''
             && $metric !~ /$self->{option_results}->{filter_metric}/);
-
         push @{$self->{az_metrics}}, $metric;
     }
-
-}
-
-sub manage_selection {
-    my ($self, %options) = @_;
-
-    my %metric_results;
-    my $raw_results;
-    foreach my $resource (@{$self->{az_resource}}) {
-        my ($resource_group, $resource_name) = ($self->{az_resource_group}, $resource);
-
-        if ($resource =~ /^\/subscriptions\/.*\/resourceGroups\/(.*)\/providers\/Microsoft\.Network\/loadBalancers\/(.*)$/) {
-            $resource_group = $1;
-            $resource_name = $2;
-        }
-
-        ($metric_results{$resource_name}, $raw_results) = $options{custom}->azure_get_metrics(
-            resource => $resource_name,
-            resource_group => $resource_group,
-            resource_type => $self->{az_resource_type},
-            resource_namespace => $self->{az_resource_namespace},
-            metrics => $self->{az_metrics},
-            aggregations => $self->{az_aggregations},
-            timeframe => $self->{az_timeframe},
-            interval => $self->{az_interval}
-        );
-
-        foreach my $metric (@{$self->{az_metrics}}) {
-            foreach my $aggregation (@{$self->{az_aggregations}}) {
-                next if (!defined($metric_results{$resource_name}->{$metric}->{lc($aggregation)}) && !defined($self->{option_results}->{zeroed}));
-
-                $self->{metrics}->{$resource_name}->{display} = $resource_name;
-                $self->{metrics}->{$resource_name}->{statistics}->{lc($aggregation)}->{display} = lc($aggregation);
-                $self->{metrics}->{$resource_name}->{statistics}->{lc($aggregation)}->{timeframe} = $self->{az_timeframe};
-                $self->{metrics}->{$resource_name}->{statistics}->{lc($aggregation)}->{$metric} =
-                    defined($metric_results{$resource_name}->{$metric}->{lc($aggregation)}) ?
-                    $metric_results{$resource_name}->{$metric}->{lc($aggregation)} : 0;
-            }
-        }
-    }
-
-    if (scalar(keys %{$self->{metrics}}) <= 0) {
-        $self->{output}->add_option_msg(short_msg => 'No metrics. Check your options or use --zeroed option to set 0 on undefined values');
-        $self->{output}->option_exit();
-    }
-
-
 }
 
 1;
@@ -234,13 +109,13 @@ Using resource name :
 
 perl centreon_plugins.pl --plugin=cloud::azure::network::loadbalancer::plugin --mode=datapath --custommode=azcli
 --resource=<loadbalancer_id> --resource-group=<resourcegroup_id> --aggregation='average'
---warning-datapath-availability-percentage=100: --critical-datapath-availability-percentage='50:'
+--warning-datapath-availability-percentage='100:' --critical-datapath-availability-percentage='50:'
 
 Using resource id :
 
 perl centreon_plugins.pl --plugin=cloud::azure::network::loadbalancer::plugin --mode=datapath --custommode=azcli
 --resource='/subscriptions/<subscription_id>/resourceGroups/<resourcegroup_id>/providers/Microsoft.Network/loadBalancers/<loadbalancer_id>'
---aggregation='average' --warning-datapath-availability-percentage=100: --critical-datapath-availability-percentage='50:'
+--aggregation='average' --warning-datapath-availability-percentage='100:' --critical-datapath-availability-percentage='50:'
 
 Default aggregation: 'average' / 'total', 'minimum' and 'maximum' are valid.
 
@@ -253,11 +128,6 @@ Set resource name or id (Required).
 =item B<--resource-group>
 
 Set resource group (Required if resource's name is used).
-
-=item B<--filter-metric>
-
-Filter on specific metrics. The Azure format must be used, for example: 'sessions_percent'
-(Can be a regexp).
 
 =item B<--warning-datapath-availability-percentage>
 
