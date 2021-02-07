aeb84bc2adc1c1ac0f24d0dfad0cc4634ac82758
PendaGTP <potergtodream@gmail.com>
2021-02-07 09:52:32 +0100

    happy new year

--- apps/centreon/map/jmx/mode/brokerstats.pm
+++ apps/centreon/map/jmx/mode/brokerstats.pm
@@ -1,4 +1,4 @@
-# Copyright 2020 Centreon (http://www.centreon.com/)
+# Copyright 2021 Centreon (http://www.centreon.com/)
 #
 # Centreon is a full-fledged industry-strength solution that meets
 # the needs in IT infrastructure and application monitoring for
