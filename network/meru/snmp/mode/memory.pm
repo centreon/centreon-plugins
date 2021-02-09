282e536d2dd6551e9fa18d5ae3ab712d8f6ebd2c
qgarnier <garnier.quentin@gmail.com>
2021-02-09 15:18:38 +0100

    add meru snmp plugin (#2584)

---
+++ network/meru/snmp/mode/memory.pm
@@ -0,0 +1,122 @@
+#
+# Copyright 2021 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::meru::snmp::mode::memory;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub custom_usage_output {
+    my ($self, %options) = @_;
+
+    return sprintf(
+        'Memory total: %s %s used: %s %s (%.2f%%) free: %s %s (%.2f%%)',
+        $self->{perfdata}->change_bytes(value => $self->{result_values}->{total}),
+        $self->{perfdata}->change_bytes(value => $self->{result_values}->{used}),
+        $self->{result_values}->{prct_used},
+        $self->{perfdata}->change_bytes(value => $self->{result_values}->{free}),
+        $self->{result_values}->{prct_free}
+    );
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'ram', type => 0, skipped_code => { -10 => 1 } }
+    ];
+
+    $self->{maps_counters}->{ram} = [
+        { label => 'usage', nlabel => 'memory.usage.bytes', set => {
+                key_values => [ { name => 'used' }, { name => 'free' }, { name => 'prct_used' }, { name => 'prct_free' }, { name => 'total' } ],
+                closure_custom_output => $self->can('custom_usage_output'),
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total', unit => 'B', cast_int => 1 }
+                ]
+            }
+        },
+        { label => 'usage-free', display_ok => 0, nlabel => 'memory.free.bytes', set => {
+                key_values => [ { name => 'free' }, { name => 'used' }, { name => 'prct_used' }, { name => 'prct_free' }, { name => 'total' } ],
+                closure_custom_output => $self->can('custom_usage_output'),
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total', unit => 'B', cast_int => 1 }
+                ]
+            }
+        },
+        { label => 'usage-prct', display_ok => 0, nlabel => 'memory.usage.percentage', set => {
+                key_values => [ { name => 'prct_used' }, { name => 'used' }, { name => 'free' }, { name => 'prct_free' }, { name => 'total' } ],
+                closure_custom_output => $self->can('custom_usage_output'),
+                perfdatas => [
+                    { template => '%.2f', min => 0, max => 100, unit => '%' }
+                ]
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+    
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $oid_total = '.1.3.6.1.4.1.15983.1.1.3.1.14.4.0'; # mwSystemResourceMemoryTotalSize
+    my $oid_free = '.1.3.6.1.4.1.15983.1.1.3.1.14.6.0'; # mwSystemResourceMemoryFreeSize
+    my $snmp_result = $options{snmp}->get_leef(
+        oids => [ $oid_total, $oid_free ],
+        nothing_quit => 1
+    );
+
+    $snmp_result->{$oid_total} *= 1024;
+    $snmp_result->{$oid_free} *= 1024;
+    $self->{ram} = {
+        total => $snmp_result->{$oid_total},
+        used => $snmp_result->{$oid_total} - $snmp_result->{$oid_free},
+        free => $snmp_result->{$oid_free},
+        prct_used => 100 - ($snmp_result->{$oid_free} * 100 / $snmp_result->{$oid_total}),
+        prct_free => $snmp_result->{$oid_free} * 100 / $snmp_result->{$oid_total}
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check memory.
+
+=over 8
+
+=item B<--warning-*> B<--critical-*>
+
+Thresholds.
+Can be: 'usage' (B), 'usage-free' (B), 'usage-prct' (%).
+
+=back
+
+=cut
