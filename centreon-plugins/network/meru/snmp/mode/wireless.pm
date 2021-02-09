86a68e6e6275816badc4403d906ac76e6d0e0ac8
qgarnier <garnier.quentin@gmail.com>
2021-02-09 15:18:38 +0100

    add meru snmp plugin (#2584)

--- centreon-plugins/network/meru/snmp/mode/wireless.pm
+++ centreon-plugins/network/meru/snmp/mode/wireless.pm
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-package network::fiberstore::snmp::mode::cpu;
+package network::meru::snmp::mode::wireless;
 
 use base qw(centreon::plugins::templates::counter);
 
@@ -29,18 +29,26 @@ sub set_counters {
     my ($self, %options) = @_;
 
     $self->{maps_counters_type} = [
-        { name => 'global', type => 0, skipped_code => { -10 => 1 } }
+        { name => 'global', type => 0, message_separator => ' - ', skipped_code => { -10 => 1 } }
     ];
 
     $self->{maps_counters}->{global} = [
-        { label => 'cpu-utilization', nlabel => 'cpu.utilization.percentage', set => {
-                key_values => [ { name => 'cpu_util' } ],
-                output_template => 'Cpu utilization: %.2f%%',
+        { label => 'accesspoints-online', nlabel => 'accesspoints.online.count', set => {
+                key_values => [ { name => 'online_ap' } ],
+                output_template => 'number of online access points: %s',
                 perfdatas => [
-                    { template => '%s', min => 0, max => 100, unit => '%' }
+                    { template => '%s', min => 0 }
                 ]
             }
-        }
+        },
+        { label => 'stations-wireless', nlabel => 'stations.wireless.count', set => {
+                key_values => [ { name => 'wireless_stations' } ],
+                output_template => 'number of wireless stations: %s',
+                perfdatas => [
+                    { template => '%s', min => 0 }
+                ]
+            }
+        },
     ];
 }
 
@@ -48,13 +56,13 @@ sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
-    
+
     return $self;
 }
 
 my $mapping = {
-    idle_legacy => { oid => '.1.3.6.1.4.1.27975.1.2.11' }, # ssCpuIdle
-    idle        => { oid => '.1.3.6.1.4.1.52642.1.1.2.11' }  # ssCpuIdle
+    online_ap         => { oid => '.1.3.6.1.4.1.15983.1.1.3.1.13.9' }, # mwSystemGeneralTotalOnlineAps
+    wireless_stations => { oid => '.1.3.6.1.4.1.15983.1.1.3.1.13.11' }  # mwSystemGeneralTotalWirelessStations
 };
 
 sub manage_selection {
@@ -64,13 +72,7 @@ sub manage_selection {
         oids => [ map($_->{oid} . '.0', values(%$mapping)) ],
         nothing_quit => 1
     );
-    my $result = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => 0);
-    my $idle = 'idle';
-    $idle = 'idle_legacy' if (!defined($result->{idle}));
-
-    $self->{global} = {
-        cpu_util => 100 - $result->{$idle}
-    };
+    $self->{global} = $options{snmp}->map_instance(mapping => $mapping, results => $snmp_result, instance => 0);
 }
 
 1;
@@ -79,14 +81,14 @@ __END__
 
 =head1 MODE
 
-Check cpu.
+Check wireless statistics.
 
 =over 8
 
 =item B<--warning-*> B<--critical-*>
 
 Thresholds.
-Can be: 'cpu-utilization' (%).
+Can be: 'accesspoints-online', 'stations-wireless'.
 
 =back
 
