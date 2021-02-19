d3ac05e6e66abb3a7820b7ff7c438551488f35cd
qgarnier <garnier.quentin@gmail.com>
2021-02-19 11:24:25 +0100

    Fix #2006 (#2600)

--- apps/antivirus/mcafee/webgateway/snmp/mode/ftpstatistics.pm
+++ apps/antivirus/mcafee/webgateway/snmp/mode/ftpstatistics.pm
@@ -98,15 +98,17 @@ my $oid_stFtpBytesToServer = '.1.3.6.1.4.1.1230.2.7.2.4.5.0';
 sub manage_selection {
     my ($self, %options) = @_;
 
-    $self->{cache_name} = "mcafee_" . $options{snmp}->get_hostname()  . '_' . $options{snmp}->get_port() . '_' . $self->{mode} . '_' .
+    $self->{cache_name} = 'mcafee_' . $options{snmp}->get_hostname()  . '_' . $options{snmp}->get_port() . '_' . $self->{mode} . '_' .
         (defined($self->{option_results}->{filter_name}) ? md5_hex($self->{option_results}->{filter_name}) : md5_hex('all')) . '_' .
         (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all'));
 
-    my $results = $options{snmp}->get_leef(oids => [ $oid_stFtpBytesFromClient, $oid_stFtpBytesFromServer,
-                                                     $oid_stFtpBytesToClient, $oid_stFtpBytesToServer ], 
-                                           nothing_quit => 1);
-    
-    $self->{traffics} = {};
+    my $results = $options{snmp}->get_leef(
+        oids => [
+            $oid_stFtpBytesFromClient, $oid_stFtpBytesFromServer,
+            $oid_stFtpBytesToClient, $oid_stFtpBytesToServer
+        ], 
+        nothing_quit => 1
+    );
 
     $self->{traffics} = {
         stFtpBytesFromClient => $results->{$oid_stFtpBytesFromClient} * 8,
--- apps/antivirus/mcafee/webgateway/snmp/mode/system.pm
+++ apps/antivirus/mcafee/webgateway/snmp/mode/system.pm
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-package apps::antivirus::mcafee::webgateway::snmp::mode::clients;
+package apps::antivirus::mcafee::webgateway::snmp::mode::system;
 
 use base qw(centreon::plugins::templates::counter);
 
@@ -33,21 +33,19 @@ sub set_counters {
     ];
 
     $self->{maps_counters}->{global} = [
-        { label => 'clients', nlabel => 'clients.connected.count', set => {
-                key_values => [ { name => 'stClientCount' } ],
-                output_template => 'Connected clients: %d',
+        { label => 'cpu-utilization', nlabel => 'system.cpu.utilization.percentage', set => {
+                key_values => [ { name => 'cpu_util' } ],
+                output_template => 'cpu usage: %.2f%%',
                 perfdatas => [
-                    { label => 'connected_clients', template => '%d',
-                      min => 0, unit => 'clients' }
+                    { template => '%.2f', unit => '%', min => 0, max => 100 }
                 ]
             }
         },
-        { label => 'sockets', nlabel => 'sockets.connected.count', set => {
-                key_values => [ { name => 'stConnectedSockets' } ],
-                output_template => 'Open network sockets: %d',
+        { label => 'dns-resolve-time', nlabel => 'system.dns.resolve.time.milliseconds', set => {
+                key_values => [ { name => 'dns_time' } ],
+                output_template => 'time to resolve dns: %sms',
                 perfdatas => [
-                    { label => 'open_sockets', template => '%d',
-                      min => 0, unit => 'sockets' }
+                    { template => '%s', unit => 'ms', min => 0 }
                 ]
             }
         }
@@ -56,7 +54,7 @@ sub set_counters {
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
 
     $options{options}->add_options(arguments => {
@@ -64,20 +62,20 @@ sub new {
     return $self;
 }
 
-my $oid_stClientCount = '.1.3.6.1.4.1.1230.2.7.2.5.2.0';
-my $oid_stConnectedSockets = '.1.3.6.1.4.1.1230.2.7.2.5.3.0';
+my $oid_stCPULoad = '.1.3.6.1.4.1.1230.2.7.2.5.1.0';
+my $oid_stResolveHostViaDNS = '.1.3.6.1.4.1.1230.2.7.2.5.6.0';
 
 sub manage_selection {
     my ($self, %options) = @_;
 
     my $results = $options{snmp}->get_leef(
-        oids => [ $oid_stClientCount, $oid_stConnectedSockets ], 
+        oids => [ $oid_stCPULoad, $oid_stResolveHostViaDNS ], 
         nothing_quit => 1
     );
 
     $self->{global} = {
-        stClientCount => $results->{$oid_stClientCount},
-        stConnectedSockets => $results->{$oid_stConnectedSockets},
+        cpu_util => $results->{$oid_stCPULoad},
+        dns_time => $results->{$oid_stResolveHostViaDNS}
     };
 }
 
@@ -87,24 +85,19 @@ __END__
 
 =head1 MODE
 
-Check connected clients and open network sockets.
+Check system.
 
 =over 8
 
 =item B<--filter-counters>
 
 Only display some counters (regexp can be used).
-(Example: --filter-counters='clients')
+(Example: --filter-counters='cpu')
 
-=item B<--warning-*>
+=item B<--warning-*> B<--critical-*>
 
-Threshold warning.
-Can be: 'clients', 'sockets'.
-
-=item B<--critical-*>
-
-Threshold critical.
-Can be: 'clients', 'sockets'.
+Thresholds.
+Can be: 'cpu-utilization', 'dns-resolve-time'.
 
 =back
 
