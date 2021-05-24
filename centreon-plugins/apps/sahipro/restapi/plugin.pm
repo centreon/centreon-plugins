c4d6ecc44275d22e279c3bac18687cffbc0791ce
itoussies <65223458+itoussies@users.noreply.github.com>
2021-05-24 17:56:49 +0200

    enh(plugin): SAHI Pro REST API metricsv2 (#2821)
    * enh(plugin): SAHI Pro REST API metricsv2
    
    * nlabel name fix

--- centreon-plugins/apps/sahipro/restapi/plugin.pm
+++ centreon-plugins/apps/sahipro/restapi/plugin.pm
@@ -30,9 +30,9 @@ sub new {
     bless $self, $class;
 
     $self->{version} = '0.1';
-    %{$self->{modes}} = (
-        'scenario' => 'apps::sahipro::restapi::mode::scenario',
-    );
+    $self->{modes} => {
+        'scenario' => 'apps::sahipro::restapi::mode::scenario'
+    };
 
     return $self;
 }
