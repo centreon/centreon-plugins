343e0915e6fee99464e45ddff64b74362457539f
matoy <matoy@users.noreply.github.com>
2021-06-08 19:54:55 +0200

    Update cloud/azure/storage/storagesync/mode/heartbeat.pm
    Co-authored-by: itoussies <65223458+itoussies@users.noreply.github.com>

--- cloud/azure/storage/storagesync/mode/heartbeat.pm
+++ cloud/azure/storage/storagesync/mode/heartbeat.pm
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-package cloud::azure::storage::storagesync::mode::heartbeat;
+package cloud::azure::storage::storagesync::mode::serverstatus;
 
 use base qw(cloud::azure::custom::mode);
 
