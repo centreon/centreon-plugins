4da4727ce39b9fb69962aef7d7ca7aa554e7884f
garnier-quentin <garnier.quentin@gmail.com>
2021-06-29 15:21:42 +0200

    wip

--- cloud/aws/ebs/mode/volumeio.pm
+++ cloud/aws/ebs/mode/volumeio.pm
@@ -20,139 +20,47 @@
 
 package cloud::aws::ebs::mode::volumeio;
 
-use base qw(centreon::plugins::templates::counter);
+use base qw(cloud::aws::custom::mode);
 
 use strict;
 use warnings;
 
-my %metrics_mapping = (
-    'VolumeReadBytes' => {
-        'output'    => 'Volume Read Bytes',
-        'label'     => 'volume-read-bytes',
-        'nlabel'    => {
-            'absolute'      => 'ebs.volume.bytes.read.bytes',
-            'per_second'    => 'ebs.volume.bytes.read.bytespersecond'
+sub get_metrics_mapping {
+    my ($self, %options) = @_;
+
+    my $metrics_mapping = {
+        extra_params => {
+            message_multiple => 'All EBS metrics are ok'
         },
-        'unit'      => 'B'
-    },
-    'VolumeWriteBytes' => {
-        'output'    => 'Volume Write Bytes',
-        'label'     => 'volume-write-bytes',
-        'nlabel'    => {
-            'absolute'      => 'ebs.volume.bytes.write.bytes',
-            'per_second'    => 'ebs.volume.bytes.write.bytespersecond'
-        },
-        'unit'      => 'B'
-    }
-);
+        metrics => {
+            'VolumeReadBytes' => {
+                'output'    => 'Volume Read Bytes',
+                'label'     => 'volume-read-bytes',
+                'nlabel'    => {
+                    'absolute'      => 'ebs.volume.bytes.read.bytes',
+                    'per_second'    => 'ebs.volume.bytes.read.bytespersecond'
+                },
+                'unit'      => 'B'
+            },
+            'VolumeWriteBytes' => {
+                'output'    => 'Volume Write Bytes',
+                'label'     => 'volume-write-bytes',
+                'nlabel'    => {
+                    'absolute'      => 'ebs.volume.bytes.write.bytes',
+                    'per_second'    => 'ebs.volume.bytes.write.bytespersecond'
+                },
+                'unit'      => 'B'
+            }
+        }
+    };
 
-
-sub custom_metric_calc {
-    my ($self, %options) = @_;
-
-    $self->{result_values}->{timeframe} = $options{new_datas}->{$self->{instance} . '_timeframe'};
-    $self->{result_values}->{value} = $options{new_datas}->{$self->{instance} . '_' . $options{extra_options}->{metric}};
-    $self->{result_values}->{value_per_sec} = $self->{result_values}->{value} / $self->{result_values}->{timeframe};
-    $self->{result_values}->{metric} = $options{extra_options}->{metric};
-    return 0;
-}
-
-sub custom_metric_threshold {
-    my ($self, %options) = @_;
-
-    my $exit = $self->{perfdata}->threshold_check(
-        value       => defined($self->{instance_mode}->{option_results}->{per_sec}) ? $self->{result_values}->{value_per_sec} : $self->{result_values}->{value},
-        threshold   => [
-            { label => 'critical-' . $metrics_mapping{$self->{result_values}->{metric}}->{label} , exit_litteral => 'critical' },
-            { label => 'warning-' . $metrics_mapping{$self->{result_values}->{metric}}->{label}, exit_litteral => 'warning' }
-        ]
-    );
-    return $exit;
-}
-
-sub custom_metric_perfdata {
-    my ($self, %options) = @_;
-
-    $self->{output}->perfdata_add(
-        instances => $self->{instance},
-        label       => $metrics_mapping{$self->{result_values}->{metric}}->{label},
-        nlabel      => defined($self->{instance_mode}->{option_results}->{per_sec}) ?
-            $metrics_mapping{$self->{result_values}->{metric}}->{nlabel}->{per_second} :
-            $metrics_mapping{$self->{result_values}->{metric}}->{nlabel}->{absolute},
-        unit        => defined($self->{instance_mode}->{option_results}->{per_sec}) ?
-            $metrics_mapping{$self->{result_values}->{metric}}->{unit} . '/s' :
-            $metrics_mapping{$self->{result_values}->{metric}}->{unit},
-        value       => sprintf("%.2f", defined($self->{instance_mode}->{option_results}->{per_sec}) ?
-            $self->{result_values}->{value_per_sec} :
-            $self->{result_values}->{value}),
-        warning     => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $metrics_mapping{$self->{result_values}->{metric}}->{label}),
-        critical    => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $metrics_mapping{$self->{result_values}->{metric}}->{label}),
-    );
-}
-
-sub custom_metric_output {
-    my ($self, %options) = @_;
-    my $msg = "";
-
-    if (defined($self->{instance_mode}->{option_results}->{per_sec})) {
-        my ($value, $unit) = ($metrics_mapping{$self->{result_values}->{metric}}->{unit} eq 'B') ?
-            $self->{perfdata}->change_bytes(value => $self->{result_values}->{value_per_sec}) :
-            ($self->{result_values}->{value_per_sec}, $metrics_mapping{$self->{result_values}->{metric}}->{unit});
-        $msg = sprintf("%s: %.2f %s", $metrics_mapping{$self->{result_values}->{metric}}->{output}, $value, $unit . '/s');
-    } else {
-        my ($value, $unit) = ($metrics_mapping{$self->{result_values}->{metric}}->{unit} eq 'B') ?
-            $self->{perfdata}->change_bytes(value => $self->{result_values}->{value}) :
-            ($self->{result_values}->{value}, $metrics_mapping{$self->{result_values}->{metric}}->{unit});
-        $msg = sprintf("%s: %.2f %s", $metrics_mapping{$self->{result_values}->{metric}}->{output}, $value, $unit);
-    }
-    return $msg;
-}
-
-sub prefix_metric_output {
-    my ($self, %options) = @_;
-
-    return "'" . $options{instance_value}->{display} . "' ";
-}
-
-sub prefix_statistics_output {
-    my ($self, %options) = @_;
-
-    return "Statistic '" . $options{instance_value}->{display} . "' Metrics ";
+    return $metrics_mapping;
 }
 
 sub long_output {
     my ($self, %options) = @_;
 
-    return "AWS EBS Volume'" . $options{instance_value}->{display} . "' ";
-}
-
-sub set_counters {
-    my ($self, %options) = @_;
-
-    $self->{maps_counters_type} = [
-        { name => 'metrics', type => 3, cb_prefix_output => 'prefix_metric_output', cb_long_output => 'long_output',
-          message_multiple => 'All EBS metrics are ok', indent_long_output => '    ',
-            group => [
-                { name => 'statistics', display_long => 1, cb_prefix_output => 'prefix_statistics_output',
-                  message_multiple => 'All metrics are ok', type => 1, skipped_code => { -10 => 1 } },
-            ]
-        }
-    ];
-
-    foreach my $metric (keys %metrics_mapping) {
-        my $entry = {
-            label => $metrics_mapping{$metric}->{label},
-            set => {
-                key_values                          => [ { name => $metric }, { name => 'timeframe' }, { name => 'display' } ],
-                closure_custom_calc                 => $self->can('custom_metric_calc'),
-                closure_custom_calc_extra_options   => { metric => $metric },
-                closure_custom_output               => $self->can('custom_metric_output'),
-                closure_custom_perfdata             => $self->can('custom_metric_perfdata'),
-                closure_custom_threshold_check      => $self->can('custom_metric_threshold')
-            }
-        };
-        push @{$self->{maps_counters}->{statistics}}, $entry;
-    }
+    return "AWS EBS Volume '" . $options{instance_value}->{display} . "' ";
 }
 
 sub new {
@@ -161,9 +69,7 @@ sub new {
     bless $self, $class;
 
     $options{options}->add_options(arguments => {
-        'volume-id:s@'       => { name => 'volume_id' },
-        'per-sec'            => { name => 'per_sec' },
-        'filter-metric:s'    => { name => 'filter_metric' }
+        'volume-id:s@' => { name => 'volume_id' }
     });
 
     return $self;
@@ -183,24 +89,6 @@ sub check_options {
             push @{$self->{aws_instance}}, $instance;
         };
     }
-
-    $self->{aws_timeframe} = defined($self->{option_results}->{timeframe}) ? $self->{option_results}->{timeframe} : 600;
-    $self->{aws_period} = defined($self->{option_results}->{period}) ? $self->{option_results}->{period} : 60;
-
-    $self->{aws_statistics} = ['Average'];
-    if (defined($self->{option_results}->{statistic})) {
-        $self->{aws_statistics} = [];
-        foreach my $stat (@{$self->{option_results}->{statistic}}) {
-            if ($stat ne '') {
-                push @{$self->{aws_statistics}}, ucfirst(lc($stat));
-            }
-        }
-    };
-    foreach my $metric (keys %metrics_mapping) {
-        next if (defined($self->{option_results}->{filter_metric}) && $self->{option_results}->{filter_metric} ne ''
-            && $metric !~ /$self->{option_results}->{filter_metric}/);
-        push @{$self->{aws_metrics}}, $metric;
-    };
 }
 
 sub manage_selection {
