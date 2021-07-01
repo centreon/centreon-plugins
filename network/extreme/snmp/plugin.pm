3761cf7543ea61ec8018b049853431946a0fbeae
garnier-quentin <garnier.quentin@gmail.com>
2021-07-01 15:32:31 +0200

    enh(extreme/snmp): interfaces mode - add congestion dropped

--- network/extreme/snmp/plugin.pm
+++ network/extreme/snmp/plugin.pm
@@ -30,14 +30,14 @@ sub new {
     bless $self, $class;
 
     $self->{version} = '1.0';
-    %{$self->{modes}} = (
-        'cpu'              => 'network::extreme::snmp::mode::cpu',
-        'hardware'         => 'network::extreme::snmp::mode::hardware',
-        'interfaces'       => 'snmp_standard::mode::interfaces',
-        'list-interfaces'  => 'snmp_standard::mode::listinterfaces',
-        'memory'           => 'network::extreme::snmp::mode::memory',
-        'stack'            => 'network::extreme::snmp::mode::stack',
-    );
+    $self->{modes} = {
+        'cpu'             => 'network::extreme::snmp::mode::cpu',
+        'hardware'        => 'network::extreme::snmp::mode::hardware',
+        'interfaces'      => 'network::extreme::snmp::mode::interfaces',
+        'list-interfaces' => 'snmp_standard::mode::listinterfaces',
+        'memory'          => 'network::extreme::snmp::mode::memory',
+        'stack'           => 'network::extreme::snmp::mode::stack'
+    };
 
     return $self;
 }
