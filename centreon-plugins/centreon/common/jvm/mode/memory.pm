1a701de7f4bf3af5b89d17f4b4e60bc9a79bd39b
qgarnier <garnier.quentin@gmail.com>
2021-08-03 14:17:40 +0200

    break(jvm/jmx): force metric v2 - tomcat/jboss/weblogic (#3011)

--- centreon-plugins/centreon/common/jvm/mode/memory.pm
+++ centreon-plugins/centreon/common/jvm/mode/memory.pm
@@ -25,42 +25,12 @@ use base qw(centreon::plugins::templates::counter);
 use strict;
 use warnings;
 
-sub set_counters {
-    my ($self, %options) = @_;
-    
-    $self->{maps_counters_type} = [
-        { name => 'heap', type => 0 },
-        { name => 'nonheap', type => 0 },
-    ];
-    
-    $self->{maps_counters}->{heap} = [
-        { label => 'heap', set => {
-                key_values => [ { name => 'used' }, { name => 'max' }, { name => 'label' } ],
-                closure_custom_calc => $self->can('custom_usage_calc'),
-                closure_custom_output => $self->can('custom_usage_output'),
-                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
-                closure_custom_threshold_check => $self->can('custom_usage_threshold'),
-            }
-        },
-    ];
-    $self->{maps_counters}->{nonheap} = [
-        { label => 'nonheap', set => {
-                key_values => [ { name => 'used' }, { name => 'max' }, { name => 'label' } ],
-                closure_custom_calc => $self->can('custom_usage_calc'),
-                closure_custom_output => $self->can('custom_usage_output'),
-                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
-                closure_custom_threshold_check => $self->can('custom_usage_threshold'),
-            }
-        },
-    ];
-}
-
 sub custom_usage_perfdata {
     my ($self, %options) = @_;
     
     my $use_th = 1;
     $use_th = 0 if ($self->{instance_mode}->{option_results}->{units} eq '%' && $self->{result_values}->{max} <= 0);
-    
+
     my $value_perf = $self->{result_values}->{used};
     my %total_options = ();
     if ($self->{instance_mode}->{option_results}->{units} eq '%' && $self->{result_values}->{max} > 0) {
@@ -68,16 +38,19 @@ sub custom_usage_perfdata {
         $total_options{cast_int} = 1;
     }
 
-    $self->{output}->perfdata_add(label => $self->{result_values}->{label}, unit => 'B',
-                                  value => $value_perf,
-                                  warning => $use_th == 1 ? $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{label}, %total_options) : undef,
-                                  critical => $use_th == 1 ? $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{label}, %total_options) : undef,
-                                  min => 0, max => $self->{result_values}->{max} > 0 ? $self->{result_values}->{max} : undef);
+    $self->{output}->perfdata_add(
+        nlabel => $self->{nlabel},
+        unit => 'B',
+        value => $value_perf,
+        warning => $use_th == 1 ? $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{thlabel}, %total_options) : undef,
+        critical => $use_th == 1 ? $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{thlabel}, %total_options) : undef,
+        min => 0, max => $self->{result_values}->{max} > 0 ? $self->{result_values}->{max} : undef
+    );
 }
 
 sub custom_usage_threshold {
     my ($self, %options) = @_;
-    
+
     # Cannot use percent without total
     return 'ok' if ($self->{result_values}->{max} <= 0 && $self->{instance_mode}->{option_results}->{units} eq '%');
     my ($exit, $threshold_value);
@@ -85,7 +58,12 @@ sub custom_usage_threshold {
     if ($self->{instance_mode}->{option_results}->{units} eq '%') {
         $threshold_value = $self->{result_values}->{prct_used};
     }
-    $exit = $self->{perfdata}->threshold_check(value => $threshold_value, threshold => [ { label => 'critical-' . $self->{label}, exit_litteral => 'critical' }, { label => 'warning-'. $self->{label}, exit_litteral => 'warning' } ]);
+    $exit = $self->{perfdata}->threshold_check(
+        value => $threshold_value, threshold => [
+            { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' },
+            { label => 'warning-'. $self->{thlabel}, exit_litteral => 'warning' }
+        ]
+    );
     return $exit;
 }
 
@@ -97,14 +75,20 @@ sub custom_usage_output {
     if ($self->{result_values}->{max} > 0) {
         my ($total_size_value, $total_size_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{max});
         my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{max} - $self->{result_values}->{used});
-        $msg = sprintf("%s Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)", $self->{result_values}->{label},
-                   $total_size_value . " " . $total_size_unit,
-                   $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used},
-                   $total_free_value . " " . $total_free_unit, 100 - $self->{result_values}->{prct_used});
+        $msg = sprintf(
+            "%s Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)", $self->{result_values}->{label},
+            $total_size_value . " " . $total_size_unit,
+            $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used},
+            $total_free_value . " " . $total_free_unit, 100 - $self->{result_values}->{prct_used}
+        );
     } else {
-        $msg = sprintf("%s Used: %s", $self->{result_values}->{label},
-                   $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used});
+        $msg = sprintf(
+            "%s Used: %s",
+            $self->{result_values}->{label},
+            $total_used_value . " " . $total_used_unit
+        );
     }
+
     return $msg;
 }
 
@@ -114,7 +98,7 @@ sub custom_usage_calc {
     $self->{result_values}->{label} = $options{new_datas}->{$self->{label} . '_label'};    
     $self->{result_values}->{max} = $options{new_datas}->{$self->{instance} . '_max'};
     $self->{result_values}->{used} = $options{new_datas}->{$self->{instance} . '_used'};
-    
+
     if ($self->{result_values}->{max} > 0) {
         $self->{result_values}->{prct_used} = $self->{result_values}->{used} * 100 / $self->{result_values}->{max};
     }
@@ -122,13 +106,44 @@ sub custom_usage_calc {
     return 0;
 }
 
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'heap', type => 0 },
+        { name => 'nonheap', type => 0 }
+    ];
+
+    $self->{maps_counters}->{heap} = [
+        { label => 'heap', nlabel => 'memory.heap.usage.bytes', set => {
+                key_values => [ { name => 'used' }, { name => 'max' }, { name => 'label' } ],
+                closure_custom_calc => $self->can('custom_usage_calc'),
+                closure_custom_output => $self->can('custom_usage_output'),
+                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_usage_threshold')
+            }
+        },
+    ];
+
+    $self->{maps_counters}->{nonheap} = [
+        { label => 'nonheap', nlabel => 'memory.nonheap.usage.bytes', set => {
+                key_values => [ { name => 'used' }, { name => 'max' }, { name => 'label' } ],
+                closure_custom_calc => $self->can('custom_usage_calc'),
+                closure_custom_output => $self->can('custom_usage_output'),
+                closure_custom_perfdata => $self->can('custom_usage_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_usage_threshold')
+            }
+        }
+    ];
+}
+
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
-    
+
     $options{options}->add_options(arguments => {
-        "units:s"   => { name => 'units', default => '%' },
+        'units:s' => { name => 'units', default => '%' }
     });
 
     return $self;
@@ -138,13 +153,13 @@ sub manage_selection {
     my ($self, %options) = @_;
 
     $self->{request} = [
-         { mbean => "java.lang:type=Memory" }
+         { mbean => 'java.lang:type=Memory' }
     ];
 
     my $result = $options{custom}->get_attributes(request => $self->{request}, nothing_quit => 1);
     
-    $self->{heap} = { label => 'HeapMemory', used => $result->{"java.lang:type=Memory"}->{HeapMemoryUsage}->{used}, max => $result->{"java.lang:type=Memory"}->{HeapMemoryUsage}->{max} };
-    $self->{nonheap} = { label => 'NonHeapMemoryUsage', used => $result->{"java.lang:type=Memory"}->{NonHeapMemoryUsage}->{used}, max => $result->{"java.lang:type=Memory"}->{NonHeapMemoryUsage}->{max}  };
+    $self->{heap} = { label => 'HeapMemory', used => $result->{'java.lang:type=Memory'}->{HeapMemoryUsage}->{used}, max => $result->{'java.lang:type=Memory'}->{HeapMemoryUsage}->{max} };
+    $self->{nonheap} = { label => 'NonHeapMemoryUsage', used => $result->{'java.lang:type=Memory'}->{NonHeapMemoryUsage}->{used}, max => $result->{'java.lang:type=Memory'}->{NonHeapMemoryUsage}->{max}  };
 }
 
 1;
@@ -183,4 +198,3 @@ Units of thresholds (Default: '%') ('%', 'B').
 =back
 
 =cut
-
