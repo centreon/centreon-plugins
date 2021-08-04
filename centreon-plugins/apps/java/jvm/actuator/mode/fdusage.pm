b446d1ce9241f16e7a10ebfc2b98a7c7d06dc478
qgarnier <garnier.quentin@gmail.com>
2021-08-04 11:16:59 +0200

    add(plugin): jvm actuator (#3016)

---
+++ centreon-plugins/apps/java/jvm/actuator/mode/fdusage.pm
@@ -0,0 +1,115 @@
+#
+# Copyright 2021 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::java::jvm::actuator::mode::fdusage;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub custom_usage_output {
+    my ($self, %options) = @_;
+
+    return sprintf(
+        "File descriptor usage total: %s used: %s (%.2f%%) free: %s (%.2f%%)",
+        $self->{result_values}->{total},
+        $self->{result_values}->{used}, $self->{result_values}->{prct_used},
+        $self->{result_values}->{free}, $self->{result_values}->{prct_free}
+    );
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 }
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'usage', nlabel => 'fd.opened.usage.count', set => {
+                key_values => [ { name => 'used' }, { name => 'free' }, { name => 'prct_used' }, { name => 'prct_free' }, { name => 'total' } ],
+                closure_custom_output => $self->can('custom_usage_output'),
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total', cast_int => 1 },
+                ]
+            }
+        },
+        { label => 'usage-free', display_ok => 0, nlabel => 'fd.opened.free.count', set => {
+                key_values => [ { name => 'free' }, { name => 'used' }, { name => 'prct_used' }, { name => 'prct_free' }, { name => 'total' } ],
+                closure_custom_output => $self->can('custom_usage_output'),
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total', cast_int => 1 }
+                ]
+            }
+        },
+        { label => 'usage-prct', display_ok => 0, nlabel => 'fd.opened.usage.percentage', set => {
+                key_values => [ { name => 'prct_used' }, { name => 'used' }, { name => 'free' }, { name => 'prct_free' }, { name => 'total' } ],
+                output_template => 'Used : %.2f %%',
+                perfdatas => [
+                    { template => '%.2f', min => 0, max => 100, unit => '%' }
+                ]
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {});
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->request_api(endpoint => '/metrics/process.files.open');
+    $self->{global} = { used => $result->{measurements}->[0]->{value} };
+
+    $result = $options{custom}->request_api(endpoint => '/metrics/process.files.max');
+    $self->{global}->{total} = $result->{measurements}->[0]->{value};
+    $self->{global}->{free} = $self->{global}->{total} - $self->{global}->{used};
+    $self->{global}->{prct_used} = $self->{global}->{used} / $self->{global}->{total} * 100;
+    $self->{global}->{prct_free} = 100 - $self->{global}->{prct_used};
+
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check number/percentage of file descriptors.
+
+=over 8
+
+=item B<--warning-*> B<--critical-*>
+
+Thresholds.
+Can be: 'usage', 'usage-free', 'usage-prct' (%).
+
+=back
+
+=cut
---
+++ centreon-plugins/apps/java/jvm/actuator/mode/loadaverage.pm
@@ -0,0 +1,89 @@
+#
+# Copyright 2021 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::java::jvm::actuator::mode::loadaverage;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0 }
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'load1', nlabel => 'system.load.1m.count', set => {
+                key_values => [ { name => 'load' } ],
+                output_template => 'System load average: %.2f (1m)',
+                perfdatas => [
+                    { template => '%.2f', min => 0 }
+                ]
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {});
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->request_api(endpoint => '/metrics/system.load.average.1m');
+    if ($result->{measurements}->[0]->{value} == -1) {
+        $self->{output}->add_option_msg(short_msg => 'System load average is not set');
+        $self->{output}->option_exit();
+    }
+
+    $self->{global} = { load => $result->{measurements}->[0]->{value} };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check system load average.
+
+=over 8
+
+=item B<--warning-load1>
+
+Warning threshold for loadaverage
+
+=item B<--critical-load1>
+
+Critical threshold for loadaverage
+
+=back
+
+=cut
