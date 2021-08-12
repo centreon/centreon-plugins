d14888a0f96f35028304c98aeb77e561f4249ea4
qgarnier <garnier.quentin@gmail.com>
2021-08-12 09:02:32 +0200

    enh(broadcom/fastpath): new metric labels (#3041)

--- centreon-plugins/centreon/common/broadcom/fastpath/snmp/mode/hardware.pm
+++ centreon-plugins/centreon/common/broadcom/fastpath/snmp/mode/hardware.pm
@@ -40,7 +40,7 @@ sub set_system {
             ['notpowering', 'WARNING'],
             ['powering', 'OK'],
             ['nopower', 'OK'],
-            ['incompatible', 'WARNING'],
+            ['incompatible', 'WARNING']
         ],
         temperature => [        
             ['low', 'OK'],
@@ -49,8 +49,8 @@ sub set_system {
             ['critical', 'CRITICAL'],
             ['notpresent', 'OK'],
             ['shutdown', 'OK'],
-            ['notoperational', 'WARNING'],
-        ],
+            ['notoperational', 'WARNING']
+        ]
     };
     
     $self->{components_path} = 'centreon::common::broadcom::fastpath::snmp::mode::components';
@@ -66,12 +66,12 @@ sub snmp_execute {
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
-    
+
     $options{options}->add_options(arguments => { 
     });
-    
+
     return $self;
 }
 
