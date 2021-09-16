78ae0e8994dd51bdadd9b41c73f89540591497e8
qgarnier <garnier.quentin@gmail.com>
2021-09-16 14:36:20 +0200

    enh(collection): add builtin variables (#3113)

--- apps/protocols/snmp/mode/collection.pm
+++ apps/protocols/snmp/mode/collection.pm
@@ -97,7 +97,7 @@ sub custom_select_output {
     # without formatting: [name: xxxxxx][test: xxxx][test2: xxx][mytable.plcRead: xxx][mytable.plcWrite: xxx]
     my $output = '';
     foreach (sort keys %{$self->{result_values}->{expand}}) {
-        next if (/^constants\./);
+        next if (/^(?:constants|builtin)\./);
         $output .= '[' . $_ . ': ' . $self->{result_values}->{expand}->{$_} . ']';
     }
 
@@ -138,6 +138,8 @@ sub new {
 
     $self->{safe_func} = Safe->new();
     $self->{safe_func}->share('$assign_var');
+
+    $self->{builtin} = {};
 
     $self->{snmp_cache} = centreon::plugins::statefile->new(%options);
     return $self;
@@ -401,6 +403,7 @@ sub collect_snmp {
         $self->{output}->option_exit();
     }
 
+    $self->add_builtin(name => 'currentTime', value => time());
     if ($self->use_snmp_cache(snmp => $options{snmp}) == 0) {
         $self->{snmp_collected_sampling} = { tables => {}, leefs => {}, epoch => time() };
         $self->{snmp_collected} = { tables => {}, leefs => {}, epoch => time(), sampling => 0 };
@@ -751,6 +754,20 @@ sub substitute_string {
     }
 
     return $str;
+}
+
+sub add_builtin {
+    my ($self, %options) = @_;
+
+    $self->{builtin}->{ $options{name} } = $options{value};
+}
+
+sub set_builtin {
+    my ($self, %options) = @_;
+
+    foreach (keys %{$self->{builtin}}) {
+        $self->{expand}->{ 'builtin.' . $_ } = $self->{builtin}->{$_};
+    }
 }
 
 sub set_constants {
@@ -1298,6 +1315,7 @@ sub add_selection {
         $i++;
         my $config = {};
         $self->{expand} = $self->set_constants();
+        $self->set_builtin();
         $self->{expand}->{name} = $_->{name} if (defined($_->{name}));
         $self->set_functions(section => "selection > $i > functions", functions => $_->{functions}, position => 'before_expand');
         $self->set_expand_table(section => "selection > $i > expand_table", expand => $_->{expand_table});
@@ -1336,6 +1354,7 @@ sub add_selection_loop {
 
         foreach my $instance (keys %{$self->{snmp_collected}->{tables}->{ $result->{table} }}) {
             $self->{expand} = $self->set_constants();
+            $self->set_builtin();
             $self->{expand}->{ $result->{table} . '.instance' } = $instance;
             foreach my $label (keys %{$self->{snmp_collected}->{tables}->{ $result->{table} }->{$instance}}) {
                 $self->{expand}->{ $result->{table} . '.' . $label } =
@@ -1393,10 +1412,10 @@ sub disco_show {
     foreach (values %{$self->{selections}}) {
         my $entry = {};
         foreach my $label (keys %{$_->{expand}}) {
-            next if ($label =~ /^constants\./);
+            next if ($label =~ /^(?:constants|builtin)\./);
             my $name = $label;
             $name =~ s/\./_/g;
-            $entry->{$name} = $_->{expand}->{$label};
+            $entry->{$name} = defined($_->{expand}->{$label}) ? $_->{expand}->{$label} : '';
         }
         $self->{output}->add_disco_entry(%$entry);
     }
