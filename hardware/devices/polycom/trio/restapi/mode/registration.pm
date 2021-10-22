5a80533396084125defe8f1e67c59909cfa57143
qgarnier <garnier.quentin@gmail.com>
2021-10-22 10:49:47 +0200

    enh(polycom/trio): add modes paired and registration (#3213)

---
+++ hardware/devices/polycom/trio/restapi/mode/registration.pm
@@ -0,0 +1,103 @@
+#
+# Copyright 2021 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package hardware::devices::polycom::trio::restapi::mode::registration;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold_ng);
+
+sub custom_status_output {
+    my ($self, %options) = @_;
+
+    return "SIP status is '" . $self->{result_values}->{status} . "'";
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, skipped_code => { -10 => 1 } }
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'status', type => 2, critical_default => '%{status} !~ /registered/i', set => {
+                key_values => [ { name => 'status' } ],
+                closure_custom_output => $self->can('custom_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => \&catalog_status_threshold_ng
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+
+    $options{options}->add_options(arguments => {
+    });
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->request_api(url_path => '/api/v1/webCallControl/sipStatus');
+
+    $self->{global} = {};
+    foreach my $user (@{$result->{data}->{User}}) {
+        foreach my $events (@{$user->{Events}}) {
+            next if ($events->{Type} !~ /register/i);
+            $self->{global}->{status} = $events->{RegistrationState};
+        }
+    }
+    if (!defined($self->{global}->{status})) {
+        $self->{output}->add_option_msg(short_msg => 'Cannot find registration event');
+        $self->{output}->option_exit();
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check SIP registration.
+
+=over 8
+
+=item B<--warning-status>
+
+Set warning threshold for status.
+Can used special variables like: %{status}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '%{status} !~ /registred/i').
+Can used special variables like: %{status}
+
+=back
+
+=cut
