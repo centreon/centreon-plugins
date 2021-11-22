2e0d0218823c4e84245bf1cd862e3738fc8f9fd7
OlivierKuziemko <83703305+OlivierKuziemko@users.noreply.github.com>
2021-11-22 12:44:28 +0100

    Update login.pm

--- apps/protocols/sftp/mode/login.pm
+++ apps/protocols/sftp/mode/login.pm
@@ -18,14 +18,15 @@
 # limitations under the License.
 #
 
-package apps::protocols::sftp::mode::login;
+package apps::protocols::sftp::mode::date;
 
 use base qw(centreon::plugins::mode);
 
 use strict;
 use warnings;
-use Time::HiRes qw(gettimeofday tv_interval);
+use centreon::plugins::misc;
 use apps::protocols::sftp::lib::sftp;
+use Fcntl ":mode";
 
 sub new {
     my ($class, %options) = @_;
@@ -37,11 +38,17 @@ sub new {
          "hostname:s"       => { name => 'hostname' },
          "port:s"           => { name => 'port', },
          "ssh-options:s@"   => { name => 'ssh_options' },
+         "ssh-priv-key:s"   => { name => 'ssh_priv_key' },
+         "passphrase:s" => { name => 'passphrase' },
+         "directory:s@"     => { name => 'directory' },
+         "file-only"    => { name => 'file_only' },
+         "file:s@"          => { name => 'file' },
          "username:s"   => { name => 'username' },
          "password:s"   => { name => 'password' },
          "warning:s"    => { name => 'warning' },
          "critical:s"   => { name => 'critical' },
          "timeout:s"    => { name => 'timeout', default => '30' },
+         "timezone:s"   => { name => 'timezone' },
          });
     return $self;
 }
@@ -58,30 +65,86 @@ sub check_options {
         $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
         $self->{output}->option_exit();
     }
-
     if (!defined($self->{option_results}->{hostname})) {
         $self->{output}->add_option_msg(short_msg => "Please set the hostname option");
         $self->{output}->option_exit();
+    }
+
+    if (defined($self->{option_results}->{timezone}) && $self->{option_results}->{timezone} ne '') {
+        centreon::plugins::misc::mymodule_load(module => 'DateTime',
+                                               error_msg => "Cannot load module 'DateTime'.");
     }
 }
 
 sub run {
     my ($self, %options) = @_;
-    
-    my $timing0 = [gettimeofday];
-    
-    apps::protocols::sftp::lib::sftp::connect($self, connection_exit => 'critical');  
+    my %file_times = ();
 
-    my $timeelapsed = tv_interval ($timing0, [gettimeofday]);
-    
-    my $exit = $self->{perfdata}->threshold_check(value => $timeelapsed,
-                                                  threshold => [ { label => 'critical', 'exit_litteral' => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-    $self->{output}->output_add(severity => $exit,
-                                short_msg => sprintf("Response time %.3f ", $timeelapsed));
-    $self->{output}->perfdata_add(label => "time",
-                                  value => sprintf('%.3f', $timeelapsed),
-                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
-                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'));
+    apps::protocols::sftp::lib::sftp::connect($self);
+    my $current_time = time();
+    my $dirs = ['.'];
+    if (defined($self->{option_results}->{directory}) && scalar(@{$self->{option_results}->{directory}}) != 0) {
+        $dirs = $self->{option_results}->{directory};
+
+        foreach my $dir (@$dirs) {
+            my $files;
+
+            if (!($files = apps::protocols::sftp::lib::sftp::execute($self, command => 'ls', command_args => [$dir]))) {
+                $self->{output}->output_add(severity => 'UNKNOWN',
+                                            short_msg => sprintf("Command 'ls' issue for directory '$dir': %s", apps::protocols::sftp::lib::sftp::message()));
+                $self->{output}->display();
+                $self->{output}->exit();
+            }
+            foreach my $file (@$files) {
+                my $time_result;
+                if (defined($self->{option_results}->{file_only}) && !S_ISREG($file->{a}->perm)) {
+                    next;
+                }
+                if (!($time_result = $file->{a}->mtime)) {
+                    next;
+                }
+                $file = $dir . '/' . $file->{filename} if ($file->{filename} !~ /^$dir/); # some sftp only give filename (not the complete path)
+
+                $file_times{$file} = $time_result;
+            }
+        }
+    }
+    foreach my $file (@{$self->{option_results}->{file}}) {
+        my $time_result;
+
+        if (!($time_result = apps::protocols::sftp::lib::sftp::execute($self, command => 'stat', command_args => [$file]))) {
+            $self->{output}->output_add(severity => 'UNKNOWN',
+                                    short_msg => sprintf("Command 'stat' issue for file '$file': %s", apps::protocols::sftp::lib::sftp::message()));
+            $self->{output}->display();
+            $self->{output}->exit();
+        }
+        $file_times{$file} = $time_result->{mtime};
+    }
+
+    $self->{output}->output_add(severity => 'OK',
+                                short_msg => "All file times are ok.");
+    my $tz = centreon::plugins::misc::set_timezone(name => $self->{option_results}->{timezone});
+    foreach my $name (sort keys %file_times) {
+        my $diff_time = $current_time - $file_times{$name};
+
+        my $exit_code = $self->{perfdata}->threshold_check(value => $diff_time,
+                                                           threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
+        my $display_date = scalar(localtime($file_times{$name}));
+        if (defined($self->{option_results}->{timezone}) && $self->{option_results}->{timezone} ne '') {
+            $display_date = DateTime->from_epoch(epoch => $file_times{$name}, %$tz)->datetime()
+        }
+
+        $self->{output}->output_add(long_msg => sprintf("%s: %s seconds (time: %s)", $name, $diff_time, $display_date));
+        if (!$self->{output}->is_status(litteral => 1, value => $exit_code, compare => 'ok')) {
+            $self->{output}->output_add(severity => $exit_code,
+                                        short_msg => sprintf("%s: %s seconds (time: %s)", $name, $diff_time, $display_date));
+        }
+        $self->{output}->perfdata_add(label => $name, unit => 's',
+                                      value => $diff_time,
+                                      warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
+                                      critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
+                                      );
+    }
 
     $self->{output}->display();
     $self->{output}->exit();
@@ -93,7 +156,7 @@ __END__
 
 =head1 MODE
 
-Check Connection (also login) to an SFTP Server.
+Check modified time of files.
 
 =over 8
 
@@ -105,9 +168,18 @@ IP Addr/FQDN of the sftp host
 
 Port used
 
+=item B<--ssh-priv-key>
+
+Private keys for SSH connection.
+
+=item B<--passphrase>
+
+Passphrase for private keys.
+
 =item B<--ssh-options>
 
-Add custom ssh options.
+SSH Options
+E.g --ssh-options='-v' for ssh verbose output
 
 =item B<--username>
 
@@ -116,6 +188,7 @@ Specify username for authentification
 =item B<--password>
 
 Specify password for authentification
+Need Perl 'IO::Pty' module
 
 =item B<--timeout>
 
@@ -123,11 +196,28 @@ Connection timeout in seconds (Default: 30)
 
 =item B<--warning>
 
-Threshold warning in seconds
+Threshold warning in seconds for each files (diff time)
 
 =item B<--critical>
 
-Threshold critical in seconds
+Threshold critical in seconds for each files (diff time)
+
+=item B<--directory>
+
+Check files in the directory (no recursive) (Multiple option)
+
+=itme B<--files-only>
+
+Check only files in directory
+
+=item B<--file>
+
+Check file (Multiple option)
+
+=item B<--timezone>
+
+Set the timezone of display date.
+Can use format: 'Europe/London' or '+0100'.
 
 =back
 
