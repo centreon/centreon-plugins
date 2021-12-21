ecc8d14753ff2e8d443ca328e52f5d153f31328f
garnier-quentin <garnier.quentin@gmail.com>
2021-12-21 09:30:00 +0100

    wip

--- database/mssql/mode/backupage.pm
+++ database/mssql/mode/backupage.pm
@@ -31,15 +31,14 @@ sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
-    
-    $options{options}->add_options(arguments =>
-                                { 
-                                  "filter:s"                => { name => 'filter', },
-                                  "skip"                    => { name => 'skip', },
-                                  "skip-no-backup"          => { name => 'skip_no_backup', },
-                                  "warning:s"               => { name => 'warning', },
-                                  "critical:s"              => { name => 'critical', },
-                                });
+
+    $options{options}->add_options(arguments => { 
+        'filter:s'       => { name => 'filter' },
+        'skip'           => { name => 'skip' },
+        'skip-no-backup' => { name => 'skip_no_backup' },
+        'warning:s'      => { name => 'warning' },
+        'critical:s'     => { name => 'critical' }
+    });
 
     return $self;
 }
@@ -63,39 +62,42 @@ sub run {
     # $options{sql} = sqlmode object
     $self->{sql} = $options{sql};
 
-    $self->{output}->output_add(severity => 'OK',
-                                short_msg => "All backups are ok.");
+    $self->{output}->output_add(
+        severity => 'OK',
+        short_msg => "All backups are ok."
+    );
 
-    $self->{sql}->connect();
+    $options{sql}->connect();
 
     my $count = 0;
     my $query = 'SELECT
-              a.name, a.recovery_model,
-              DATEDIFF(SS, MAX(b.backup_finish_date), GETDATE()),
-              DATEDIFF(SS, MAX(b.backup_start_date), MAX(b.backup_finish_date))
-            FROM master.dbo.sysdatabases a LEFT OUTER JOIN msdb.dbo.backupset b
-            ON b.database_name = a.name
-            GROUP BY a.name
-            ORDER BY a.name
+            a.name, a.recovery_model,
+            DATEDIFF(SS, MAX(b.backup_finish_date), GETDATE()),
+            DATEDIFF(SS, MAX(b.backup_start_date), MAX(b.backup_finish_date))
+        FROM master.dbo.sysdatabases a LEFT OUTER JOIN msdb.dbo.backupset b
+        ON b.database_name = a.name
+        GROUP BY a.name
+        ORDER BY a.name
     ';
 
-    if (($self->{sql}->is_version_minimum(version => '9.x'))) {
+    if (($options{sql}->is_version_minimum(version => '9.x'))) {
         $query = "SELECT D.name AS [database_name], D.recovery_model, BS1.last_backup, BS1.last_duration
-                 FROM sys.databases D
-                 LEFT JOIN (
-                    SELECT BS.[database_name],
-                    DATEDIFF(SS,MAX(BS.[backup_finish_date]),GETDATE()) AS last_backup,
-                    DATEDIFF(SS,MAX(BS.[backup_start_date]),MAX(BS.[backup_finish_date])) AS last_duration
-                    FROM msdb.dbo.backupset BS
-                    WHERE BS.type = 'D'
-                    GROUP BY BS.[database_name]
-                ) BS1 ON D.name = BS1.[database_name]
-                WHERE D.source_database_id IS NULL
-                ORDER BY D.[name]";
+            FROM sys.databases D
+            LEFT JOIN (
+                SELECT
+                    BS.[database_name],
+                    DATEDIFF(SS, MAX(BS.[backup_finish_date]), GETDATE()) AS last_backup,
+                    DATEDIFF(SS, MAX(BS.[backup_start_date]), MAX(BS.[backup_finish_date])) AS last_duration
+                FROM msdb.dbo.backupset BS
+                WHERE BS.type = 'D'
+                GROUP BY BS.[database_name]
+            ) BS1 ON D.name = BS1.[database_name]
+            WHERE D.source_database_id IS NULL
+            ORDER BY D.[name]";
     }
 
-    $self->{sql}->query(query => $query);
-    my $result = $self->{sql}->fetchall_arrayref();
+    $options{sql}->query(query => $query);
+    my $result = $options{sql}->fetchall_arrayref();
     foreach my $row (@$result) {
         next if (defined($self->{option_results}->{filter}) && $$row[0] !~ /$self->{option_results}->{filter}/);
         $count++;
@@ -104,37 +106,48 @@ sub run {
         my $backup_duration = $$row[3];
         if (!defined($last_backup)) {
             if (!defined($self->{option_results}->{skip_no_backup})) {
-                $self->{output}->output_add(severity => 'CRITICAL',
-                                            short_msg => sprintf("No backup found for DB '%s'", $$row[0]));
+                $self->{output}->output_add(
+                    severity => 'CRITICAL',
+                    short_msg => sprintf("No backup found for DB '%s'", $$row[0])
+                );
             }
         } else {
             $self->{output}->output_add(long_msg => sprintf("DB '%s' backup age : %ds [Duration : %ds]", $$row[0], $last_backup, $backup_duration));
             my $exit_code = $self->{perfdata}->threshold_check(value => $last_backup, threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
             if (!$self->{output}->is_status(value => $exit_code, compare => 'ok', litteral => 1)) {
-                $self->{output}->output_add(severity => $exit_code,
-                                            short_msg => sprintf("DB '%s' backup age: %ds [Duration : %ds]", $$row[0], $last_backup, $backup_duration));
-            
+                $self->{output}->output_add(
+                    severity => $exit_code,
+                    short_msg => sprintf("DB '%s' backup age: %ds [Duration : %ds]", $$row[0], $last_backup, $backup_duration)
+                );
             }
             
-            $self->{output}->perfdata_add(label => sprintf("db_%s_backup_age",$$row[0]),
-                                          unit => 's',
-                                          value => $last_backup,
-                                          warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
-                                          critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
-                                          min => 0);
-            $self->{output}->perfdata_add(label => sprintf("db_%s_backup_duration",$$row[0]),
-                                          unit => 's',
-                                          value => $backup_duration,
-                                          min => 0);
+            $self->{output}->perfdata_add(
+                label => sprintf("db_%s_backup_age", $$row[0]),
+                unit => 's',
+                value => $last_backup,
+                warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning'),
+                critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical'),
+                min => 0
+            );
+            $self->{output}->perfdata_add(
+                label => sprintf("db_%s_backup_duration",$$row[0]),
+                unit => 's',
+                value => $backup_duration,
+                min => 0
+            );
         }
     }
 
     if ($count == 0) {
-        $self->{output}->output_add(severity => 'OK',
-                                    short_msg => "No backup found");
+        $self->{output}->output_add(
+            severity => 'OK',
+            short_msg => "No backup found"
+        );
         if (!defined($self->{option_results}->{skip})) {
-            $self->{output}->output_add(severity => 'UNKNOWN',
-                                        short_msg => "No backup found");
+            $self->{output}->output_add(
+                severity => 'UNKNOWN',
+                short_msg => "No backup found"
+            );
         }
     }
 
