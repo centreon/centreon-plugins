e2d40f4c0fe1cb61462a8add949e2509089044f7
qgarnier <garnier.quentin@gmail.com>
2021-12-22 11:37:17 +0100

    add(contrib): script to check modes execution (#3335)

---
+++ contrib/check_plugins.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+#
+# Copyright 2021 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+use strict;
+use warnings;
+use FindBin;
+
+my $plugins = [];
+
+sub check_directory {
+    my ($directory) = @_;
+
+    opendir(my $dh, $directory) || return ;
+    while (my $filename = readdir $dh) {
+        check_directory($directory . '/' . $filename) if ($filename !~ /^\./ && -d $directory . '/' . $filename);
+        if ($filename eq 'plugin.pm') {
+            push @$plugins, $directory . '/' . $filename;
+        }
+    }
+    closedir $dh;
+}
+
+my $plugin = "$FindBin::Bin/../centreon_plugins.pl";
+check_directory("$FindBin::Bin/..");
+
+foreach my $plugin_path (@$plugins) {
+    $plugin_path =~ s{$FindBin::Bin/../}{};
+    print "plugin: $plugin_path\n";
+    my $modes = `$plugin --plugin=$plugin_path --list-mode`;
+    if ($modes =~ /Modes Available:\n(.*)/ms) {
+        foreach my $mode (split /\n/, $1) {
+            $mode =~ s/\s+//g;
+            my $result = `$plugin --plugin=$plugin_path --mode=$mode`;
+            print "    mode: $mode, result=$result";
+        }
+    } else {
+        print "error: $modes\n";
+    }
+}
+
+exit(0);
