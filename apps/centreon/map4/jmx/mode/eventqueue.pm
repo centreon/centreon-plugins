e40d869bf4a584d1b5228b3b31686ee49a80606a
qgarnier <garnier.quentin@gmail.com>
2022-01-04 14:55:10 +0100

    update copyright - happy new year (#3375)

--- apps/centreon/map4/jmx/mode/eventqueue.pm
+++ apps/centreon/map4/jmx/mode/eventqueue.pm
@@ -1,4 +1,4 @@
-# Copyright 2021 Centreon (http://www.centreon.com/)
+# Copyright 2022 Centreon (http://www.centreon.com/)
 #
 # Centreon is a full-fledged industry-strength solution that meets
 # the needs in IT infrastructure and application monitoring for
