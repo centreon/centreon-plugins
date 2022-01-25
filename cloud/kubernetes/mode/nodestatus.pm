9aec85135e0906d96644b659e63ab84f8a8c0e65
garnier-quentin <garnier.quentin@gmail.com>
2022-01-25 15:20:03 +0100

    (plugin) cloud::kubernetes - add name for node-status

--- cloud/kubernetes/mode/nodestatus.pm
+++ cloud/kubernetes/mode/nodestatus.pm
@@ -24,53 +24,17 @@ use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
-use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold);
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold_ng);
 
 sub custom_condition_status_output {
     my ($self, %options) = @_;
 
-    return sprintf("Status is '%s', Reason: '%s', Message: '%s'",
+    return sprintf(
+        "Status is '%s', Reason: '%s', Message: '%s'",
         $self->{result_values}->{status},
         $self->{result_values}->{reason},
-        $self->{result_values}->{message});
-}
-
-sub custom_condition_status_calc {
-    my ($self, %options) = @_;
-
-    $self->{result_values}->{type} = $options{new_datas}->{$self->{instance} . '_type'};
-    $self->{result_values}->{status} = $options{new_datas}->{$self->{instance} . '_status'};
-    $self->{result_values}->{reason} = $options{new_datas}->{$self->{instance} . '_reason'};
-    $self->{result_values}->{message} = $options{new_datas}->{$self->{instance} . '_message'};
-
-    return 0;
-}
-
-sub set_counters {
-    my ($self, %options) = @_;
-
-    $self->{maps_counters_type} = [
-        { name => 'nodes', type => 3, cb_prefix_output => 'prefix_output', cb_long_output => 'long_output',
-          message_multiple => 'All Nodes status are ok', indent_long_output => '    ',
-            group => [
-                { name => 'conditions', display_long => 1, cb_prefix_output => 'prefix_condition_output',
-                  message_multiple => 'Conditions are ok', type => 1, skipped_code => { -10 => 1 } },
-            ]
-        }
-    ];
-
-    $self->{maps_counters}->{conditions} = [
-        { label => 'status', set => {
-                key_values => [
-                    { name => 'type' }, { name => 'status' }, { name => 'reason' }, { name => 'message' }
-                ],
-                closure_custom_calc => $self->can('custom_condition_status_calc'),
-                closure_custom_output => $self->can('custom_condition_status_output'),
-                closure_custom_perfdata => sub { return 0; },
-                closure_custom_threshold_check => \&catalog_status_threshold,
-            }
-        },
-    ];
+        $self->{result_values}->{message}
+    );
 }
 
 sub prefix_output {
@@ -91,37 +55,56 @@ sub long_output {
     return "Checking node '" . $options{instance_value}->{display} . "'";
 }
 
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'nodes', type => 3, cb_prefix_output => 'prefix_output', cb_long_output => 'long_output',
+          message_multiple => 'All Nodes status are ok', indent_long_output => '    ',
+            group => [
+                { name => 'conditions', display_long => 1, cb_prefix_output => 'prefix_condition_output',
+                  message_multiple => 'Conditions are ok', type => 1, skipped_code => { -10 => 1 } },
+            ]
+        }
+    ];
+
+    $self->{maps_counters}->{conditions} = [
+        {
+            label => 'status',
+            type => 2,
+            critical_default => '(%{type} =~ /Ready/i && %{status} !~ /True/i) || (%{type} =~ /.*Pressure/i && %{status} !~ /False/i)', 
+            set => {
+                key_values => [
+                    { name => 'type' }, { name => 'status' }, { name => 'reason' },
+                    { name => 'message' }, { name => 'name' }
+                ],
+                closure_custom_output => $self->can('custom_condition_status_output'),
+                closure_custom_perfdata => sub { return 0; },
+                closure_custom_threshold_check => \&catalog_status_threshold_ng
+            }
+        }
+    ];
+}
+
 sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
     
     $options{options}->add_options(arguments => {
-        "filter-name:s"     => { name => 'filter_name' },
-        "warning-status:s"  => { name => 'warning_status', default => '' },
-        "critical-status:s" => {
-            name => 'critical_status',
-            default => '(%{type} =~ /Ready/i && %{status} !~ /True/i) || (%{type} =~ /.*Pressure/i && %{status} !~ /False/i)'
-        },
+        'filter-name:s' => { name => 'filter_name' }
     });
    
     return $self;
 }
 
-sub check_options {
-    my ($self, %options) = @_;
-    $self->SUPER::check_options(%options);
-        
-    $self->change_macros(macros => ['warning_status', 'critical_status']);    
-}
-
 sub manage_selection {
     my ($self, %options) = @_;
 
-    $self->{nodes} = {};
-
     my $results = $options{custom}->kubernetes_list_nodes();
-    
+
+    $self->{nodes} = {};
     foreach my $node (@{$results}) {
         if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' &&
             $node->{metadata}->{name} !~ /$self->{option_results}->{filter_name}/) {
@@ -133,6 +116,7 @@ sub manage_selection {
 
         foreach my $condition (@{$node->{status}->{conditions}}) {
             $self->{nodes}->{$node->{metadata}->{uid}}->{conditions}->{$condition->{type}} = {
+                name => $node->{metadata}->{name},
                 type => $condition->{type},
                 status => $condition->{status},
                 reason => $condition->{reason},
@@ -164,12 +148,12 @@ Filter node name (can be a regexp).
 =item B<--warning-status>
 
 Set warning threshold for status (Default: '').
-Can used special variables like: %{type}, %{status}, %{reason}, %{message}.
+Can used special variables like: %{type}, %{status}, %{reason}, %{message}, %{name}.
 
 =item B<--critical-status>
 
 Set critical threshold for status (Default: '(%{type} =~ /Ready/i && %{status} !~ /True/i) || (%{type} =~ /.*Pressure/i && %{status} !~ /False/i)').
-Can used special variables like: %{type}, %{status}, %{reason}, %{message}.
+Can used special variables like: %{type}, %{status}, %{reason}, %{message}, %{name}.
 
 =back
 
