2094a4e5facac38237804af0e9b2e5686dc82262
Simon Bomm <sbomm@centreon.com>
2022-03-26 10:06:36 +0100

    + use latest discovery code formatting

--- cloud/azure/database/mariadb/mode/discovery.pm
+++ cloud/azure/database/mariadb/mode/discovery.pm
@@ -20,93 +20,17 @@
 
 package cloud::azure::database::mariadb::mode::discovery;
 
-use base qw(centreon::plugins::mode);
+use base qw(cloud::azure::management::monitor::mode::discovery);
 
 use strict;
 use warnings;
-use JSON::XS;
-
-sub new {
-    my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
-    bless $self, $class;
-    
-    $options{options}->add_options(arguments => {
-        'namespace:s'      => { name => 'namespace' },
-        'type:s'           => { name => 'type' },
-        'resource-group:s' => { name => 'resource_group' },
-        'location:s'       => { name => 'location' },
-        'prettify'         => { name => 'prettify' },
-    });
-
-    return $self;
-}
 
 sub check_options {
     my ($self, %options) = @_;
-    $self->SUPER::init(%options);
+    $self->SUPER::check_options(%options);
 
-    $self->{namespace} = $self->{option_results}->{namespace};
-    $self->{type} = $self->{option_results}->{type};
-    $self->{location} = $self->{option_results}->{location};
-    $self->{resource_group} = $self->{option_results}->{resource_group};
-}
-
-sub run {
-    my ($self, %options) = @_;
-
-    my @disco_data;
-    my $disco_stats;
-
-    $disco_stats->{start_time} = time();
-
-    my $resources = $options{custom}->azure_list_resources(
-        namespace => $self->{namespace},
-        resource_type => $self->{type},
-        location => $self->{location},
-        resource_group => $self->{resource_group}
-    );
-
-    $disco_stats->{end_time} = time();
-    $disco_stats->{duration} = $disco_stats->{end_time} - $disco_stats->{start_time};
-
-    foreach my $resource (@{$resources}) {
-        next if ($resource->{type} !~ /Microsoft\.DBforMariaDB/);
-        $resource->{type} =~ s/Microsoft\.DBforMariaDB\///;
-        my $resource_group = '';
-        $resource_group = $resource->{resourceGroup} if (defined($resource->{resourceGroup}));
-        $resource_group = $1 if ($resource_group eq '' && defined($resource->{id}) && $resource->{id} =~ /resourceGroups\/(.*)\/providers/);
-        $resource->{resourceGroup} = $resource_group;
-
-        foreach my $entry (keys %{$resource}) {
-            next if (ref($resource->{$entry}) ne "HASH");
-            my @array;
-            foreach my $key (keys %{$resource->{$entry}}) {
-                push @array, { key => $key, value => $resource->{$entry}->{$key} };
-            }
-            $resource->{$entry} = \@array;
-        }
-        push @disco_data, $resource;
-    }
-
-    $disco_stats->{discovered_items} = @disco_data;
-    $disco_stats->{results} = \@disco_data;
-
-    my $encoded_data;
-    eval {
-        if (defined($self->{option_results}->{prettify})) {
-            $encoded_data = JSON::XS->new->utf8->pretty->encode($disco_stats);
-        } else {
-            $encoded_data = JSON::XS->new->utf8->encode($disco_stats);
-        }
-    };
-    if ($@) {
-        $encoded_data = '{"code":"encode_error","message":"Cannot encode discovered data into JSON format"}';
-    }
-    
-    $self->{output}->output_add(short_msg => $encoded_data);
-    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1);
-    $self->{output}->exit();
+    $self->{namespace} = 'Microsoft.DBforMariaDB';
+    $self->{type} = 'servers';
 }
 
 1;
@@ -115,21 +39,9 @@ __END__
 
 =head1 MODE
 
-Resources discovery.
+Azure MariaDB Resources discovery.
 
 =over 8
-
-=item B<--namespace>
-
-Specify resources namespace.
-
-=item B<--type>
-
-Specify resources type.
-
-=item B<--resource-group>
-
-Specify resources resource group.
 
 =item B<--location>
 
