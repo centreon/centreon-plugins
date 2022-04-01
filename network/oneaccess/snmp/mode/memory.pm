cbe77a931164fe9edfa2c72d98772a60658a0e2a
qgarnier <garnier.quentin@gmail.com>
2022-04-01 13:56:58 +0200

    (plugin)  network::oneaccess::snmp - add mode cells-radio (#3576)

--- network/oneaccess/snmp/mode/memory.pm
+++ network/oneaccess/snmp/mode/memory.pm
@@ -27,14 +27,13 @@ use warnings;
 
 sub new {
     my ($class, %options) = @_;
-    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
     bless $self, $class;
-    
-    $options{options}->add_options(arguments =>
-                                {
-                                  "warning:s"   => { name => 'warning', },
-                                  "critical:s"  => { name => 'critical', },
-                                });
+
+    $options{options}->add_options(arguments => {
+        'warning:s'  => { name => 'warning' },
+        'critical:s' => { name => 'critical' }
+    });
 
     return $self;
 }
@@ -42,7 +41,7 @@ sub new {
 sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
-    
+
     if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
         $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
         $self->{output}->option_exit();
@@ -55,37 +54,44 @@ sub check_options {
 
 sub run {
     my ($self, %options) = @_;
-    $self->{snmp} = $options{snmp};
 
     my $oid_oacSysMemoryTotal = '.1.3.6.1.4.1.13191.10.3.3.1.1.3.0';
     my $oid_oacSysMemoryUsed = '.1.3.6.1.4.1.13191.10.3.3.1.1.4.0';
 
-    my $result = $self->{snmp}->get_leef(oids => [$oid_oacSysMemoryTotal, $oid_oacSysMemoryUsed], nothing_quit => 1);
+    my $result = $options{snmp}->get_leef(oids => [$oid_oacSysMemoryTotal, $oid_oacSysMemoryUsed], nothing_quit => 1);
     my $total = $result->{$oid_oacSysMemoryTotal};
     my $used = $result->{$oid_oacSysMemoryUsed} * $total / 100;
     my $free = $total - $used; 
     my $prct_used = $result->{$oid_oacSysMemoryUsed};
     my $prct_free = 100 - $prct_used;
-    
+
     my ($total_size_value, $total_size_unit) = $self->{perfdata}->change_bytes(value => $total);
     my ($total_used_value, $total_used_unit) = $self->{perfdata}->change_bytes(value => $used);
     my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => $free);
 
-    my $exit = $self->{perfdata}->threshold_check(value => $prct_used, 
-                                                  threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]);
-    $self->{output}->output_add(severity => $exit,
-                                short_msg => sprintf("Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
-                                    $total_size_value . " " . $total_size_unit,
-                                    $total_used_value . " " . $total_used_unit, $prct_used,
-                                    $total_free_value . " " . $total_free_unit, $prct_free));
-       
-    $self->{output}->perfdata_add(label => "used", unit => 'B',
-                                  value => sprintf("%d", $used),
-                                  warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning', total => $total, cast_int => 1),
-                                  critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical', total => $total, cast_int => 1),
-                                  min => 0, max => $total
-                                 );
-    
+    my $exit = $self->{perfdata}->threshold_check(
+        value => $prct_used, 
+        threshold => [ { label => 'critical', exit_litteral => 'critical' }, { label => 'warning', exit_litteral => 'warning' } ]
+    );
+    $self->{output}->output_add(
+        severity => $exit,
+        short_msg => sprintf(
+            "Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
+            $total_size_value . " " . $total_size_unit,
+            $total_used_value . " " . $total_used_unit, $prct_used,
+            $total_free_value . " " . $total_free_unit, $prct_free
+        )
+    );
+
+    $self->{output}->perfdata_add(
+        nlabel => 'memory.usage.bytes',
+        unit => 'B',
+        value => sprintf('%d', $used),
+        warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning', total => $total, cast_int => 1),
+        critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical', total => $total, cast_int => 1),
+        min => 0, max => $total
+    );
+
     $self->{output}->display();
     $self->{output}->exit();
 }
@@ -111,4 +117,3 @@ Threshold critical in percent.
 =back
 
 =cut
-    
