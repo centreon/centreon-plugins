f3935cb697fbe51fe4c06edc7bd11b93fccbee88
Zakaria Guennoune <zguennoune@centreon.com>
2022-04-14 11:53:40 +0200

    fix perm

--- connectors/vmware/ci/scripts/vmware-deliver-deb-package.sh
+++ connectors/vmware/ci/scripts/vmware-deliver-deb-package.sh
@@ -7,5 +7,5 @@ BULLSEYEPACKAGES=`echo *.deb`
 for i in $BULLSEYEPACKAGES
 do
   echo "Sending $i"
-  curl -u \"$1:$2\" -H "Content-Type: multipart/form-data" --data-binary \"@/$i\" "https://apt.centreon.com/repository/$VERSION"
+  curl -u \"$1:$2\" -H "Content-Type: multipart/form-data" --data-binary \"@/$i\" "https://apt.centreon.com/repository/22.04"
 done
