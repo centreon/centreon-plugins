45e54077e3f638fe9f997a540312f28c7c9fcb56
lchrdn <lchardon@centreon.com>
2022-04-25 17:05:24 +0200

    initial discovery for watchers

---
+++ apps/monitoring/kadiska/custom/api.pm
@@ -0,0 +1,206 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::kadiska::custom::api;
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+use centreon::plugins::statefile;
+use DateTime;
+use Digest::MD5 qw(md5_hex);
+use JSON::XS;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self  = {};
+    bless $self, $class;
+
+    if (!defined($options{output})) {
+        print "Class Custom: Need to specify 'output' argument.\n";
+        exit 3;
+    }
+    if (!defined($options{options})) {
+        $options{output}->add_option_msg(short_msg => "Class Custom: Need to specify 'options' argument.");
+        $options{output}->option_exit();
+    }
+
+    if (!defined($options{noptions})) {
+        $options{options}->add_options(arguments => {
+            'client-id:s'   => { name => 'client_id' },
+            'client-secret:s'   => { name => 'client_secret' },
+            'hostname:s'       => { name => 'hostname' },
+            'port:s'           => { name => 'port' },
+            'proto:s'          => { name => 'proto' },
+            'timeout:s'        => { name => 'timeout' },
+            'url-path:s'       => { name => 'url_path' }
+        });
+    }
+    $options{options}->add_help(package => __PACKAGE__, sections => 'REST API OPTIONS', once => 1);
+
+    $self->{output} = $options{output};
+    $self->{http} = centreon::plugins::http->new(%options);
+    $self->{cache} = centreon::plugins::statefile->new(%options);
+
+    return $self;
+}
+
+sub set_options {
+    my ($self, %options) = @_;
+
+    $self->{option_results} = $options{option_results};
+}
+
+sub set_defaults {}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->{hostname} = (defined($self->{option_results}->{hostname})) ? $self->{option_results}->{hostname} : 'app.kadiska.com';
+    $self->{port} = (defined($self->{option_results}->{port})) ? $self->{option_results}->{port} : 443;
+    $self->{proto} = (defined($self->{option_results}->{proto})) ? $self->{option_results}->{proto} : 'https';
+    $self->{url_path} = (defined($self->{option_results}->{url_path})) ? $self->{option_results}->{url_path} : '/api/';
+    $self->{endpoint} = (defined($self->{option_results}->{endpoint})) ? $self->{option_results}->{endpoint} : '';
+    $self->{timeout} = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 10;
+    $self->{client_id} = (defined($self->{option_results}->{client_id})) ? $self->{option_results}->{client_id} : '';
+    $self->{client_secret} = (defined($self->{option_results}->{client_secret})) ? $self->{option_results}->{client_secret} : '';
+
+    if (!defined($self->{client_id}) || $self->{client_id} eq '') {
+        $self->{output}->add_option_msg(short_msg => "Need to specify --client-id option.");
+        $self->{output}->option_exit();
+    }
+    if (!defined($self->{client_secret}) || $self->{client_secret} eq '') {
+        $self->{output}->add_option_msg(short_msg => "Need to specify --client-secret option.");
+        $self->{output}->option_exit();
+    }
+
+    $self->{cache}->check_options(option_results => $self->{option_results});
+    
+    return 0;
+   
+}
+
+sub build_options_for_httplib {
+    my ($self, %options) = @_;
+
+    $self->{option_results}->{hostname} = $self->{hostname};
+    $self->{option_results}->{timeout} = $self->{timeout};
+    $self->{option_results}->{port} = $self->{port};
+    $self->{option_results}->{proto} = $self->{proto};
+    $self->{option_results}->{timeout} = $self->{timeout};
+    $self->{option_results}->{warning_status} = '';
+    $self->{option_results}->{critical_status} = '';
+    $self->{option_results}->{unknown_status} = '';
+}
+
+sub settings {
+    my ($self, %options) = @_;
+
+    $self->build_options_for_httplib();
+    $self->{http}->add_header(key => 'Accept', value => 'application/json');
+    $self->{http}->add_header(key => 'Content-Type', value => 'application/json');
+    $self->{http}->add_header(key => 'Authorization', value => 'Bearer ' . $self->{access_token});
+    $self->{http}->set_options(%{$self->{option_results}});
+}
+
+sub get_access_token {
+    my ($self, %options) = @_;
+
+    my $has_cache_file = $options{statefile}->read(statefile => 'kadiska_' . md5_hex($self->{hostname}) . '_' . md5_hex($self->{client_id}));
+    my $access_token = $options{statefile}->get(name => 'access_token');
+
+    if ( $has_cache_file == 0 || !defined($access_token) ) {
+        my $credentials = { 'client_id' => $self->{client_id}, 'secret' => $self->{client_secret} };
+        my $post_json = JSON::XS->new->utf8->encode($credentials);
+
+        $self->settings();
+
+        my $content = $self->{http}->request(
+            method => 'POST',
+            query_form_post => $post_json,
+            url_path => $self->{url_path} . "config/auth/token"
+        );
+
+        if (!defined($content) || $content eq '') {
+            $self->{output}->add_option_msg(short_msg => "Authentication endpoint returns empty content [code: '" . $self->{http}->get_code() . "'] [message: '" . $self->{http}->get_message() . "']");
+            $self->{output}->option_exit();
+        }
+
+        $content =~ s/^"(.*)"$/$1/;
+        $access_token = $content;
+        my $datas = { access_token => $access_token };
+        
+        $options{statefile}->write(data => $datas);
+    }
+
+    return $access_token;
+}
+
+sub request_api {
+    my ($self, %options) = @_;
+
+    if (!defined($self->{access_token})) {
+        $self->{access_token} = $self->get_access_token(statefile => $self->{cache});
+    }
+
+    $self->settings(environment_header => 1, organization_header => 1);
+
+    my $encoded_form_post;
+    if (defined($options{query_form_post})) {
+        eval {
+            $encoded_form_post = JSON::XS->new->utf8->encode($options{query_form_post});
+        };
+        if ($@) {
+            $self->{output}->add_option_msg(short_msg => "Cannot encode json request");
+            $self->{output}->option_exit();
+        }
+    }
+
+    my ($content) = $self->{http}->request(
+        method => 'POST',
+        url_path => $self->{url_path} . $options{endpoint},
+        query_form_post => $encoded_form_post,
+    );
+
+    if (!defined($content) || $content eq '') {
+        $self->{output}->add_option_msg(short_msg => "API returns empty content [code: '" . $self->{http}->get_code() . "'] [message: '" . $self->{http}->get_message() . "']");
+        $self->{output}->option_exit();
+    }
+
+    my $decoded;
+    eval {
+        $decoded = JSON::XS->new->utf8->decode($content);
+    };
+    
+    if ($@) {
+        $self->{output}->add_option_msg(short_msg => "Cannot decode response (add --debug option to display returned content)");
+        $self->{output}->option_exit();
+    }
+
+    return $decoded;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Kadiska Rest API
