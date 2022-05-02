3ed428a09ac7ec246a2f97613c61befa1451adb7
lchrdn <89968908+lchrdn@users.noreply.github.com>
2022-05-02 16:51:20 +0200

    (plugin) apps::monitoring::kadiska - add new plugin for Kadiska (#3622)
    * initial discovery for watchers
    
    * divide discovery for station and watcher
    
    * initial commit
    
    * add period option and set help
    
    * remove unused dependancy
    
    * fix couters nlabel and period calculation method
    
    * modify default value for period

---
+++ apps/monitoring/kadiska/mode/liststations.pm
@@ -0,0 +1,120 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::kadiska::mode::liststations;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use JSON::XS;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'prettify' => { name => 'prettify' }
+    });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my @disco_data;
+    my $disco_stats;
+
+    $disco_stats->{start_time} = time();
+
+    my $raw_form_post = {
+        "select" => [
+            "station_name",
+            "station_id"
+        ],
+            "from" => "traceroute",
+            "groupby" => [
+            "station_id"
+            ],
+        "options" => {"sampling" => \1 }
+    };  
+
+    my $results = $options{custom}->request_api(
+        method => 'POST',
+        endpoint => 'query',
+        query_form_post => $raw_form_post
+    );
+
+    foreach my $current_station (@{$results->{data}}) {
+        my %station;
+        $station{station_name} = $current_station->{station_name};
+        $station{station_id} = $current_station->{station_id};
+        push @disco_data, \%station;
+    }
+    
+    $disco_stats->{end_time} = time();
+    $disco_stats->{duration} = $disco_stats->{end_time} - $disco_stats->{start_time};
+    $disco_stats->{discovered_items} = @disco_data;
+    $disco_stats->{results} = \@disco_data;
+
+    return $disco_stats;
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my $encoded_data;
+
+    eval {
+        if (defined($self->{option_results}->{prettify})) {
+            $encoded_data = JSON::XS->new->utf8->pretty->encode($self->manage_selection(%options));
+        } else {
+            $encoded_data = JSON::XS->new->utf8->encode($self->manage_selection(%options));
+        }
+    };
+    if ($@) {
+        $encoded_data = '{"code":"encode_error","message":"Cannot encode discovered data into JSON format"}';
+    }
+
+    $self->{output}->output_add(short_msg => $encoded_data);
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1);
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Kadiska hosts discovery for stations.
+
+=over 8
+
+=back
+
+=cut
---
+++ apps/monitoring/kadiska/mode/watcherstatistics.pm
@@ -0,0 +1,206 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::kadiska::mode::watcherstatistics;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold_ng);
+
+sub prefix_output {
+    my ($self, %options) = @_;
+    
+    return "Watcher '" . $options{instance} . "' ";
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'watchers', type => 1, cb_prefix_output => 'prefix_output', message_multiple => 'All Watchers are OK' }
+    ];
+
+    $self->{maps_counters}->{watchers} = [
+        { label => 'errors-prct', nlabel => 'watcher.errors.percentage', set => {
+                key_values => [ { name => 'errors_prct' } ],
+                output_template => 'errors: %.2f%%',
+                perfdatas => [
+                    { label => 'errors_prct', template => '%.2f', unit => '%', min => 0, max => 100, label_extra_instance => 1 },
+                ],
+            }
+        },
+        { label => 'sessions', nlabel => 'watcher.sessions.count', set => {
+                key_values => [ { name => 'sessions' } ],
+                output_template => 'sessions: %s',
+                perfdatas => [
+                    { label => 'sessions', template => '%s', min => 0, label_extra_instance => 1 },
+                ],
+            }
+        },
+        { label => 'requests', nlabel => 'watcher.requests.count', set => {
+                key_values => [ { name => 'requests' } ],
+                output_template => 'requests: %s',
+                perfdatas => [
+                    { label => 'requests', template => '%s', min => 0, label_extra_instance => 1 },
+                ],
+            }
+        },
+        { label => 'pages', nlabel => 'watcher.pages.count', set => {
+                key_values => [ { name => 'pages' } ],
+                output_template => 'pages: %d',
+                perfdatas => [
+                    { label => 'pages', template => '%d', min => 0, label_extra_instance => 1 },
+                ],
+            }
+        }              
+    ];
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'filter-watcher-name:s' => { name => 'filter_watcher_name' },
+    });
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $raw_form_post = {
+        "select" => [
+            {
+                "watcher_id:group" => "watcher_name"
+            },
+            {
+                "\%errors:avg|hits" => 
+                    [
+                        "*",100,["/",["sumFor","hits","error_count"],["countFor","hits"]]
+                    ]                
+            },
+            {
+                "session:sum|hits" => ["sumFor","hits","session_count"]
+            },
+            {
+                "item:count|requests" => ["countFor","requests"]
+            },
+            {
+                "item:count|pages" => ["countFor","pages"]
+            }
+        ],
+        "from" => "rum",
+        "groupby" => [
+            "watcher_name"
+        ],
+        "offset" => 0,
+        "options" => {"sampling" => \1 }
+    };  
+
+    if (defined($self->{option_results}->{filter_watcher_name}) && $self->{option_results}->{filter_watcher_name} ne ''){
+        $raw_form_post->{where} = ["=","watcher_name",["\$", $self->{option_results}->{filter_watcher_name}]],
+    }
+
+    my $results = $options{custom}->request_api(
+        method => 'POST',
+        endpoint => 'query',
+        query_form_post => $raw_form_post
+    );
+
+    $self->{watchers} = {};
+    foreach my $watcher (@{$results->{data}}) {
+        next if (defined($self->{option_results}->{filter_watcher_name}) && $self->{option_results}->{filter_watcher_name} ne ''
+            && $watcher->{'watcher_id:group'} !~ /$self->{option_results}->{filter_watcher_name}/);
+
+        my $instance = $watcher->{'watcher_id:group'};
+
+        $self->{watchers}->{$instance} = { display => $instance, 
+                                    errors_prct => $watcher->{'%errors:avg|hits'},
+                                    sessions => $watcher->{'session:sum|hits'},
+                                    requests => $watcher->{'item:count|requests'},
+                                    pages => $watcher->{'item:count|pages'} };
+    };
+
+    if (scalar(keys %{$self->{watchers}}) <= 0) {
+        $self->{output}->add_option_msg(short_msg => "No instances or results found.");
+        $self->{output}->option_exit();
+    }
+
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check Kadiska application watchers' statistics during the period specified.
+
+=over 8
+
+=item B<--filter-watcher-name>
+
+Filter on an application watcher to only display related statistics.
+
+=item B<--warning-errors-prct>
+
+Warning threshold for web browser errors (4xx and 5xx types) in percentage.
+
+=item B<--critical-errors-prct>
+
+Critical threshold for web browser errors (4xx and 5xx types) in percentage.
+
+=item B<--warning-sessions>
+
+Warning threshold for web sessions number.
+
+=item B<--critical-sessions>
+
+Critical threshold for web sessions number.
+
+=item B<--warning-request>
+
+Warning threshold for requests number.
+
+=item B<--critical-request>
+
+Critical threshold for requests number.
+
+=item B<--warning-pages>
+
+Warning threshold for requested pages by the application.
+
+=item B<--critical-pages>
+
+Critical threshold for requested pages by the application.
+
+=back
+
+=cut
