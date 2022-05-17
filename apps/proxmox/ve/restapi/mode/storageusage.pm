177ebaabb1f895d162e687868e4252ddaf894e26
garnier-quentin <garnier.quentin@gmail.com>
2022-05-17 11:59:04 +0200

    (plugin) apps::proxmox::ve::restapi - add filtering capabilities to storageusage

--- apps/proxmox/ve/restapi/mode/storageusage.pm
+++ apps/proxmox/ve/restapi/mode/storageusage.pm
@@ -25,22 +25,12 @@ use base qw(centreon::plugins::templates::counter);
 use strict;
 use warnings;
 use Digest::MD5 qw(md5_hex);
-use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold);
+use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold_ng);
 
 sub custom_status_output {
     my ($self, %options) = @_;
-    my $msg = 'state : ' . $self->{result_values}->{state};
 
-    return $msg;
-}
-
-sub custom_status_calc {
-    my ($self, %options) = @_;
-
-    $self->{result_values}->{state} = $options{new_datas}->{$self->{instance} . '_state'};
-    $self->{result_values}->{name} = $options{new_datas}->{$self->{instance} . '_name'};
-
-    return 0;
+    return 'state : ' . $self->{result_values}->{state};
 }
 
 sub custom_storage_perfdata {
@@ -59,8 +49,10 @@ sub custom_storage_perfdata {
 sub custom_storage_threshold {
     my ($self, %options) = @_;
 
-    my $exit = $self->{perfdata}->threshold_check(value => $self->{result_values}->{prct_used},
-                                                  threshold => [ { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' }, { label => 'warning-' . $self->{thlabel}, exit_litteral => 'warning' } ]);
+    my $exit = $self->{perfdata}->threshold_check(
+        value => $self->{result_values}->{prct_used},
+        threshold => [ { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' }, { label => 'warning-' . $self->{thlabel}, exit_litteral => 'warning' } ]
+    );
     return $exit;
 }
 
@@ -71,11 +63,12 @@ sub custom_storage_output {
     my ($total_used_value, $total_used_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{used});
     my ($total_free_value, $total_free_unit) = $self->{perfdata}->change_bytes(value => $self->{result_values}->{free});
 
-    my $msg = sprintf("Storage Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
+    return sprintf(
+        "Storage Total: %s Used: %s (%.2f%%) Free: %s (%.2f%%)",
         $total_size_value . " " . $total_size_unit,
         $total_used_value . " " . $total_used_unit, $self->{result_values}->{prct_used},
-        $total_free_value . " " . $total_free_unit, $self->{result_values}->{prct_free});
-    return $msg;
+        $total_free_value . " " . $total_free_unit, $self->{result_values}->{prct_free}
+    );
 }
 
 sub custom_storage_calc {
@@ -87,7 +80,14 @@ sub custom_storage_calc {
     $self->{result_values}->{free} = $self->{result_values}->{total} - $self->{result_values}->{used};
     $self->{result_values}->{prct_free} = $self->{result_values}->{free} * 100 / $self->{result_values}->{total};
     $self->{result_values}->{prct_used} = $self->{result_values}->{used} * 100 / $self->{result_values}->{total};
+
     return 0;
+}
+
+sub prefix_storages_output {
+    my ($self, %options) = @_;
+
+    return "Storage '" . $options{instance_value}->{display} . "' ";
 }
 
 sub set_counters {
@@ -95,16 +95,15 @@ sub set_counters {
 
     $self->{maps_counters_type} = [
         { name => 'storages', type => 1, cb_prefix_output => 'prefix_storages_output',
-          message_multiple => 'All storages are ok', skipped_code => { -11 => 1 } },
+          message_multiple => 'All storages are ok', skipped_code => { -11 => 1 } }
     ];
 
     $self->{maps_counters}->{storages} = [
-        { label => 'storage-status', threshold => 0, set => {
+        { label => 'storage-status', type => 2, set => {
                 key_values => [ { name => 'state' }, { name => 'name' } ],
-                closure_custom_calc => $self->can('custom_status_calc'),
                 closure_custom_output => $self->can('custom_status_output'),
                 closure_custom_perfdata => sub { return 0; },
-                closure_custom_threshold_check => \&catalog_status_threshold,
+                closure_custom_threshold_check => \&catalog_status_threshold_ng
             }
         },
         { label => 'storage', set => {
@@ -123,15 +122,14 @@ sub new {
     my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1);
     bless $self, $class;
 
-    $options{options}->add_options(arguments =>
-                                {
-                                    "storage-id:s"                  => { name => 'storage_id' },
-                                    "storage-name:s"                => { name => 'storage_name' },
-                                    "filter-name:s"                 => { name => 'filter_name' },
-                                    "use-name"                      => { name => 'use_name' },
-                                    "warning-storage-status:s"      => { name => 'warning_storage_status', default => '' },
-                                    "critical-storage-status:s"     => { name => 'critical_storage_status', default => '' },
-                                });
+    $options{options}->add_options(arguments => {
+        'storage-id:s'   => { name => 'storage_id' },
+        'storage-name:s' => { name => 'storage_name' },
+        'filter-name:s'  => { name => 'filter_name' },
+        'use-name'       => { name => 'use_name' },
+        'node-id:s'      => { name => 'node_id' },
+        'node-name:s'    => { name => 'node_name' }
+    });
 
     $self->{statefile_cache_storages} = centreon::plugins::statefile->new(%options);
     return $self;
@@ -141,27 +139,21 @@ sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::check_options(%options);
 
-    $self->change_macros(macros => ['warning_storage_status', 'critical_storage_status']);
     $self->{statefile_cache_storages}->check_options(%options);
-}
-
-sub prefix_storages_output {
-    my ($self, %options) = @_;
-
-    return "Storage '" . $options{instance_value}->{display} . "' ";
 }
 
 sub manage_selection {
     my ($self, %options) = @_;
 
-    $self->{storages} = {};
-
     my $result = $options{custom}->api_get_storages(
         storage_id => $self->{option_results}->{storage_id},
         storage_name => $self->{option_results}->{storage_name},
+        node_id => $self->{option_results}->{node_id},
+        node_name => $self->{option_results}->{node_name},
         statefile => $self->{statefile_cache_storages}
     );
 
+    $self->{storages} = {};
     foreach my $storage_id (keys %{$result}) {
         next if (!defined($result->{$storage_id}->{Stats}));
 
@@ -171,12 +163,13 @@ sub manage_selection {
             $self->{output}->output_add(long_msg => "skipping  '" . $name . "': no matching filter.", debug => 1);
             next;
         }
+
         $self->{storages}->{$storage_id} = {
             display => defined($self->{option_results}->{use_name}) ? $name : $storage_id,
             name => $name,
             state => $result->{$storage_id}->{State},
             storage_usage => $result->{$storage_id}->{Stats}->{used},
-            storage_total => $result->{$storage_id}->{Stats}->{total},
+            storage_total => $result->{$storage_id}->{Stats}->{total}
         };
     }
 
@@ -186,11 +179,16 @@ sub manage_selection {
     }
 
     my $hostnames = $options{custom}->get_hostnames();
-    $self->{cache_name} = "proxmox_" . $self->{mode} . '_' .$hostnames . '_' . $options{custom}->get_port() . '_' .
-        (defined($self->{option_results}->{filter_counters}) ? md5_hex($self->{option_results}->{filter_counters}) : md5_hex('all')) . '_' .
-        (defined($self->{option_results}->{filter_name}) ? md5_hex($self->{option_results}->{filter_name}) : md5_hex('all')) . '_' .
-        (defined($self->{option_results}->{storage_id}) ? md5_hex($self->{option_results}->{storage_id}) : md5_hex('all')) . '_' .
-        (defined($self->{option_results}->{storage_name}) ? md5_hex($self->{option_results}->{storage_name}) : md5_hex('all'));
+    $self->{cache_name} = 'proxmox_' . $self->{mode} . '_' . $hostnames . '_' . $options{custom}->get_port() . '_' .
+        md5_hex(
+            (defined($self->{option_results}->{filter_counters}) ? $self->{option_results}->{filter_counters} : '') . '_' .
+            (defined($self->{option_results}->{filter_name}) ? $self->{option_results}->{filter_name} : '') . '_' .
+            (defined($self->{option_results}->{storage_id}) ? $self->{option_results}->{storage_id} : '') . '_' .
+            (defined($self->{option_results}->{storage_name}) ? $self->{option_results}->{storage_name} : '') . '_' .
+            (defined($self->{option_results}->{node_id}) ? $self->{option_results}->{node_id} : '') . '_' .
+            (defined($self->{option_results}->{node_name}) ? $self->{option_results}->{node_name} : '')
+        );
+        
 }
 
 1;
@@ -202,6 +200,11 @@ __END__
 Check storage usage.
 
 =over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: --filter-counters='^storage-status$'
 
 =item B<--storage-id>
 
@@ -219,30 +222,28 @@ Use storage name for perfdata and display.
 
 Filter by storage name (can be a regexp).
 
-=item B<--filter-counters>
+=item B<--node-id>
 
-Only display some counters (regexp can be used).
-Example: --filter-counters='^storage-status$'
+Exact node ID.
 
-=item B<--warning-*>
+=item B<--node-name>
 
-Threshold warning.
-Can be:  'storage' (%).
-
-=item B<--critical-*>
-
-Threshold critical.
-Can be:  'storage' (%).
+Exact node name.
 
 =item B<--warning-storage-status>
 
-Set warning threshold for status (Default: -)
+Set warning threshold for status.
 Can used special variables like: %{name}, %{state}.
 
 =item B<--critical-storage-status>
 
-Set critical threshold for status (Default: -).
+Set critical threshold for status.
 Can used special variables like: %{name}, %{state}.
+
+=item B<--warning-*> B<--critical-*>
+
+Thresholds.
+Can be: 'storage' (%).
 
 =back
 
