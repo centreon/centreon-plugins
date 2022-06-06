a3a51ce57787d2ab5efb9963420a7b3c7d7623b6
garnier-quentin <garnier.quentin@gmail.com>
2022-06-06 14:30:25 +0200

    wip

--- apps/protocols/whois/mode/domain.pm
+++ apps/protocols/whois/mode/domain.pm
@@ -207,7 +207,7 @@ sub get_status {
         'The domain has not been registered',
         'no\s+se\s+encuentra\s+registrado',
         #xxxxx is free
-        '\s+is\s+free$'
+        '^[^\s+]+\s+is\s+free$'
     );
     foreach (@unregistered) {
         if ($options{output} =~ /$_/msi) {
