138228b3fa1a5c701b41de0e70b359b17c0ce269
lchrdn <lchardon@centreon.com>
2022-09-21 18:02:53 +0200

    draft for splunk plugin

---
+++ apps/monitoring/splunk/custom/api.pm
@@ -0,0 +1,288 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::splunk::custom::api;
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+use centreon::plugins::statefile;
+use XML::LibXML::Simple;
+use URI::Encode;
+use Digest::MD5 qw(md5_hex);
+
+sub new {
+    my ($class, %options) = @_;
+    my $self  = {};
+    bless $self, $class;
+
+    if (!defined($options{output})) {
+        print "Class Custom: Need to specify 'output' argument.\n";
+        exit 3;
+    }
+    if (!defined($options{options})) {
+        $options{output}->add_option_msg(short_msg => "Class Custom: Need to specify 'options' argument.");
+        $options{output}->option_exit();
+    }
+    
+    if (!defined($options{noptions})) {
+        $options{options}->add_options(arguments =>  {
+            'hostname:s'             => { name => 'hostname' },
+            'url-path:s'             => { name => 'url_path' },
+            'port:s'                 => { name => 'port' },
+            'proto:s'                => { name => 'proto' },
+            'api-username:s'         => { name => 'api_username' },
+            'api-password:s'         => { name => 'api_password' },
+            'timeout:s'              => { name => 'timeout' },
+            'unknown-http-status:s'  => { name => 'unknown_http_status' },
+            'warning-http-status:s'  => { name => 'warning_http_status' },
+            'critical-http-status:s' => { name => 'critical_http_status' }
+        });
+    }
+    $options{options}->add_help(package => __PACKAGE__, sections => 'REST API OPTIONS', once => 1);
+
+    $self->{output} = $options{output};
+    $self->{http} = centreon::plugins::http->new(%options);
+    $self->{cache} = centreon::plugins::statefile->new(%options);
+    
+    return $self;
+}
+
+sub set_options {
+    my ($self, %options) = @_;
+
+    $self->{option_results} = $options{option_results};
+}
+
+sub set_defaults {}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->{hostname} = (defined($self->{option_results}->{hostname})) ? $self->{option_results}->{hostname} : '';
+    $self->{port} = (defined($self->{option_results}->{port})) ? $self->{option_results}->{port} : 8089;
+    $self->{proto} = (defined($self->{option_results}->{proto})) ? $self->{option_results}->{proto} : 'https';
+    $self->{url_path} = (defined($self->{option_results}->{url_path})) ? $self->{option_results}->{url_path} : '';
+    $self->{timeout} = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 10;
+    $self->{api_username} = (defined($self->{option_results}->{api_username})) ? $self->{option_results}->{api_username} : '';
+    $self->{api_password} = (defined($self->{option_results}->{api_password})) ? $self->{option_results}->{api_password} : '';
+    $self->{unknown_http_status} = (defined($self->{option_results}->{unknown_http_status})) ? $self->{option_results}->{unknown_http_status} : '';
+    $self->{warning_http_status} = (defined($self->{option_results}->{warning_http_status})) ? $self->{option_results}->{warning_http_status} : '';
+    $self->{critical_http_status} = (defined($self->{option_results}->{critical_http_status})) ? $self->{option_results}->{critical_http_status} : '';
+
+    if ($self->{hostname} eq '') {
+        $self->{output}->add_option_msg(short_msg => 'Need to specify hostname option.');
+        $self->{output}->option_exit();
+    }
+    if ($self->{api_username} eq '') {
+        $self->{output}->add_option_msg(short_msg => 'Need to specify --api-username option.');
+        $self->{output}->option_exit();
+    }
+    if ($self->{api_password} eq '') {
+        $self->{output}->add_option_msg(short_msg => 'Need to specify --api-password option.');
+        $self->{output}->option_exit();
+    } 
+
+    $self->{cache}->check_options(option_results => $self->{option_results});
+    return 0;
+}
+
+sub build_options_for_httplib {
+    my ($self, %options) = @_;
+
+    $self->{option_results}->{hostname} = $self->{hostname};
+    $self->{option_results}->{timeout} = $self->{timeout};
+    $self->{option_results}->{port} = $self->{port};
+    $self->{option_results}->{proto} = $self->{proto};
+}
+
+sub settings {
+    my ($self, %options) = @_;
+
+    $self->build_options_for_httplib();
+    # $self->{http}->add_header(key => 'Content-Type', value => 'Content-Type: application/x-www-form-urlencoded');
+    $self->{http}->add_header(key => 'X-Requested-By', value => $self->{requested_by});
+    $self->{http}->add_header(key => 'Authorization', value => 'Splunk ' . $self->{session_token}) if defined($self->{session_token});
+    $self->{http}->set_options(%{$self->{option_results}});
+}
+
+# sub get_connection_info {
+#     my ($self, %options) = @_;
+
+#     return $self->{hostname} . ":" . $self->{port};
+# }
+
+# sub get_hostname {
+#     my ($self, %options) = @_;
+
+#     return $self->{hostname};
+# }
+
+# sub get_port {
+#     my ($self, %options) = @_;
+    
+#     return $self->{port};
+# }
+
+sub clean_session_token {
+    my ($self, %options) = @_;
+
+    my $datas = {};
+    $options{statefile}->write(data => $datas);
+    $self->{session_token} = undef;
+}
+
+sub authenticate {
+    my ($self, %options) = @_;
+   
+    my $has_cache_file = $options{statefile}->read(statefile => 'splunk_api_' . md5_hex($self->{option_results}->{hostname}) . '_' . md5_hex($self->{option_results}->{api_username}));
+    my $session_token = $options{statefile}->get(name => 'session_token');
+
+    if ($has_cache_file == 0 || !defined($session_token)) {
+        my $credentials = [
+            'username=' . $self->{api_username},
+            'password=' . $self->{api_password}
+        ];
+
+        my ($content) = $self->{http}->request(
+            method => 'POST',
+            url_path => '/services/auth/login',
+            post_param => $credentials,
+            unknown_status => $self->{unknown_http_status},
+            warning_status => $self->{warning_http_status},
+            critical_status => $self->{critical_http_status}
+        );
+
+        if ($self->{http}->get_code() != 200) {
+            $self->{output}->add_option_msg(short_msg => "login error [code: '" . $self->{http}->get_code() . "'] [message: '" . $self->{http}->get_message() . "']");
+            $self->{output}->option_exit();
+        }
+
+        my $xml_result;
+        eval {
+            $SIG{__WARN__} = sub {};
+            $xml_result = XMLin($content, ForceArray => $options{force_array}, KeyAttr => []);
+        };
+        if ($@) {
+            $self->{output}->add_option_msg(short_msg => "Cannot decode xml response: $@");
+            $self->{output}->option_exit();
+        }
+        if (defined($xml_result->{XPathError})) {
+            $self->{output}->add_option_msg(short_msg => "Api return error: " . $xml_result->{XPathError}->{Reason});
+            $self->{output}->option_exit();
+        }
+
+        if (!defined($xml_result) || !defined($xml_result->{sessionKey})) {
+            $self->{output}->add_option_msg(short_msg => 'error retrieving session_token');
+            $self->{output}->option_exit();
+        }
+       
+        $session_token = $xml_result->{sessionKey};
+
+        my $datas = { session_token => $session_token };
+        $options{statefile}->write(data => $datas);
+    }
+
+    $self->{session_token} = $session_token;
+}
+
+sub query_count {
+    my ($self, %options) = @_;
+
+    my $query_sid = $self->request_api(
+        method => 'POST',
+        endpoint => '/services/search/jobs',
+        post_param => [
+        'search=' . 'search ' . $options{query} . $options{timeframe} . ' latest=now | stats count'
+        ]
+    );
+    sleep(1);
+
+    my $query_count = $self->request_api(
+        method => 'GET',
+        endpoint => '/services/search/jobs/' . $query_sid->{sid},
+        # post_param => ''
+    );
+
+    my $content = $query_count->{content}->{'s:dict'}->{'s:key'};
+
+    use Data::Dumper;
+    print Dumper $content;
+    exit 1;
+
+    # return $query_sid;
+}
+
+sub request_api {
+    my ($self, %options) = @_;
+
+    $self->settings();
+    if (!defined($self->{session_token})) {
+        $self->authenticate(statefile => $self->{cache});
+    }
+    
+    my $content = $self->{http}->request(
+        method => $options{method},
+        url_path => $options{endpoint},
+        post_param => $options{post_param},
+        unknown_status => $self->{unknown_http_status},
+        warning_status => $self->{warning_http_status},
+        critical_status => $self->{critical_http_status}
+    );
+
+    if ($self->{http}->get_code() < 200 || $self->{http}->get_code() >= 300) {
+        $self->clean_session_token(statefile => $self->{cache});     
+        $self->authenticate(statefile => $self->{cache});
+        $content = $self->{http}->request(
+            method => $options{method},
+            url_path => $options{endpoint},
+            post_param => $options{post_param},
+            unknown_status => $self->{unknown_http_status},
+            warning_status => $self->{warning_http_status},
+            critical_status => $self->{critical_http_status}
+        );
+    }
+
+    my $xml_result;
+    eval {
+        $SIG{__WARN__} = sub {};
+        $xml_result = XMLin($content, ForceArray => $options{force_array}, KeyAttr => []);
+    };
+    if ($@) {
+        $self->{output}->add_option_msg(short_msg => "Cannot decode xml response: $@");
+        $self->{output}->option_exit();
+    }
+    if (defined($xml_result->{XPathError})) {
+        $self->{output}->add_option_msg(short_msg => "Api return error: " . $xml_result->{XPathError}->{Reason});
+        $self->{output}->option_exit();
+    }
+
+    return $xml_result;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+=head1 REST API OPTIONS
+
+=cut
---
+++ apps/monitoring/splunk/mode/query.pm
@@ -0,0 +1,107 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::splunk::mode::query;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, skipped_code => { -10 => 1 } }
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'query-matches', nlabel => 'graylog.query.match.count', set => {
+                key_values => [ { name => 'query_matches' } ],
+                output_template => 'current queue messages: %s',
+                perfdatas => [
+                    { template => '%d', min => 0 }
+                ]
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'query:s'     => { name => 'query' },
+        'timeframe:s' => { name => 'timeframe', default => 300 }
+    });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+
+    # if (!defined($self->{option_results}->{query}) || $self->{option_results}->{query} eq '') {
+    #     $self->{output}->add_option_msg(short_msg => 'Please set --query option.');
+    #     $self->{output}->option_exit();
+    # }
+    
+    # if (defined($self->{option_results}->{timeframe}) && $self->{option_results}->{timeframe} eq '') {
+    #     $self->{output}->add_option_msg(short_msg => 'Please set --timeframe value.');
+    #     $self->{output}->option_exit();
+    # }
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->query_count(
+        query => 'host="xxxx" "tata" earliest=',
+        timeframe => '-15000min'
+    );
+
+    use Data::Dumper;
+    print Dumper $result;
+
+    # $self->{global} = {
+    #     query_matches => $result->{total_results}
+    # };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+WIP for Splunk query mode.
+
+=over 8
+
+=item B<--query>
+
+=item B<--timeframe>
+
+=back
+
+=cut
