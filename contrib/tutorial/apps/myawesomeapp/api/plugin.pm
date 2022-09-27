d6b7ed25ab7602c6e4d85e7740e4ac6b9ff7cca0
Simon Bomm <sbomm@centreon.com>
2022-09-27 12:10:50 +0200

    (contrib) Everything your grandma needs to know to write a plugin and a mode (#3923)
    * (plugin) cloud::azure::management::costs - Fix disco_* subs
    
    * (wip)tutorial
    
    * (wip)tutorial
    
    * + final tutorial resource commit

---
+++ contrib/tutorial/apps/myawesomeapp/api/plugin.pm
@@ -0,0 +1,62 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Name of your perl package
+package apps::myawesomeapp::api::plugin;
+
+# Always use strict and warnings, will guarantee that your code is clean and help debugging it
+use strict;
+use warnings;
+# Load the base for your plugin, here we don't do SNMP, SQL or have a custom directory, so we use the _simple base
+use base qw(centreon::plugins::script_simple);
+
+# Global sub to create and return the perl object. Don't bother understand what each instruction is doing. 
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    # A version, we don't really use it but could help if your want to version your code
+    $self->{version} = '0.1';
+    # Important part! 
+    #    On the left, the name of the mode as users will use it in their command line
+    #    On the right, the path to the file (note that .pm is not present at the end)
+    $self->{modes} = {
+        'app-metrics' => 'apps::myawesomeapp::api::mode::appmetrics'
+    };
+
+    return $self;
+}
+
+# Declare this file as a perl module/package
+1;
+
+# Beginning of the documenation/help. __END__ Specify to the interpreter that instructions below don't need to be compiled
+# =head1 [..] Specify the section level and the label when using the plugin with --help
+# Check my-awesome [..] Quick overview of wath the plugin is doing
+# =cut Close the head1 section
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check my-awesome-app health and metrics through its custom API
+
+=cut
