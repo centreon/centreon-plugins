10a282c4b964c13be1dfce44bf01d8e76775264e
qgarnier <garnier.quentin@gmail.com>
2022-09-28 13:20:51 +0200

    (plugin) network::nortel::standard::snmp - mode interfaces add entreprise duplex oid (#3932)

---
+++ network/nortel/standard/snmp/mode/interfaces.pm
@@ -0,0 +1,204 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::nortel::standard::snmp::mode::interfaces;
+
+use base qw(snmp_standard::mode::interfaces);
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    return $self;
+}
+
+sub load_status {
+    my ($self, %options) = @_;
+
+    $self->SUPER::load_status(%options);
+
+    $self->{oid_rcPortOperDuplex} = '.1.3.6.1.4.1.2272.1.4.10.1.1.13';
+    $self->{oid_rcPortOperDuplex_mapping} = {
+        0 => 'unknown', 1 => 'halfDuplex', 2 => 'fullDuplex',
+    };
+
+    $self->{snmp}->load(oids => [$self->{oid_rcPortOperDuplex}], instances => $self->{array_interface_selected});
+}
+
+sub add_result_status {
+    my ($self, %options) = @_;
+
+    $self->SUPER::add_result_status(%options);
+    if ($self->{int}->{ $options{instance} }->{duplexstatus} eq 'n/a') {
+        $self->{int}->{ $options{instance} }->{duplexstatus} = defined($self->{results}->{ $self->{oid_rcPortOperDuplex} . '.' . $options{instance} }) ? $self->{oid_rcPortOperDuplex_mapping}->{ $self->{results}->{ $self->{oid_rcPortOperDuplex} . '.' . $options{instance} } } : 'n/a';
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check interfaces.
+
+=over 8
+
+=item B<--add-global>
+
+Check global port statistics (By default if no --add-* option is set).
+
+=item B<--add-status>
+
+Check interface status.
+
+=item B<--add-duplex-status>
+
+Check duplex status (with --warning-status and --critical-status).
+
+=item B<--add-traffic>
+
+Check interface traffic.
+
+=item B<--add-errors>
+
+Check interface errors.
+
+=item B<--add-cast>
+
+Check interface cast.
+
+=item B<--add-speed>
+
+Check interface speed.
+
+=item B<--add-volume>
+
+Check interface data volume between two checks (not supposed to be graphed, useful for BI reporting).
+
+=item B<--check-metrics>
+
+If the expression is true, metrics are checked (Default: '%{opstatus} eq "up"').
+
+=item B<--warning-status>
+
+Set warning threshold for status.
+Can used special variables like: %{admstatus}, %{opstatus}, %{duplexstatus}, %{display}
+
+=item B<--critical-status>
+
+Set critical threshold for status (Default: '%{admstatus} eq "up" and %{opstatus} ne "up"').
+Can used special variables like: %{admstatus}, %{opstatus}, %{duplexstatus}, %{display}
+
+=item B<--warning-*> B<--critical-*>
+
+Thresholds.
+Can be: 'total-port', 'total-admin-up', 'total-admin-down', 'total-oper-up', 'total-oper-down',
+'in-traffic', 'out-traffic', 'in-error', 'in-discard', 'out-error', 'out-discard',
+'in-ucast', 'in-bcast', 'in-mcast', 'out-ucast', 'out-bcast', 'out-mcast',
+'speed' (b/s).
+
+=item B<--units-traffic>
+
+Units of thresholds for the traffic (Default: 'percent_delta') ('percent_delta', 'bps', 'counter').
+
+=item B<--units-errors>
+
+Units of thresholds for errors/discards (Default: 'percent_delta') ('percent_delta', 'percent', 'delta', 'counter').
+
+=item B<--units-cast>
+
+Units of thresholds for communication types (Default: 'percent_delta') ('percent_delta', 'percent', 'delta', 'counter').
+
+=item B<--nagvis-perfdata>
+
+Display traffic perfdata to be compatible with nagvis widget.
+
+=item B<--interface>
+
+Set the interface (number expected) ex: 1,2,... (empty means 'check all interface').
+
+=item B<--name>
+
+Allows to use interface name with option --interface instead of interface oid index (Can be a regexp)
+
+=item B<--speed>
+
+Set interface speed for incoming/outgoing traffic (in Mb).
+
+=item B<--speed-in>
+
+Set interface speed for incoming traffic (in Mb).
+
+=item B<--speed-out>
+
+Set interface speed for outgoing traffic (in Mb).
+
+=item B<--map-speed-dsl>
+
+Get interface speed configuration for interface type 'adsl' and 'vdsl2'.
+
+Syntax: --map-speed-dsl=interface-src-name,interface-dsl-name
+
+E.g: --map-speed-dsl=Et0.835,Et0-vdsl2
+
+=item B<--force-counters64>
+
+Force to use 64 bits counters only. Can be used to improve performance.
+
+=item B<--force-counters32>
+
+Force to use 32 bits counters (even in snmp v2c and v3). Should be used when 64 bits counters are buggy.
+
+=item B<--reload-cache-time>
+
+Time in minutes before reloading cache file (default: 180).
+
+=item B<--oid-filter>
+
+Choose OID used to filter interface (default: ifName) (values: ifDesc, ifAlias, ifName, IpAddr).
+
+=item B<--oid-display>
+
+Choose OID used to display interface (default: ifName) (values: ifDesc, ifAlias, ifName, IpAddr).
+
+=item B<--oid-extra-display>
+
+Add an OID to display.
+
+=item B<--display-transform-src>
+
+Regexp src to transform display value.
+
+=item B<--display-transform-dst>
+
+Regexp dst to transform display value.
+
+=item B<--show-cache>
+
+Display cache interface datas.
+
+=back
+
+=cut
