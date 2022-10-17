5716a541f7993431342769677a7dd800a229d3f7
garnier-quentin <garnier.quentin@gmail.com>
2022-10-17 10:48:56 +0200

    wip

--- apps/thales/mistral/vs9/restapi/mode/devices.pm
+++ apps/thales/mistral/vs9/restapi/mode/devices.pm
@@ -24,7 +24,40 @@ use base qw(centreon::plugins::templates::counter);
 
 use strict;
 use warnings;
+use DateTime;
+use POSIX;
+use centreon::plugins::misc;
 use centreon::plugins::templates::catalog_functions qw(catalog_status_threshold_ng);
+
+my $unitdiv = { s => 1, w => 604800, d => 86400, h => 3600, m => 60 };
+my $unitdiv_long = { s => 'seconds', w => 'weeks', d => 'days', h => 'hours', m => 'minutes' };
+
+sub custom_connection_perfdata {
+    my ($self, %options) = @_;
+
+    $self->{output}->perfdata_add(
+        nlabel => $self->{nlabel} . '.' . $unitdiv_long->{ $self->{instance_mode}->{option_results}->{time_unit} },
+        unit => $self->{instance_mode}->{option_results}->{time_unit},
+        instances => $self->{result_values}->{sn},
+        value => floor($self->{result_values}->{connection_seconds} / $unitdiv->{ $self->{instance_mode}->{option_results}->{time_unit} }),
+        warning => $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{thlabel}),
+        critical => $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{thlabel}),
+        min => 0
+    );
+}
+
+sub custom_connection_threshold {
+    my ($self, %options) = @_;
+
+    return $self->{perfdata}->threshold_check(
+        value => floor($self->{result_values}->{connection_seconds} / $unitdiv->{ $self->{instance_mode}->{option_results}->{time_unit} }),
+        threshold => [
+            { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' },
+            { label => 'warning-'. $self->{thlabel}, exit_litteral => 'warning' },
+            { label => 'unknown-'. $self->{thlabel}, exit_litteral => 'unknown' }
+        ]
+    );
+}
 
 sub device_long_output {
     my ($self, %options) = @_;
@@ -67,9 +100,8 @@ sub set_counters {
         {
             name => 'devices', type => 3, cb_prefix_output => 'prefix_device_output', cb_long_output => 'device_long_output', indent_long_output => '    ', message_multiple => 'All devices are ok',
             group => [
-                { name => 'connection', type => 0 },
-                { name => 'dedup', type => 0 },
-                { name => 'services', type => 1, cb_prefix_output => 'prefix_service_output', message_multiple => 'services are ok', display_long => 1, skipped_code => { -10 => 1 } },
+                { name => 'connection', type => 0, skipped_code => { -10 => 1 } },
+                { name => 'services', type => 1, cb_prefix_output => 'prefix_service_output', message_multiple => 'services are ok', display_long => 1, skipped_code => { -10 => 1 } }
             ]
         }
     ];
@@ -97,6 +129,14 @@ sub set_counters {
                 closure_custom_perfdata => sub { return 0; },
                 closure_custom_threshold_check => \&catalog_status_threshold_ng
             }
+        },
+        { label => 'connection-last-time', nlabel => 'device.connection.last.time', set => {
+                key_values      => [ { name => 'connection_seconds' }, { name => 'connection_human' }, { name => 'sn' } ],
+                output_template => 'last connection: %s',
+                output_use => 'connection_human',
+                closure_custom_perfdata => $self->can('custom_connection_perfdata'),
+                closure_custom_threshold_check => $self->can('custom_connection_threshold')
+            }
         }
     ];
 
@@ -122,7 +162,9 @@ sub new {
     $options{options}->add_options(arguments => {
         'filter-id:s' => { name => 'filter_id' },
         'filter-sn:s' => { name => 'filter_sn' },
-        'add-status'  => { name => 'add_status' }
+        'add-status'  => { name => 'add_status' },
+        'time-unit:s' => { name => 'time_unit', default => 's' },
+        'timezone:s'  => { name => 'timezone' }
     });
 
     return $self;
@@ -139,6 +181,11 @@ sub check_options {
     if ($selected == 0) {
         $self->{option_results}->{add_status} = 1;
     }
+
+    if ($self->{option_results}->{time_unit} eq '' || !defined($unitdiv->{$self->{option_results}->{time_unit}})) {
+        $self->{option_results}->{time_unit} = 's';
+    }
+    $self->{option_results}->{timezone} = 'UTC' if (!defined($self->{option_results}->{timezone}) || $self->{option_results}->{timezone} eq '');
 }
 
 sub manage_selection {
@@ -162,6 +209,13 @@ sub manage_selection {
                 sn => $device->{serialNumber},
                 connectionStatus => lc($device->{status}->{connectedStatus})
             };
+
+            my $tz = centreon::plugins::misc::set_timezone(name => $self->{option_results}->{timezone});
+            my $dt = DateTime->from_epoch(epoch => $device->{status}->{statusEpochMilli} / 1000);
+            $self->{devices}->{ $device->{id} }->{connection}->{connection_seconds} = time() - $dt->epoch();
+            $self->{devices}->{ $device->{id} }->{connection}->{connection_human} = centreon::plugins::misc::change_seconds(
+                value => $self->{devices}->{ $device->{id} }->{connection}->{connection_seconds}
+            );
         }
     }
 }
