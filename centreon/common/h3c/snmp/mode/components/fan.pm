8a0606d90730d9577e1c726a126d91b7a85a8099
garnier-quentin <garnier.quentin@gmail.com>
2022-11-21 16:47:18 +0100

    (plugin) network::h3c::snmp - mode hardware skip high temperature

--- centreon/common/h3c/snmp/mode/components/fan.pm
+++ centreon/common/h3c/snmp/mode/components/fan.pm
@@ -28,7 +28,7 @@ my %map_fan_status = (
     2 => 'normal',
     4 => 'entityAbsent',
     41 => 'fanError',
-    91 => 'hardwareFaulty',
+    91 => 'hardwareFaulty'
 );
 
 sub check {
@@ -39,7 +39,7 @@ sub check {
     return if ($self->check_filter(section => 'fan'));
 
     my $mapping = {
-        EntityExtErrorStatus => { oid => $self->{branch} . '.19', map => \%map_fan_status },
+        EntityExtErrorStatus => { oid => $self->{branch} . '.19', map => \%map_fan_status }
     };
 
     foreach my $instance (sort $self->get_instance_class(class => { 7 => 1 })) {
@@ -51,17 +51,23 @@ sub check {
             $self->absent_problem(section => 'fan', instance => $instance);
             next;
         }
-        
+
         my $name = '';
         $name = $self->get_short_name(instance => $instance) if (defined($self->{short_name}) && $self->{short_name} == 1);
         $name = $self->get_long_name(instance => $instance) unless (defined($self->{short_name}) && $self->{short_name} == 1 && defined($name) && $name ne '');
         $self->{components}->{fan}->{total}++;
-        $self->{output}->output_add(long_msg => sprintf("Fan '%s' status is '%s' [instance = %s]",
-                                                        $name, $result->{EntityExtErrorStatus}, $instance));
+        $self->{output}->output_add(
+            long_msg => sprintf(
+                "Fan '%s' status is '%s' [instance: %s]",
+                $name, $result->{EntityExtErrorStatus}, $instance
+            )
+        );
         my $exit = $self->get_severity(section => 'fan', value => $result->{EntityExtErrorStatus});
         if (!$self->{output}->is_status(value => $exit, compare => 'ok', litteral => 1)) {
-            $self->{output}->output_add(severity => $exit,
-                                        short_msg => sprintf("Fan '%s' status is '%s'", $name, $result->{EntityExtErrorStatus}));
+            $self->{output}->output_add(
+                severity => $exit,
+                short_msg => sprintf("Fan '%s' status is '%s'", $name, $result->{EntityExtErrorStatus})
+            );
         }
     }
 }
