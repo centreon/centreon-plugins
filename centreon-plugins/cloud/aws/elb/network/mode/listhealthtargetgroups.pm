d702df59aadba198613c3913d6d6fdc2cf0ae420
ponchoh <hmorales@centreon.com>
2023-01-02 02:58:26 -0500

    update copyright - happy new year 🎉 (#4124)

--- centreon-plugins/cloud/aws/elb/network/mode/listhealthtargetgroups.pm
+++ centreon-plugins/cloud/aws/elb/network/mode/listhealthtargetgroups.pm
@@ -1,6 +1,6 @@
 
 #
-# Copyright 2022 Centreon (http://www.centreon.com/)
+# Copyright 2023 Centreon (http://www.centreon.com/)
 #
 # Centreon is a full-fledged industry-strength solution that meets
 # the needs in IT infrastructure and application monitoring for
