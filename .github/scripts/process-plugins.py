50ba83a77c45797a02924415b685fc99387bd6c9
Kevin Duret <kduret@centreon.com>
2023-01-11 08:24:31 +0100

    add debug

--- .github/scripts/process-plugins.py
+++ .github/scripts/process-plugins.py
@@ -9,12 +9,12 @@ common = argv[1] == 'true'
 packages = argv[2]
 plugins = argv[3]
 
+print(plugins)
+
 list_plugins_dir = set()
 list_packages = set()
 
 if not common:
-    print('hello')
-    print(plugins)
     for plugin in plugins.split(' '):
         try:
             found = re.search('(.*)\/mode\/.*', plugin).group(1)
@@ -26,6 +26,8 @@ if not common:
             except AttributeError:
                 pass
     updated_packages = packages.split(' ')
+
+print(*list_plugins_dir)
 
 for filepath in os.popen('find packaging -type f -name pkg.json').read().split('\n')[0:-1]:
         packaging_file = open(filepath)
@@ -48,7 +50,6 @@ for filepath in os.popen('find packaging -type f -name pkg.json').read().split('
                         pkg_file_dir = 'centreon-plugins/' + found
                     except AttributeError:
                         pass
-                print('pkg_file_dir', pkg_file_dir)
                 if pkg_file_dir in list_plugins_dir:
                     list_packages.add(packaging_path)
 
