2775f9d1271b6c82a96442062637283529225441
Roman Morandell <46994680+rmorandell-pgum@users.noreply.github.com>
2023-05-25 12:36:07 +0200

    fortimail plugin based on a FortiMail 400F Model

---
+++ src/network/fortinet/fortimail/snmp/plugin.pm
@@ -0,0 +1,58 @@
+#
+# Copyright 2022 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::fortinet::fortimail::snmp::plugin;
+
+use strict;
+use warnings;
+use base qw(centreon::plugins::script_snmp);
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{version} = '1.0';
+    $self->{modes} = {
+        'cpu'             => 'network::fortinet::fortimail::snmp::mode::cpu',
+        'memory'          => 'network::fortinet::fortimail::snmp::mode::memory',
+        'ha'              => 'network::fortinet::fortimail::snmp::mode::ha',
+        'hardware'        => 'network::fortinet::fortimail::snmp::mode::hardware',
+        'diskusage'       => 'network::fortinet::fortimail::snmp::mode::diskusage',
+        'load'            => 'network::fortinet::fortimail::snmp::mode::load',
+        'queues'          => 'network::fortinet::fortimail::snmp::mode::queues',
+        'list-queues'     => 'network::fortinet::fortimail::snmp::mode::listqueues',
+        'uptime'          => 'snmp_standard::mode::uptime',
+        'interfaces'      => 'snmp_standard::mode::interfaces',
+        'list-interfaces' => 'snmp_standard::mode::listinterfaces',
+    };
+
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check fortimail in SNMP.
+
+=cut
