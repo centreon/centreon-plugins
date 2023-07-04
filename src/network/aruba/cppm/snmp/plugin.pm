0406bce2de9c7a08b7ba933e60b285849210a53c
garnier-quentin <garnier.quentin@gmail.com>
2023-07-04 09:18:15 +0200

    wip

--- src/network/aruba/cppm/snmp/plugin.pm
+++ src/network/aruba/cppm/snmp/plugin.pm
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Authors : Alexandre Moreau <alexandre.moreau@cheops.fr> (@SpyL1nk)
 
 package network::aruba::cppm::snmp::plugin;
 
