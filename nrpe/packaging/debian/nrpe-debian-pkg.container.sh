61307bd0b315528b214b6d9dc80cea1e7fc29eef
Kevin Duret <kduret@centreon.com>
2023-07-20 15:44:28 +0200

    fix debian packaging

--- nrpe/packaging/debian/nrpe-debian-pkg.container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-
-cd /usr/local/src/$PROJECT-$VERSION
-sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
-debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -y -r ${DISTRIB}
-debuild-pbuilder
