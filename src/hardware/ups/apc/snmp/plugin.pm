c21dd75d02d48e5586c4896d822319392a7b57bb
qgarnier <garnier.quentin@gmail.com>
2023-08-07 16:55:27 +0200

    (plugin) hardware::ups::apc::snmp - metric v2 + option --replace-lasttime-format (#4551)

--- src/hardware/ups/apc/snmp/plugin.pm
+++ src/hardware/ups/apc/snmp/plugin.pm
@@ -27,14 +27,13 @@ sub new {
     my $self = $class->SUPER::new(package => __PACKAGE__, %options);
     bless $self, $class;
 
-    $self->{version} = '0.1';
     $self->{modes} = {
         'battery-status' => 'hardware::ups::apc::snmp::mode::batterystatus',
         'input-lines'    => 'hardware::ups::apc::snmp::mode::inputlines',
         'output-lines'   => 'hardware::ups::apc::snmp::mode::outputlines',
         'sensors'        => 'hardware::ups::apc::snmp::mode::sensors',
         'time'           => 'hardware::ups::apc::snmp::mode::ntp',
-        'uptime'         => 'snmp_standard::mode::uptime'
+        'uptime'         => 'hardware::ups::apc::snmp::mode::uptime'
     };
 
     return $self;
