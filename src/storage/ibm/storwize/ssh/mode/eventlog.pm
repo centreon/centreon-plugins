ef1f16aba60a46e3d9819de431804f617cb589dd
Matteo D'Alfonso <mdalfonso@centreon.com>
2023-10-19 13:56:41 +0200

    Revert "fix: Converting from epoch to normal date, format was not valid for localtime()"
    This reverts commit df5880bbfac851d12d372fd7c82ee2f9d6fbc9b2.

--- src/storage/ibm/storwize/ssh/mode/eventlog.pm
+++ src/storage/ibm/storwize/ssh/mode/eventlog.pm
@@ -57,10 +57,8 @@ sub check_options {
 
     my $last_timestamp = '';
     if (defined($self->{option_results}->{retention}) && $self->{option_results}->{retention} =~ /^\d+$/) {
-        my $epoch = time();
-        $retention_epoch = $epoch - $self->{option_results}->{retention};
         # by default UTC timezone used
-        my $dt = DateTime->from_epoch(epoch => $retention_epoch);
+        my $dt = DateTime->from_epoch(epoch => time() - $self->{option_results}->{retention});
         my $dt_format = sprintf("%d%02d%02d%02d%02d%02d", substr($dt->year(), 2), $dt->month(), $dt->day(), $dt->hour(), $dt->minute(), $dt->second());
         $last_timestamp = 'last_timestamp>=' . $dt_format . ":";
     }
@@ -90,7 +88,7 @@ sub run {
         $self->{output}->output_add(
             long_msg => sprintf(
                 '%s : %s - %s', 
-                scalar(localtime($_->{retention_epoch})),
+                scalar(localtime($_->{last_timestamp})),
                 $_->{event_id}, $_->{description}
             )
         );
