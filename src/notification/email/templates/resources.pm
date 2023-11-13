4e5c84eddcfa893b3afe9c9dfd6c4a9b9145c5e5
Tpo76 <fthepaut@centreon.com>
2023-11-13 12:06:31 -0500

    Enhance notification look

---
+++ src/notification/email/templates/resources.pm
@@ -0,0 +1,14 @@
+package notification::email::templates::resources;
+        
+use strict;
+use warnings;
+
+use notification::email::templates::style qw(get_css);
+use notification::email::templates::host qw(get_host_template);
+use notification::email::templates::service qw(get_service_template);
+
+our $get_service_template = get_service_template();
+our $get_host_template    = get_host_template();
+our $get_css              = get_css();
+
+1;
