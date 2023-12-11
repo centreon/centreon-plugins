bacef5806adfe821336aa4c67bedbd66e03d12ea
omercier <32134301+omercier@users.noreply.github.com>
2023-12-11 13:47:48 +0100

    Update src/network/vectra/restapi/mode/uptime.pm
    Co-authored-by: sdepassio <114986849+sdepassio@users.noreply.github.com>

--- src/network/vectra/restapi/mode/uptime.pm
+++ src/network/vectra/restapi/mode/uptime.pm
@@ -146,7 +146,7 @@ Display model and firmware informations
 
 =item B<--unit>
 
-Select the time unit for the performance data and thresholds. May be 's' for seconds,'m' for minutes, 'h' for hours, 'd' for days, 'w' for weeks. Default is seconds.
+Select the time unit for the performance data and thresholds. May be 's' for seconds, 'm' for minutes, 'h' for hours, 'd' for days, 'w' for weeks. Default is seconds.
 
 =item B<--warning-*> B<--critical-*>
 
