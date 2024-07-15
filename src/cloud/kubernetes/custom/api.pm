92a9c40a061c670ab19a1177b63d00cbed0a19e2
Lucie Dubrunfaut <ldubrunfaut@CNTR-PORT-A198>
2024-07-15 16:22:29 +0200

    change option name

--- src/cloud/kubernetes/custom/api.pm
+++ src/cloud/kubernetes/custom/api.pm
@@ -51,7 +51,7 @@ sub new {
             'limit:s'       => { name => 'limit' },
             'config-file:s' => { name => 'config_file' },
             'namespace:s'   => { name => 'namespace' },
-            'legacy:s'       => { name => 'legacy' }
+            'legacy-api-beta:s'       => { name => 'legacy_api_beta' }
         });
     }
     $options{options}->add_help(package => __PACKAGE__, sections => 'REST API OPTIONS', once => 1);
@@ -192,7 +192,7 @@ sub kubernetes_list_cronjobs {
     my ($self, %options) = @_;
 
     my $url_path=$self->{namespace} ne '' ? '/apis/batch/v1/namespaces/' . $self->{namespace} . '/cronjobs' : '/apis/batch/v1/cronjobs';
-    if (defined($self->{legacy})){
+    if (defined($self->{legacy_api_beta})){
         $url_path=$self->{namespace} ne '' ? '/apis/batch/v1beta1/namespaces/' . $self->{namespace} . '/cronjobs' : '/apis/batch/v1beta1/cronjobs';
     };
 
@@ -241,7 +241,7 @@ sub kubernetes_list_ingresses {
     my ($self, %options) = @_;
 
     my $url_path=$self->{namespace} ne '' ? '/apis/networking.k8s.io/v1/namespaces/' . $self->{namespace} . '/ingresses' : '/apis/networking.k8s.io/v1/ingresses';
-    if (defined($self->{legacy})){
+    if (defined($self->{legacy_api_beta})){
         $url_path=$self->{namespace} ne '' ? '/apis/extensions/v1beta1/namespaces/' . $self->{namespace} . '/ingresses' : '/apis/extensions/v1beta1/ingresses';
     };
 
@@ -382,7 +382,7 @@ See https://kubernetes.io/docs/reference/kubernetes-api/common-parameters/common
 
 Set namespace to get information.
 
-=item B<--legacy>
+=item B<--legacy-api-beta>
 
 If this option is set the legacy API path are set for this api calls:
 kubernetes_list_cronjobs will use this path: /apis/batch/v1beta1/namespaces/
