7335edadfd056033a6a7a608a2464c4e5ae7739d
omercier <omercier@centreon.com>
2024-09-30 23:19:17 +0200

    enh(copyright): updated/added missing copyrights

--- connectors/vmware/src/centreon/script/centreon_vmware.pm
+++ connectors/vmware/src/centreon/script/centreon_vmware.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# Copyright 2015 Centreon (http://www.centreon.com/)
+# Copyright 2024 Centreon (http://www.centreon.com/)
 #
 # Centreon is a full-fledged industry-strength solution that meets
 # the needs in IT infrastructure and application monitoring for
--- connectors/vmware/src/centreon/vmware/cmddiscovery.pm
+++ connectors/vmware/src/centreon/vmware/cmddiscovery.pm
@@ -1,5 +1,5 @@
 #
-# Copyright 2019 Centreon (http://www.centreon.com/)
+# Copyright 2024 Centreon (http://www.centreon.com/)
 #
 # Centreon is a full-fledged industry-strength solution that meets
 # the needs in IT infrastructure and application monitoring for
