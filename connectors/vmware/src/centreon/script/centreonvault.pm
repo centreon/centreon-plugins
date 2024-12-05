651095a4eaf03d311d62a1f318b1414ef5f9451f
omercier <32134301+omercier@users.noreply.github.com>
2024-12-05 14:23:12 +0100

    enh(passwordmgr-centreonvault): adapt vault module for plugins to the new specs (#5302)
    Refs: CTOR-1151

--- connectors/vmware/src/centreon/script/centreonvault.pm
+++ connectors/vmware/src/centreon/script/centreonvault.pm
@@ -335,7 +335,7 @@ Centreon Vault password manager
 
 =head1 SYNOPSIS
 
-Allows to retrieve secrets (usually username and password) from a Hashicorp vault compatible api given a config file as constructor.
+Allows to retrieve secrets (usually username and password) from a Hashicorp vault compatible API.
 
     use centreon::vmware::logger;
     use centreon::script::centreonvault;
@@ -364,8 +364,8 @@ The expected file format for Centreon Vault is:
 
     {
       "name": "hashicorp_vault",
-      "url": "vault-server.mydomain.com",
-      "salt": "<base64 encoded(<32 bytes long key used to hash the crypted data)>",
+      "url": "vault-server.my-domain.com",
+      "salt": "<base64 encoded(<32 bytes long key used to hash the encrypted data)>",
       "port": 443,
       "root_path": "vmware_daemon",
       "role_id": "<base64 encoded(<iv><hmac_hash><encrypted_role_id>)",
