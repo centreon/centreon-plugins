2c8a73640c4b6a11d249ddc03f440c22b3df10b3
Kevin Duret <kduret@centreon.com>
2024-12-09 14:01:32 +0100

    test(ci): test release 8

--- src/blockchain/hyperledger/exporter/plugin.pm
+++ src/blockchain/hyperledger/exporter/plugin.pm
@@ -27,7 +27,7 @@ use base qw(centreon::plugins::script_custom);
 sub new {
     my ($class, %options) = @_;
     my $self = $class->SUPER::new(package => __PACKAGE__, %options);
-    bless $self, $class;
+    bless $self, $class; 
 
     $self->{version} = '0.1';
     %{$self->{modes}} = (
