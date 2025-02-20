52a8084f19f575a7392185706fa762ae90354dcb
David Boucher <dboucher@centreon.com>
2025-02-20 13:33:52 +0100

    enh(experimental): thresholds taken into acount

--- experimental/src/main.rs
+++ experimental/src/main.rs
@@ -7,26 +7,26 @@ extern crate regex;
 extern crate serde;
 extern crate serde_json;
 
-mod lib;
 mod generic;
+mod lib;
 
 use clap::Parser;
+use generic::{Command, CommandExt};
 use lib::r_snmp_get;
 use serde_json::Result;
 use std::fs;
-use generic::{Command};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Cli {
     /// Hostname to operate on
-    #[arg(long, short='H')]
+    #[arg(long, short = 'H')]
     hostname: String,
 
     #[arg(long, short, default_value_t = 161)]
     port: u16,
 
-    #[arg(long, short='v')]
+    #[arg(long, short = 'v')]
     snmp_version: String,
 
     #[arg(long, short)]
@@ -34,15 +34,25 @@ struct Cli {
 
     #[arg(long, short)]
     json_conf: String,
+
+    #[arg(long, short)]
+    warning_core: Option<String>,
+
+    #[arg(long, short = 'C')]
+    critical_core: Option<String>,
+
+    #[arg(long, short = 'a')]
+    warning_agregation: Option<String>,
+
+    #[arg(long, short = 'b')]
+    critical_agregation: Option<String>,
 }
 
 fn json_to_command(file_name: &str) -> Result<Command> {
-
     // Transform content of the file into a string
-    let contents = match fs::read_to_string(file_name)
-     {
+    let contents = match fs::read_to_string(file_name) {
         Ok(ret) => ret,
-        Err(err) => panic!("Could not deserialize the file, error code: {}", err)
+        Err(err) => panic!("Could not deserialize the file, error code: {}", err),
     };
 
     let module: Result<Command> = serde_json::from_str(&contents.as_str());
@@ -54,7 +64,13 @@ fn main() {
     let url = format!("{}:{}", cli.hostname, cli.port);
     let cmd = json_to_command(&cli.json_conf);
     let cmd = cmd.unwrap();
-    let result = cmd.execute(&url);
+    let ext = CommandExt {
+        warning_core: cli.warning_core,
+        critical_core: cli.critical_core,
+        warning_agregation: cli.warning_agregation,
+        critical_agregation: cli.critical_agregation,
+    };
+    let result = cmd.execute(&url, &ext);
     println!("{}", result.output);
     std::process::exit(result.status);
 }
