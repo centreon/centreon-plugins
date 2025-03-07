c55f929666524256b7fc48ce2a8eab0791563f27
Lucie Dubrunfaut <ldubrunfaut@CNTR-PORT-A198>
2025-03-07 17:27:12 +0100

    fix(storage::ibm::fs900::snmp): remove wrong default value all

--- src/storage/ibm/fs900/snmp/mode/hardware.pm
+++ src/storage/ibm/fs900/snmp/mode/hardware.pm
@@ -79,7 +79,7 @@ Check hardware (batteries, fan modules, fibre channels, flashcards, power suppli
 
 =item B<--component>
 
-Which component to check (default: 'all').
+Which component to check (default: '.*').
 Can be: 'battery', 'fan', 'fibrechannel', 'flashcard', 'psu'.
 
 =item B<--filter>
