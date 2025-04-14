d11c4c3e89dc6a3b71de9e93997cd84972d8761b
David Boucher <dboucher@centreon.com>
2025-04-14 10:43:32 +0200

    wip(generic-snmp): new lexer

--- experimental/src/compute/mod.rs
+++ experimental/src/compute/mod.rs
@@ -1,4 +1,10 @@
+mod lexer;
+
+use lalrpop_util::lalrpop_mod;
 use serde::Deserialize;
+use snmp::SnmpResult;
+
+lalrpop_mod!(grammar);
 
 #[derive(Deserialize, Debug)]
 pub struct Metric {
@@ -14,4 +20,89 @@ pub struct Metric {
 pub struct Compute {
     pub metrics: Vec<Metric>,
     pub aggregations: Option<Vec<Metric>>,
+}
+
+pub struct Parser<'a> {
+    collect: &'a Vec<SnmpResult>,
+    parser: grammar::ExprParser,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(collect: &'a Vec<SnmpResult>) -> Parser<'a> {
+        Parser {
+            collect,
+            parser: grammar::ExprParser::new(),
+        }
+    }
+
+    pub fn eval(&self, expr: &str) -> f32 {
+        2.0_f32
+    }
+}
+
+mod Test {
+    use super::*;
+
+    #[test]
+    fn term() {
+        assert!(grammar::ExprParser::new().parse("132").is_ok());
+        assert!(grammar::ExprParser::new().parse("((132))").is_ok());
+        assert!(grammar::ExprParser::new().parse("((132)))").is_err());
+    }
+
+    #[test]
+    fn sum() {
+        let res = grammar::ExprParser::new().parse("1 + 2");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 3_f32);
+        let res = grammar::ExprParser::new().parse("1 + 2 + 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 6_f32);
+        let res = grammar::ExprParser::new().parse("1 - 2 + 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 2_f32);
+        let res = grammar::ExprParser::new().parse("1 + 2 - 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 0_f32);
+    }
+
+    #[test]
+    fn product() {
+        let res = grammar::ExprParser::new().parse("2 * 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 6_f32);
+
+        let res = grammar::ExprParser::new().parse("2 * 3 * 4");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 24_f32);
+
+        let res = grammar::ExprParser::new().parse("2 * 3 / 2");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 3_f32);
+
+        //        let res = grammar::ProductParser::new().parse("2 / 0");
+        //        assert!(res.is_err());
+    }
+
+    #[test]
+    fn sum_product() {
+        let res = grammar::ExprParser::new().parse("1 + 2 * 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 7_f32);
+
+        let res = grammar::ExprParser::new().parse("1 + (3 + 2 * 3) / 3");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 4_f32);
+    }
+
+    #[test]
+    fn function() {
+        let res = grammar::ExprParser::new().parse("Average(1, 2, 3)");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 2_f32);
+
+        let res = grammar::ExprParser::new().parse("Average(1 + 2 * 2, 3, 4)");
+        assert!(res.is_ok());
+        assert!(res.unwrap() == 4_f32);
+    }
 }
