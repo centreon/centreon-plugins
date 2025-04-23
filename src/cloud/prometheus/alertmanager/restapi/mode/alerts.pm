66e7efcdd7219155a6ef123bb0d887d0de839747
garnier-quentin <garnier.quentin@gmail.com>
2025-04-23 07:24:18 -0400

    add plugin prometheus alertmanager

---
+++ src/cloud/prometheus/alertmanager/restapi/mode/alerts.pm
@@ -0,0 +1,195 @@
+#
+# Copyright 2025 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package cloud::prometheus::alertmanager::restapi::mode::alerts;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub prefix_output_active {
+    my ($self, %options) = @_;
+
+    return 'Alerts active ';
+}
+
+sub prefix_output_other {
+    my ($self, %options) = @_;
+
+    return 'Alerts ';
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'active', type => 0, cb_prefix_output => 'prefix_output_active' },
+        { name => 'other', type => 0, cb_prefix_output => 'prefix_output_other' }
+    ];
+
+    $self->{maps_counters}->{active} = [
+        { label => 'active', nlabel => 'alerts.active.count', set => {
+                key_values => [ { name => 'active' }, { name => 'total' } ],
+                output_template => 'detected: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        },
+        { label => 'active-warning', nlabel => 'alerts.active.warning.count', set => {
+                key_values => [ { name => 'warning' }, { name => 'total' } ],
+                output_template => 'warning: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        },
+        { label => 'active-critical', nlabel => 'alerts.active.critical.count', set => {
+                key_values => [ { name => 'critical' }, { name => 'total' } ],
+                output_template => 'critical: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        },
+        { label => 'active-info', nlabel => 'alerts.active.info.count', set => {
+                key_values => [ { name => 'info' }, { name => 'total' } ],
+                output_template => 'info: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        }
+    ];
+
+    $self->{maps_counters}->{other} = [
+        { label => 'total', nlabel => 'alerts.total.count', display_ok => 0, set => {
+                key_values => [ { name => 'total' } ],
+                output_template => 'total: %d',
+                perfdatas => [
+                    { template => '%d', min => 0 }
+                ]
+            }
+        },
+        { label => 'unprocessed', nlabel => 'alerts.unprocessed.count', display_ok => 0, set => {
+                key_values => [ { name => 'unprocessed' }, { name => 'total' } ],
+                output_template => 'unprocessed: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        },
+        { label => 'suppressed', nlabel => 'alerts.suppressed.count', display_ok => 0, set => {
+                key_values => [ { name => 'suppressed' }, { name => 'total' } ],
+                output_template => 'suppressed: %d',
+                perfdatas => [
+                    { template => '%d', min => 0, max => 'total' }
+                ]
+            }
+        }
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'filter-name:s'     => { name => 'filter_name' },
+        'filter-severity:s' => { name => 'filter_severity' },
+        'display-alerts'    => { name => 'display_alerts' }
+    });
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $results = $options{custom}->get_endpoint(
+        endpoint => '/alerts'
+    );
+
+    $self->{active} = { total => 0, active => 0, warning => 0, critical => 0, info => 0 };
+    $self->{other} = { total => 0, unprocessed => 0, suppressed => 0 };
+    foreach my $alert (@$results) {
+        next if (defined($self->{option_results}->{filter_name}) && $self->{option_results}->{filter_name} ne '' 
+            && $alert->{labels}->{alertname} !~ /$self->{option_results}->{filter_name}/);
+        next if (defined($self->{option_results}->{filter_severity}) && $self->{option_results}->{filter_severity} ne '' 
+            && $alert->{labels}->{severity} !~ /$self->{option_results}->{filter_severity}/);
+
+        if ($alert->{status}->{state} eq 'active') {
+            $self->{active}->{active}++;
+            if ($alert->{labels}->{severity} =~ /warning|critical|info/) {
+                $self->{active}->{ $alert->{labels}->{severity} }++;
+            }
+        } elsif ($alert->{status}->{state} =~ /unprocessed|suppressed/) {
+            $self->{other}->{ $alert->{status}->{state} }++;
+        }
+
+        $self->{other}->{other}++;
+        $self->{active}->{total}++;
+        if (defined($self->{option_results}->{display_alerts})) {
+            $self->{output}->output_add(
+                long_msg => sprintf(
+                    'alert [start: %s] [state: %s] [severity: %s]: %s',
+                    $alert->{startsAt},
+                    $alert->{status}->{state},
+                    $alert->{labels}->{severity},
+                    $alert->{labels}->{alertname}
+                )
+            );
+        }
+    }
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check alerts.
+
+=over 8
+
+=item B<--filter-name>
+
+Filter alerts by name (can use regexp).
+
+=item B<--filter-severity>
+
+Filter alerts by severity (can use regexp).
+
+=item B<--display-alerts>
+
+Display alerts in verbose output.
+
+=item B<--warning-*> B<--critical-*> 
+
+Thresholds.
+Can be: 'active', 'active-warning', 'active-critical', 'active-info',
+'total', 'unprocessed', 'suppressed'.
+
+=back
+
+=cut
