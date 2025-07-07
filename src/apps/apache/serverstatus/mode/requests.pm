df1d9808aeb25fd6db90663d1d6d5329d60fd788
Sylvain Cresto <scresto@centreon.com>
2025-07-07 09:56:18 +0200

    Revert "CTOR-1200: Fix previous commit"
    This reverts commit fa72882051faa4bbabda7da335756d093efe133e.

---
+++ src/apps/apache/serverstatus/mode/requests.pm
@@ -0,0 +1,319 @@
+#
+# Copyright 2024 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::apache::serverstatus::mode::requests;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+use centreon::plugins::statefile;
+use centreon::plugins::misc;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, statefile => 1, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        'hostname:s'        => { name => 'hostname' },
+        'port:s'            => { name => 'port', },
+        'proto:s'           => { name => 'proto' },
+        'urlpath:s'         => { name => 'url_path', default => "/server-status/?auto" },
+        'credentials'       => { name => 'credentials' },
+        'basic'             => { name => 'basic' },
+        'username:s'        => { name => 'username' },
+        'password:s'        => { name => 'password' },
+        'header:s@'         => { name => 'header' },
+        'warning:s'         => { name => 'warning', redirect => 'warning-apache-request-average-persecond' },
+        'critical:s'        => { name => 'critical', redirect => 'critical-apache-request-average-persecond' },
+        'warning-bytes:s'   => { name => 'warning_bytes', redirect => 'warning-apache-bytes-persecond' },
+        'critical-bytes:s'  => { name => 'critical_bytes', redirect => 'critical-apache-bytes-persecond' },
+        'warning-access:s'  => { name => 'warning_access', redirect => 'warning-apache-access-persecond' },
+        'critical-access:s' => { name => 'critical_access', redirect => 'critical-apache-access-persecond' },
+        'timeout:s'         => { name => 'timeout' },
+    });
+    $self->{http} = centreon::plugins::http->new(%options);
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->SUPER::check_options(%options);
+
+    if (($self->{perfdata}->threshold_validate(label => 'warning', value => $self->{option_results}->{warning})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning threshold '" . $self->{option_results}->{warning} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical', value => $self->{option_results}->{critical})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical threshold '" . $self->{option_results}->{critical} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'warning-bytes', value => $self->{option_results}->{warning_bytes})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning-bytes threshold '" . $self->{option_results}->{warning_bytes} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical-bytes', value => $self->{option_results}->{critical_bytes})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical-bytes threshold '" . $self->{option_results}->{critical_bytes} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'warning-access', value => $self->{option_results}->{warning_access})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong warning-access threshold '" . $self->{option_results}->{warning_access} . "'.");
+        $self->{output}->option_exit();
+    }
+    if (($self->{perfdata}->threshold_validate(label => 'critical-access', value => $self->{option_results}->{critical_access})) == 0) {
+        $self->{output}->add_option_msg(short_msg => "Wrong critical-access threshold '" . $self->{option_results}->{critical_access} . "'.");
+        $self->{output}->option_exit();
+    }
+
+    $self->{http}->set_options(%{$self->{option_results}});
+}
+
+sub custom_bytes_persecond_calc {
+    my ($self, %options) = @_;
+
+    unless (defined $options{old_datas}->{global_total_bytes}) {
+        $self->{error_msg} = "Buffer creation";
+        return -1;
+    }
+
+    my $delta_time = $options{delta_time} || 1;
+
+    my $old_total_bytes = $options{old_datas}->{global_total_bytes} || 0;
+    $old_total_bytes = 0 if $old_total_bytes > $options{new_datas}->{global_total_bytes};
+
+    $self->{result_values}->{bPerSec} = ($options{new_datas}->{global_total_bytes} - $old_total_bytes) / $delta_time;
+
+    return 0;
+}
+
+sub custom_access_persecond_calc {
+    my ($self, %options) = @_;
+
+    unless (defined $options{old_datas}->{global_total_access}) {
+        $self->{error_msg} = "Buffer creation";
+        return -1;
+    }
+    my $delta_time = $options{delta_time} || 1;
+
+    my $old_total_access = $options{old_datas}->{global_total_access} || 0;
+    $old_total_access = 0 if $old_total_access > $options{new_datas}->{global_total_access};
+
+    $self->{result_values}->{aPerSec} = ($options{new_datas}->{global_total_access} - $old_total_access) / $delta_time;
+
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, cb_prefix_output => 'prefix_global_output', askipped_code => { -2 => 1 } }
+    ];
+
+    $self->{maps_counters}->{global} =  [
+        {   label => 'bytesPerSec', nlabel => 'apache.bytes.persecond',
+            set => {
+                key_values => [ { name => 'bPerSec' }, { name => 'total_bytes' } ],
+                output_template => 'BytesPerSec: %.2f %s',
+                output_change_bytes => 1,
+                closure_custom_calc => $self->can('custom_bytes_persecond_calc'),
+                perfdatas => [ { template => '%.2f', min => 0, unit => 'B' } ]
+           }
+        },
+        {   label => 'accessPerSec', nlabel => 'apache.access.persecond',
+            set => {
+                key_values => [ { name => 'aPerSec' }, { name => 'total_access' } ],
+                closure_custom_calc => $self->can('custom_access_persecond_calc'),
+                output_template => 'AccessPerSec: %.2f',
+                perfdatas => [ { template => '%.2f', min => 0, } ]
+            }
+        },
+        { label => 'avg_RequestPerSec', nlabel => 'apache.request.average.persecond',
+            set => {
+                key_values => [ { name => 'rPerSec' } ],
+                output_template => 'RequestPerSec: %.2f',
+                perfdatas => [ { template => '%.2f', min => 0, } ]
+            }
+        },
+        { label => 'avg_bytesPerRequest', nlabel => 'apache.bytes.average.perrequest',
+            set => {
+                key_values => [ { name => 'bPerReq' } ],
+                output_change_bytes => 1,
+                output_template => 'BytesPerRequest: %.2f %s',
+                perfdatas => [ { template => '%.2f', min => 0, unit => 'B', } ]
+            }
+        },
+        { label => 'avg_bytesPerSec', nlabel => 'apache.bytes.average.persecond',
+           display_ok => 0,
+           set => {
+               key_values => [ { name => 'avg_bPerSec' } ],
+               perfdatas => [ { min => 0, unit => 'B' } ]
+           }
+        },
+    ];
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my ($webcontent) = $self->{http}->request();
+
+    #Total accesses: 7323 - Total Traffic: 243.7 MB - Total Duration: 7175675
+    #CPU Usage: u1489.98 s1118.39 cu0 cs0 - .568% CPU load
+    #.0159 requests/sec - 555 B/second - 34.1 kB/request - 979.882 ms/request
+    my ($rPerSec, $bPerReq, $total_access, $total_bytes, $avg_bPerSec);
+
+    $total_access = $1 if ($webcontent =~ /^Total Accesses:\s+([^\s]+)/mi);
+
+    $total_bytes = $1 * 1024 if ($webcontent =~ /^Total kBytes:\s+([^\s]+)/mi);
+    if ($webcontent =~ /Total\s+Traffic:\s+(\S+)\s+(.|)B\s+/mi) {
+        $total_bytes = centreon::plugins::misc::convert_bytes(value => $1, unit => $2 . 'B');
+    }
+    $rPerSec = $1 if ($webcontent =~ /^ReqPerSec:\s+([^\s]+)/mi);
+    if ($webcontent =~ /^(\S+)\s+requests\/sec/mi) {
+        $rPerSec = $1;
+        $rPerSec = '0' . $rPerSec if ($rPerSec =~ /^\./);
+    }
+
+    # Need a little time to init
+    $bPerReq = $1 if ($webcontent =~ /^BytesPerReq:\s+([^\s]+)/mi);
+    if ($webcontent =~ /(\S+)\s+(.|)B\/request/mi) {
+        $bPerReq = centreon::plugins::misc::convert_bytes(value => $1, unit => $2 . 'B');
+    }
+
+    $avg_bPerSec = $1 if ($webcontent =~ /^BytesPerSec:\s+([^\s]+)/mi);
+    if ($webcontent =~ /(\S+)\s+(.|)B\/second/mi) {
+        $avg_bPerSec = centreon::plugins::misc::convert_bytes(value => $1, unit => $2 . 'B');
+    }
+
+    if (!defined($avg_bPerSec)) {
+        $self->{output}->add_option_msg(short_msg => "Apache 'ExtendedStatus' option is off.");
+        $self->{output}->option_exit();
+    }
+
+    $rPerSec = '0' . $rPerSec if ($rPerSec =~ /^\./);
+    $avg_bPerSec = '0' . $avg_bPerSec if ($avg_bPerSec =~ /^\./);
+    $bPerReq = '0' . $bPerReq if ($bPerReq =~ /^\./);
+
+    $self->{global} = {
+        rPerSec => $rPerSec,
+        bPerReq => $bPerReq,
+        avg_bPerSec => $avg_bPerSec,
+        total_bytes => $total_bytes,
+        total_access => $total_access,
+        bPerSec => 0, # Will be calculated in custom_bytes_persecond_calc
+        aPerSec => 0, # Will be calculated in custom_access_persecond_calc
+
+    };
+
+    $self->{cache_name} = 'apache_' . $self->{option_results}->{hostname}  . '_' . $self->{http}->get_port() . '_' . $self->{mode};
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check Apache WebServer Request statistics
+
+=over 8
+
+=item B<--hostname>
+
+IP Addr/FQDN of the web server host
+
+=item B<--port>
+
+Port used by Apache
+
+=item B<--proto>
+
+Specify https if needed
+
+=item B<--urlpath>
+
+Set path to get server-status page in auto mode (default: '/server-status/?auto')
+
+=item B<--credentials>
+
+Specify this option if you access server-status page with authentication
+
+=item B<--username>
+
+Specify the username for authentication (mandatory if --credentials is specified)
+
+=item B<--password>
+
+Specify the password for authentication (mandatory if --credentials is specified)
+
+=item B<--basic>
+
+Specify this option if you access server-status page over basic authentication and don't want a '401 UNAUTHORIZED' error to be logged on your web server.
+
+Specify this option if you access server-status page over hidden basic authentication or you'll get a '404 NOT FOUND' error.
+
+(use with --credentials)
+
+=item B<--timeout>
+
+Threshold for HTTP timeout
+
+=item B<--header>
+
+Set HTTP headers (multiple option)
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Can be : bytesPerSec, accessPerSec, avg_RequestPerSec, avg_bytesPerRequest, avg_bytesPerSec
+Example : --filter-counters='^accessPerSec$'
+
+=item B<--warning>
+
+Warning Threshold for Request per seconds
+
+=item B<--critical>
+
+Critical Threshold for Request per seconds
+
+=item B<--warning-bytes>
+
+Warning Threshold for Bytes per seconds
+
+=item B<--critical-bytes>
+
+Critical Threshold for Bytes per seconds
+
+=item B<--warning-access>
+
+Warning Threshold for Access per seconds
+
+=item B<--critical-access>
+
+Critical Threshold for Access per seconds
+
+=back
+
+=cut
---
+++ src/apps/apache/serverstatus/mode/slotstates.pm
@@ -0,0 +1,336 @@
+#
+# Copyright 2024 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::apache::serverstatus::mode::slotstates;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+
+sub custom_value_threshold {
+    my ($self, %options) = @_;
+    
+    my $exit = 'ok';
+    if ($self->{instance_mode}->{option_results}->{units} eq '%') {
+        $exit = $self->{perfdata}->threshold_check(value => $self->{result_values}->{prct}, threshold => [ { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' }, { label => 'warning-' . $self->{thlabel}, exit_litteral => 'warning' } ]);
+    } else {
+        $exit = $self->{perfdata}->threshold_check(value => $self->{result_values}->{used}, threshold => [ { label => 'critical-' . $self->{thlabel}, exit_litteral => 'critical' }, { label => 'warning-' . $self->{thlabel}, exit_litteral => 'warning' } ]);
+    }
+    return $exit;
+}
+
+sub custom_value_perfdata {
+    my ($self, %options) = @_;
+    
+    my ($warning, $critical);
+    if ($self->{instance_mode}->{option_results}->{units} eq '%') {
+        $warning = $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{thlabel}, total => $self->{result_values}->{total}, cast_int => 1);
+        $critical = $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{thlabel}, total => $self->{result_values}->{total}, cast_int => 1);
+    } else {
+        $warning = $self->{perfdata}->get_perfdata_for_output(label => 'warning-' . $self->{thlabel});
+        $critical = $self->{perfdata}->get_perfdata_for_output(label => 'critical-' . $self->{thlabel});
+    }
+    
+    $self->{output}->perfdata_add(
+        label => $self->{result_values}->{label},
+        nlabel => $self->{nlabel},
+        value => $self->{result_values}->{used},
+        warning => $warning,
+        critical => $critical,
+        min => 0, max => $self->{result_values}->{total}
+    );
+}
+
+sub custom_value_output {
+    my ($self, %options) = @_;
+    
+    my $label = $self->{result_values}->{label};
+    $label =~ s/_/ /g;
+    return sprintf(
+        "%s: %s (%.2f %%)",
+        $label,
+        $self->{result_values}->{used},
+        $self->{result_values}->{prct}
+    );
+}
+
+sub custom_value_calc {
+    my ($self, %options) = @_;
+    
+    $self->{result_values}->{total} = $options{new_datas}->{$self->{instance} . '_total'};
+    if ($self->{result_values}->{total} == 0) {
+        $self->{error_msg} = "skipped";
+        return -2;
+    }
+    $self->{result_values}->{used} = $options{new_datas}->{$self->{instance} . '_' . $options{extra_options}->{label_ref}};    
+    $self->{result_values}->{prct} = $self->{result_values}->{used} * 100 / $self->{result_values}->{total};
+    $self->{result_values}->{label} = $options{extra_options}->{label_ref};
+
+    return 0;
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, cb_prefix_output => 'prefix_global_output', skipped_code => { -2 => 1 } }
+    ];
+    
+    $self->{maps_counters}->{global} = [
+        { label => 'busy', nlabel => 'apache.slot.busy.count', set => {
+                key_values => [ { name => 'busy' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'busy' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'free', nlabel => 'apache.slot.free.count', set => {
+                key_values => [ { name => 'free' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'free' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'waiting', nlabel => 'apache.slot.waiting.count', set => {
+                key_values => [ { name => 'waiting' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'waiting' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'starting', nlabel => 'apache.slot.starting.count', set => {
+                key_values => [ { name => 'starting' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'starting' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'reading', nlabel => 'apache.slot.reading.count', set => {
+                key_values => [ { name => 'reading' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'reading' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'sending', nlabel => 'apache.slot.sending.count', set => {
+                key_values => [ { name => 'sending' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'sending' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'keepalive', nlabel => 'apache.slot.keepalive.count', set => {
+                key_values => [ { name => 'keepalive' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'keepalive' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'dns-lookup', nlabel => 'apache.slot.dnslookup.count', set => {
+                key_values => [ { name => 'dns_lookup' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'dns_lookup' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'closing', nlabel => 'apache.slot.closing.count', set => {
+                key_values => [ { name => 'closing' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'closing' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'logging', nlabel => 'apache.slot.logging.count', set => {
+                key_values => [ { name => 'logging' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'logging' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+        { label => 'gracefully-finishing', nlabel => 'apache.slot.gracefullyfinishing.count', set => {
+                key_values => [ { name => 'gracefully_finishing' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'gracefully_finishing' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+         { label => 'idle-cleanup-worker', nlabel => 'apache.slot.idlecleanupworker.count', set => {
+                key_values => [ { name => 'idle_cleanup_worker' }, { name => 'total' } ],
+                closure_custom_calc => $self->can('custom_value_calc'), closure_custom_calc_extra_options => { label_ref => 'idle_cleanup_worker' },
+                closure_custom_output => $self->can('custom_value_output'),
+                closure_custom_threshold_check => $self->can('custom_value_threshold'),
+                closure_custom_perfdata => $self->can('custom_value_perfdata'),
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {
+        'hostname:s'  => { name => 'hostname' },
+        'port:s'      => { name => 'port', },
+        'proto:s'     => { name => 'proto' },
+        'urlpath:s'   => { name => 'url_path', default => "/server-status/?auto" },
+        'credentials' => { name => 'credentials' },
+        'basic'       => { name => 'basic' },
+        'username:s'  => { name => 'username' },
+        'password:s'  => { name => 'password' },
+        'header:s@'   => { name => 'header' },
+        'timeout:s'   => { name => 'timeout' },
+        'units:s'     => { name => 'units', default => '%' },
+        # To keep compatibility with old thresholds
+        'warning-gracefuly-finished:s' => { name => 'warning_gracefuly_finished', redirect => 'warning-gracefully-finishing' },
+        'warning-apache-slot-gracefulyfinished-count:s' => { name => 'warning_apache_smpt_gracefulyfinished_count', redirect => 'warning-apache-slot-gracefullyfinishing-count' },
+        'critical-gracefuly-finished:s' => { name => 'critical_gracefuly_finished', redirect => 'critical-gracefully-finishing' },
+        'critical-apache-slot-gracefulyfinished-count:s' => { name => 'critical_apache_smpt_gracefulyfinished_count', redirect => 'critical-apache-slot-gracefullyfinishing-count' },
+    });
+    
+    $self->{http} = centreon::plugins::http->new(%options);
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::check_options(%options);
+    
+    $self->{http}->set_options(%{$self->{option_results}});
+}
+
+sub prefix_global_output {
+    my ($self, %options) = @_;
+    
+    return 'Slots ';
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my ($webcontent) = $self->{http}->request();
+    my $ScoreBoard = "";
+    if ($webcontent =~ /^Scoreboard:\s+([^\s]+)/mi) {
+        $ScoreBoard = $1;
+    }
+
+    $self->{global} = { 
+        total => length($ScoreBoard),
+        free => ($ScoreBoard =~ tr/\.//) + ($ScoreBoard =~ tr/\_//),
+        busy => length($ScoreBoard) - ($ScoreBoard =~ tr/\.//) - ($ScoreBoard =~ tr/\_//),
+        waiting => ($ScoreBoard =~ tr/\_//), starting => ($ScoreBoard =~ tr/S//),
+        reading => ($ScoreBoard =~ tr/R//), sending => ($ScoreBoard =~ tr/W//),
+        keepalive => ($ScoreBoard =~ tr/K//), dns_lookup => ($ScoreBoard =~ tr/D//),
+        closing => ($ScoreBoard =~ tr/C//), logging => ($ScoreBoard =~ tr/L//),
+        gracefully_finishing => ($ScoreBoard =~ tr/G//), idle_cleanup_worker => ($ScoreBoard =~ tr/I//)
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check Apache WebServer Slots informations
+
+=over 8
+
+=item B<--hostname>
+
+IP Address or FQDN of the web server host
+
+=item B<--port>
+
+Port used by Apache
+
+=item B<--proto>
+
+Protocol used http or https
+
+=item B<--urlpath>
+
+Set path to get server-status page in auto mode (default: '/server-status/?auto')
+
+=item B<--credentials>
+
+Specify this option if you access server-status page with authentication
+
+=item B<--username>
+
+Specify the username for authentication (mandatory if --credentials is specified)
+
+=item B<--password>
+
+Specify the password for authentication (mandatory if --credentials is specified)
+
+=item B<--basic>
+
+Specify this option if you access server-status page over basic authentication and don't want a '401 UNAUTHORIZED' error to be logged on your web server.
+
+Specify this option if you access server-status page over hidden basic authentication or you'll get a '404 NOT FOUND' error.
+
+(use with --credentials)
+
+=item B<--timeout>
+
+Threshold for HTTP timeout
+
+=item B<--header>
+
+Set HTTP headers (multiple option)
+
+=item B<--units>
+
+Threshold unit (default: '%'. Can be: '%' or 'absolute')
+
+=item B<--warning-*>
+
+Warning threshold.
+Can be: 'busy', 'free', 'waiting', 'starting', 'reading',
+'sending', 'keepalive', 'dns-lookup', 'closing',
+'logging', 'gracefuly-finished', 'idle-cleanup-worker'.
+
+=item B<--critical-*>
+
+Critical threshold.
+Can be: 'busy', 'free', 'waiting', 'starting', 'reading',
+'sending', 'keepalive', 'dns-lookup', 'closing',
+'logging', 'gracefuly-finished', 'idle-cleanup-worker'.
+
+=over 8)
+
+=back
+
+=cut
