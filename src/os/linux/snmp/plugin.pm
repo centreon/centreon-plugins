b7eaf99110d1c6159435aa93fb685432e24cf9c4
sfarouq-ext <116093375+sfarouq-ext@users.noreply.github.com>
2025-07-10 10:31:12 +0200

    WIP3

--- src/os/linux/snmp/plugin.pm
+++ src/os/linux/snmp/plugin.pm
@@ -16,9 +16,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-##
 #
-
+#
 package os::linux::snmp::plugin;
 
 use strict;
