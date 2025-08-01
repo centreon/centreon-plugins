79c5b6cee4e3b82826d39034b5b440ba675734d7
rmorandell_pgum <roman.morandell@pgum.eu>
2025-08-01 18:02:09 +0200

    initial commit

---
+++ src/network/westermo/standard/snmp/mode/cpu.pm
@@ -0,0 +1,139 @@
+#
+# Copyright 2024 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::westermo::standard::snmp::mode::cpu;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, cb_prefix_output => 'prefix_load_output' }
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'cpu-load-1min', set => {
+            key_values      => [ { name => 'cpu_load1' } ],
+            output_template => '%s',
+            perfdatas       => [
+                { label => 'cpu_load1', value => 'cpu_load1', template => '%s', min => 0 },
+            ],
+        }
+        },
+        { label => 'cpu-load-5min', set => {
+            key_values      => [ { name => 'cpu_load5' } ],
+            output_template => '%s',
+            perfdatas       => [
+                { label => 'cpu_load5', value => 'cpu_load5', template => '%s', min => 0 },
+            ],
+        }
+        },
+        { label => 'cpu-load-15min', set => {
+            key_values      => [ { name => 'cpu_load15' } ],
+            output_template => '%s',
+            perfdatas       => [
+                { label => 'cpu_load15', value => 'cpu_load15', template => '%s', min => 0 },
+            ],
+        }
+        },
+    ];
+}
+
+sub prefix_load_output {
+    my ($self, %options) = @_;
+
+    return "Load average: ";
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+    });
+
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $oid_cpuLoadAvg1Min = '.1.3.6.1.4.1.16177.2.1.5.3.2.1.0';
+    my $oid_cpuLoadAvg5Min = '.1.3.6.1.4.1.16177.2.1.5.3.2.2.0';
+    my $oid_cpuLoadAvg15Min = '.1.3.6.1.4.1.16177.2.1.5.3.2.3.0';
+    my $snmp_result = $options{snmp}->get_leef(
+        oids         => [ $oid_cpuLoadAvg1Min, $oid_cpuLoadAvg5Min, $oid_cpuLoadAvg15Min ],
+        nothing_quit => 1
+    );
+
+    $self->{global} = {
+        cpu_load1  => $snmp_result->{$oid_cpuLoadAvg1Min},
+        cpu_load5  => $snmp_result->{$oid_cpuLoadAvg5Min},
+        cpu_load15 => $snmp_result->{$oid_cpuLoadAvg15Min},
+    };
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Check average cpu load.
+
+=over 8
+
+=item B<--filter-counters>
+
+Only display some counters (regexp can be used).
+Example: C<--filter-counters='cpu-load-15min'>
+
+=item B<--warning-cpu-load-1min>
+
+Thresholds.
+
+=item B<--critical-cpu-load-1min>
+
+Thresholds.
+
+=item B<--warning-cpu-load-5min>
+
+Thresholds.
+
+=item B<--critical-cpu-load-5min>
+
+Thresholds.
+
+=item B<--warning-cpu-load-15min>
+
+Thresholds.
+
+=item B<--critical-cpu-load-15min>
+
+Thresholds.
+
+=back
+
+=cut
