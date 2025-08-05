39ca43ae2f3b71e42ca31a921331c62dca3c002c
omercier <32134301+omercier@users.noreply.github.com>
2025-08-05 16:22:56 +0200

    fix(packaging): reorganize common code for azure  (#5679)
    because ApplicationInsights fatpacked plugin (and others) lacked a module
    
    Refs: CTOR-1193

--- src/cloud/azure/common/discoverytenant.pm
+++ src/cloud/azure/common/discoverytenant.pm
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-package cloud::azure::management::monitor::mode::discoverytenant;
+package cloud::azure::common::discoverytenant;
 
 use base qw(centreon::plugins::mode);
 
