38cfdc07b3ceb20918a8315dd8b63103b98b2747
itoussies@centreon.com <itoussies@centreon.com>
2025-08-20 16:50:00 +0200

    enh(meraki): enhance cache generation
    Add new info to the cache file
    to limit API requests
    
    Refs:CTOR-1704

--- src/network/cisco/meraki/cloudcontroller/restapi/custom/api.pm
+++ src/network/cisco/meraki/cloudcontroller/restapi/custom/api.pm
@@ -296,8 +296,11 @@ sub call_datas {
 
         if (defined($options{addDeviceConnection}) && $options{addDeviceConnection} == 1) {
             $self->{datas}->{devices_connection_stats} = {};
-            foreach my $network_id (keys %{$self->{datas}->{networks}}) {
-                $self->get_network_device_connection_stats(network_id => $network_id);
+            foreach my $serial (keys %{$self->{datas}->{devices}}) {
+                $self->get_network_device_connection_stats(
+                    serial     => $serial,
+                    network_id => $self->{datas}->{devices}->{$serial}->{network_id}
+                );
                 $self->{datas}->{devices_connection_stats}->{$network_id} = $self->{devices_connection_stats}->{$network_id};
             }
         }
