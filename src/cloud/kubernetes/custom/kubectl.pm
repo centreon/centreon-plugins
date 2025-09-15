baf594daad700e775adec6c348b299e7533a6d5a
Sophie Depassio <sdepassio@centreon.com>
2025-09-15 12:37:46 +0200

    fix(cloud::kubernetes): fix kubectl command with extra options.

--- src/cloud/kubernetes/custom/kubectl.pm
+++ src/cloud/kubernetes/custom/kubectl.pm
@@ -120,7 +120,7 @@ sub execute {
     $cmd_options .= " --v='9'" if ($self->{output}->is_debug());
 
     if (defined($self->{option_results}->{command_options}) && $self->{option_results}->{command_options} ne '') {
-        $cmd_options = $self->{option_results}->{command_options};
+        $cmd_options .= " " . $self->{option_results}->{command_options};
     }
 
     $self->{output}->output_add(long_msg => "Command line: '" . $command . " " . $cmd_options . "'", debug => 1);
