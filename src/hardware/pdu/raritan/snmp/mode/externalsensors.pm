45fdf16eaa8a106958b3824297ee7af89eac79c6
lucie-tirand <ldubrunfaut@CNTR-PORT-A198.localdomain>
2025-10-02 16:57:12 +0200

    fix 30 hardware pods and split snmp_standard with components files

--- src/hardware/pdu/raritan/snmp/mode/externalsensors.pm
+++ src/hardware/pdu/raritan/snmp/mode/externalsensors.pm
@@ -121,6 +121,11 @@ Which component to check (default: '.*').
 Exclude the items given as a comma-separated list (example: --filter=airPressure --filter=rmsVoltage).
 You can also exclude items from specific instances: --filter=rmsVoltage,I1
 
+=item B<--absent-problem>
+
+Return an error if a component is not 'present' (default is skipping).
+It can be set globally or for a specific instance: --absent-problem='component_name' or --absent-problem='component_name,instance_value'.
+
 =item B<--no-component>
 
 Define the expected status if no components are found (default: critical).
@@ -139,6 +144,14 @@ Example: --warning='powerQuality,.*,30'
 
 Set critical threshold for temperatures (syntax: type,instance,threshold)
 Example: --critical='powerQuality,.*,40'
+
+=item B<--warning-count-*>
+
+Define the warning threshold for the number of components of one type (replace '*' with the component type).
+
+=item B<--critical-count-*>
+
+Define the critical threshold for the number of components of one type (replace '*' with the component type).
 
 =back
 
