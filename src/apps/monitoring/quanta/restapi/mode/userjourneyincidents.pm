9d1d9be991ae99f24ebd3b4a4bf8f4106bcc0400
lucie-tirand <ldubrunfaut@CNTR-PORT-A198.localdomain>
2025-10-27 18:00:23 +0100

    fix typo

--- src/apps/monitoring/quanta/restapi/mode/userjourneyincidents.pm
+++ src/apps/monitoring/quanta/restapi/mode/userjourneyincidents.pm
@@ -165,7 +165,6 @@ sub manage_selection {
 
     $self->{output}->option_exit(short_msg => "Couldn't get incidents for user journey id ".$self->{journey_id}." and site id ".$self->{site_id})
         if (scalar(keys %{$self->{incidents}}) <= 0);
-    }
 }
 
 1;
