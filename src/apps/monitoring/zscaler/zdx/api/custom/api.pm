d63642d50964d35e8a26dcb92e80a245ec01b3a1
omercier <32134301+omercier@users.noreply.github.com>
2026-01-06 14:30:19 +0100

    feat(apps::monitoring::zscaler::zdx::api): mode discovery (#5898)
    Refs:CTOR-2054

---
+++ src/apps/monitoring/zscaler/zdx/api/custom/api.pm
@@ -0,0 +1,225 @@
+#
+# Copyright 2025-Present Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::zscaler::zdx::api::custom::api;
+
+use strict;
+use warnings;
+use centreon::plugins::http;
+use centreon::plugins::statefile;
+use DateTime;
+use JSON::XS;
+use Digest::MD5 qw(md5_hex);
+use centreon::plugins::misc qw(json_decode is_empty value_of is_excluded);
+
+sub new {
+    my ($class, %options) = @_;
+    my $self  = {};
+    bless $self, $class;
+
+    if (!defined($options{output})) {
+        print "Class Custom: Need to specify 'output' argument.\n";
+        exit 3;
+    }
+    if (!defined($options{options})) {
+        $options{output}->add_option_msg(short_msg => "Class Custom: Need to specify 'options' argument.");
+        $options{output}->option_exit();
+    }
+    
+    if (!defined($options{noptions})) {
+        $options{options}->add_options(arguments => {
+            'key-id:s'     => { name => 'key_id', default => '' },
+            'key-secret:s' => { name => 'key_secret', default => '' },
+            'api-path:s'   => { name => 'api_path', default => '/v1' },
+            'hostname:s'   => { name => 'hostname', default => 'api.zdxcloud.net' },
+            'port:s'       => { name => 'port', default => 443 },
+            'proto:s'      => { name => 'proto', default => 'https' },
+            'timeout:s'    => { name => 'timeout', default => 10 }
+        });
+    }
+    $options{options}->add_help(package => __PACKAGE__, sections => 'REST API OPTIONS', once => 1);
+
+    $self->{output} = $options{output};
+    $self->{http} = centreon::plugins::http->new(%options, default_backend => 'curl');
+    $self->{cache} = centreon::plugins::statefile->new(%options);
+
+    return $self;
+}
+
+sub get_token {
+    my ($self, %options) = @_;
+
+    return $self->{token} if defined($self->{token});
+
+    $self->settings();
+
+    my $has_cache_file = $self->{cache}->read(
+            statefile => 'zdx_token_' . md5_hex(
+                    $self->{hostname}
+                    . ':' . $self->{port}
+                    . '_' . $self->{key_id})
+    );
+    # return token stored in cache if exists after checking it is still valid
+    $self->{token} = $self->{cache}->get(name => 'token') if $has_cache_file;
+    my $expiration = $self->{cache}->get(name => 'expiration') if defined($self->{token});
+
+    return $self->{token} if (defined($expiration) && $expiration > time() + 60);
+
+    # if we do not have a token or if it has to be renewed
+    my $content = $self->{http}->request(
+        method          => 'POST',
+        url_path        => '/v1/oauth/token',
+        query_form_post => '{ "key_id": "' . $self->{key_id} . '", "key_secret": "' . $self->{key_secret} . '"}',
+
+    );
+    my $decoded_content = json_decode($content, output => $self->{output});
+
+    $self->{output}->option_exit(short_msg => "No token found in '$content'") unless ($decoded_content->{token});
+    $self->{token} = $decoded_content->{token};
+    $self->{cache}->write(data => { token => $decoded_content->{token}, expiration => ($decoded_content->{expires_in} + time()) });
+    return $self->{token};
+}
+
+sub set_options {
+    my ($self, %options) = @_;
+
+    $self->{option_results} = $options{option_results};
+}
+
+sub get_unique_app {
+    my ($self, %options) = @_;
+
+    my $content = $self->{http}->request(
+        method   => 'GET',
+        url_path => $self->{option_results}->{api_path} . '/apps/' . $options{application_id}
+    );
+    return json_decode($content, output => $self->{output});
+}
+
+sub get_apps {
+    my ($self, %options) = @_;
+
+    $self->get_token();
+
+    my @stats;
+    # either we have a single app to get by its id
+    if (!is_empty($options{application_id})) {
+        push @stats, $self->get_unique_app(application_id => $options{application_id});
+        return \@stats;
+    }
+
+    # or we have to get all apps and check which ones match the filters
+    my $all_apps_json = $self->{http}->request(
+        method     => 'GET',
+        url_path   => $self->{option_results}->{api_path} . '/apps/'
+    );
+    my $all_apps = json_decode($all_apps_json, output => $self->{output});
+
+    foreach my $app_obj (@$all_apps) {
+        next if is_excluded(
+            $app_obj->{name},
+            $options{include_application_name},
+            $options{exclude_application_name});
+
+        push @stats, $self->get_unique_app(application_id => $app_obj->{id});
+    }
+
+    return \@stats;
+}
+
+sub set_defaults {}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->{$_} = $self->{option_results}->{$_} foreach qw(hostname port proto api_path timeout key_id key_secret );
+
+    foreach (qw(key_id key_secret)) {
+        $self->{output}->option_exit(short_msg => "Mandatory option '$_' is missing.") if ($self->{$_} eq '');
+        $self->{output}->option_exit(short_msg => "Option '$_' contains illegal characters '$1'") if ($self->{$_} =~ /([\b\f\n\r\t\"\\]+)/);
+    }
+
+    $self->{cache}->check_options(option_results => $self->{option_results});
+
+    return 0;
+}
+
+sub settings {
+    my ($self, %options) = @_;
+
+    #$self->build_options_for_httplib();
+    $self->{http}->add_header(key => 'Accept', value => 'application/json');
+    $self->{http}->add_header(key => 'Content-Type', value => 'application/json');
+    $self->{http}->set_options(%{$self->{option_results}});
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Zscaler Digital Experience (ZDX) Rest API
+
+=head1 SYNOPSIS
+
+Custom mode for Zscaler Digital Experience (ZDX) Rest API
+
+=head1 REST API OPTIONS
+
+Zscaler Digital Experience (ZDX) Rest API
+
+=over 8
+
+=item B<--hostname>
+
+ZDX API hostname (default: C<api.zdxcloud.net>)
+
+=item B<--port>
+
+API port (default: 443)
+
+=item B<--proto>
+
+Specify http if needed (default: 'https')
+
+=item B<--api-path>
+
+API URL path (default: '/api')
+
+=item B<--key-id>
+
+Key ID (see L<here|https://help.zscaler.com/zdx/managing-zdx-api-keys> for more details).
+
+=item B<--key-secret>
+
+Key secret (see L<here|https://help.zscaler.com/zdx/managing-zdx-api-keys> for more details).
+
+=item B<--timeout>
+
+Define HTTP timeout.
+
+=back
+
+=head1 DESCRIPTION
+
+B<custom>.
+
+=cut
---
+++ src/apps/monitoring/zscaler/zdx/api/mode/application.pm
@@ -0,0 +1,147 @@
+#
+# Copyright 2025-Present Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::zscaler::zdx::api::mode::application;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+# All filter parameters that can be used
+my @_options = qw/application_id include_application_name exclude_application_name/;
+
+sub new {
+    my ($class, %options) = @_;
+
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        ( map { ($_ =~ s/_/-/gr).':s' => { name => $_, default => '' } } @_options )
+    });
+
+    return $self;
+}
+
+sub prefix_app_output {
+    my ($self, %options) = @_;
+
+    return 'App "' . $options{instance_value}->{name} . '" - ';
+}
+
+sub set_counters {
+    my ($self, %options) = @_;
+    
+    $self->{maps_counters_type} = [
+        { name => 'application', type => 1, cb_prefix_output => 'prefix_app_output', message_multiple => 'All apps are ok' }
+    ];
+    
+    $self->{maps_counters}->{application} = [
+        {
+            label  => 'total-users',
+            nlabel => 'application.total-users.count',
+            set    => {
+                key_values      => [ { name => 'total_users' } ],
+                output_template => 'Users count: %s',
+                perfdatas       => [ { template => '%d', min => 0, label_extra_instance => 1 } ]
+            }
+        },
+        {
+            label  => 'score',
+            nlabel => 'application.score.value',
+            set    => {
+                key_values      => [ { name => 'score' } ],
+                output_template => 'Score: %s',
+                perfdatas       => [ { template => '%d', label_extra_instance => 1 } ]
+            }
+        }
+    ];
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+
+    $self->SUPER::check_options(%options);
+    foreach (@_options) {
+        $self->{$_} = $self->{option_results}->{$_};
+    }
+
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $apps = $options{custom}->get_apps( map {$_ => $self->{$_}} @_options );
+
+    foreach (@$apps) {
+        $self->{application}->{$_->{name}} = {
+            name        => $_->{name},
+            score       => $_->{score},
+            total_users => $_->{stats}->{active_users}
+        }
+    }
+    return 1;
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Monitor an application overall stats.
+
+=over 8
+
+=item B<--application-id>
+
+Define the C<appid> to monitor. Using this option is recommended to monitor one app because it will
+only retrieves the data related to the targeted app.
+
+=item B<--include-application-name>
+
+Regular expression to include applications to monitor by their name. Using this option is not recommended to monitor
+one app because it will first retrieve the list of all apps and then filter to get the targeted app.
+
+=item B<--exclude-application-name>
+
+Regular expression to exclude applications to monitor by their name. Using this option is not recommended to monitor
+one app because it will first retrieve the list of all apps and then filter to get the targeted app.
+
+=back
+
+=item B<--warning-score>
+
+Threshold.
+
+=item B<--critical-score>
+
+Threshold.
+
+=item B<--warning-total-users>
+
+Threshold.
+
+=item B<--critical-total-users>
+
+Threshold.
+
+=cut
