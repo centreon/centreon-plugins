47f4cb5c749b4af84ff3482d2f53927a06cab6b1
omercier <32134301+omercier@users.noreply.github.com>
2026-01-30 09:06:19 +0100

    feat(apps::monitoring::zscaler::zdx::api): new plugin (#5938)
    Co-authored-by: Sylvain Cresto <scresto@centreon.com>
    
    Refs: CTOR-1961

---
+++ src/apps/monitoring/zscaler/zdx/api/mode/discovery.pm
@@ -0,0 +1,91 @@
+#
+# Copyright 2025-Present Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::monitoring::zscaler::zdx::api::mode::discovery;
+
+use base qw(centreon::plugins::mode);
+
+use strict;
+use warnings;
+use centreon::plugins::misc qw/json_encode/;
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+    
+    $options{options}->add_options(arguments => {
+        "prettify" => { name => 'prettify' },
+    });
+
+    return $self;
+}
+
+sub check_options {
+    my ($self, %options) = @_;
+    $self->SUPER::init(%options);
+}
+
+sub run {
+    my ($self, %options) = @_;
+
+    my @disco_data;
+    my $disco_stats;
+
+    $disco_stats->{start_time} = time();
+
+    my $results = $options{custom}->get_apps();
+
+    foreach (@$results) {
+        push @disco_data, {
+            id          => $_->{id},
+            name        => $_->{name},
+            score       => $_->{score},
+            total_users => $_->{total_users},
+            hostname    => $self->{option_results}->{hostname} # for stupid reason host discovery can only take discovery results for the host address
+        };
+    }
+
+    $disco_stats->{end_time} = time();
+    $disco_stats->{duration} = $disco_stats->{end_time} - $disco_stats->{start_time};
+
+    $disco_stats->{discovered_items} = @disco_data;
+    $disco_stats->{results} = \@disco_data;
+
+    my $encoded_data = json_encode($disco_stats, prettify => $self->{option_results}->{prettify}, output => $self->{output}, no_exit => 1);
+    
+    $self->{output}->output_add(short_msg => $encoded_data);
+    $self->{output}->display(nolabel => 1, force_ignore_perfdata => 1);
+    $self->{output}->exit();
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Resources discovery.
+
+=over 8
+
+=back
+
+=cut
