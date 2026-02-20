de53e24aff9eeb828a928afebe1835d9b5b28f94
Stéphane Duret <sduret@centreon.com>
2026-02-20 12:19:23 +0100

    Update chassis filter to handle undefined ChassisType

--- src/centreon/common/redfish/restapi/mode/components/chassis.pm
+++ src/centreon/common/redfish/restapi/mode/components/chassis.pm
@@ -36,7 +36,7 @@ sub check {
         my $instance = $chassis->{Id};
         
         next if ($self->check_filter(section => 'chassis', instance => $instance));
-        next if ($chassis->{ChassisType} eq 'Enclosure');
+        next if (!defined($chassis->{ChassisType}) || $chassis->{ChassisType} eq 'Enclosure');
         $self->{components}->{chassis}->{total}++;
 
         $self->{output}->output_add(
