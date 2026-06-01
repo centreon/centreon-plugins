fe10dcc83aaa88ff0d04dc6c0404d00e7d052c37
Sylvain Cresto <scresto@centreon.com>
2026-06-01 15:21:19 +0200

    FIx tests

--- tests/network/paloalto/api/fixed_date.pm
+++ tests/network/paloalto/api/fixed_date.pm
@@ -3,8 +3,12 @@ package fixed_date;
 # Copyright 2026-Present Centreon
 # Always use the same fixed date to test certificate validity
 
+use DateTime;
+
 BEGIN {
-   *CORE::GLOBAL::time = sub { 1738368000 };
+   $now = 1738368000;
+   *CORE::GLOBAL::time = sub { $now };
+   *DateTime::now = sub { DateTime->from_epoch(epoch => $now) };
 }
 
 1
